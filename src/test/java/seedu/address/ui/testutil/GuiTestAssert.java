7530bf6e79ae6385699c498d7904b03058a38b36
Darren <d4rr3n.96@gmail.com>
2019-02-19 22:57:11 +0800

    Remove unnecessary imports

--- src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
+++ src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import guitests.guihandles.PersonCardHandle;
 import guitests.guihandles.PersonListPanelHandle;

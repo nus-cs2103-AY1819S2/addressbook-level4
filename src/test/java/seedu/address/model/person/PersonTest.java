21bae584e1c8a6228cf92d0e452e05f2a83540cd
Sreyans Sipani <sreyans.sipani99@gmail.com>
2019-02-20 23:31:21 +0800

    Avoided star import

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -7,7 +7,10 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-import static seedu.address.testutil.TypicalPersons.*;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BOB;
+import static seedu.address.testutil.TypicalPersons.VIP_PERSON;
+import static seedu.address.testutil.TypicalPersons.VIP_PERSON2;
 
 import org.junit.Rule;
 import org.junit.Test;

1c667ccdaf43800764eb78b19f0b237d68e7b77d
Qing-Yuan <qingyuan2910@gmail.com>
2019-02-20 17:57:36 +0800

    Add alias to "find" command and update relevant tests

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -77,6 +77,9 @@ public class AddressBookParser {
 
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
+            
+        case FindCommand.COMMAND_ALIAS:
+            return new FindCommandParser().parse(arguments);
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();

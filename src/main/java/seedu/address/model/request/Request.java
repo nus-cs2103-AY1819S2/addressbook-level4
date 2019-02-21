b242c0af8b9d28debf9c5e7a53995d19af15923e
David Livingston <ddl.tdh@gmail.com>
2019-02-22 00:02:51 +0800

    Fixed style settings

--- src/main/java/seedu/address/model/request/Request.java
+++ src/main/java/seedu/address/model/request/Request.java
@@ -1,35 +1,37 @@
 package seedu.address.model.request;
 
-import seedu.address.model.person.Person;
-import java.util.Set;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Set;
+
+import seedu.address.model.person.Person;
 
 /**
  * Represents a request made by a patient in the request book.
  */
 public class Request {
-  private final String id;
-  private final Person patient;
-  private final String requestDate;
-  private final Set<String> conditions;
-  private Person healthStaff;
-  private boolean isCompleted; // represents the state of the request
+    private final String id;
+    private final Person patient;
+    private final String requestDate;
+    private final Set<String> conditions;
+    private Person healthStaff;
+    private boolean isCompleted; // represents the state of the request
 
   /*
    * Minimally requires the following parameters to be non-null.
    */
   public Request(String id, Person patient, String
       requestDate, Set<String> conditions, boolean isCompleted) {
-    requireAllNonNull(id, patient, requestDate, conditions, isCompleted);
-    this.id = id;
-    this.patient = patient;
-    this.requestDate = requestDate;
-    this.conditions = conditions;
-    this.isCompleted = isCompleted;
+      requireAllNonNull(id, patient, requestDate, conditions, isCompleted);
+      this.id = id;
+      this.patient = patient;
+      this.requestDate = requestDate;
+      this.conditions = conditions;
+      this.isCompleted = isCompleted;
   }
 
   public void setHealthStaff(Person healthStaff) {
-    this.healthStaff = healthStaff;
+      this.healthStaff = healthStaff;
   }
 
   /**
@@ -38,69 +40,69 @@ public class Request {
    * This defines a weaker notion of equality between two requests.
    */
   public boolean isSameRequest(Request otherRequest) {
-    if (otherRequest == this) {
-      return true;
-    }
+      if (otherRequest == this) {
+        return true;
+      }
 
-    return otherRequest != null
-        && otherRequest.getId().equals(this.id)
-        && ((otherRequest.getPatient().equals(this.patient)) || otherRequest
-        .getConditions().equals(this.conditions));
+      return otherRequest != null
+          && otherRequest.getId().equals(this.id)
+          && ((otherRequest.getPatient().equals(this.patient)) || otherRequest
+          .getConditions().equals(this.conditions));
   }
 
   @Override
   public String toString() {
-    String healthStaff = this.healthStaff == null ? "Unassigned" : this
-        .healthStaff.toString();
-    String status = isCompleted ? "Completed" : "Pending";
+      String healthStaff = this.healthStaff == null ? "Unassigned" : this
+          .healthStaff.toString();
+      String status = isCompleted ? "Completed" : "Pending";
 
-    return "----------Request----------\n" +
-        "ID: " + this.id + "\n" +
-        "Patient: " + this.patient + "\n" +
-        "Assigned staff: " + healthStaff + "\n" +
-        "Request Date: " + this.requestDate + "\n" +
-        "Condition(s): " + this.conditions + "\n" +
-        "Status: " + status + "\n" +
-        "----------End of Request----------\n";
+      return "----------Request----------\n" +
+          "ID: " + this.id + "\n" +
+          "Patient: " + this.patient + "\n" +
+          "Assigned staff: " + healthStaff + "\n" +
+          "Request Date: " + this.requestDate + "\n" +
+          "Condition(s): " + this.conditions + "\n" +
+          "Status: " + status + "\n" +
+          "----------End of Request----------\n";
   }
 
   @Override
   public boolean equals(Object other) {
-    if (other == this) return true;
+      if (other == this) return true;
 
-    if (!(other instanceof Request)) {
-      return false;
-    }
+      if (!(other instanceof Request)) {
+        return false;
+      }
 
-    Request otherRequest = (Request) other;
-    return otherRequest.getId().equals(this.id)
-        && otherRequest.getPatient().equals(this.patient)
-        && (otherRequest.getRequestDate().equals(this.requestDate))
-        && otherRequest.getHealthStaff().equals(this.healthStaff)
-        && (otherRequest.isComplete() == this.isCompleted);
+      Request otherRequest = (Request) other;
+      return otherRequest.getId().equals(this.id)
+          && otherRequest.getPatient().equals(this.patient)
+          && (otherRequest.getRequestDate().equals(this.requestDate))
+          && otherRequest.getHealthStaff().equals(this.healthStaff)
+          && (otherRequest.isComplete() == this.isCompleted);
   }
 
   public Set<String> getConditions() {
-    return this.conditions;
+      return this.conditions;
   }
 
   public String getId() {
-    return this.id;
+      return this.id;
   }
 
   public Person getPatient() {
-    return patient;
+      return patient;
   }
 
   public String getRequestDate() {
-    return requestDate;
+      return requestDate;
   }
 
   public Person getHealthStaff() {
-    return healthStaff;
+      return healthStaff;
   }
 
   public boolean isComplete() {
-    return isCompleted;
+      return isCompleted;
   }
 }

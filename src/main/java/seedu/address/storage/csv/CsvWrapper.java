836f8bc39b0fc3c3425fc747ea3593eb78dbfd50
JonathanLeeWH <leeweihao1996@outlook.com>
2019-02-23 23:49:25 +0800

    Update CsvWrapper.java to support exporting the current list displayed in the GUI to csv file format
    Update CsvWrapper.java to support exporting the current list displayed in the GUI to csv file format
    Note: In this version, the csv file contains the current list displayed in the GUI when the export command is executed
    I have personally tested manually to check for potential bugs and fixed some bugs I discovered as well during testing
    JUnit test will be added in future release to better automate the testing process to ensure the export command works as expected
    Add doCleanUp() method in CsvWrapper.java to clean up any empty csv file that is created when the export command is executed but displayed an exception such as CommandException due to possible invalid command
    Update export() method in CsvWrapper.java to support the doCleanUp() method even it encounter a CommandException so as to clean up any empty csv file that is created when the export command is executed
    This fix the bug where an empty csv file with default heading is still created despite the export command is unable to complete its execution due to a CommandException

--- src/main/java/seedu/address/storage/csv/CsvWrapper.java
+++ src/main/java/seedu/address/storage/csv/CsvWrapper.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.List;
 
 import com.opencsv.CSVWriter;
@@ -37,9 +38,14 @@ public class CsvWrapper {
      * @throws CommandException If there is an error exporting the current list in the GUI to a csv file.
      */
     public void export() throws CommandException {
-        List<Medicine> currentGuiList = model.getFilteredMedicineList();
-        createCsvFile(csvFileName);
-        writeDataToCsv(currentGuiList);
+        try {
+            List<Medicine> currentGuiList = model.getFilteredMedicineList();
+            createCsvFile(csvFileName);
+            writeDataToCsv(currentGuiList);
+        } catch (CommandException ce) {
+            doCleanUp();
+            throw ce;
+        }
     }
 
     /**
@@ -57,13 +63,30 @@ public class CsvWrapper {
     }
 
     /**
+     * Cleans up empty csv that is created in the exported directory if an error occurs.
+     * @throws CommandException If there is an error cleaning up the empty csv file created.
+     */
+    private void doCleanUp() throws CommandException {
+        try {
+            Files.deleteIfExists(csvFilePath);
+        } catch (IOException ioe) {
+            throw new CommandException(FILE_OPS_ERROR_MESSAGE + ioe, ioe);
+        }
+    }
+
+    /**
      * Writes data from current list displayed in GUI when export command is called to a csv file.
      * @param currentGuiList The current list displayed in the GUI when the export command is called.
-     * @throws CommandException If tere is an error exporting the current list in the GUI to a csv file.
+     * @throws CommandException If there is an error exporting the current list in the GUI to a csv file.
      */
     private void writeDataToCsv(List currentGuiList) throws CommandException {
         try (CSVWriter csvWriter = new CSVWriter(new FileWriter(csvFilePath.toString()))) {
             csvWriter.writeNext(defaultHeading);
+            Iterator iterator = currentGuiList.listIterator();
+            while (iterator.hasNext()) {
+                Medicine current = (Medicine) iterator.next();
+                csvWriter.writeNext(current.toStringArray());
+            }
         } catch (IOException ioe) {
             throw new CommandException(FILE_OPS_ERROR_MESSAGE + ioe, ioe);
         }

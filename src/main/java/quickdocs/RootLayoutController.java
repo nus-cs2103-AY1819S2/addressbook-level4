3651c976a8e8626f656f0acac59b4e902c71de42
simjiazhi <e0177123@u.nus.edu>
2019-02-23 15:20:45 +0800

    Created UI for QuickDocs

---
+++ src/main/java/quickdocs/RootLayoutController.java
@@ -0,0 +1,51 @@
+package quickdocs;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+/**
+ * This class handles user interaction with the root layout
+ */
+public class RootLayoutController {
+
+    private static int currentInputPointer = 0;
+
+    @FXML
+    private TextArea display;
+
+    @FXML
+    private TextField userInput;
+
+    @FXML
+    private TextArea inputFeedback;
+
+    /**
+     * This method will pass the command into the parser whenever the user presses enter
+     * @param event Event associated with the user pressing enter to confirm a command
+     */
+    @FXML
+    public void enterInput(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            display.appendText(userInput.getText());
+            display.appendText("\n");
+            userInput.setText("");
+            inputFeedback.setText("Command entered");
+        }
+    }
+
+    /**
+     * This method will check if the parameters entered into the command text field is valid
+     * by calling the various checkers across the module
+     * @param event Event associated with the user pressing space bar between parameters
+     */
+    @FXML
+    public void checkInput(KeyEvent event) {
+        if (event.getCode() == KeyCode.SPACE) {
+            inputFeedback.setText("space entered");
+        }
+    }
+
+}

5f99dbd9afd610c26cd3d81e83ebbb43d0cf5800
eugenefdw <eugenefoodw@gmail.com>
2019-02-24 01:20:53 +0800

    Add parallel skeleton, opencsv dependency
    Added a copy of addressbook's folder structure for more incremental
    additions and porting over AB4 components as needed
    
    This means we don't need to fully rip out each component as we try
    to modify and morph them and break the whole program in the process

---
+++ src/main/java/braintrain/commons/util/ConfigUtil.java
@@ -0,0 +1,23 @@
+package braintrain.commons.util;
+
+import braintrain.commons.core.Config;
+import braintrain.commons.exceptions.DataConversionException;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+/**
+ * A class for accessing the Config File.
+ */
+public class ConfigUtil {
+
+    public static Optional<Config> readConfig(Path configFilePath) throws DataConversionException {
+        return JsonUtil.readJsonFile(configFilePath, Config.class);
+    }
+
+    public static void saveConfig(Config config, Path configFilePath) throws IOException {
+        JsonUtil.saveJsonFile(config, configFilePath);
+    }
+
+}

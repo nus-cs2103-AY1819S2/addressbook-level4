447ebc20c74f30359b1571c6dede30e05945d8ad
Sreyans Sipani <sreyans.sipani99@gmail.com>
2019-02-25 15:00:28 +0800

    Added 3 models

---
+++ src/main/java/seedu/address/model/util/TimeRange.java
@@ -0,0 +1,57 @@
+package seedu.address.model.util;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+/**
+ * Represents a time range of format HH:00 - HH:00.
+ */
+public class TimeRange {
+
+    final LocalTime startTime;
+    final LocalTime endTime;
+
+    public TimeRange(int startHour, int endHour) {
+        this.startTime = LocalTime.of(startHour, 0);
+        this.endTime = LocalTime.of(endHour, 0);
+    }
+
+    public boolean withinTiming(TimeRange other) {
+        return other.getStartTime().isAfter(this.startTime) && other.getEndTime().isBefore(this.endTime);
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return this.startTime.format(formatter)
+            + " - "
+            + this.endTime.format(formatter);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof TimeRange)) {
+            return false;
+        }
+        TimeRange timeRange = (TimeRange) o;
+        return getStartTime().equals(timeRange.getStartTime()) &&
+            getEndTime().equals(timeRange.getEndTime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getStartTime(), getEndTime());
+    }
+}

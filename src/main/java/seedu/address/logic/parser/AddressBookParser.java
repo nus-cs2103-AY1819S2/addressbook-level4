e06efd8c76bde407ee463ff3549e44011e9d00d3
kjiaxuan <kjiaxuan12@gmail.com>
2019-02-25 22:54:20 +0800

    Updated AddressBookParser.java to support case sensitivity in command words

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -47,7 +47,7 @@ public class AddressBookParser {
         final String commandWord = matcher.group("commandWord");
         final String arguments = matcher.group("arguments");
 
-        switch (commandWord.toLowerCase()) {
+        switch (commandWord) {
         case AddCommand.COMMAND_WORD:
         case AddCommand.COMMAND_ALIAS:
             return new AddCommandParser().parse(arguments);

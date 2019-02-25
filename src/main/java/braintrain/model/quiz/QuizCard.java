264ce27f9ff03dbb8fa201ff85bc0247981c3260
halcyoneee <ting00733@gmail.com>
2019-02-25 22:32:17 +0800

    Add QuizCard with tests
    Now is just the basic constructor with getters.

---
+++ src/main/java/braintrain/model/quiz/QuizCard.java
@@ -0,0 +1,60 @@
+package braintrain.model.quiz;
+
+import static braintrain.commons.util.AppUtil.checkArgument;
+import static braintrain.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.List;
+
+/**
+ * Represents a partial of Card, only contains the necessary information for Quiz.
+ */
+public class QuizCard {
+
+    public static final String MESSAGE_CONSTRAINTS = "Question/answer can take any values, and it"
+        + " should not be blank";
+
+    private String question;
+    private String answer;
+    private List<String> opt;
+    private boolean swap; //swap qn/ans
+    private int totalAttempts;
+    private int streak;
+
+    public QuizCard(String question, String answer, List<String> opt) {
+        requireAllNonNull(question, answer, opt);
+        checkArgument(!question.isEmpty() && !answer.isEmpty(), MESSAGE_CONSTRAINTS);
+
+        this.question = question;
+        this.answer = answer;
+        this.opt = opt;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public List<String> getOpt() {
+        return opt;
+    }
+
+    //
+    //  public QuizCard(Card card, int totalAttempts, int streak) {
+    //    this.card = card;
+    //    this.totalAttempts = totalAttempts;
+    //    this.streak = streak;
+    //  }
+    //
+    //  public void setStreakTotal(boolean isCorrect) {
+    //    this.totalAttempts += 1;
+    //
+    //    if (isCorrect) {
+    //      this.streak += 1;
+    //    } else {
+    //      this.streak = 0;
+    //    }
+    //  }
+}
---
+++ src/test/java/braintrain/model/quiz/QuizCardTest.java
@@ -0,0 +1,64 @@
+package braintrain.model.quiz;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import braintrain.testutil.Assert;
+
+public class QuizCardTest {
+
+    private static final String QUESTION = "Japan";
+    private static final String ANSWER = "Tokyo";
+
+    private static final List<String> FIELDS_OPTIONALS = Arrays.asList("JP", "Asia");
+    private static final List<String> FIELDS_OPTIONALS_EMPTY = Arrays.asList("", "");
+
+    private static final QuizCard VALID_QUIZCARD = new QuizCard(QUESTION, ANSWER, FIELDS_OPTIONALS);
+
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () ->
+            new QuizCard(null, null, null));
+
+        Assert.assertThrows(NullPointerException.class, () ->
+            new QuizCard(QUESTION, null, FIELDS_OPTIONALS));
+
+        Assert.assertThrows(NullPointerException.class, () ->
+            new QuizCard(QUESTION, ANSWER, null));
+    }
+
+    @Test
+    public void constructor_invalidQuizCard_throwsIllegalArgumentException() {
+        String invalidQn = "";
+        String invalidAns = "";
+        Assert.assertThrows(IllegalArgumentException.class, () ->
+            new QuizCard(invalidQn, invalidAns, FIELDS_OPTIONALS_EMPTY));
+
+        Assert.assertThrows(IllegalArgumentException.class, () ->
+            new QuizCard(invalidQn, ANSWER, FIELDS_OPTIONALS_EMPTY));
+
+        Assert.assertThrows(IllegalArgumentException.class, () ->
+            new QuizCard(QUESTION, invalidAns, FIELDS_OPTIONALS_EMPTY));
+    }
+
+    @Test
+    public void getQuestion() {
+        assertEquals(VALID_QUIZCARD.getQuestion(), QUESTION);
+    }
+
+    @Test
+    public void getAnswer() {
+        assertEquals(VALID_QUIZCARD.getAnswer(), ANSWER);
+    }
+
+    @Test
+    public void getOpt() {
+        assertEquals(VALID_QUIZCARD.getOpt(), FIELDS_OPTIONALS);
+    }
+
+}

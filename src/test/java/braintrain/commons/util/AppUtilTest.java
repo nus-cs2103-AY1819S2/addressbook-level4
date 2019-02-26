932a70df46f8a131f30af693f8629203c3cd97ba
halcyoneee <ting00733@gmail.com>
2019-02-27 00:03:37 +0800

    Integrate AB4 AppUtilTest

---
+++ src/test/java/braintrain/commons/util/AppUtilTest.java
@@ -0,0 +1,31 @@
+package braintrain.commons.util;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class AppUtilTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void checkArgument_true_nothingHappens() {
+        AppUtil.checkArgument(true);
+        AppUtil.checkArgument(true, "");
+    }
+
+    @Test
+    public void checkArgument_falseWithoutErrorMessage_throwsIllegalArgumentException() {
+        thrown.expect(IllegalArgumentException.class);
+        AppUtil.checkArgument(false);
+    }
+
+    @Test
+    public void checkArgument_falseWithErrorMessage_throwsIllegalArgumentException() {
+        String errorMessage = "error message";
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage(errorMessage);
+        AppUtil.checkArgument(false, errorMessage);
+    }
+}

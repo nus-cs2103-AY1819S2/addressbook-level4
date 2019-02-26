a1e7931a26b9a8f41885d0e302a6fab91443368f
melpulomas <e0202854@u.nus.edu>
2019-02-26 20:03:20 +0800

    still fixing code style..

--- src/main/java/seedu/address/logic/commands/LabelCommand.java
+++ src/main/java/seedu/address/logic/commands/LabelCommand.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 import seedu.address.commons.core.Messages;

cdba7ce07bdff8908d7c10babf90895546353676
Kyler Wong Shen Meng <wsm.kyler@gmail.com>
2019-02-26 23:50:03 +0800

    Added crucial attributes to Status class

--- src/main/java/seedu/address/model/person/exceptions/Status.java
+++ src/main/java/seedu/address/model/person/exceptions/Status.java
@@ -1,4 +1,12 @@
 package seedu.address.model.person.exceptions;
 
+import java.util.Date;
+
+/**
+ * Represents the status of a teeth.
+ * Attributes of class is immutable to uphold information integrity.
+ */
 public class Status {
+    private String description;
+    private Date dateModified;
 }

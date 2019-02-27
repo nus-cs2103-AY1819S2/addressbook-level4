e329f32456470eee70e2c96eece33f24ce1e4f26
Qing-Yuan <qingyuan2910@gmail.com>
2019-02-28 02:15:11 +0800

    Fix checkstyle errors again

--- src/main/java/seedu/address/logic/parser/RestOrRantParser.java
+++ src/main/java/seedu/address/logic/parser/RestOrRantParser.java
@@ -7,6 +7,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import seedu.address.logic.Mode;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
@@ -17,7 +18,6 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.Mode;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 

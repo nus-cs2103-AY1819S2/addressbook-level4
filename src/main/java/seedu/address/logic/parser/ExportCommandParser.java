29e86732877a314a3ebd97bdb455184a51382ecb
JiaHaoLim <jiahaolimwork@hotmail.com>
2019-02-28 00:46:38 +0800

    Added ExportCommandParser.java

---
+++ src/main/java/seedu/address/logic/parser/ExportCommandParser.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.io.File;
+
+import seedu.address.logic.commands.ExportCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new ImportCommand object
+ */
+public class ExportCommandParser implements Parser<ExportCommand> {
+
+    /**
+     * Parses the given argument {@code String} in the context of the ExportCommand
+     * and returns an ImportCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ExportCommand parse(String args) throws ParseException {
+        try {
+            File file = ParserUtil.parseFile(args);
+            return new ExportCommand(file);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ExportCommand.MESSAGE_USAGE), pe);
+        }
+    }
+    // TODO: Combine ImportCommandParser and ExportCommandParser
+
+}

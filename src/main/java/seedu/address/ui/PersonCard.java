ba84ab761a3f524f6f69dd0b5abebe20df78072a
alexchong711 <hongyun711@gmail.com>
2019-02-28 03:00:33 +0800

    Revert "fixed GUI expectedmingrade"
    This reverts commit 963fac1a

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -35,7 +35,7 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label address;
     @FXML
-    private Label email;
+    private Label expectedMinGrade;
     @FXML
     private FlowPane tags;
 
@@ -43,9 +43,13 @@ public class PersonCard extends UiPart<Region> {
         super(FXML);
         this.person = person;
         id.setText(displayedIndex + ". ");
+        System.out.println(name);
         name.setText(person.getModuleInfo().fullName);
+        System.out.println(name);
         phone.setText(person.getSemester().value);
-        email.setText(person.getExpectedMinGrade().toString());
+        System.out.println(expectedMinGrade);
+        //expectedMinGrade = new Label(person.getExpectedMinGrade().toString());
+        //expectedMinGrade.setText(person.getExpectedMinGrade().toString());
         address.setText(person.getExpectedMaxGrade().value);
         person.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }

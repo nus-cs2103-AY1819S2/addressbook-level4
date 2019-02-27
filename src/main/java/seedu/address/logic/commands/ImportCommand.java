07897e8c30f40f402fb3e99b2dd2abf8afdd5d41
JiaHaoLim <jiahaolimwork@hotmail.com>
2019-02-27 19:48:39 +0800

    Added ImportCommand.java

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -3,22 +3,35 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.nio.file.Path;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 
 /**
- * Lists all persons in the address book to the user.
+ * Imports the records.
  */
-public class ListCommand extends Command {
+public class ImportCommand extends Command {
 
-    public static final String COMMAND_WORD = "list";
+    public static final String COMMAND_WORD = "import";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Imports the file \n"
+            + "Parameters: FILENAME\n"
+            + "Example: " + COMMAND_WORD + " records1.txt";
 
+    public static final String MESSAGE_SUCCESS = "Imported the records!";
+
+    private final String path;
+
+    public ImportCommand(String path) {
+        this.path = path;
+    }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
+        System.out.println(path);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         return new CommandResult(MESSAGE_SUCCESS);
     }

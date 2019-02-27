bc8d19791f8c330d537b1710c04bf9d8ef642fa7
David Livingston <ddl.tdh@gmail.com>
2019-02-27 15:03:40 +0800

    Integrated HealthWorker into the Request Object

--- src/test/java/seedu/address/testutil/TypicalHealthWorkers.java
+++ src/test/java/seedu/address/testutil/TypicalHealthWorkers.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import seedu.address.model.AddressBook;
+import seedu.address.model.person.HealthWorker;
 import seedu.address.model.person.Person;
 
 /**
@@ -12,23 +13,29 @@ import seedu.address.model.person.Person;
  */
 public class TypicalHealthWorkers {
 
-    public static final Person ANDY = new PersonBuilder().withName("Andy Tan")
+    public static final HealthWorker ANDY = new HealthWorkerBuilder().withName("Andy Tan")
             .withAddress("125, Jurong West Ave 6, #08-111").withEmail("andye@example.com")
-            .withPhone("94358253")
+            .withPhone("94358253").withOrganization("NUH").withNric("S8312942G")
             .withTags("Physiotherapy", "Dialysis").build();
-    public static final Person BETTY = new PersonBuilder().withName("Betty Meier")
+    public static final HealthWorker BETTY = new HealthWorkerBuilder().withName("Betty Meier")
             .withAddress("312, Clementi Ave 2, #02-25")
             .withEmail("betty@example.com").withPhone("98761232")
+            .withOrganization("NUH").withNric("S8312942G")
             .withTags("Eldercare", "Stroke").build();
-    public static final Person CARLIE = new PersonBuilder().withName("Carlie Kurz").withPhone("95358463")
+    public static final HealthWorker CARLIE = new HealthWorkerBuilder().withName("Carlie Kurz")
+            .withPhone("95358463").withOrganization("NUH").withNric("S9312942G")
             .withEmail("Kurz@example.com").withAddress("wall street").withTags("Palliative").build();
-    public static final Person PANIEL = new PersonBuilder().withName("Paniel Meier").withPhone("87652133")
-            .withEmail("panda@example.com").withAddress("10th street").withTags("Doctor").build();
-    public static final Person ELLA = new PersonBuilder().withName("Ella Meyer").withPhone("94824524")
+    public static final HealthWorker PANIEL = new HealthWorkerBuilder().withName("Paniel Meier").withPhone("87652133")
+            .withEmail("panda@example.com").withOrganization("NUH").withNric("S8412942G")
+            .withAddress("10th street").withTags("Doctor").build();
+    public static final HealthWorker ELLA = new HealthWorkerBuilder().withName("Ella Meyer")
+            .withPhone("94824524").withOrganization("NUH").withNric("S9112942G")
             .withEmail("meyer@example.com").withAddress("michegan ave").withTags("Nurse", "Dementia").build();
-    public static final Person FIONE = new PersonBuilder().withName("Fione Kunz").withPhone("9482237")
+    public static final HealthWorker FIONE = new HealthWorkerBuilder().withName("Fione Kunz")
+            .withPhone("9482237").withOrganization("NUH").withNric("S7812942G")
             .withEmail("fione@example.com").withAddress("little tokyo").withTags("Nurse", "Alzheimers").build();
-    public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
+    public static final HealthWorker GEORGE = new HealthWorkerBuilder().withName("George Best")
+            .withPhone("9482442").withOrganization("NUH").withNric("S8812942G")
             .withEmail("george@example.com").withAddress("4th street").withTags("Cancer").build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
@@ -41,13 +48,13 @@ public class TypicalHealthWorkers {
      */
     public static AddressBook getTypicalHealthStaffBook() {
         AddressBook ab = new AddressBook();
-        for (Person person : getTypicalHealthStaff()) {
+        for (HealthWorker person : getTypicalHealthStaff()) {
             ab.addPerson(person);
         }
         return ab;
     }
 
-    public static List<Person> getTypicalHealthStaff() {
+    public static List<HealthWorker> getTypicalHealthStaff() {
         return new ArrayList<>(Arrays.asList(ANDY, BETTY, CARLIE, PANIEL, ELLA, FIONE, GEORGE));
     }
 }

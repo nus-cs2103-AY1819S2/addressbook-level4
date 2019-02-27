a0fbc804cb5b939e725947f927b837ffef19a564
JeremyLoye <jeremyloye@gmail.com>
2019-02-27 16:35:54 +0800

    Created Amount and Date classes for person fields

---
+++ src/main/java/seedu/address/model/person/Date.java
@@ -0,0 +1,53 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's address in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
+public class Date {
+
+    public static final String MESSAGE_CONSTRAINTS = "";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "^([0-2][0-9]||3[0-1])/(0[0-9]||1[0-2])/([0-9][0-9])?[0-9][0-9]$";
+
+    public final String value;
+
+    /**
+     * Constructs an {@code Address}.
+     *
+     * @param date A valid date.
+     */
+    public Date(String date) {
+        requireNonNull(date);
+        checkArgument(isValidAddress(date), MESSAGE_CONSTRAINTS);
+        value = date;
+    }
+
+    /**
+     * Returns true if a given string is a valid email.
+     */
+    public static boolean isValidAddress(String test) { return test.matches(VALIDATION_REGEX); }
+
+    @Override
+    public String toString() { return value; }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Address // instanceof handles nulls
+                && value.equals(((Address) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}

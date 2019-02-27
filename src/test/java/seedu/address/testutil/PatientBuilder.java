e0eee8dfd34c0af6e762fbd3e765563501520839
LAPTOP-QAQAFI16\Rohan Dev <rohandev@u.nus.edu>
2019-02-27 18:53:32 +0800

    more lexicographic checkstyle changes again

--- src/test/java/seedu/address/testutil/PatientBuilder.java
+++ src/test/java/seedu/address/testutil/PatientBuilder.java
@@ -3,12 +3,13 @@ package seedu.address.testutil;
 import java.util.HashSet;
 import java.util.Set;
 
-import seedu.address.model.person.Patient;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Nric;
+import seedu.address.model.person.Patient;
 import seedu.address.model.person.Phone;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Address;
+
 import seedu.address.model.tag.Tag;
 
 /**

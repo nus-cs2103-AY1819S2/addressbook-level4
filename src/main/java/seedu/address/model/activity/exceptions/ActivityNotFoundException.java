127364d916368f9ecd43b0f39a948c1b912ad731
ivyyangyq <yangyiqing1013@gmail.com>
2019-02-27 12:06:24 +0800

    Created UniqueActivityList and modified exceptions to inherit from java Exception

--- src/main/java/seedu/address/model/activity/exceptions/ActivityNotFoundException.java
+++ src/main/java/seedu/address/model/activity/exceptions/ActivityNotFoundException.java
@@ -3,5 +3,5 @@ package seedu.address.model.activity.exceptions;
 /**
  * Signals that the operation is unable to find the specified activity.
  */
-public class ActivityNotFoundException {
+public class ActivityNotFoundException extends RuntimeException {
 }

709152ab400d438f70a5f6c10aa3b8a2e3c82243
KerrynEer <kerryneer98@gmail.com>
2019-03-01 00:39:50 +0800

    Refactor code: CardFolderParser to CommandParser

--- src/main/java/seedu/address/logic/parser/CommandParser.java
+++ src/main/java/seedu/address/logic/parser/CommandParser.java
@@ -24,7 +24,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses user input.
  */
-public class CardFolderParser {
+public class CommandParser {
 
     /**
      * Used for initial separation of command word and args.

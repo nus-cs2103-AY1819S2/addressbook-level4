faaa278ec498adb0d9f9de81c2778a42797695a2
Darren <d4rr3n.96@gmail.com>
2019-02-28 16:51:00 +0800

    Edit indenting for Card.java

--- src/main/java/seedu/address/model/deck/Card.java
+++ src/main/java/seedu/address/model/deck/Card.java
@@ -13,87 +13,87 @@ import seedu.address.model.tag.Tag;
  * Guarantees: details are present and not null, field values are validated and immutable
  */
 public class Card {
-	//Identity field
+    //Identity field
 	private final String question;
 
 	private final String answer;
 
-	private Set<Tag> tags = new HashSet<Tag>();
+    private Set<Tag> tags = new HashSet<Tag>();
 
-	/**
-	 * Every field must be present and not null.
-	 */
-	public Card(String question, String answer, Set<Tag> tags) {
-		requireNonNull(question, answer);
-		this.question = question;
-		this.answer = answer;
-		this.tags.addAll(tags);
-	}
+    /**
+     * Every field must be present and not null.
+     */
+    public Card(String question, String answer, Set<Tag> tags) {
+        requireNonNull(question, answer);
+        this.question = question;
+        this.answer = answer;
+        this.tags.addAll(tags);
+    }
 
-	public String getAnswer() {
-		return answer;
-	}
+    public String getAnswer() {
+        return answer;
+    }
 
-	public String getQuestion() {
-		return question;
-	}
+    public String getQuestion() {
+        return question;
+    }
 
-	/**
-	 * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
-	 * if modification is attempted.
-	 */
-	public Set<Tag> getTags() {
-		return Collections.unmodifiableSet(tags);
-	}
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(tags);
+    }
 
-	/**
-	 * Returns true if both cards have the same question.
-	 * This defines a weaker notion of equality between two persons.
-	 */
-	public boolean isSameCard(Card otherCard) {
-		if (otherCard == this) {
-			return true;
-		}
+    /**
+     * Returns true if both cards have the same question.
+     * This defines a weaker notion of equality between two persons.
+     */
+    public boolean isSameCard(Card otherCard) {
+        if (otherCard == this) {
+            return true;
+        }
 
-		return otherCard != null
-				&& otherCard.getQuestion().equals(question);
-	}
+        return otherCard != null
+                && otherCard.getQuestion().equals(question);
+    }
 
 
-	/**
-	 * Returns true if both cards have the same question and answers.
-	 * Defines a stronger notion of equality between 2 cards.
-	 */
-	@Override
-	public boolean equals(Object other) {
-		if (other == this) {
-			return true;
-		}
+    /**
+     * Returns true if both cards have the same question and answers.
+     * Defines a stronger notion of equality between 2 cards.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
 
-		if (!(other instanceof Card)) {
-			return false;
-		}
+        if (!(other instanceof Card)) {
+            return false;
+        }
 
-		Card otherCard = (Card) other;
-		return this.answer.equals(otherCard.answer)
-				&& this.question.equals(otherCard.question);
-	}
+        Card otherCard = (Card) other;
+        return this.answer.equals(otherCard.answer)
+                && this.question.equals(otherCard.question);
+    }
 
-	@Override
-	public int hashCode() {
-		int hash = 7;
-		return hash + question.hashCode() + answer.hashCode();
-	}
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash + question.hashCode() + answer.hashCode();
+    }
 
-	@Override
-	public String toString() {
-		final StringBuilder builder = new StringBuilder();
-		builder.append("Question: ")
-				.append(getQuestion())
-				.append(" Answer: ")
-				.append(getAnswer())
-				.append(" Tags: ");
-		getTags().forEach(builder::append);
-		return builder.toString();
-	}
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Question: ")
+                .append(getQuestion())
+                .append(" Answer: ")
+                .append(getAnswer())
+                .append(" Tags: ");
+        getTags().forEach(builder::append);
+        return builder.toString();
+    }
 }

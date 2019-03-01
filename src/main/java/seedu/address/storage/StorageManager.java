e62c2693524a888d344ec7de884bba5c6db721ba
Darren <d4rr3n.96@gmail.com>
2019-03-02 01:09:22 +0800

    Edit Storage manager to change from AB4 to TopDeck

--- src/main/java/seedu/address/storage/StorageManager.java
+++ src/main/java/seedu/address/storage/StorageManager.java
@@ -7,7 +7,6 @@ import java.util.logging.Logger;
 
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.DataConversionException;
-import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyTopDeck;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
@@ -18,13 +17,13 @@ import seedu.address.model.UserPrefs;
 public class StorageManager implements Storage {
 
     private static final Logger logger = LogsCenter.getLogger(StorageManager.class);
-    private AddressBookStorage addressBookStorage;
+    private TopDeckStorage topDeckStorage;
     private UserPrefsStorage userPrefsStorage;
 
 
-    public StorageManager(AddressBookStorage addressBookStorage, UserPrefsStorage userPrefsStorage) {
+    public StorageManager(TopDeckStorage topDeckStorage, UserPrefsStorage userPrefsStorage) {
         super();
-        this.addressBookStorage = addressBookStorage;
+        this.topDeckStorage = topDeckStorage;
         this.userPrefsStorage = userPrefsStorage;
     }
 
@@ -50,29 +49,29 @@ public class StorageManager implements Storage {
 
     @Override
     public Path getTopDeckFilePath() {
-        return addressBookStorage.getAddressBookFilePath();
+        return topDeckStorage.getTopDeckFilePath();
     }
 
     @Override
     public Optional<ReadOnlyTopDeck> readTopDeck() throws DataConversionException, IOException {
-        return readAddressBook(addressBookStorage.getAddressBookFilePath());
+        return readTopDeck(topDeckStorage.getTopDeckFilePath());
     }
 
     @Override
-    public Optional<ReadOnlyAddressBook> readAddressBook(Path filePath) throws DataConversionException, IOException {
+    public Optional<ReadOnlyTopDeck> readTopDeck(Path filePath) throws DataConversionException, IOException {
         logger.fine("Attempting to read data from file: " + filePath);
-        return addressBookStorage.readAddressBook(filePath);
+        return topDeckStorage.readTopDeck(filePath);
     }
 
     @Override
     public void saveTopDeck(ReadOnlyTopDeck topDeck) throws IOException {
-        saveAddressBook(topDeck, addressBookStorage.getAddressBookFilePath());
+        saveTopDeck(topDeck, topDeckStorage.getTopDeckFilePath());
     }
 
     @Override
-    public void saveAddressBook(ReadOnlyAddressBook addressBook, Path filePath) throws IOException {
+    public void saveTopDeck(ReadOnlyTopDeck topDeck, Path filePath) throws IOException {
         logger.fine("Attempting to write to data file: " + filePath);
-        addressBookStorage.saveAddressBook(addressBook, filePath);
+        topDeckStorage.saveTopDeck(topDeck, filePath);
     }
 
 }

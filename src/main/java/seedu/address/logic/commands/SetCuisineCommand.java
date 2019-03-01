f3c0a94dc696e39e5249cc0475c6d3ac873b1419
rbth7e5 <caijie96@gmail.com>
2019-03-01 15:38:23 +0800

    setCuisineCommand: implement skeleton of execute method

--- src/main/java/seedu/address/logic/commands/SetCuisineCommand.java
+++ src/main/java/seedu/address/logic/commands/SetCuisineCommand.java
@@ -1,10 +1,14 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_RESTAURANTS;
+import java.util.List;
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.restaurant.Restaurant;
 import seedu.address.model.restaurant.categories.Cuisine;
 
 public class SetCuisineCommand extends Command {
@@ -32,6 +36,23 @@ public class SetCuisineCommand extends Command {
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-        return null;
+        requireNonNull(model);
+        List<Restaurant> lastShownList = model.getFilteredRestaurantList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_RESTAURANT_DISPLAYED_INDEX);
+        }
+
+        Restaurant restaurantToAddCuisine = lastShownList.get(index.getZeroBased());
+        Restaurant restaurantWithCuisineAdded = createRestaurantWithCuisine(restaurantToAddCuisine);
+
+        model.setRestaurant(restaurantToAddCuisine, restaurantWithCuisineAdded);
+        model.updateFilteredRestaurantList(PREDICATE_SHOW_ALL_RESTAURANTS);
+        model.commitFoodDiary();
+        return new CommandResult(String.format(MESSAGE_SET_CUISINE_SUCCESS, restaurantWithCuisineAdded));
+    }
+
+    private static Restaurant createRestaurantWithCuisine(Restaurant restaurantToAddCuisine) {
+        return restaurantToAddCuisine;
     }
 }

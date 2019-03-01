c18a9c33e5bf61e7169a3bf7d8b87164f7e42507
JiaHaoLim <jiahaolimwork@hotmail.com>
2019-03-01 20:08:03 +0800

    Fixed codestyle in ImportCommandParser.java

--- src/main/java/seedu/address/logic/parser/ImportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ImportCommandParser.java
@@ -6,7 +6,7 @@ import seedu.address.logic.commands.ImportCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new ImportCommand object
+ * Parses input arguments and creates a new ImportCommand object.
  */
 public class ImportCommandParser implements Parser<ImportCommand> {
 

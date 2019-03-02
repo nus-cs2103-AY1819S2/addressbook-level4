1732714b6e871a0d8eca5ff2c16015de56987f0b
bava98 <bavadharini.s11@gmail.com>
2019-03-02 22:47:17 +0800

    MenuMode Command to convert panels to MenuMode

---
+++ src/main/java/seedu/address/logic/commands/MenuMode.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.Mode;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.ui.MainWindow;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+/**
+ * Change the RestOrRant's mode to MenuMode.
+ * Used to adding, deleting, editing menu items from menu.
+ */
+public class MenuMode extends Command {
+    public static final String COMMAND_WORD = "menu mode"; // change to standardize with other modes
+    public static final String MESSAGE_SUCCESS = "Mode changed to Menu Mode";
+    public static final String MESSAGE_FAIL = "Mode is already Menu Mode";
+    
+    public MainWindow mainWindow;
+    
+    public CommandResult execute(Mode mode, Model model, CommandHistory history) {
+        requireAllNonNull(mode, model);
+        
+        if (!mode.equals(Mode.MENU_MODE)) {
+            mode = Mode.MENU_MODE;
+            mainWindow.fillInnerParts(mode);
+            return new CommandResult(MESSAGE_SUCCESS);
+        } else {
+            return new CommandResult(MESSAGE_FAIL);
+        }
+    }
+}

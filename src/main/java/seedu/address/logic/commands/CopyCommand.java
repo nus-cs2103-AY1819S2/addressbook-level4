da582a2b3354d975b1fc2750ccbe5c335ed1efd4
wSemis <debang@u.nus.edu>
2019-03-02 17:20:52 +0800

    Main logic for CopyCommand

--- src/main/java/seedu/address/logic/commands/CopyCommand.java
+++ src/main/java/seedu/address/logic/commands/CopyCommand.java
@@ -1,23 +1,29 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.commands.EditCommand.MESSAGE_EDIT_PERSON_SUCCESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
+
+import java.util.List;
 
 /**
  * Adds a person to the address book.
  */
 public class CopyCommand extends Command {
 
-    public static final String COMMAND_WORD = "add";
+    public static final String COMMAND_WORD = "copy";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
             + "Parameters: "
@@ -37,33 +43,38 @@ public class CopyCommand extends Command {
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
 
-    private final Person toCopy;
+    private final Index index;
 
     /**
      * Creates an AddCommand to add the specified {@code Person}
      */
-    public CopyCommand(Person person) {
-        requireNonNull(person);
-        toCopy = person;
+    public CopyCommand(Index index) {
+        requireNonNull(index);
+        this.index = index;
     }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
 
-        if (model.hasPerson(toCopy)) {
-            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
-        model.addPerson(toCopy);
+        Person personToCopy = lastShownList.get(index.getZeroBased());
+        Person copyPerson = personToCopy.copy();
+
+        model.addPerson(copyPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         model.commitAddressBook();
-        return new CommandResult(String.format(MESSAGE_SUCCESS, toCopy));
+        return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, copyPerson));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof CopyCommand // instanceof handles nulls
-                && toCopy.equals(((CopyCommand) other).toCopy));
+                && index.equals(((CopyCommand) other).index));
     }
 }

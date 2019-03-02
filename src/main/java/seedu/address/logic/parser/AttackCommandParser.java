7953b2778f5a368713ba6b9ee9a2c335566d997e
s-tr <swchong25+github@gmail.com>
2019-03-02 18:07:21 +0800

    Add stub Attack command parser

---
+++ src/main/java/seedu/address/logic/parser/AttackCommandParser.java
@@ -0,0 +1,11 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.AttackCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class AttackCommandParser implements Parser<AttackCommand> {
+	@Override
+	public AttackCommand parse(String userInput) throws ParseException {
+		return null;
+	}
+}

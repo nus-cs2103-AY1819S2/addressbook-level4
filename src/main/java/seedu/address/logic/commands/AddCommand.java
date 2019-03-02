ead40f1002c2b8475f138e3bc6ac9982d695a379
Wklee96 <lweikang96@gmail.com>
2019-03-02 15:44:37 +0800

    updated addCommand help to restaurant

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -27,12 +27,12 @@ public class AddCommand extends Command {
             + PREFIX_ADDRESS + "ADDRESS "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + "John Doe "
-            + PREFIX_PHONE + "98765432 "
-            + PREFIX_EMAIL + "johnd@example.com "
+            + PREFIX_NAME + "KFC "
+            + PREFIX_PHONE + "68765432 "
+            + PREFIX_EMAIL + "kfc@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + PREFIX_TAG + "fastfood "
+            + PREFIX_TAG + "friedChicken ";
 
     public static final String MESSAGE_SUCCESS = "New restaurant added: %1$s";
     public static final String MESSAGE_DUPLICATE_RESTAURANT = "This restaurant already exists in the food diary";

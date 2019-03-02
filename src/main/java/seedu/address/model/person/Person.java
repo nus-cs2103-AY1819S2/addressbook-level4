b3de78199c0c9eebec6522ba8d040a2fa9382da2
wSemis <debang@u.nus.edu>
2019-03-02 18:24:48 +0800

    = Add Copy tag for copies

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -48,6 +48,7 @@ public class Person {
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
+        this.tags.add(new Tag("Copy"));
         this.copyCount = copyCount;
         copyInfo = new CopyTag(personToCopy,"$Copy"+copyCount);
     }

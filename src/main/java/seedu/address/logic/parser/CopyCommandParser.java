29012f6ec1ce80c646388d19047df132241cea9f
wSemis <debang@u.nus.edu>
2019-03-02 18:04:04 +0800

    Copy command can be called

--- src/main/java/seedu/address/logic/parser/CopyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CopyCommandParser.java
@@ -1,4 +1,25 @@
 package seedu.address.logic.parser;
 
-public class CopyCommandParser {
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.CopyCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+public class CopyCommandParser implements Parser<CopyCommand> {
+    public CopyCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(args);
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, CopyCommand.MESSAGE_USAGE), pe);
+        }
+
+
+        return new CopyCommand(index);
+    }
 }

7c4ab48e304af2fb22af58165075615429e58098
chanqingzhou <chanqingzhou@hotmail.com>
2019-03-02 22:41:12 +0800

    added view command that allows you to view your details

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -1,4 +1,24 @@
 package seedu.address.logic.commands;
 
-public class View {
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+/**
+ * Lists all restaurants in the food diary to the user.
+ */
+public class ViewCommand extends Command {
+
+    public static final String COMMAND_WORD = "view";
+
+    public static final String MESSAGE_SUCCESS = "View your personalised statistics";
+
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
+        int size  = model.getSize();
+        return new CommandResult(MESSAGE_SUCCESS + size);
+    }
 }

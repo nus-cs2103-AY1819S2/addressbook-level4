e5cc4bd788a542b73fa0bd7ae4edfbf1682969f4
bava98 <bavadharini.s11@gmail.com>
2019-03-02 21:08:19 +0800

    Changes for ListPanel to show menu (or order) items - incomplete

---
+++ src/main/java/seedu/address/model/Item.java
@@ -0,0 +1,7 @@
+package seedu.address.model;
+
+public abstract class Item {
+    public abstract int hashCode();
+    public abstract String toString();
+    public abstract boolean equals(Object other);
+}
--- src/main/java/seedu/address/ui/ListPanel.java
+++ src/main/java/seedu/address/ui/ListPanel.java
@@ -11,6 +11,8 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.Item;
+import seedu.address.model.menu.MenuItem;
 import seedu.address.model.person.Person;
 
 /**
@@ -22,50 +24,92 @@ public class ListPanel extends UiPart<Region> {
     
     @FXML
     private ListView<Person> personListView;
+    private ListView<Item> itemListView;
     
-    public ListPanel(ObservableList<Person> personList, ObservableValue<Person> selectedPerson,
-                           Consumer<Person> onSelectedPersonChange) {
+    public ListPanel(ObservableList<Item> itemList, ObservableValue<Item> selectedItem,
+                     Consumer<Item> onSelectedItemChange) {
         super(FXML);
-        personListView.setItems(personList);
-        personListView.setCellFactory(listView -> new ListPanel.PersonListViewCell());
-        personListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            logger.fine("Selection in person list panel changed to : '" + newValue + "'");
-            onSelectedPersonChange.accept(newValue);
+        itemListView.setItems(itemList);
+        itemListView.setCellFactory(listView -> new ListPanel.ItemListViewCell());
+        itemListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            logger.fine("Selection in list panel changed to : '" + newValue +"'");
+            onSelectedItemChange.accept(newValue);
         });
-        selectedPerson.addListener((observable, oldValue, newValue) -> {
-            logger.fine("Selected person changed to: " + newValue);
+        selectedItem.addListener(((observable, oldValue, newValue) -> {
+            logger.fine("Selected item changed to: " + newValue);
             
-            // Don't modify selection if we are already selecting the selected person,
+            // Don't modify selection if we are already selecting the selected item,
             // otherwise we would have an infinite loop.
-            if (Objects.equals(personListView.getSelectionModel().getSelectedItem(), newValue)) {
+            if (Objects.equals(itemListView.getSelectionModel().getSelectedItem(), newValue)) {
                 return;
             }
             
             if (newValue == null) {
-                personListView.getSelectionModel().clearSelection();
+                itemListView.getSelectionModel().clearSelection();
             } else {
-                int index = personListView.getItems().indexOf(newValue);
-                personListView.scrollTo(index);
-                personListView.getSelectionModel().clearAndSelect(index);
+                int index = itemListView.getItems().indexOf(newValue);
+                itemListView.scrollTo(index);
+                itemListView.getSelectionModel().clearAndSelect(index);
             }
-        });
+        }));
     }
-    
+
+    //    public ListPanel(ObservableList<Person> personList, ObservableValue<Person> selectedPerson,
+    //                           Consumer<Person> onSelectedPersonChange) {
+    //        super(FXML);
+    //        personListView.setItems(personList);
+    //        personListView.setCellFactory(listView -> new ListPanel.PersonListViewCell());
+    //        personListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+    //            logger.fine("Selection in person list panel changed to : '" + newValue + "'");
+    //            onSelectedPersonChange.accept(newValue);
+    //        });
+    //        selectedPerson.addListener((observable, oldValue, newValue) -> {
+    //            logger.fine("Selected person changed to: " + newValue);
+    //            
+    //            // Don't modify selection if we are already selecting the selected person,
+    //            // otherwise we would have an infinite loop.
+    //            if (Objects.equals(personListView.getSelectionModel().getSelectedItem(), newValue)) {
+    //                return;
+    //            }
+    //            
+    //            if (newValue == null) {
+    //                personListView.getSelectionModel().clearSelection();
+    //            } else {
+    //                int index = personListView.getItems().indexOf(newValue);
+    //                personListView.scrollTo(index);
+    //                personListView.getSelectionModel().clearAndSelect(index);
+    //            }
+    //        });
+    //    }
+
     /**
-     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
+     * Custom {@code ListCell} that displays the graphics of a {@code Item} using a {@code ItemCard}.
      */
-    class PersonListViewCell extends ListCell<Person> {
+    class ItemListViewCell extends ListCell<Item> {
         @Override
-        protected void updateItem(Person person, boolean empty) {
-            super.updateItem(person, empty);
+        protected void updateItem(Item item, boolean empty) {
+            super.updateItem(item, empty);
             
-            if (empty || person == null) {
+            if (empty || item == null) {
                 setGraphic(null);
                 setText(null);
             } else {
-                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
+                setGraphic(new ItemCard(item, getIndex() + 1).getRoot());
             }
         }
     }
+    //    class PersonListViewCell extends ListCell<Person> {
+    //        @Override
+    //        protected void updateItem(Person person, boolean empty) {
+    //            super.updateItem(person, empty);
+    //            
+    //            if (empty || person == null) {
+    //                setGraphic(null);
+    //                setText(null);
+    //            } else {
+    //                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
+    //            }
+    //        }
+    //    }
     
 }

eb91b420335b2ecc5733590a8bccc2a206c6544b
s-tr <swchong25+github@gmail.com>
2019-03-03 10:42:45 +0800

    Add stub Attack command parser test

---
+++ src/test/java/seedu/address/logic/parser/AttackCommandParserTest.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.SelectCommand;
+
+/**
+ * Test scope:
+ *
+ *
+ */
+public class AttackCommandParserTest {
+
+	private AttackCommandParser parser = new AttackCommandParser();
+
+	@Test
+	public void parse_validArgs_returnsAttackCommand() {
+		//assertParseSuccess(parser, "1", new SelectCommand(INDEX_FIRST_PERSON));
+		org.junit.Assert.assertTrue(true);
+	}
+
+	@Test
+	public void parse_invalidArgs_throwsParseException() {
+		//assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
+		org.junit.Assert.assertTrue(true);
+	}
+}

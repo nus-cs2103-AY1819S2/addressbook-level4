d790b23c1bd8489c21c891c04958ae8094db41e7
mmdlow <mmdlow@gmail.com>
2019-03-03 21:31:30 +0800

    Remove email fields in remaining json files

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -25,13 +25,11 @@ public class ParserUtilTest {
     private static final String INVALID_QUESTION = " ";
     private static final String INVALID_ANSWER = " ";
     private static final String INVALID_ADDRESS = " ";
-    private static final String INVALID_EMAIL = "example.com";
     private static final String INVALID_HINT = "#friend";
 
     private static final String VALID_QUESTION = "Rachel Walker";
     private static final String VALID_ANSWER = "123456";
     private static final String VALID_ADDRESS = "123 Main Street #0505";
-    private static final String VALID_EMAIL = "rachel@example.com";
     private static final String VALID_HINT_1 = "friend";
     private static final String VALID_HINT_2 = "neighbour";
 

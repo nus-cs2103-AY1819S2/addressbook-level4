a6a55514c7a5bab1b729e39e59f4d4e71db4cee9
halcyoneee <ting00733@gmail.com>
2019-03-03 19:24:35 +0800

    Add more tests for UserPrefsTest

--- src/test/java/braintrain/model/UserPrefsTest.java
+++ src/test/java/braintrain/model/UserPrefsTest.java
@@ -1,7 +1,12 @@
 package braintrain.model;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
+import braintrain.commons.core.GuiSettings;
 import braintrain.testutil.Assert;
 
 public class UserPrefsTest {
@@ -10,5 +15,38 @@ public class UserPrefsTest {
     public void setGuiSettings_nullGuiSettings_throwsNullPointerException() {
         UserPrefs userPref = new UserPrefs();
         Assert.assertThrows(NullPointerException.class, () -> userPref.setGuiSettings(null));
+    }
+
+    @Test
+    public void equals() {
+        UserPrefs userPrefs = new UserPrefs();
+
+        // same values -> returns true
+        UserPrefs userPrefsCopy = new UserPrefs();
+        assertTrue(userPrefs.equals(userPrefsCopy));
+
+        // same object -> returns true
+        assertTrue(userPrefs.equals(userPrefs));
+
+        // null -> returns false
+        assertFalse(userPrefs == null);
+
+        // different types -> returns false
+        assertFalse(userPrefs.equals(5));
+    }
+
+    @Test
+    public void hashcode() {
+        GuiSettings guiSettings = new GuiSettings(1, 2, 3, 4);
+        UserPrefs userPrefs = new UserPrefs();
+        userPrefs.setGuiSettings(guiSettings);
+
+        UserPrefs userPrefsCopy = new UserPrefs();
+        userPrefsCopy.setGuiSettings(guiSettings);
+
+        UserPrefs userPrefsDiff = new UserPrefs();
+        userPrefsDiff.setGuiSettings(new GuiSettings(1, 2, 3, 5));
+        // same values -> returns same hashcode
+        assertNotEquals(userPrefs.hashCode(), userPrefsDiff.hashCode());
     }
 }

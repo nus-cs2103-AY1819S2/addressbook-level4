40cf289f3f582dedc610fd77dfe218f7804805c4
skpai27 <skpai27@yahoo.com.sg>
2019-03-03 16:46:26 +0800

    Modified CliSyntax to include Prefix definitions for REVIEWENTRY, REVIEWRATING, and INDEX (prefix for index used to parse command for AddReview)

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,5 +11,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_REVIEWENTRY = new Prefix("re/");
+    public static final Prefix PREFIX_REVIEWRATING = new Prefix("rr/");
+    public static final Prefix PREFIX_INDEX = new Prefix("i/");
 
 }

b3e49d6b136185be5d1c387aba1b21136a155fec
lallanachang <lallanachang@gmail.com>
2019-03-03 11:09:28 +0800

    session class

--- src/main/java/braintrain/model/card/SRSCard.java
+++ src/main/java/braintrain/model/card/SRSCard.java
@@ -1,4 +1,51 @@
 package braintrain.model.card;
 
-public class SRSCard {
+import java.time.Instant;
+
+import braintrain.model.lesson.Lesson;
+import braintrain.model.user.CardData;
+
+/**
+ * Represents a flashcard contains both core fields(question and answer) and srs data.
+ */
+public class SrsCard {
+    private Card card;
+    private CardData cardData;
+    private Lesson lesson;
+    private int questionIndex;
+    private int answerIndex;
+
+    public SrsCard(Card card, CardData cardData, Lesson lesson) {
+        this.card = card;
+        this.cardData = cardData;
+        this.lesson = lesson;
+
+        questionIndex = lesson.getQuestionIndex();
+        answerIndex = lesson.getAnswerIndex();
+    }
+    public SrsCard() {
+
+    }
+
+    public String getQuestion() {
+        return card.getCore(questionIndex);
+    }
+    public String getAnswer() {
+        return card.getCore(answerIndex);
+    }
+    public Instant getSrsDueDate() {
+        return cardData.getSrsDueDate();
+    }
+    public int getHashcode() {
+        return cardData.getHashCode();
+    }
+    public int getNumOfAttempts() {
+        return cardData.getNumOfAttempts();
+    }
+    public int getStreak() {
+        return cardData.getStreak();
+    }
+    public Lesson getLesson() {
+        return lesson;
+    }
 }

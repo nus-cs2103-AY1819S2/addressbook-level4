b4d2a288546385c0c33e626d08413a4bf50ed6a6
e0191632 <e0191632@u.nus.edu>
2019-03-05 03:42:49 +0800

    checkstyletest correction

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -5,12 +5,12 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import seedu.address.model.EquipmentManager;
+import seedu.address.model.ReadOnlyEquipmentManager;
 import seedu.address.model.equipment.Address;
 import seedu.address.model.equipment.Email;
 import seedu.address.model.equipment.Equipment;
 import seedu.address.model.equipment.Name;
 import seedu.address.model.equipment.Phone;
-import seedu.address.model.ReadOnlyEquipmentManager;
 import seedu.address.model.tag.Tag;
 
 /**

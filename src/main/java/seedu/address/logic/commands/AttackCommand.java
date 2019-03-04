60e94e86f3f843d0015c5fecf7df20b9267bc484
s-tr <swchong25+github@gmail.com>
2019-03-04 08:40:48 +0800

    Add simple behaviour to AttackCommand

--- src/main/java/seedu/address/logic/commands/AttackCommand.java
+++ src/main/java/seedu/address/logic/commands/AttackCommand.java
@@ -1,13 +1,17 @@
 package seedu.address.logic.commands;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.cell.Coordinates;
 
 import static java.util.Objects.requireNonNull;
 
 /**
  * Attacks a cell on the board.
+ *
+ *
  */
 public class AttackCommand extends Command {
 
@@ -16,10 +20,22 @@ public class AttackCommand extends Command {
 	public static final String COMMAND_ALIAS2 = "hit";
 	public static final String COMMAND_ALIAS3 = "fire";
 
+	public static final String MESSAGE_USAGE = COMMAND_WORD + ": Attacks the specified cell.\n" +
+		"Parameters: " +
+		"SQUARE (a letter followed by a positive integer)\n" +
+		"Example: attack b5";
+
+	private Coordinates coord;
+
+	public AttackCommand(Coordinates coord) {
+		this.coord = coord;
+	}
+
 	@Override
 	public CommandResult execute(Model model, CommandHistory history) throws CommandException {
 		requireNonNull(model);
 
-		return null;
+		return new CommandResult(
+		        String.format("Attacked cell %s", coord.toString()));
 	}
 }

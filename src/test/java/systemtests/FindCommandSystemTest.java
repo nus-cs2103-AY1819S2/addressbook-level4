ba8c95946dab14d2538d669be5a1113ee1382313
A0143487X-Kevin <k3v1n.y30ng@gmail.com>
2019-03-04 22:04:58 +0800

    Change name back to FindCommandSystemTest

--- src/test/java/systemtests/FindCommandSystemTest.java
+++ src/test/java/systemtests/FindCommandSystemTest.java
@@ -21,7 +21,7 @@ import seedu.address.logic.commands.UndoCommand;
 import seedu.address.model.Model;
 import seedu.address.model.tag.Tag;
 
-public class MemberFindCommandSystemTest extends AddressBookSystemTest {
+public class FindCommandSystemTest extends AddressBookSystemTest {
 
     @Test
     public void find() {

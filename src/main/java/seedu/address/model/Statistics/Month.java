fce2bda080429937c6d14f7e30a1cc0f191af034
beesaycheese <beesaycheese@users.noreply.github.com>
2019-03-04 21:44:27 +0800

    Add Day Month and Year class to model

--- src/main/java/seedu/address/model/Statistics/Month.java
+++ src/main/java/seedu/address/model/Statistics/Month.java
@@ -1,4 +1,55 @@
 package seedu.address.model.Statistics;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 public class Month {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Month should be in the format <double digit integer>, and it should not be blank";
+
+    /*
+     * The first character of the item code must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[0-9][0-9]";
+
+    public final String month;
+
+    /**
+     * Constructs a {@code Month}.
+     *
+     * @param month A valid code.
+     */
+    public Month(String month) {
+        requireNonNull(month);
+        checkArgument(isValidCode(month), MESSAGE_CONSTRAINTS);
+        this.month = month;
+    }
+
+    /**
+     * Returns true if a given string is a valid code.
+     */
+    public static boolean isValidCode(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return month;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Month // instanceof handles nulls
+                && month.equals(((Month) other).month)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return month.hashCode();
+    }
+
 }
--- src/main/java/seedu/address/model/Statistics/Year.java
+++ src/main/java/seedu/address/model/Statistics/Year.java
@@ -1,4 +1,55 @@
 package seedu.address.model.Statistics;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 public class Year {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Year should be in the format <four digit integer>, and it should not be blank";
+
+    /*
+     * The first character of the item code must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[0-9][0-9][0-9][0-9]";
+
+    public final String year;
+
+    /**
+     * Constructs a {@code Year}.
+     *
+     * @param year A valid code.
+     */
+    public Year(String year) {
+        requireNonNull(year);
+        checkArgument(isValidCode(year), MESSAGE_CONSTRAINTS);
+        this.year = year;
+    }
+
+    /**
+     * Returns true if a given string is a valid code.
+     */
+    public static boolean isValidCode(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return year;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Year // instanceof handles nulls
+                && year.equals(((Year) other).year)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return year.hashCode();
+    }
+
 }

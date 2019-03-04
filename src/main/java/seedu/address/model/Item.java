753f7a1e29f212bcf161e7c5101332b88502543d
bava98 <bavadharini.s11@gmail.com>
2019-03-04 18:41:33 +0800

    remove unnecessary files

--- src/main/java/seedu/address/model/Item.java
+++
@@ -1,7 +0,0 @@
-package seedu.address.model;
-
-public abstract class Item {
-    public abstract int hashCode();
-    public abstract String toString();
-    public abstract boolean equals(Object other);
-}

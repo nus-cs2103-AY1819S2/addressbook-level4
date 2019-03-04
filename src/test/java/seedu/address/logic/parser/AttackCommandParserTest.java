bec4b4c3082f266f1afc11ab4df137dabc67fb36
s-tr <swchong25+github@gmail.com>
2019-03-04 10:25:42 +0800

    Modify tabs to spaces in AttackCommandParserTest

--- src/test/java/seedu/address/logic/parser/AttackCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AttackCommandParserTest.java
@@ -17,35 +17,35 @@ import seedu.address.model.cell.Coordinates;
  */
 public class AttackCommandParserTest {
 
-	private AttackCommandParser parser = new AttackCommandParser();
+    private AttackCommandParser parser = new AttackCommandParser();
 
-	@Test
+    @Test
     public void parse_validArgs_returnsAttackCommand() {
         assertParseSuccess(parser, "a1", new AttackCommand(new Coordinates("a1")));
-	}
+    }
 
-	@Test
-	public void parse_invalidColumn_throwsParseException() {
-		assertParseFailure(parser, "$1", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
-	}
+    @Test
+    public void parse_invalidColumn_throwsParseException() {
+        assertParseFailure(parser, "$1", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
+    }
 
-	@Test
-	public void parse_invalidRow_throwsParseException() {
-		assertParseFailure(parser, "bb", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
-	}
+    @Test
+    public void parse_invalidRow_throwsParseException() {
+        assertParseFailure(parser, "bb", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
+    }
 
-	@Test
-	public void parse_invalidRowColumn_throwsParseException() {
-		assertParseFailure(parser, "7c", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
-	}
+    @Test
+    public void parse_invalidRowColumn_throwsParseException() {
+        assertParseFailure(parser, "7c", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
+    }
 
-	@Test
-	public void parse_invalidFormat_throwsParseException() {
-		assertParseFailure(parser, "asfdafdgar", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
-	}
+    @Test
+    public void parse_invalidFormat_throwsParseException() {
+        assertParseFailure(parser, "asfdafdgar", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
+    }
 
-	@Test
-	public void parse_emptyString_throwsParseException() {
-		assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
-	}
+    @Test
+    public void parse_emptyString_throwsParseException() {
+        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
+    }
 }

6487c08960d3b5f8027d514ac117057a3c320762
s-tr <swchong25+github@gmail.com>
2019-03-04 09:36:09 +0800

    Add test for AttackCommandParser

--- src/test/java/seedu/address/logic/parser/AttackCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AttackCommandParserTest.java
@@ -7,7 +7,8 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.Test;
 
-import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.commands.AttackCommand;
+import seedu.address.model.cell.Coordinates;
 
 /**
  * Test scope:
@@ -20,13 +21,31 @@ public class AttackCommandParserTest {
 
 	@Test
     public void parse_validArgs_returnsAttackCommand() {
-        //assertParseSuccess(parser, "1", new SelectCommand(INDEX_FIRST_PERSON));
-		org.junit.Assert.assertTrue(true);
+        assertParseSuccess(parser, "a1", new AttackCommand(new Coordinates("a1")));
 	}
 
 	@Test
-	public void parse_invalidArgs_throwsParseException() {
-		//assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
-		org.junit.Assert.assertTrue(true);
+	public void parse_invalidColumn_throwsParseException() {
+		assertParseFailure(parser, "$1", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
+	}
+
+	@Test
+	public void parse_invalidRow_throwsParseException() {
+		assertParseFailure(parser, "bb", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
+	}
+
+	@Test
+	public void parse_invalidRowColumn_throwsParseException() {
+		assertParseFailure(parser, "7c", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
+	}
+
+	@Test
+	public void parse_invalidFormat_throwsParseException() {
+		assertParseFailure(parser, "asfdafdgar", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
+	}
+
+	@Test
+	public void parse_emptyString_throwsParseException() {
+		assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AttackCommand.MESSAGE_USAGE));
 	}
 }

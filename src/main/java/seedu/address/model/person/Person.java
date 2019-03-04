cd4c1a34693588fdf215aa8c21fbd323ce6107bf
Daniel <DanielDSSim@gmail.com>
2019-03-04 23:47:32 +0800

    Edit Person to require non null for major

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -30,7 +30,7 @@ public class Person {
      * Every field must be present and not null.
      */
     public Person(Name name, Phone phone, Email email, Address address, School school, Major major, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, school, tags);
+        requireAllNonNull(name, phone, email, address, school, major, tags);
 
         this.name = name;
         this.phone = phone;

b9a5a7f504de5f2378b1cbe8a18b6cb8de00ceee
JeremyLoye <jeremyloye@gmail.com>
2019-03-04 22:17:08 +0800

    Checkstyle fixes

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -65,8 +65,8 @@ public class CommandTestUtil {
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
     public static final String INVALID_AMOUNT_DESC = " " + PREFIX_AMOUNT + "*2"; // "*" not allowed in amounts
-    public static final String INVALID_DATE_DESC
-            = " " + PREFIX_DATE + "29/29/2019"; // invalid date not allowed in dates
+    public static final String INVALID_DATE_DESC =
+            " " + PREFIX_DATE + "29/29/2019"; // invalid date not allowed in dates
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
 
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";

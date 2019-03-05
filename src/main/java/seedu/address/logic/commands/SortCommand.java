57d1860e2ffd08e7c93e7489436912e35306cda4
WeiTangLau <lauweitang@gmail.com>
2019-03-05 10:30:16 +0800

    v1.1Sort added the global change, sort
    Implemented the sort command that the GUI will register the
    sort command and output the success message. The changes made
    will have an global impact on other commands. For instance, the
    list command will have a different output due to the sort. In current
    version, the sorting will not perform any action. However, in future
    versions, we will then implement the sort.
    
    Resolves: #36
    See also: #67

---
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -0,0 +1,16 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+public class SortCommand extends  Command {
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_SUCCESS = "Sort success!";
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        return new CommandResult(MESSAGE_SUCCESS, false, false);
+    }
+
+}

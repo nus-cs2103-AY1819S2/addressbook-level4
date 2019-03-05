12c212bdcb65dba9fa4848e5c1d63dab57502ae8
siyingpoof <alvina.ongg@gmail.com>
2019-03-05 14:22:11 +0800

    Edited AddDoctorCommand file

--- src/main/java/seedu/address/logic/commands/AddDoctorCommand.java
+++ src/main/java/seedu/address/logic/commands/AddDoctorCommand.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Doctor;
 
 public class AddDoctorCommand extends Command {
     public static final String COMMAND_WORD = "add-doctor";

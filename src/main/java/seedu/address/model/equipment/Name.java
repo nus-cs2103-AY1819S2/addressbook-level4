227752741352cc4475472481b2602365c21892b9
e0191632 <e0191632@u.nus.edu>
2019-03-06 01:14:25 +0800

    javadoc

--- src/main/java/seedu/address/model/equipment/Name.java
+++ src/main/java/seedu/address/model/equipment/Name.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Equipment's name in the address book.
+ * Represents a Equipment's name in the Equipment Manager.
  * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
  */
 public class Name {

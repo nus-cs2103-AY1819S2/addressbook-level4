c54d258d4c3934297acb21044ac9d7f025e808a4
chiuyuhua <chiuyuhua@u.nus.edu>
2019-03-05 20:44:56 +0800

    v.1.3 Edit Syntax For "Race"

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -23,7 +23,7 @@ import seedu.address.testutil.Assert;
 public class JsonAdaptedPersonTest {
     private static final String INVALID_NAME = "R@chel";
     private static final String INVALID_PHONE = "+651234";
-    private static final String INVALID_RACE = "Ang Moh"
+    private static final String INVALID_RACE = "Ang Moh";
     private static final String INVALID_ADDRESS = " ";
     private static final String INVALID_EMAIL = "example.com";
     private static final String INVALID_SCHOOL = " ";

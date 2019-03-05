ddd05ed18aa0770c55060ebbcc07ad8248fe8802
bentwj <bentwj3@gmail.com>
2019-03-05 16:33:26 +0800

    add FinancesStatisticsTest

--- src/main/java/seedu/address/model/record/ConsultationRecord.java
+++ src/main/java/seedu/address/model/record/ConsultationRecord.java
@@ -2,15 +2,20 @@ package seedu.address.model.record;
 
 import java.math.BigDecimal;
 
+import seedu.address.model.consultation.Prescription;
+
 /**
  * Record representation of a consultation
  */
 public class ConsultationRecord extends Record {
-    // TODO Constructor
+    private Prescription prescription;
+    public ConsultationRecord(Prescription prescription) {
+        this.prescription = prescription;
+    }
     @Override
     public Statistics toStatistics() {
         BigDecimal consultationFee = Statistics.getConsultationFee();
-        // TODO Add prescription expenditures
+        // TODO BigDecimal prescriptionFee = prescription.getQuantity();
         return new Statistics(1, consultationFee, BigDecimal.ZERO);
     }
 }

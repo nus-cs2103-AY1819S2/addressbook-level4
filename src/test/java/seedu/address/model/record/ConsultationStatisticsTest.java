e21a5813d0b9da71850ad6d5536f35c729fcc21f
bentwj <bentwj3@gmail.com>
2019-03-05 16:34:38 +0800

    add ConsultationStatisticsTest

--- src/test/java/seedu/address/model/record/ConsultationStatisticsTest.java
+++ src/test/java/seedu/address/model/record/ConsultationStatisticsTest.java
@@ -1,5 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+package seedu.address.model.record;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
 class ConsultationStatisticsTest {
-
+    private Statistics stats = new Statistics(3, BigDecimal.ZERO, BigDecimal.ZERO);
+    private Statistics consultStats = new ConsultationStatistics(stats);
+    private StringBuilder sb = new StringBuilder();
+    @Test
+    void toStringTest() {
+        sb.append("Number of consultations: ")
+                .append(3)
+                .append("\n\n");
+        Assert.assertEquals(consultStats.toString(), sb.toString());
+    }
 }

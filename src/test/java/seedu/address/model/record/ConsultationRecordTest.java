61a09c73a9c25b9f8df85649c39aad364da14e47
bentwj <bentwj3@gmail.com>
2019-03-05 16:32:28 +0800

    add ConsultationRecordTest

---
+++ src/test/java/seedu/address/model/record/ConsultationRecordTest.java
@@ -0,0 +1,18 @@
+package seedu.address.model.record;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.consultation.Prescription;
+
+class ConsultationRecordTest {
+    @Test
+    void toStatistics() {
+        Statistics stats = new Statistics(1, Statistics.getConsultationFee(), BigDecimal.ZERO);
+        Prescription prescription = new Prescription("test", 3);
+        ConsultationRecord consultRecord = new ConsultationRecord(prescription);
+        Assert.assertEquals(consultRecord.toStatistics(), stats);
+    }
+}

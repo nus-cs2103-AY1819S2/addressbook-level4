b88a7cbc6227ad29fcd139740dd15bfe97785e71
Liuyy99 <e0253712@u.nus.edu>
2019-03-05 11:27:00 +0800

    add WriteUp and MedicalHistory classes

---
+++ src/main/java/seedu/address/model/medicalHistory/MedicalHistory.java
@@ -0,0 +1,37 @@
+package seedu.address.model.medicalHistory;
+
+import seedu.address.model.person.Name;
+
+public class MedicalHistory {
+
+    // Identity field
+/*    private Patient patient;
+    private Doctor doctor;
+*/
+    private Name name;
+    private WriteUp writeUp;
+    /*
+        // Data field
+        private Date time;
+    */
+    //Constructor
+    public MedicalHistory(/*Patient patient, Doctor doctor, */Name name, WriteUp writeUp /* Date time*/) {
+/*
+        this.patient = patient;
+        this.doctor = doctor;
+        this.writeup = note;
+        this.time = time;
+*/
+        this.name = name;
+        this.writeUp = writeUp;
+    }
+
+    public Name getName() {
+        return this.name;
+    }
+
+    public WriteUp getWriteUp() {
+        return this.writeUp;
+    }
+}
+

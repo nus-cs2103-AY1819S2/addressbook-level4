7f121cc75a2c0fd0f77c109df453dff44f4f8655
nerrons <coniinedieu@gmail.com>
2019-03-06 09:49:26 +0800

    Fix logic tests

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -69,7 +69,7 @@ public class LogicManagerTest {
 
     @Test
     public void execute_commandExecutionError_throwsCommandException() {
-        String deleteCommand = "delete 9";
+        String deleteCommand = "deletecustomer 9";
         assertCommandException(deleteCommand, MESSAGE_INVALID_CUSTOMER_DISPLAYED_INDEX);
         assertHistoryCorrect(deleteCommand);
     }

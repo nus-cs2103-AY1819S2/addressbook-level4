6a23f5b01517a7aaee15a15ffaee0becd171be4f
Lookuz <cweyyeh512@gmail.com>
2019-03-06 10:48:56 +0800

    Fixed unlimited amount of checkstyle errors for AddCommandParser class.

--- src/test/java/seedu/address/logic/commands/AddPersonCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddPersonCommandTest.java
@@ -23,8 +23,8 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
-import seedu.address.model.person.healthworker.HealthWorker;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.healthworker.HealthWorker;
 import seedu.address.testutil.PersonBuilder;
 
 public class AddPersonCommandTest {

f3d7960b54e7b28beb2f64945b8fa69a277dcd21
jetkan-yk <jetkan.yk@gmail.com>
2019-03-06 22:12:08 +0800

    (v1.1) Implement skeleton Label function Update code to resolve Travis CL build error

--- src/main/java/seedu/address/logic/commands/LabelCommand.java
+++ src/main/java/seedu/address/logic/commands/LabelCommand.java
@@ -5,15 +5,14 @@ import seedu.address.model.Model;
 
 /**
  * Label a PDF file.
- */
+  */
 public class LabelCommand extends Command {
     public static final String COMMAND_WORD = "label";
 
     public static final String MESSAGE_SUCCESS = "Label success!";
-
+    
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         return new CommandResult(MESSAGE_SUCCESS, false, false);
     }
-
 }

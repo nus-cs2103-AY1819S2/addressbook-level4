20c4dc4a86d08a381f1dafec74349acd4f265476
Kyler Wong Shen Meng <wsm.kyler@gmail.com>
2019-03-07 20:52:24 +0800

    Updated attributes in Tooth class

--- src/main/java/seedu/address/model/patient/Tooth.java
+++ src/main/java/seedu/address/model/patient/Tooth.java
@@ -4,46 +4,33 @@ package seedu.address.model.patient;
  * Represents a tooth of a Person.
  */
 class Tooth {
-    private boolean isPresent = true;
-    private boolean isOnStatus = false;
-    private Status status;
+    private boolean isPresent;
+    private boolean onStatus;
+    private Status status = new Status();
 
     Tooth(boolean isPresent) {
         this.isPresent = isPresent;
-    }
-
-    Tooth(Status status) {
-        this.isOnStatus = true;
-        this.status = status;
-    }
-
-    Tooth(boolean isPresent, boolean hasStatus) {
-        this.isPresent = isPresent;
-        this.isOnStatus = hasStatus;
+        this.onStatus = false;
     }
 
     public void setPresent(boolean present) {
         isPresent = present;
     }
 
-    public void setOnStatus(boolean onStatus) {
-        isOnStatus = onStatus;
+    public boolean isPresent() {
+        return isPresent;
     }
 
     public void setStatus(Status status) {
         this.status = status;
     }
 
-    public boolean isPresent() {
-        return isPresent;
+    public Status getStatus() {
+        return status;
     }
 
     public boolean isOnStatus() {
-        return isOnStatus;
-    }
-
-    public Status getStatus() {
-        return status;
+        return onStatus;
     }
 
 }

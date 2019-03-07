45bca4a4334381979096ecf246e51c3f595cb6d7
Kyler Wong Shen Meng <wsm.kyler@gmail.com>
2019-03-07 20:18:49 +0800

    Finished methods that build default teeth layouts for users

--- src/main/java/seedu/address/model/patient/Teeth.java
+++ src/main/java/seedu/address/model/patient/Teeth.java
@@ -10,20 +10,27 @@ import seedu.address.model.patient.exceptions.TeethLayoutException;
 public class Teeth {
     private static final String NONE = "none";
     private static final String CHILD = "child";
-    private static final int PRIMARYTEETHCOUNT = 20;
     private static final String ADULT = "adult";
+    private static final int PRIMARYTEETHCOUNT = 20;
     private static final int PERMANENTTEETHCOUNT = 32;
 
     private ArrayList<Tooth> permanentTeeth;
     private ArrayList<Tooth> primaryTeeth;
 
+    /**
+     * Default constructor. Builds a teeth template according to the specified teeth layout.
+     * @param teethLayout the teeth layout specified by the user.
+     */
     Teeth(String teethLayout) {
+        buildNoTeeth();
         switch (teethLayout) {
         case NONE:
             break;
         case CHILD:
+            addPrimaryTeeth();
             break;
         case ADULT:
+            addPermanentTeeth();
             break;
         default:
             throw new TeethLayoutException();
@@ -31,33 +38,63 @@ public class Teeth {
     }
 
     /**
+     * Initialises an empty set of primary and permanent teeth.
+     */
+    private void buildNoTeeth() {
+        ArrayList<Tooth> primaryTeeth = new ArrayList<>();
+        ArrayList<Tooth> permanentTeeth = new ArrayList<>();
+
+        for (int i = 0; i < PRIMARYTEETHCOUNT; i++) {
+            primaryTeeth.add(new Tooth(false));
+        }
+
+        this.primaryTeeth = primaryTeeth;
+
+        for (int i = 0; i < PERMANENTTEETHCOUNT; i++) {
+            permanentTeeth.add(new Tooth(false));
+        }
+
+        this.permanentTeeth = permanentTeeth;
+    }
+
+    /**
      * Initialises a set of good primary teeth.
      */
-    private void buildPrimaryTeeth() {
-        ArrayList<Tooth> primaryTeeth = new ArrayList<>();
-        for (int i = 0; i < PRIMARYTEETHCOUNT; i++) {
-            primaryTeeth.add(new Tooth());
+    private void addPrimaryTeeth() {
+        for (Tooth tooth : primaryTeeth) {
+            tooth.setPresent(true);
         }
-        this.primaryTeeth = primaryTeeth;
     }
 
     /**
      * Initialises a set of good permanent teeth.
      */
-    private void buildPermanentTeeth() {
-        ArrayList<Tooth> permanentTeeth = new ArrayList<>();
-        for (int i = 0; i < PERMANENTTEETHCOUNT; i++) {
-            permanentTeeth.add(new Tooth());
+    private void addPermanentTeeth() {
+        for (Tooth tooth : permanentTeeth) {
+            tooth.setPresent(true);
         }
-        this.permanentTeeth = permanentTeeth;
     }
 
     /**
+     * For primary tooth.
+     * Returns a Tooth representing a patient's tooth using a tooth alphabet.
+     * @param index the tooth alphabet of tooth to be retrieved.
+     * @return the tooth represented by provided tooth alphabet.
+     */
+    public Tooth getTooth(char index) {
+        int parsedIndex = (int) index - (int) 'a';
+        return primaryTeeth.get(parsedIndex);
+    }
+
+    /**
+     * For permanent tooth.
      * Returns a Tooth representing a patient's tooth using a tooth number.
      * @param index the tooth number of tooth to be retrieved.
      * @return the tooth represented by provided tooth number.
      */
     public Tooth getTooth(int index) {
-        return null;
+        int adjustedIndex = index - 1;
+        return permanentTeeth.get(adjustedIndex);
     }
+
 }

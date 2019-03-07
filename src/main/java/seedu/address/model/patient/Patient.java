94a1241b96ba355ff141458e261dc392b3b91093
Kyler Wong Shen Meng <wsm.kyler@gmail.com>
2019-03-07 13:24:21 +0800

    Added new attributes and essential methods to Patient class

--- src/main/java/seedu/address/model/patient/Patient.java
+++ src/main/java/seedu/address/model/patient/Patient.java
@@ -1,13 +1,20 @@
 package seedu.address.model.patient;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Set;
 
 import seedu.address.model.patient.exceptions.TeethLayoutException;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Nric;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.record.Record;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -18,26 +25,35 @@ public class Patient extends Person {
     private static final String NONE = "none";
     private static final String CHILD = "child";
     private static final String ADULT = "adult";
+    private Nric nric;
+    private Date dateOfBirth;
     private Teeth teeth = null;
     private boolean buildSpecified = false;
+    private ArrayList<Record> records = new ArrayList<>();
 
     /**
      * Every field must be present and not null.
      */
-    public Patient(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Patient(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Nric nric, Date dateOfBirth) {
         super(name, phone, email, address, tags);
+        requireAllNonNull(nric, dateOfBirth);
+        this.nric = nric;
+        this.dateOfBirth = dateOfBirth;
     }
 
     public Patient(Name name, Phone phone, Email email, Address address, Set<Tag> tags,
-                   Person personToCopy, int copyCount) {
+                   Person personToCopy, int copyCount, Nric nric, Date dateOfBirth) {
         super(name, phone, email, address, tags, personToCopy, copyCount);
+        requireAllNonNull(nric, dateOfBirth);
+        this.nric = nric;
+        this.dateOfBirth = dateOfBirth;
     }
 
     /**
      * Takes in the age of the patient and infers the teeth build he/she has.
-     * @param age the age of the patient.
      */
-    private void inferTeethBuild(int age) {
+    private void inferTeethBuild() {
+        int age = getPatientAge();
         if (age < 2) {
             specifyBuild(NONE);
         } else if (age < 13) {
@@ -93,8 +109,26 @@ public class Patient extends Person {
         }
     }
 
+    private int getPatientAge() {
+        Date today = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(today);
+        int currentYear = cal.get(Calendar.DAY_OF_YEAR);
+        cal.setTime(dateOfBirth);
+        int birthYear = cal.get(Calendar.DAY_OF_YEAR);
+        return currentYear - birthYear;
+    }
+
     public Teeth getTeeth() {
         return teeth;
+    }
+
+    public Nric getNric() {
+        return nric;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
     }
 
     public boolean isBuildSpecified() {

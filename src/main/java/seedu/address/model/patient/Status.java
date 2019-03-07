487df3f8b32bc700d6b52290aa22840306933ba2
Kyler Wong Shen Meng <wsm.kyler@gmail.com>
2019-03-07 20:35:39 +0800

    Simplified attributes of status class

--- src/main/java/seedu/address/model/patient/Status.java
+++ src/main/java/seedu/address/model/patient/Status.java
@@ -1,25 +1,17 @@
 package seedu.address.model.patient;
 
-import java.util.Date;
-
 /**
  * Represents the status of a teeth.
  * Attributes of class is immutable to uphold information integrity.
  */
 public class Status {
     private String description;
-    private Date dateCreated;
 
     public Status(String description) {
         this.description = description;
-        this.dateCreated = new Date();
     }
 
     public String getDescription() {
         return description;
-    }
-
-    public Date getDateCreated() {
-        return dateCreated;
     }
 }

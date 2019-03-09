fdd27b04faec547c905ce6b886db7c2a8d18e871
Ian Wu <iwle@u.nus.edu>
2019-03-10 01:06:05 +0800

    Update TypicalIndexes.java
    Update TypicalIndexes.java to include next vertical cell after a1, which
    is b1.

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -14,6 +14,7 @@ public class TypicalIndexes {
     public static final int MAP_SIZE_TEN = 10;
     public static final Coordinates COORDINATES_FIRST_CELL = new Coordinates("a1");
     public static final Coordinates COORDINATES_FIRST_CELL_NEXT_HORIZONTAL = new Coordinates("a2");
+    public static final Coordinates COORDINATES_FIRST_CELL_NEXT_VERTICAL = new Coordinates("b1");
 
     public static final Coordinates COORDINATES_LAST_CELL = new Coordinates("j10");
 }

500d477bf33b489189b72ccc7b97fa1af340548b
Ian Wu <iwle@u.nus.edu>
2019-03-10 00:49:19 +0800

    Update TypicalIndexes.java
    Update TypicalIndexes.java to include next horizontal cell after a1,
    which is a2.

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -13,5 +13,7 @@ public class TypicalIndexes {
 
     public static final int MAP_SIZE_TEN = 10;
     public static final Coordinates COORDINATES_FIRST_CELL = new Coordinates("a1");
+    public static final Coordinates COORDINATES_FIRST_CELL_NEXT_HORIZONTAL = new Coordinates("a2");
+
     public static final Coordinates COORDINATES_LAST_CELL = new Coordinates("j10");
 }

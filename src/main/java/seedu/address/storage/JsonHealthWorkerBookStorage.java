d7018197b79fd8edbbdab8da325e6c30b8abe3ff
jing1324 <jinjing123@gmail.com>
2019-03-10 21:55:21 +0800

    Implemented Storage to include RequestBook and HealthWorkerBook

--- src/main/java/seedu/address/storage/JsonHealthWorkerBookStorage.java
+++ src/main/java/seedu/address/storage/JsonHealthWorkerBookStorage.java
@@ -11,12 +11,12 @@ import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.FileUtil;
 import seedu.address.commons.util.JsonUtil;
-import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyHealthWorkerBook;
 
 /**
- * A class to access AddressBook data stored as a json file on the hard disk.
+ * A class to access HealthWorkerBook data stored as a json file on the hard disk.
  */
-public class JsonHealthWorkerBookStorage implements AddressBookStorage {
+public class JsonHealthWorkerBookStorage implements HealthWorkerBookStorage {
 
     private static final Logger logger = LogsCenter.getLogger(JsonHealthWorkerBookStorage.class);
 
@@ -26,32 +26,32 @@ public class JsonHealthWorkerBookStorage implements AddressBookStorage {
         this.filePath = filePath;
     }
 
-    public Path getAddressBookFilePath() {
+    public Path getHealthWorkerBookFilePath() {
         return filePath;
     }
 
     @Override
-    public Optional<ReadOnlyAddressBook> readAddressBook() throws DataConversionException {
-        return readAddressBook(filePath);
+    public Optional<ReadOnlyHealthWorkerBook> readHealthWorkerBook() throws DataConversionException {
+        return readHealthWorkerBook(filePath);
     }
 
     /**
-     * Similar to {@link #readAddressBook()}.
+     * Similar to {@link #readHealthWorkerBook()}.
      *
      * @param filePath location of the data. Cannot be null.
      * @throws DataConversionException if the file is not in the correct format.
      */
-    public Optional<ReadOnlyAddressBook> readAddressBook(Path filePath) throws DataConversionException {
+    public Optional<ReadOnlyHealthWorkerBook> readHealthWorkerBook(Path filePath) throws DataConversionException {
         requireNonNull(filePath);
 
-        Optional<JsonSerializableAddressBook> jsonAddressBook = JsonUtil.readJsonFile(
-                filePath, JsonSerializableAddressBook.class);
-        if (!jsonAddressBook.isPresent()) {
+        Optional<JsonSerializableHealthWorkerBook> jsonHealthWorkerBook = JsonUtil.readJsonFile(
+                filePath, JsonSerializableHealthWorkerBook.class);
+        if (!jsonHealthWorkerBook.isPresent()) {
             return Optional.empty();
         }
 
         try {
-            return Optional.of(jsonAddressBook.get().toModelType());
+            return Optional.of(jsonHealthWorkerBook.get().toModelType());
         } catch (IllegalValueException ive) {
             logger.info("Illegal values found in " + filePath + ": " + ive.getMessage());
             throw new DataConversionException(ive);
@@ -59,21 +59,21 @@ public class JsonHealthWorkerBookStorage implements AddressBookStorage {
     }
 
     @Override
-    public void saveAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
-        saveAddressBook(addressBook, filePath);
+    public void saveHealthWorkerBook(ReadOnlyHealthWorkerBook healthWorkerBook) throws IOException {
+        saveHealthWorkerBook(healthWorkerBook, filePath);
     }
 
     /**
-     * Similar to {@link #saveAddressBook(ReadOnlyAddressBook)}.
+     * Similar to {@link #saveHealthWorkerBook(ReadOnlyHealthWorkerBook)}.
      *
      * @param filePath location of the data. Cannot be null.
      */
-    public void saveAddressBook(ReadOnlyAddressBook addressBook, Path filePath) throws IOException {
-        requireNonNull(addressBook);
+    public void saveHealthWorkerBook(ReadOnlyHealthWorkerBook healthWorkerBook, Path filePath) throws IOException {
+        requireNonNull(healthWorkerBook);
         requireNonNull(filePath);
 
         FileUtil.createIfMissing(filePath);
-        JsonUtil.saveJsonFile(new JsonSerializableAddressBook(addressBook), filePath);
+        JsonUtil.saveJsonFile(new JsonSerializableHealthWorkerBook(healthWorkerBook), filePath);
     }
 
 }
--- src/main/java/seedu/address/storage/JsonSerializableHealthWorkerBook.java
+++ src/main/java/seedu/address/storage/JsonSerializableHealthWorkerBook.java
@@ -1,7 +1,6 @@
 package seedu.address.storage;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,71 +9,57 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.AddressBook;
-import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.person.Person;
+import seedu.address.model.HealthWorkerBook;
+import seedu.address.model.ReadOnlyHealthWorkerBook;
 
-//import seedu.address.model.person.HealthWorker;
+import seedu.address.model.person.healthworker.HealthWorker;
 
 /**
- * An Immutable AddressBook that is serializable to JSON format.
+ * An Immutable HealthWorkerBook that is serializable to JSON format.
  */
-@JsonRootName(value = "addressbook")
+@JsonRootName(value = "healthWorkerBook")
 class JsonSerializableHealthWorkerBook {
 
-    public static final String MESSAGE_DUPLICATE_PERSON = "Persons list contains duplicate person(s).";
+    public static final String MESSAGE_DUPLICATE_HEALTHWORKER = "HealthWorkers list contains duplicate healthWorker(s).";
 
-    private static HashMap<String, Person> personHashMap = new HashMap<>();
-    private final List<JsonAdaptedPerson> persons = new ArrayList<>();
-    //private final List<JsonAdaptedHealthWorker> healthWorkers = new ArrayList<>();
+
+    private final List<JsonAdaptedHealthWorker> healthWorkers = new ArrayList<>();
 
     /**
-     * Constructs a {@code JsonSerializableAddressBook} with the given persons.
+     * Constructs a {@code JsonSerializableHealthWorkerBook} with the given healthWorkers.
      */
     @JsonCreator
-    public JsonSerializableHealthWorkerBook(@JsonProperty("persons") List<JsonAdaptedPerson> persons) {
-        this.persons.addAll(persons);
+    public JsonSerializableHealthWorkerBook(@JsonProperty("healthWorkers") List<JsonAdaptedHealthWorker> healthWorkers) {
+        this.healthWorkers.addAll(healthWorkers);
     }
 
     /**
-     * Converts a given {@code ReadOnlyAddressBook} into this class for Jackson use.
+     * Converts a given {@code ReadOnlyHealthWorkerBook} into this class for Jackson use.
      *
-     * @param source future changes to this will not affect the created {@code JsonSerializableAddressBook}.
+     * @param source future changes to this will not affect the created {@code JsonSerializableHealthWorkerBook}.
      */
-    public JsonSerializableHealthWorkerBook(ReadOnlyAddressBook source) {
-        persons.addAll(source.getPersonList().stream().map(JsonAdaptedPerson::new).collect(Collectors.toList()));
-        //healthWorkers.addAll(source.getHealthWorkerList().stream()
-        //.map(JsonAdaptedHealthWorker::new).collect(Collectors.toList()));
+    public JsonSerializableHealthWorkerBook(ReadOnlyHealthWorkerBook source) {
+        healthWorkers.addAll(source.getHealthWorkerList().stream().map(JsonAdaptedHealthWorker::new).collect(Collectors.toList()));
+
     }
 
     /**
-     * Converts this address book into the model's {@code AddressBook} object.
+     * Converts this address book into the model's {@code HealthWorkerBook} object.
      *
      * @throws IllegalValueException if there were any data constraints violated.
      */
-    public AddressBook toModelType() throws IllegalValueException {
-        AddressBook addressBook = new AddressBook();
-        for (JsonAdaptedPerson jsonAdaptedPerson : persons) {
-            Person person = jsonAdaptedPerson.toModelType();
-            if (addressBook.hasPerson(person)) {
-                throw new IllegalValueException(MESSAGE_DUPLICATE_PERSON);
-            }
-            addressBook.addPerson(person);
-            personHashMap.put(person.getNric().toString(), person);
-        }
-        /*for (JsonAdaptedHealthWorker jsonAdaptedHealthWorker: healthWorkers) {
+    public HealthWorkerBook toModelType() throws IllegalValueException {
+        HealthWorkerBook healthWorkerBook = new HealthWorkerBook();
+        for (JsonAdaptedHealthWorker jsonAdaptedHealthWorker : healthWorkers) {
             HealthWorker healthWorker = jsonAdaptedHealthWorker.toModelType();
-            if (addressBook.hasHealthWorker(healthWorker)) {
-                throw new IllegalValueException(MESSAGE_DUPLICATE_PERSON);
+            if (healthWorkerBook.hasHealthWorker(healthWorker)) {
+                throw new IllegalValueException(MESSAGE_DUPLICATE_HEALTHWORKER);
             }
-        }*/
-        return addressBook;
+            healthWorkerBook.addHealthWorker(healthWorker);
+        }
+        return healthWorkerBook;
     }
 
-    public static Person obtainPersonFromHashmap(String nric) {
 
-        return personHashMap.get(nric);
-
-    }
 
 }

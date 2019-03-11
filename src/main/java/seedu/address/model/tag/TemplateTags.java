bbcf6860e7dbe9b57b0aef849802b3612ad585da
Kyler Wong Shen Meng <wsm.kyler@gmail.com>
2019-03-11 22:11:46 +0800

    Added new Tag types and template tag messages

---
+++ src/main/java/seedu/address/model/tag/TemplateTags.java
@@ -0,0 +1,11 @@
+package seedu.address.model.tag;
+
+/**
+ * Common Tag content for TeethTag and StatusTag.
+ */
+public class TemplateTags {
+    public static final String HEALTHY = "Healthy";
+    public static final String STATUS = "Status";
+    public static final String CHILD = "Primary";
+    public static final String ADULT = "Permanent";
+}

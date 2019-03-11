c01fc0e0297f84a99970a9d912e3f60fb6a5f2b7
kthSim <kth_sim@hotmail.com>
2019-03-11 10:51:26 +0800

    Skeleton code for Sort and Stats command

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,7 +1,42 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.Comparator;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.patient.Patient;
+
+
+
+
 /**
  * Sorts data in accordance to the given parameter
  */
-public class SortCommand {
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+        + " : Sorts the displayed list of patients OR a patient's records according to the desired parameter.\n"
+        + "Parameters: [record] PARAMETER \n"
+        + "Example 1: " + COMMAND_WORD + "name\n"
+        + "Example 2: " + COMMAND_WORD + "record procedure\n";
+
+    public static final String MESSAGE_SUCCESS = "List has been sorted!";
+
+    public final Comparator<Patient> attrCompare;
+
+    SortCommand(Comparator<Patient> chosenCompare) {
+        requireNonNull(chosenCompare);
+        this.attrCompare = chosenCompare;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+
+        return new CommandResult(String.format(MESSAGE_SUCCESS));
+    }
 }
--- src/main/java/seedu/address/logic/commands/StatsCommand.java
+++ src/main/java/seedu/address/logic/commands/StatsCommand.java
@@ -1,9 +1,18 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.patient.Patient;
 import seedu.address.model.person.Person;
+
+
 
 /**
  * Shows statistics for the given patient
@@ -18,14 +27,46 @@ public class StatsCommand extends Command {
         + "Example 1: " + COMMAND_WORD + "3\n"
         + "Example 2: " + COMMAND_WORD + "alice\n";
 
-    private final Person toStat;
+    public static final String MESSAGE_SUCCESS = "Statistic for patient %1$s printed!";
 
-    StatsCommand(Person person) {
+    private Patient toStat;
+    private Index index;
+    private boolean isIndex;
+
+    StatsCommand(Patient person) {
+        requireNonNull(person);
         toStat = person;
+        this.isIndex = false;
+    }
+
+    StatsCommand(Index index) {
+        requireNonNull(index);
+        this.index = index;
+        this.isIndex = true;
     }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-        return null;
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Patient patientToStat = (Patient) lastShownList.get(index.getZeroBased());
+
+
+
+
+
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toStat.getName()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+            || (other instanceof StatsCommand // instanceof handles nulls
+            && toStat.equals(((StatsCommand) other).toStat));
     }
 }

febbb9bafbb2f3f4c2aa1e7fb87309600841ff7b
s-tr <swchong25+github@gmail.com>
2019-03-11 18:07:16 +0800

    Add new kinds of AttackResults

---
+++ src/main/java/seedu/address/logic/battle/AttackDestroyedShip.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.battle;
+
+import seedu.address.model.battleship.Battleship;
+import seedu.address.model.cell.Coordinates;
+import seedu.address.model.player.Player;
+
+/**
+ * Represents the result of an attack that hit and destroyed a ship.
+ */
+public class AttackDestroyedShip extends AttackResult {
+    private Battleship destroyedShip;
+
+    public AttackDestroyedShip(Player attacker, Player target, Coordinates cell, Battleship destroyedShip) {
+        super(attacker, target, cell);
+        this.destroyedShip = destroyedShip;
+    }
+
+    @Override
+    public boolean isHit() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Attack by player %s on cell %s of player %s destroyed %s",
+            attacker, coords, target, destroyedShip.getName());
+    }
+}
---
+++ src/main/java/seedu/address/logic/battle/AttackHit.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.battle;
+
+import seedu.address.model.cell.Coordinates;
+import seedu.address.model.player.Player;
+
+/**
+ * Represents the result of an attack that hit, but did not destroy, a ship
+ */
+public class AttackHit extends AttackResult {
+    public AttackHit(Player attacker, Player target, Coordinates cell) {
+        super(attacker, target, cell);
+    }
+
+    @Override
+    public boolean isHit() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Attack by player %s on cell %s of player %s hit",
+            attacker, coords, target);
+    }
+}

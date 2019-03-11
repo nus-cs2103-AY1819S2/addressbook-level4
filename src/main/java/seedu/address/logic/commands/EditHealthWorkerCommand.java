39417e9cae69b4aa7e16755b76ce2901314b676b
Lookuz <cweyyeh512@gmail.com>
2019-03-11 13:43:14 +0800

    Implemented execute method in EditHealthWorkerCommand.

--- src/main/java/seedu/address/logic/commands/EditHealthWorkerCommand.java
+++ src/main/java/seedu/address/logic/commands/EditHealthWorkerCommand.java
@@ -2,9 +2,11 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.CommandHistory;
@@ -46,8 +48,22 @@ public class EditHealthWorkerCommand extends EditCommand implements HealthWorker
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-        // TODO: create getFilteredHealthWorkerList method. Implement execute.
-        return null;
+        requireNonNull(model);
+        List<HealthWorker> lastShownList = model.getFilteredHealthWorkerList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        HealthWorker toEdit = lastShownList.get(index.getZeroBased());
+        HealthWorker edited = createEditedHealthWorker(toEdit, editHealthWorkerDescriptor);
+
+        if (!toEdit.isSameHealthWorker(edited) && model.hasHealthWorker(edited)) {
+            throw new CommandException(DUPLICATE_HEALTH_WORKER);
+        }
+
+        edit(model, toEdit, edited);
+        return new CommandResult(String.format(MESSAGE_EDIT_HEALTHWORKER_SUCCESS, edited));
     }
 
     /**

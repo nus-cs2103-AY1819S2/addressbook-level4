15cf2232de2ab677d8e0060342a31276dca1c4fb
Daniel <DanielDSSim@gmail.com>
2019-03-11 14:50:46 +0800

    Edit typicalpersons

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -71,11 +71,11 @@ public class TypicalPersons {
     public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY).withRace(VALID_RACE_AMY)
             .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
             .withTags(VALID_TAG_FRIEND).withSchool(VALID_SCHOOL_AMY).withPastJobs(VALID_PASTJOB_PROFESSSOR)
-            .withKnownProgLangs(VALID_KNOWNPROGLANG_PYTHON).withMajor(VALID_MAJOR_AMY).build();
+            .withMajor(VALID_MAJOR_AMY).build();
     public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withRace(VALID_RACE_BOB)
             .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
             .withMajor(VALID_MAJOR_BOB).withSchool(VALID_SCHOOL_BOB).withPastJobs(VALID_PASTJOB_PROFESSSOR)
-            .withKnownProgLangs(VALID_KNOWNPROGLANG_PYTHON).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
+            .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 

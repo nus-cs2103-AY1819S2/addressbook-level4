565d3e4811b4201fdcd4bcec1ca917ef16218a4e
Kyler Wong Shen Meng <wsm.kyler@gmail.com>
2019-03-11 21:10:36 +0800

    Fixed alone edits on NRIC or DOB

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -176,7 +176,7 @@ public class EditCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyNonNull(name, phone, email, address, tags);
+            return CollectionUtil.isAnyNonNull(name, nric, dateOfBirth, phone, email, address, tags);
         }
 
         public void setName(Name name) {

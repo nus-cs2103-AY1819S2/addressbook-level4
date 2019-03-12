098fd9e3e7b3d606655b76f14af58585783e97f4
Jason Tan <j.tankw3@gmail.com>
2019-03-13 00:57:32 +0800

    Improve and add JUnit tests for UpdateCommand

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -9,4 +9,5 @@ public class TypicalIndexes {
     public static final Index INDEX_FIRST_MEDICINE = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_MEDICINE = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_MEDICINE = Index.fromOneBased(3);
+    public static final Index INDEX_FOURTH_MEDICINE = Index.fromOneBased(4);
 }

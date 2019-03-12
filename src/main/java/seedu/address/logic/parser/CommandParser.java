1b67c170de42fc9e91972217d7f1931478cd3fd8
Liang Jun Au <au.liangjun@gmail.com>
2019-03-12 17:52:02 +0800

    Register addfolder to CommandParser

--- src/main/java/seedu/address/logic/parser/CommandParser.java
+++ src/main/java/seedu/address/logic/parser/CommandParser.java
@@ -7,6 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.AddFolderCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
@@ -55,6 +56,9 @@ public class CommandParser {
 
         case AddCommand.COMMAND_WORD:
             return new AddCommandParser().parse(arguments);
+
+        case AddFolderCommand.COMMAND_WORD:
+            return new AddFolderCommandParser().parse(arguments);
 
         case EditCommand.COMMAND_WORD:
             return new EditCommandParser().parse(arguments);

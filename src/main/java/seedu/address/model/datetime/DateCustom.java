8388f1032550a341a12a0ca84ce89f80617bb0d2
cchj1995 <choo_jnthn@hotmail.com>
2019-03-12 21:29:01 +0800

    Fixed regex for DateCustom to only allow dd-mm-yyyy format

--- src/main/java/seedu/address/model/datetime/DateCustom.java
+++ src/main/java/seedu/address/model/datetime/DateCustom.java
@@ -14,17 +14,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class DateCustom {
     public static final String MESSAGE_CONSTRAINTS = "Date should only contain exactly 8 numbers and a valid date should"
-                                                   + " be in the form of dd-mm-yyyy, dd/mm/yyyy or dd.mm.yyyy";
+                                                   + " be in the form of dd-mm-yyyy";
 
-    public static final String VALIDATION_REGEX = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)"
-                                                  +"(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^"
-                                                  +"(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|"
-                                                  +"[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])"
-                                                  +"00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])"
-                                                  +"|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
-    public static String dashformat = "dd-mm-yyyy";
-    public static String slashformat = "dd/mm/yyyy";
-    public static String dotformat = "dd.mm.yyyy";
+    public static final String VALIDATION_REGEX = "^(((0[1-9]|[1-2][0-9]|3[0,1])-(01|03|05|07|08|10|12))|"
+                                                + "((0[1-9]|[1-2][0-9]|30)-(04|06|09|11))|((0[1-9]|[1-2]["
+                                                + "0-9])-(02)))-(\\d{4})$";
+
+    public static String dateformat = "dd-mm-yyyy";
 
     public final String storedDate;
 

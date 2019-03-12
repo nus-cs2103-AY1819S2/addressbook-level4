5abddcbd1b6d82f06f049f02ba2a5b9f72153fa4
cchj1995 <choo_jnthn@hotmail.com>
2019-03-12 19:47:31 +0800

    Added constructor execute method and relevant attributes for TaskAddCommand

--- src/main/java/seedu/address/logic/commands/TaskAddCommand.java
+++ src/main/java/seedu/address/logic/commands/TaskAddCommand.java
@@ -1,4 +1,65 @@
 package seedu.address.logic.commands;
 
-public class TaskAddCommand {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TITLE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STARTDATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ENDDATE;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.task.Task;
+
+
+/**
+ * Adds a patient to the address book.
+ */
+public class TaskAddCommand extends Command {
+
+    public static final String COMMAND_WORD = "taskadd";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to the address book. "
+            + "Parameters: "
+            + PREFIX_TITLE + "NAME "
+            + PREFIX_STARTDATE + "NRIC "
+            + PREFIX_ENDDATE + "DOB \n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_TITLE + "Teeth removal surgery"
+            + PREFIX_STARTDATE + "20-12-2019"
+            + PREFIX_ENDDATE + "20-13-2019 ";
+
+
+    public static final String MESSAGE_SUCCESS = "New task added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "An exact same task is already found in the records";
+
+    private final Task toAdd;
+
+    /**
+     * Creates an AddCommand to add the specified {@code Task}
+     * @param task the task to be added.
+     */
+    public TaskAddCommand(Task task) {
+        requireNonNull(task);
+        toAdd = task;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasPerson(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
+
+        model.addPerson(toAdd);
+        model.commitAddressBook();
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd.getName()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AddCommand // instanceof handles nulls
+                && toAdd.equals(((AddCommand) other).toAdd));
+    }
 }

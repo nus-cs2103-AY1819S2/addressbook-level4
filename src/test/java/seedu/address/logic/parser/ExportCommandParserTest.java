243f2dd22985af7711f7a0986987d6cf194ed4db
Yi Chong Ong <yichong96@gmail.com>
2019-03-12 17:41:32 +0800

    add export command parser test for export command

--- src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
@@ -5,7 +5,9 @@ import seedu.address.logic.commands.ExportCommand;
 
 import java.util.Arrays;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 public class ExportCommandParserTest {
@@ -14,13 +16,30 @@ public class ExportCommandParserTest {
 
     @Test
     public void parse_allFields_present() {
-        // one folder
+        // whitespace preamble
         assertParseSuccess(parser, PREAMBLE_WHITESPACE + FOLDER_DESC_SAMPLE_1
                 + FILENAME_DESC_SAMPLE, new ExportCommand(Arrays.asList(VALID_FOLDER_NAME_1), VALID_FILENAME));
 
         // multiple folders
-        assertParseSuccess(parser, PREAMBLE_WHITESPACE + FOLDER_DESC_SAMPLE_1 + FOLDER_DESC_SAMPLE_2
+        assertParseSuccess(parser, FOLDER_DESC_SAMPLE_1 + FOLDER_DESC_SAMPLE_2
                 + FILENAME_DESC_SAMPLE, new ExportCommand(Arrays.asList(VALID_FOLDER_NAME_1,
                 VALID_FOLDER_NAME_2), VALID_FILENAME));
+    }
+
+    @Test
+    public void parse_compulsoryFieldMissing_failure() {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, ExportCommand.MESSAGE_USAGE);
+
+        // missing folder prefix
+        assertParseFailure(parser, VALID_FOLDER_NAME_1
+                + FILENAME_DESC_SAMPLE, String.format(expectedMessage));
+
+        // missing filename prefix
+        assertParseFailure(parser, FOLDER_DESC_SAMPLE_1
+                + VALID_FILENAME, String.format(expectedMessage));
+
+        // missing all prefix
+        assertParseFailure(parser, VALID_FOLDER_NAME_1
+                + VALID_FILENAME, String.format(expectedMessage));
     }
 }

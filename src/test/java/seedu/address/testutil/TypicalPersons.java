9c7d0045b2e24f24e318a87553d2096312e53881
Kyler Wong Shen Meng <wsm.kyler@gmail.com>
2019-03-13 17:29:12 +0800

    Typical persons now do not have tags allocated to them

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -12,8 +12,6 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,16 +27,16 @@ public class TypicalPersons {
 
     public static final Person ALICE = new PersonBuilder().withName("Alice Pauline").withNric("S1234567A")
             .withDob("11-12-1990").withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
-            .withPhone("94351253").withTags("friends").build();
+            .withPhone("94351253").build();
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier").withNric("S1234568A")
             .withDob("12-12-1990").withAddress("311, Clementi Ave 2, #02-25").withEmail("johnd@example.com")
-            .withPhone("98765432").withTags("owesMoney", "friends").build();
+            .withPhone("98765432").build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withNric("S1234569A")
             .withDob("11-12-1991").withPhone("95352563").withEmail("heinz@example.com").withAddress("wall street")
             .build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withNric("S2234569A")
             .withDob("21-12-1991").withPhone("87652533").withEmail("cornelia@example.com").withAddress("10th street")
-            .withTags("friends").build();
+            .build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withNric("S2334569A")
             .withDob("11-06-1991").withPhone("9482224").withEmail("werner@example.com").withAddress("michegan ave")
             .build();
@@ -60,11 +58,10 @@ public class TypicalPersons {
     // edited to specify patient attributes - for testing purposes
     public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY)
             .withNric(VALID_NRIC_AMY).withDob(VALID_DOB_AMY).withPhone(VALID_PHONE_AMY)
-            .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).build();
+            .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).build();
     public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB)
             .withNric(VALID_NRIC_BOB).withDob(VALID_DOB_BOB).withPhone(VALID_PHONE_BOB)
-            .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
-            .build();
+            .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 

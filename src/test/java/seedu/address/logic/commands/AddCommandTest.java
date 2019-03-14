bc03b9575febbee24099b22efa00cee90a54ed8b
cchj1995 <choo_jnthn@hotmail.com>
2019-03-14 16:23:17 +0800

    Modified AddCommandTest to include new command in ModelStub

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -157,6 +157,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void deleteTask(Task task) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public void deletePerson(Person target) {
             throw new AssertionError("This method should not be called.");
         }

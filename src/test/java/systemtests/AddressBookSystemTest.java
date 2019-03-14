f5c7729dd742e678dead42cd4cf5dbb1dba8afa8
Tejas2805 <tejasbhuwania2805@gmail.com>
2019-03-14 22:29:38 +0800

    Error correction

--- src/test/java/systemtests/AddressBookSystemTest.java
+++ src/test/java/systemtests/AddressBookSystemTest.java
@@ -48,6 +48,7 @@ public abstract class AddressBookSystemTest {
     private static final List<String> COMMAND_BOX_DEFAULT_STYLE = Arrays.asList("text-input", "text-field");
     private static final List<String> COMMAND_BOX_ERROR_STYLE =
         Arrays.asList("text-input", "text-field", CommandBox.ERROR_STYLE_CLASS);
+
     @ClassRule
     public static ClockRule clockRule = new ClockRule();
     private MainWindowHandle mainWindowHandle;

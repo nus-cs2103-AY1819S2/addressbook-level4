d41934b1655d5eaf548520801b1e3247e843a913
0blivious <lyw@415309@gmail.com>
2019-03-16 23:58:03 +0800

    bug fix

--- src/test/java/seedu/address/logic/parser/BookShelfParserTest.java
+++ src/test/java/seedu/address/logic/parser/BookShelfParserTest.java
@@ -71,8 +71,7 @@ public class BookShelfParserTest {
         assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
     }
 
-
-    
+    @Test
     public void parseCommand_delete() throws Exception {
         DeleteCommand command = (DeleteCommand) parser.parseCommand(
                 DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());

1a81a56be6ed32b3aba47cbbbf9650a498ae318f
Kyler Wong Shen Meng <wsm.kyler@gmail.com>
2019-03-16 20:14:22 +0800

    New interface to specify requirements when create Date classes

---
+++ src/main/java/seedu/address/model/datetime/DateBuilder.java
@@ -0,0 +1,21 @@
+package seedu.address.model.datetime;
+
+/**
+ * An interface to create proper Date classes for our program usage.
+ */
+public interface DateBuilder {
+    String VALIDATION_REGEX = "^(((0[1-9]|[1-2][0-9]|3[0,1])-(01|03|05|07|08|10|12))|"
+            + "((0[1-9]|[1-2][0-9]|30)-(04|06|09|11))|((0[1-9]|[1-2]["
+            + "0-9])-(02)))-(\\d{4})$";
+
+    String DATE_FORMAT = "dd-MM-yyyy";
+
+    String[] MONTHS = {"", "January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"};
+
+    boolean equals(Object o);
+
+    String toString();
+
+    int hashCode();
+}

69ba7cfba3f1a73e5257a3f02e97af8566a73c46
kthSim <kth_sim@hotmail.com>
2019-03-16 17:11:27 +0800

    Fixed FindCommandTests

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -6,7 +6,6 @@ import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.util.predicate.ContainsKeywordsPredicate;
-import seedu.address.model.util.predicate.NameContainsKeywordsPredicate;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
@@ -21,21 +20,17 @@ public class FindCommand extends Command {
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
-    private NameContainsKeywordsPredicate predicate;
-    private ContainsKeywordsPredicate pred;
+    private ContainsKeywordsPredicate predicate;
 
     public FindCommand(ContainsKeywordsPredicate predicate) {
-        this.pred = predicate;
-    }
-
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
     }
+
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
-        model.updateFilteredPersonList(this.pred);
+        model.updateFilteredPersonList(this.predicate);
         return new CommandResult(
                 String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
     }

42d34637aec90421457f1a69369761a3efe8b83b
dlqs <donaldleeqs@gmail.com>
2019-03-16 16:03:34 +0800

    add sort command test

---
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -0,0 +1,37 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.answerFirstCard;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showCardAtIndex;
+import static seedu.address.testutil.TypicalCards.getTypicalCardFolders;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CARD;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
+ */
+public class SortCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Before
+    public void setUp() {
+        model = new ModelManager(getTypicalCardFolders(), new UserPrefs());
+        expectedModel = new ModelManager(model.getCardFolders(), new UserPrefs());
+        expectedModel.sortFilteredCard(Model.COMPARATOR_ASC_SCORE_CARDS);
+    }
+
+    @Test
+    public void execute_sort() {
+        assertCommandSuccess(new SortCommand(), model, commandHistory, SortCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}

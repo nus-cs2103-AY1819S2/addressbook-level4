7e59120bee63cd5f6f141205feca9850ac96134d
beesaycheese <beesaycheese@users.noreply.github.com>
2019-03-16 14:50:27 +0800

    Work on Ui for the BillMode. (Fixed the Null Pointer Exception but still having problems printing out the receipt)

--- src/main/java/seedu/address/ui/BillBrowserPanel.java
+++ src/main/java/seedu/address/ui/BillBrowserPanel.java
@@ -2,18 +2,21 @@ package seedu.address.ui;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.function.Consumer;
 import java.util.logging.Logger;
 
 
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.statistics.Bill;
 
 /**
- * The Browser Panel for the menu.
+ * The Browser Panel for the bill.
  */
 
 public class BillBrowserPanel extends UiPart<Region> {
@@ -22,7 +25,8 @@ public class BillBrowserPanel extends UiPart<Region> {
 
     private final Logger logger = LogsCenter.getLogger(getClass());
 
-    private Bill bill;
+    @FXML
+    private ListView<Bill> receiptView;
 
     @FXML
     private Label tableNumber;
@@ -30,50 +34,55 @@ public class BillBrowserPanel extends UiPart<Region> {
     @FXML
     private TextArea receipt;
 
-    public BillBrowserPanel(Bill bill) {
+    public BillBrowserPanel(Bill bill, ObservableValue<Bill> selectedBill, Consumer<Bill> onSelectedBillChange) {
         super(FXML);
-        requireNonNull(bill);
-        this.bill = bill;
-        tableNumber.setText("TABLE NUMBER " + bill.getTableNumber().tableNumber);
-        //TODO: Find out why there will be nullpointer exception
-        receipt.setText(bill.getReceipt());
-    }
 
-//    // TODO: constructors for different modes
-//    public BillBrowserPanel(ObservableValue<Bill> selectedBill) {
-//        super(FXML);
-//        // Load person page when selected person changes.
-//        selectedBill.addListener((observable, oldValue, newValue) -> {
-//            if (newValue == null) {
-//                loadDefaultText();
+        requireNonNull(bill);
+        tableNumber.setText("TABLE NUMBER " + bill.getTableNumber().tableNumber);
+        //TODO: Find out why the text aren't printing
+        receipt = new TextArea(" ");
+        receipt.appendText(bill.getReceipt());
+
+//        receiptView.setCellFactory(listView -> new BillBrowserPanel.BillCell());
+//        receiptView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+//            logger.fine("Selection in list panel changed to : '" + newValue + "'");
+//            onSelectedBillChange.accept(newValue);
+//        });
+//        selectedBill.addListener(((observable, oldValue, newValue) -> {
+//            logger.fine("Selected item changed to: " + newValue);
+//
+//            // Don't modify selection if we are already selecting the selected item,
+//            // otherwise we would have an infinite loop.
+//            if (Objects.equals(receiptView.getSelectionModel().getSelectedItem(), newValue)) {
 //                return;
 //            }
-//            loadBillReceipt(newValue);
-//        });
-//    }
 //
-//    public BillBrowserPanel() {
-//        super(FXML);
-//        loadDefaultText();
-//    }
-//
-//
+//            if (newValue == null) {
+//                receiptView.getSelectionModel().clearSelection();
+//            } else {
+//                int index = receiptView.getItems().indexOf(newValue);
+//                receiptView.scrollTo(index);
+//                receiptView.getSelectionModel().clearAndSelect(index);
+//            }
+//        }));
+    }
+
 //    /**
-//     * Loads the actual receipt.
+//     * Custom {@code ListCell} that displays the graphics of a {@code Item} using a {@code ItemCard}.
 //     */
-//    private void loadBillReceipt(Bill bill) {
-//        requireNonNull(bill);
-//        text = new Text(bill.getReceipt());
-//        receipt.getChildren().addAll(text);
-//        tableNumber.setText(bill.getTableNumber().tableNumber);
-//    }
+//    class BillCell extends ListCell<Bill> {
+//        @Override
+//        protected void updateItem(Bill item, boolean empty) {
+//            super.updateItem(item, empty);
 //
-//    /**
-//     * Loads a default receipt.
-//     */
-//    private void loadDefaultText() {
-//        text = new Text("No Bill Found");
-//        receipt.getChildren().addAll(text);
+//            if (empty || item == null) {
+//                setGraphic(null);
+//                setText(null);
+//            } else {
+//                setGraphic(new BillCard(bill));
+//            }
+//        }
 //    }
+
 
 }

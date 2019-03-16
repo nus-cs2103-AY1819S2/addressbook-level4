a77d9bc6b68a7a6f84e0192ff91a6bc1fc17e4c9
beesaycheese <beesaycheese@users.noreply.github.com>
2019-03-17 00:43:00 +0800

    Include Statistics related methods in ModelHelper

--- src/test/java/systemtests/ModelHelper.java
+++ src/test/java/systemtests/ModelHelper.java
@@ -8,6 +8,7 @@ import java.util.function.Predicate;
 import seedu.address.model.Model;
 import seedu.address.model.menu.MenuItem;
 import seedu.address.model.order.OrderItem;
+import seedu.address.model.statistics.DailyRevenue;
 import seedu.address.model.table.Table;
 
 /**
@@ -17,6 +18,7 @@ public class ModelHelper {
     private static final Predicate<OrderItem> PREDICATE_MATCHING_NO_ORDER_ITEMS = unused -> false;
     private static final Predicate<MenuItem> PREDICATE_MATCHING_NO_MENU_ITEMS = unused -> false;
     private static final Predicate<Table> PREDICATE_MATCHING_NO_TABLES = unused -> false;
+    private static final Predicate<DailyRevenue> PREDICATE_MATCHING_NO_DAILY_REVENUES = unused -> false;
 
     /**
      * Updates {@code model}'s order item filtered list to display only {@code toDisplay}.
@@ -40,7 +42,7 @@ public class ModelHelper {
     private static Predicate<OrderItem> getOrderItemPredicateMatching(OrderItem other) {
         return item -> item.equals(other);
     }
-    
+
     /**
      * Updates {@code model}'s menu item filtered list to display only {@code toDisplay}.
      */
@@ -72,18 +74,42 @@ public class ModelHelper {
                 toDisplay.stream().map(ModelHelper::getTablePredicateMatching).reduce(Predicate::or);
         model.updateFilteredTableList(predicate.orElse(PREDICATE_MATCHING_NO_TABLES));
     }
-    
+
     /**
      * @see ModelHelper#setTableFilteredList(Model, List)
      */
     public static void setTableFilteredList(Model model, Table... toDisplay) {
         setTableFilteredList(model, Arrays.asList(toDisplay));
     }
-    
+
     /**
      * Returns a predicate that evaluates to true if this {@code Table} equals to {@code other}.
      */
     private static Predicate<Table> getTablePredicateMatching(Table other) {
         return item -> item.equals(other);
     }
+
+    /**
+     * Updates {@code model}'s daily revenue filtered list to display only {@code toDisplay}.
+     */
+    public static void setDailyRevenueFilteredList(Model model, List<DailyRevenue> toDisplay) {
+        Optional<Predicate<DailyRevenue>> predicate =
+                toDisplay.stream().map(ModelHelper::getDailyRevenuePredicateMatching).reduce(Predicate::or);
+        model.updateFilteredDailyRevenueList(predicate.orElse(PREDICATE_MATCHING_NO_DAILY_REVENUES));
+    }
+
+    /**
+     * @see ModelHelper#setDailyRevenueFilteredList(Model, List)
+     */
+    public static void setDailyRevenueFilteredList(Model model, DailyRevenue... toDisplay) {
+        setDailyRevenueFilteredList(model, Arrays.asList(toDisplay));
+    }
+
+    /**
+     * Returns a predicate that evaluates to true if this {@code DailyRevenue} equals to {@code other}.
+     */
+    private static Predicate<DailyRevenue> getDailyRevenuePredicateMatching(DailyRevenue other) {
+        return item -> item.equals(other);
+    }
+
 }

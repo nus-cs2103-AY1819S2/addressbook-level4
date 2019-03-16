c8c0d52a36d4ba1d5ea83074327504c4cf55049d
0blivious <lyw@415309@gmail.com>
2019-03-16 23:55:18 +0800

    bug fix

--- src/test/java/seedu/address/logic/parser/BookShelfParserTest.java
+++ src/test/java/seedu/address/logic/parser/BookShelfParserTest.java
@@ -71,13 +71,8 @@ public class BookShelfParserTest {
         assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
     }
 
-    @Test
-    public void parseCommand_clear_alias() throws Exception {
-        assertTrue(parser.parseCommand(ClearCommand.COMMAND_ALIAS) instanceof ClearCommand);
-        assertTrue(parser.parseCommand(ClearCommand.COMMAND_ALIAS + " 3") instanceof ClearCommand);
-    }
 
-    @Test
+    
     public void parseCommand_delete() throws Exception {
         DeleteCommand command = (DeleteCommand) parser.parseCommand(
                 DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());

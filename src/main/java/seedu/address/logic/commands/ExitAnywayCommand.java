54d32df8d7de0e086fccaafc8508e99a39257359
wSemis <debang@u.nus.edu>
2019-03-17 17:10:23 +0800

    Add ExitAnyway class

---
+++ src/main/java/seedu/address/logic/commands/ExitAnywayCommand.java
@@ -0,0 +1,16 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+public class ExitAnywayCommand extends Command {
+    public static final String COMMAND_WORD = "exit!";
+
+    public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting Address Book as requested ...";
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        return new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true);
+    }
+
+}

0313679d8a2c7526540494c43d020423efb8e643
bos10 <bostoncheong@gmail.com>
2019-03-18 01:13:16 +0800

    setup StatsPane to display on ui

---
+++ src/main/java/seedu/address/ui/StatsPane.java
@@ -0,0 +1,12 @@
+package seedu.address.ui;
+
+/**
+ * StatsPane is a Ui Pane under the Map that displays and update scores
+ */
+public class StatsPane extends UiPart<Region> {
+
+    public StatsPane(){
+    //initialize with relevant stats
+    }
+
+}

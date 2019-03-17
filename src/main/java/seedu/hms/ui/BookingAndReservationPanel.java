c53b3e6af0231943dd218445b9098b1088953e61
ZhangCX10032 <549191059@qq.com>
2019-03-18 01:37:21 +0800

    Removed unused variables

--- src/main/java/seedu/hms/ui/BookingAndReservationPanel.java
+++ src/main/java/seedu/hms/ui/BookingAndReservationPanel.java
@@ -1,19 +1,14 @@
 package seedu.hms.ui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
-import seedu.hms.model.booking.Booking;
 
 /**
  * Panel containing the list of bookings.
  */
 public class BookingAndReservationPanel extends UiPart<Region> {
     private static final String FXML = "BookingAndReservationPanel.fxml";
-
-    @FXML
-    private ListView<Booking> bookingListView;
 
     @FXML
     private StackPane bookingListPanelPlaceholder;

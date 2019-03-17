57b30ef6f14b2366f77b9e6c1a99346b44daf46f
ZhangCX10032 <549191059@qq.com>
2019-03-18 01:34:04 +0800

    Removed unused variables

--- src/main/java/seedu/hms/ui/BookingAndReservationPanel.java
+++ src/main/java/seedu/hms/ui/BookingAndReservationPanel.java
@@ -1,12 +1,9 @@
 package seedu.hms.ui;
 
-import java.util.logging.Logger;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
-import seedu.hms.commons.core.LogsCenter;
 import seedu.hms.model.booking.Booking;
 
 /**
@@ -14,9 +11,6 @@ import seedu.hms.model.booking.Booking;
  */
 public class BookingAndReservationPanel extends UiPart<Region> {
     private static final String FXML = "BookingAndReservationPanel.fxml";
-    private final Logger logger = LogsCenter.getLogger(BookingAndReservationPanel.class);
-    private BookingListPanel bookingListPanel;
-    private ReservationListPanel reservationListPanel;
 
     @FXML
     private ListView<Booking> bookingListView;
@@ -29,9 +23,6 @@ public class BookingAndReservationPanel extends UiPart<Region> {
 
     public BookingAndReservationPanel(BookingListPanel bookingListPanel, ReservationListPanel reservationListPanel) {
         super(FXML);
-        this.bookingListPanel = bookingListPanel;
-        this.reservationListPanel = reservationListPanel;
-
         bookingListPanelPlaceholder.getChildren().add(bookingListPanel.getRoot());
         reservationListPanelPlaceholder.getChildren().add(reservationListPanel.getRoot());
     }

e85f42bce77d4586e71a0c22b90d595ab3c51fcf
Tejas2805 <tejasbhuwania2805@gmail.com>
2019-03-19 04:11:16 +0800

    Correct stuff

--- src/test/java/guitests/guihandles/CustomerCardHandle.java
+++ src/test/java/guitests/guihandles/CustomerCardHandle.java
@@ -16,7 +16,7 @@ import seedu.hms.model.customer.Customer;
 public class CustomerCardHandle extends NodeHandle<Node> {
     private static final String ID_FIELD_ID = "#id";
     private static final String NAME_FIELD_ID = "#name";
-    private static final String hms_FIELD_ID = "#hms";
+    private static final String ADDRESS_FIELD_ID = "#hms";
     private static final String IDENTIFICATION_NO_FIELD_ID = "#identificationNo";
     private static final String DATE_OF_BIRTH_FIELD_ID = "#dob";
     private static final String PHONE_FIELD_ID = "#phone";
@@ -25,12 +25,10 @@ public class CustomerCardHandle extends NodeHandle<Node> {
 
     private final Label idLabel;
     private final Label nameLabel;
-<<<<<<< HEAD
+
     private final Label addressLabel;
     private final Label dobLabel;
-=======
-    private final Label hmsLabel;
->>>>>>> f32e851bb9479d863dbfa54cb18c56bf0c85fbd6
+
     private final Label identificationNoLabel;
     private final Label phoneLabel;
     private final Label emailLabel;
@@ -42,12 +40,10 @@ public class CustomerCardHandle extends NodeHandle<Node> {
         idLabel = getChildNode(ID_FIELD_ID);
         nameLabel = getChildNode(NAME_FIELD_ID);
         identificationNoLabel = getChildNode(IDENTIFICATION_NO_FIELD_ID);
-<<<<<<< HEAD
+
         dobLabel = getChildNode(DATE_OF_BIRTH_FIELD_ID);
         addressLabel = getChildNode(ADDRESS_FIELD_ID);
-=======
-        hmsLabel = getChildNode(hms_FIELD_ID);
->>>>>>> f32e851bb9479d863dbfa54cb18c56bf0c85fbd6
+
         phoneLabel = getChildNode(PHONE_FIELD_ID);
         emailLabel = getChildNode(EMAIL_FIELD_ID);
 
@@ -68,7 +64,7 @@ public class CustomerCardHandle extends NodeHandle<Node> {
     }
 
     public String getAddress() {
-        return hmsLabel.getText();
+        return addressLabel.getText();
     }
 
     public String getDateOfBirth() {

5a30a78c884a3dde78567aa416628aa72f093b05
jiang <jiang@r-9-105-25-172.comp.nus.edu.sg>
2019-03-18 09:25:11 +0800

    fix space issue

--- src/main/java/seedu/address/ui/BookUiManager.java
+++ src/main/java/seedu/address/ui/BookUiManager.java
@@ -57,9 +57,9 @@ public class BookUiManager implements Ui {
     }
 
     /**
-	 * Shows an alert dialog on {@code owner} with the given parameters.
-	 * This method only returns after the user has closed the alert dialog.
-	 */
+     * Shows an alert dialog on {@code owner} with the given parameters.
+     * This method only returns after the user has closed the alert dialog.
+     */
     private static void showAlertDialogAndWait(Stage owner, AlertType type, String title, String headerText,
 	                                           String contentText) {
         final Alert alert = new Alert(type);

da81898146480b32357c9ab4d49b7857bf39edc6
bava98 <bavadharini.s11@gmail.com>
2019-03-18 10:48:33 +0800

    addToMenu command tests

--- src/test/java/seedu/address/logic/parser/AddToMenuCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddToMenuCommandParserTest.java
@@ -1,114 +1,80 @@
 package seedu.address.logic.parser;
 
 import org.junit.Test;
+import seedu.address.logic.commands.AddToMenuCommand;
+import seedu.address.model.menu.Code;
 import seedu.address.model.menu.MenuItem;
+import seedu.address.model.menu.Name;
+import seedu.address.model.menu.Price;
+import seedu.address.testutil.MenuItemBuilder;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
-import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_NON_EMPTY;
-import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
+import static seedu.address.logic.commands.CommandTestUtil.*;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalPersons.AMY;
-import static seedu.address.testutil.TypicalPersons.BOB;
-
+import static seedu.address.testutil.TypicalRestOrRant.FRENCH_FRIES;
 public class AddToMenuCommandParserTest {
     private AddToMenuCommandParser parser = new AddToMenuCommandParser();
-    
+
     @Test
     public void parse_allFieldsPresent_success() {
-        MenuItem expectedMenuItem = new PersonBuilder(BOB).withTags(VALID_TAG_FRIEND).build();
-        
+        MenuItem expectedMenuItem = new MenuItemBuilder(FRENCH_FRIES).build();
+
         // whitespace only preamble
-        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                                           + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
-        
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + NAME_DESC_FRIES + CODE_DESC_FRIES + PRICE_DESC_FRIES,
+                new AddToMenuCommand(expectedMenuItem));
+
         // multiple names - last name accepted
-        assertParseSuccess(parser, NAME_DESC_AMY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                                           + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
-        
-        // multiple phones - last phone accepted
-        assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_AMY + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                                           + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
-        
-        // multiple emails - last email accepted
-        assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_AMY + EMAIL_DESC_BOB
-                                           + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
-        
-        // multiple addresses - last address accepted
-        assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_AMY
-                                           + ADDRESS_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
-        
-        // multiple tags - all accepted
-        Person expectedPersonMultipleTags = new PersonBuilder(BOB).withTags(VALID_TAG_FRIEND, VALID_TAG_HUSBAND)
-                                                    .build();
-        assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
-                                           + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, new AddCommand(expectedPersonMultipleTags));
+        assertParseSuccess(parser, NAME_DESC_CHICKEN + NAME_DESC_FRIES + CODE_DESC_FRIES + PRICE_DESC_FRIES,
+                new AddToMenuCommand(expectedMenuItem));
+
+        // multiple codes - last code accepted
+        assertParseSuccess(parser, NAME_DESC_FRIES + CODE_DESC_CHICKEN + CODE_DESC_FRIES + PRICE_DESC_FRIES,
+                new AddToMenuCommand(expectedMenuItem));
+
+        // multiple prices - last price accepted
+        assertParseSuccess(parser, NAME_DESC_FRIES + CODE_DESC_FRIES + PRICE_DESC_CHICKEN + PRICE_DESC_FRIES
+                , new AddToMenuCommand(expectedMenuItem));
     }
-    
-    @Test
-    public void parse_optionalFieldsMissing_success() {
-        // zero tags
-        Person expectedPerson = new PersonBuilder(AMY).withTags().build();
-        assertParseSuccess(parser, NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY + ADDRESS_DESC_AMY,
-                new AddCommand(expectedPerson));
-    }
-    
+
     @Test
     public void parse_compulsoryFieldMissing_failure() {
-        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE);
-        
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddToMenuCommand.MESSAGE_USAGE);
+
         // missing name prefix
-        assertParseFailure(parser, VALID_NAME_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB,
-                expectedMessage);
-        
-        // missing phone prefix
-        assertParseFailure(parser, NAME_DESC_BOB + VALID_PHONE_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB,
-                expectedMessage);
-        
-        // missing email prefix
-        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + VALID_EMAIL_BOB + ADDRESS_DESC_BOB,
-                expectedMessage);
-        
-        // missing address prefix
-        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + VALID_ADDRESS_BOB,
-                expectedMessage);
-        
+        assertParseFailure(parser, VALID_NAME_FRIES + CODE_DESC_FRIES + PRICE_DESC_FRIES, expectedMessage);
+
+        // missing code prefix
+        assertParseFailure(parser, NAME_DESC_FRIES + VALID_CODE_FRIES + PRICE_DESC_FRIES, expectedMessage);
+
+        // missing price prefix
+        assertParseFailure(parser, NAME_DESC_FRIES + CODE_DESC_FRIES + VALID_PRICE_FRIES, expectedMessage);
+
         // all prefixes missing
-        assertParseFailure(parser, VALID_NAME_BOB + VALID_PHONE_BOB + VALID_EMAIL_BOB + VALID_ADDRESS_BOB,
-                expectedMessage);
+        assertParseFailure(parser, VALID_NAME_FRIES + VALID_CODE_FRIES + VALID_PRICE_FRIES, expectedMessage);
     }
-    
+
     @Test
     public void parse_invalidValue_failure() {
         // invalid name
-        assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
-                                           + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Name.MESSAGE_CONSTRAINTS);
-        
-        // invalid phone
-        assertParseFailure(parser, NAME_DESC_BOB + INVALID_PHONE_DESC + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
-                                           + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Phone.MESSAGE_CONSTRAINTS);
-        
-        // invalid email
-        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + INVALID_EMAIL_DESC + ADDRESS_DESC_BOB
-                                           + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Email.MESSAGE_CONSTRAINTS);
-        
-        // invalid address
-        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + INVALID_ADDRESS_DESC
-                                           + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Address.MESSAGE_CONSTRAINTS);
-        
-        // invalid tag
-        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
-                                           + INVALID_TAG_DESC + VALID_TAG_FRIEND, Tag.MESSAGE_CONSTRAINTS);
-        
-        // two invalid values, only first invalid value reported
-        assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB + INVALID_ADDRESS_DESC,
+        assertParseFailure(parser, INVALID_NAME_DESC + CODE_DESC_FRIES + PRICE_DESC_FRIES,
                 Name.MESSAGE_CONSTRAINTS);
-        
+
+        // invalid code
+        assertParseFailure(parser, NAME_DESC_FRIES + INVALID_CODE_DESC + PRICE_DESC_FRIES,
+                Code.MESSAGE_CONSTRAINTS);
+
+        // invalid price
+        assertParseFailure(parser, NAME_DESC_FRIES + CODE_DESC_FRIES + INVALID_PRICE_DESC,
+                Price.MESSAGE_CONSTRAINTS);
+
+        // two invalid values, only first invalid value reported
+        assertParseFailure(parser, INVALID_NAME_DESC + CODE_DESC_FRIES + INVALID_PRICE_DESC,
+                Name.MESSAGE_CONSTRAINTS);
+
         // non-empty preamble
-        assertParseFailure(parser, PREAMBLE_NON_EMPTY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                                           + ADDRESS_DESC_BOB + TAG_DESC_HUSBAND + TAG_DESC_FRIEND,
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, PREAMBLE_NON_EMPTY + NAME_DESC_FRIES + CODE_DESC_FRIES + PRICE_DESC_FRIES,
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddToMenuCommand.MESSAGE_USAGE));
     }
-    
+
 }

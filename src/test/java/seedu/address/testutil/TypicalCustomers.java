7f880a05c6072703c9756fc5d3449a8eda75cbae
Tejas2805 <tejasbhuwania2805@gmail.com>
2019-03-19 04:38:34 +0800

    Corrected

--- src/test/java/seedu/address/testutil/TypicalCustomers.java
+++ src/test/java/seedu/address/testutil/TypicalCustomers.java
@@ -1,26 +1,27 @@
 package seedu.address.testutil;
 
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_OF_BIRTH_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_OF_BIRTH_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ID_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ID_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_DATE_OF_BIRTH_AMY;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_DATE_OF_BIRTH_BOB;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_ID_AMY;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_ID_BOB;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
+import static seedu.hms.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import seedu.address.model.AddressBook;
-import seedu.address.model.customer.Customer;
+import seedu.hms.model.HotelManagementSystem;
+import seedu.hms.model.customer.Customer;
+import seedu.hms.testutil.CustomerBuilder;
 
 /**
  * A utility class containing a list of {@code Customer} objects to be used in tests.
@@ -83,8 +84,8 @@ public class TypicalCustomers {
     /**
      * Returns an {@code AddressBook} with all the typical customers.
      */
-    public static AddressBook getTypicalAddressBook() {
-        AddressBook ab = new AddressBook();
+    public static HotelManagementSystem getTypicalAddressBook() {
+        HotelManagementSystem ab = new HotelManagementSystem();
         for (Customer customer : getTypicalCustomers()) {
             ab.addCustomer(customer);
         }

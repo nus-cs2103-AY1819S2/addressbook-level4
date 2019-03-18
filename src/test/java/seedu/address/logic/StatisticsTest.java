dc2d0362f513f9dc64461739fa8a53f52919bf1b
bos10 <bostoncheong@gmail.com>
2019-03-18 10:14:07 +0800

    added the testing files and rectified checkstyle

--- src/test/java/seedu/address/logic/StatisticsTest.java
+++ src/test/java/seedu/address/logic/StatisticsTest.java
@@ -1,8 +1,6 @@
 package seedu.address.logic.statistics;
 
 import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 

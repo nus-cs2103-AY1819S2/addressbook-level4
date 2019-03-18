94a314ec0536ce5de7d156208c69ba6f351fb5a9
rbth7e5 <caijie96@gmail.com>
2019-03-18 17:01:23 +0800

    update parser to to parse category

--- src/main/java/seedu/address/logic/parser/CategoryCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CategoryCommandParser.java
@@ -3,11 +3,14 @@ package seedu.address.logic.parser;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CUISINE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_OCCASION;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.SetCategoryCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.restaurant.categories.Category;
 import seedu.address.model.restaurant.categories.Cuisine;
+import seedu.address.model.restaurant.categories.Occasion;
 
 /**
  * Parses input arguments and creates a new SetCategoryCommand object
@@ -21,9 +24,10 @@ public class CategoryCommandParser implements Parser<SetCategoryCommand> {
      */
     public SetCategoryCommand parse(String userInput) throws ParseException {
         requireNonNull(userInput);
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(userInput, PREFIX_CUISINE);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(userInput, PREFIX_CUISINE, PREFIX_OCCASION);
         Index index;
         Cuisine cuisine;
+        Occasion occasion;
 
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
@@ -35,8 +39,18 @@ public class CategoryCommandParser implements Parser<SetCategoryCommand> {
         if (argMultimap.getValue(PREFIX_CUISINE).isPresent()) {
             cuisine = ParserUtil.parseCuisine(argMultimap.getValue(PREFIX_CUISINE).get());
         } else {
+            cuisine = null;
+        }
+
+        if(argMultimap.getValue(PREFIX_OCCASION).isPresent()) {
+            occasion = ParserUtil.parseOccasion(argMultimap.getValue(PREFIX_OCCASION).get());
+        } else {
+            occasion = null;
+        }
+
+        if (cuisine == null && occasion == null) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetCategoryCommand.MESSAGE_USAGE));
         }
-        return new SetCategoryCommand(index, cuisine);
+        return new SetCategoryCommand(index, new Category(cuisine, occasion));
     }
 }

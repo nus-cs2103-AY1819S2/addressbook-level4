b3eb3c12a2dfee08486567c4f0d9c93107837695
Ian Wu <iwle@u.nus.edu>
2019-03-18 14:37:23 +0800

    Update ListCommand
    Update ListCommand to show list of deployed battleships.

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,10 +1,11 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+//import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
+import seedu.address.model.player.Fleet;
 
 /**
  * Lists all persons in the address book to the user.
@@ -19,7 +20,21 @@ public class ListCommand extends Command {
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        // model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        Fleet fleet = model.getFleet();
+
+        if (fleet.getDeployedFleet().size() <= 0) {
+            return new CommandResult("No ships put down.");
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < fleet.getDeployedFleet().size(); i++) {
+            builder.append(fleet.getDeployedFleet().get(i))
+                    .append("\n");
+        }
+
+        return new CommandResult(builder.toString());
     }
 }

a749aba9a4ba5f6a05952444559d8302440779c1
Yi Chong Ong <yichong96@gmail.com>
2019-03-18 13:50:21 +0800

    change return type of import command

--- src/main/java/seedu/address/logic/parser/ImportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ImportCommandParser.java
@@ -4,7 +4,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 
 import seedu.address.logic.commands.ImportCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-
+import seedu.address.storage.csvmanager.CsvFile;
 
 
 /**
@@ -16,10 +16,7 @@ public class ImportCommandParser implements Parser<ImportCommand> {
     @Override
     public ImportCommand parse(String userInput) throws ParseException {
         String filename = userInput.trim();
-        if (filename.isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportCommand.MESSAGE_USAGE));
-        }
-        return new ImportCommand(userInput);
+        CsvFile csvFile = ParserUtil.parseFileName(filename);
+        return new ImportCommand(csvFile);
     }
 }

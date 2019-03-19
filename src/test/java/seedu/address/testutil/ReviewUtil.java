3d7703237c1f5e1138d768453e64312117c9e55d
skpai27 <skpai27@yahoo.com.sg>
2019-03-19 15:48:56 +0800

    added ReviewUtil for test cases

---
+++ src/test/java/seedu/address/testutil/ReviewUtil.java
@@ -0,0 +1,35 @@
+package seedu.address.testutil;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REVIEWENTRY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REVIEWRATING;
+
+import seedu.address.logic.commands.EditReviewCommand.EditReviewDescriptor;
+import seedu.address.model.review.Review;
+
+/**
+ * A utility class for Reviews.
+ */
+public class ReviewUtil {
+
+    /**
+     * Returns the part of command string for the given {@code review}'s details.
+     */
+    public static String getReviewDetails(Review review) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PREFIX_REVIEWENTRY + review.getEntry().toString() + " ");
+        sb.append(PREFIX_REVIEWRATING + review.getRating().toString() + " ");
+        return sb.toString();
+    }
+
+    /**
+     * Returns the part of command string for the given {@code EditReviewDescriptor}'s details.
+     */
+    public static String getEditReviewDescriptorDetails(EditReviewDescriptor descriptor) {
+        StringBuilder sb = new StringBuilder();
+        descriptor.getEntry().ifPresent(entry -> sb.append(PREFIX_REVIEWENTRY).append(entry.toString()).append(" "));
+        descriptor.getRating().ifPresent(rating -> sb.append(PREFIX_REVIEWRATING)
+                .append(rating.toString()).append(" "));
+
+        return sb.toString();
+    }
+}

6718a25df98b0991393cd39f2ca9ddd73b533245
Liuyy99 <e0253712@u.nus.edu>
2019-03-19 13:03:24 +0800

    CheckStyle2

--- src/test/java/seedu/address/model/medicalhistory/MedicalHistoryTest.java
+++ src/test/java/seedu/address/model/medicalhistory/MedicalHistoryTest.java
@@ -1,12 +1,10 @@
 package seedu.address.model.medicalhistory;
 
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
-;
-import seedu.address.testutil.Assert;
-import seedu.address.model.person.Patient;
+
 import seedu.address.model.person.Name;
 
 
@@ -14,28 +12,28 @@ public class MedicalHistoryTest {
 
     @Test
     public void equals() {
-        MedicalHistory MH1 = new MedicalHistory(null, new Name("a"), new WriteUp("fever"));
-        MedicalHistory MH2 = new MedicalHistory(null, new Name("a"), new WriteUp("sneeze"));
-        MedicalHistory MH3 = new MedicalHistory(null, new Name("b"), new WriteUp("fever"));
-        MedicalHistory MH1Copy = new MedicalHistory(null, new Name("a"), new WriteUp("fever"));
+        MedicalHistory mh1 = new MedicalHistory(null, new Name("a"), new WriteUp("fever"));
+        MedicalHistory mh2 = new MedicalHistory(null, new Name("a"), new WriteUp("sneeze"));
+        MedicalHistory mh3 = new MedicalHistory(null, new Name("b"), new WriteUp("fever"));
+        MedicalHistory mh1Copy = new MedicalHistory(null, new Name("a"), new WriteUp("fever"));
 
         // same object -> returns true
-        assertTrue(MH1.equals(MH1));
+        assertTrue(mh1.equals(mh1));
 
         // same values -> returns true
-        assertTrue(MH1.equals(MH1Copy));
+        assertTrue(mh1.equals(mh1Copy));
 
         // null -> returns false
-        assertFalse(MH1.equals(null));
+        assertFalse(mh1.equals(null));
 
         // different type -> returns false
-        assertFalse(MH1.equals(5));
+        assertFalse(mh1.equals(5));
 
         // different writeup -> returns false
-        assertFalse(MH1.equals(MH2));
+        assertFalse(mh1.equals(mh2));
 
         // different name -> returns false
-        assertFalse(MH1.equals(MH3));
+        assertFalse(mh1.equals(mh3));
     }
 
 }

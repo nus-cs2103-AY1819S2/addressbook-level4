1275a9dc3609ef18ba95c952b653392c412db40c
monikernemo <leeyiquan95@gmail.com>
2019-03-19 22:10:34 +0800

    Created Json for adadpted Course and Course Requirement classes

---
+++ src/main/java/seedu/address/storage/coursestorage/JsonAdaptedCourseRequirement.java
@@ -0,0 +1,9 @@
+package seedu.address.storage.coursestorage;
+
+import seedu.address.model.course.CourseRequirement;
+
+/**
+ * Jackson-friendly version of {@link CourseRequirement}.
+ */
+public class JsonAdaptedCourseRequirement {
+}

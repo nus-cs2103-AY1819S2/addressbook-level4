9f779fe15dce3203f0ab1836b10ea366655cfc3b
chung-ming <chungming_tsen@outlook.com>
2019-03-19 13:53:38 +0800

    Modify formatting in AddCommand and ListCommand. Remove tests for GenerateCommand.

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -136,16 +136,6 @@ public class AddressBookParserTest {
     }
 
     @Test
-    public void parseCommand_generate() throws Exception {
-        assertTrue(parser.parseCommand(GenerateCommand.COMMAND_WORD) instanceof GenerateCommand);
-    }
-
-    @Test
-    public void parseCommand_generateAlias() throws Exception {
-        assertTrue(parser.parseCommand(GenerateCommand.COMMAND_ALIAS) instanceof GenerateCommand);
-    }
-
-    @Test
     public void parseCommand_searchTags() throws Exception {
         List<String> keywords = Arrays.asList("school", "temple", "airport");
         SearchTagsCommand command = (SearchTagsCommand) parser.parseCommand(SearchTagsCommand.COMMAND_WORD

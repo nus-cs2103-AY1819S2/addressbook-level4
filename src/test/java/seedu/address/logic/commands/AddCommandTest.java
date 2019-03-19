d6677a4d832331ba952731aef2a3a51fe2837c2d
WeeSooJun <weesj96@gmail.com>
2019-03-19 23:30:38 +0800

    Remove redundant imports

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertTrue;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.function.Predicate;
 
 import org.junit.Rule;

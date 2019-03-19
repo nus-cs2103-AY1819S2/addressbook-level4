4592a03cb7c9fb340bcf1de2564005105cd0745e
jing1324 <jinjing123@gmail.com>
2019-03-19 15:05:13 +0800

    Fix part of serializable tests

--- src/test/java/seedu/address/storage/JsonSerializablePatientBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializablePatientBookTest.java
@@ -10,42 +10,42 @@ import org.junit.rules.ExpectedException;
 import static org.junit.Assert.assertEquals;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.JsonUtil;
-import seedu.address.model.AddressBook;
-import seedu.address.testutil.TypicalPersons;
+import seedu.address.model.PatientBook;
+import seedu.address.testutil.TypicalPatients;
 
 public class JsonSerializablePatientBookTest {
 
-    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
-    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBook.json");
-    private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAddressBook.json");
-    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
+    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializablePatientBookTest");
+    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPatientsBook.json");
+    private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPatientBook.json");
+    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePatientBook.json");
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void toModelType_typicalPersonsFile_success() throws Exception {
-        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
-                JsonSerializableAddressBook.class).get();
-        AddressBook addressBookFromFile = dataFromFile.toModelType();
-        AddressBook typicalPersonsAddressBook = TypicalPersons.getTypicalAddressBook();
-        assertEquals(addressBookFromFile, typicalPersonsAddressBook);
+    public void toModelType_typicalPatientsFile_success() throws Exception {
+        JsonSerializablePatientBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
+                JsonSerializablePatientBook.class).get();
+        PatientBook PatientBookFromFile = dataFromFile.toModelType();
+        PatientBook typicalPatientsPatientBook = TypicalPatients.getTypicalPatientBook();
+        assertEquals(PatientBookFromFile, typicalPatientsPatientBook);
     }
 
     @Test
-    public void toModelType_invalidPersonFile_throwsIllegalValueException() throws Exception {
-        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_PERSON_FILE,
-                JsonSerializableAddressBook.class).get();
+    public void toModelType_invalidPatientFile_throwsIllegalValueException() throws Exception {
+        JsonSerializablePatientBook dataFromFile = JsonUtil.readJsonFile(INVALID_PERSON_FILE,
+                JsonSerializablePatientBook.class).get();
         thrown.expect(IllegalValueException.class);
         dataFromFile.toModelType();
     }
 
     @Test
-    public void toModelType_duplicatePersons_throwsIllegalValueException() throws Exception {
-        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_PERSON_FILE,
-                JsonSerializableAddressBook.class).get();
+    public void toModelType_duplicatePatients_throwsIllegalValueException() throws Exception {
+        JsonSerializablePatientBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_PERSON_FILE,
+                JsonSerializablePatientBook.class).get();
         thrown.expect(IllegalValueException.class);
-        thrown.expectMessage(JsonSerializableAddressBook.MESSAGE_DUPLICATE_PERSON);
+        thrown.expectMessage(JsonSerializablePatientBook.MESSAGE_DUPLICATE_PERSON);
         dataFromFile.toModelType();
     }
 

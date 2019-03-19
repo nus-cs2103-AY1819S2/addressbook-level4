b51436c53bb618da901cb19b9b91cbb49262c319
huichun66 <pewpewt97@gmail.com>
2019-03-19 22:13:38 +0800

    Attempted to do some minor formatting change to see if it will fix the test bug, but no.

--- src/test/java/seedu/address/testutil/TypicalPatients.java
+++ src/test/java/seedu/address/testutil/TypicalPatients.java
@@ -32,11 +32,11 @@ public class TypicalPatients {
         .withPhone("98765432")
         .withNric("S9274100D")
         .withConditions(new Conditions(new HashSet<>(
-            Arrays.asList(new ConditionTag("Eldercare"),
-                new ConditionTag("Stroke")))))
+            Arrays.asList(new ConditionTag("Stroke"),
+                new ConditionTag("Eldercare")))))
         .build();
     public static final Patient CARL = new PatientBuilder()
-        .withName("Carl Kurz").withPhone("95352563")
+        .withName("Carl Kurz").withPhone("87652533")
         .withAddress("wall street")
         .withEmail("heinz@example.com")
         .withPhone("87652533")

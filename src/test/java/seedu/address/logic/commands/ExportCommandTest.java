220b69acd7f64b78b095b884de826658776feac1
JonathanLeeWH <leeweihao1996@outlook.com>
2019-03-19 23:24:34 +0800

    Update ExportCommandTest.java with additional JUnit test cases
    Update ExportCommandTest.java with additional JUnit test cases to ensure that the data in the csv file exported matches the expected data

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -1,16 +1,24 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalMedicines.getTypicalInventory;
 
 import java.io.File;
+import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.io.FilenameUtils;
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.opencsv.CSVReader;
 
 import seedu.address.commons.util.FileName;
 import seedu.address.commons.util.csv.CsvWrapper;
@@ -18,6 +26,9 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.medicine.Batch;
+import seedu.address.model.medicine.Medicine;
+import seedu.address.model.tag.Tag;
 
 /**
  * Contains unit tests for {@code ExportCommand}.
@@ -62,5 +73,74 @@ public class ExportCommandTest {
                 + " already exists in \"exported\" directory.");
         // Clean up temporary csv file for test case created.
         filePath.delete();
+    }
+
+    @Test
+    public void execute_csvFileInformationMatchesData_success() throws Exception {
+        Path exportDirectoryPath = Paths.get("exported");
+        if (Files.isDirectory(exportDirectoryPath) == false) {
+            Files.createDirectory(exportDirectoryPath);
+        }
+        File filePath = File.createTempFile("tmp", ".csv", exportDirectoryPath.toFile());
+        String fileName = filePath.toPath().getFileName().toString();
+        String fileNameWithoutFileExtension = FilenameUtils.removeExtension(fileName);
+        ExportCommand exportCommand = new ExportCommand(new FileName(fileNameWithoutFileExtension));
+        filePath.delete();
+        String expectedMessage = String.format(ExportCommand.MESSAGE_SUCCESS, fileNameWithoutFileExtension);
+
+        assertCommandSuccess(exportCommand, model, commandHistory, expectedMessage, model);
+        CSVReader reader = new CSVReader(new FileReader(filePath));
+        String[] expectedData;
+        String[] actualData;
+        expectedData = CsvWrapper.getDefaultHeading();
+        if ((actualData = reader.readNext()) == null) {
+            filePath.delete();
+            Assert.fail("The actual csv file contains less data than the expected amount of data.");
+        }
+        assertArrayEquals(expectedData, actualData);
+        List<Medicine> currentGuiList = model.getFilteredMedicineList();
+        Iterator iterator = currentGuiList.listIterator();
+        while (iterator.hasNext()) {
+            Medicine current = (Medicine) iterator.next();
+            if (current.getBatches().size() == 0) {
+                continue;
+            }
+            // Iterate through the batches of the current medicine
+            Collection<Batch> batches = current.getBatches().values();
+            Iterator batchIterator = batches.iterator();
+            while (batchIterator.hasNext()) {
+                if ((actualData = reader.readNext()) == null) {
+                    filePath.delete();
+                    Assert.fail("The actual csv file contains less data than the expected amount of data.");
+                }
+                Batch currentBatch = (Batch) batchIterator.next();
+                final StringBuilder builder = new StringBuilder();
+                String delimiter = "|";
+                builder.append(current.getName())
+                        .append(delimiter)
+                        .append(currentBatch.getBatchNumber())
+                        .append(delimiter)
+                        .append(currentBatch.getQuantity())
+                        .append(delimiter)
+                        .append(currentBatch.getExpiry())
+                        .append(delimiter)
+                        .append(current.getCompany())
+                        .append(delimiter);
+                Iterator buildIterator = current.getTags().iterator();
+                while (buildIterator.hasNext()) {
+                    Tag currentTag = (Tag) buildIterator.next();
+                    String formattedCurrentTagString = currentTag.toStringUpperCase();
+                    builder.append(formattedCurrentTagString);
+                    builder.append(' ');
+                }
+                expectedData = builder.toString().split("\\|");
+                assertArrayEquals(expectedData, actualData);
+            }
+
+        }
+        if (reader.readNext() != null) {
+            filePath.delete();
+            Assert.fail("The actual csv file contains more data than the expected amount of data.");
+        }
     }
 }

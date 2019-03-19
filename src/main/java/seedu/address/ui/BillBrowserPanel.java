dedd8ca1d0891eddae48a992b8057ec4c5601bd4
beesaycheese <beesaycheese@users.noreply.github.com>
2019-03-19 11:20:22 +0800

    Edit the UI receipt to a label instead of a textArea

--- src/main/java/seedu/address/ui/BillBrowserPanel.java
+++ src/main/java/seedu/address/ui/BillBrowserPanel.java
@@ -9,7 +9,6 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.statistics.Bill;
@@ -25,64 +24,17 @@ public class BillBrowserPanel extends UiPart<Region> {
     private final Logger logger = LogsCenter.getLogger(getClass());
 
     @FXML
-    private ListView<Bill> receiptView;
-
-    @FXML
     private Label tableNumber;
 
     @FXML
-    private TextArea receipt;
+    private Label receipt;
 
-    public BillBrowserPanel(Bill bill, ObservableValue<Bill> selectedBill, Consumer<Bill> onSelectedBillChange) {
+    public BillBrowserPanel(Bill bill) {
         super(FXML);
 
         requireNonNull(bill);
         tableNumber.setText("TABLE NUMBER " + bill.getTableNumber().tableNumber);
-        //TODO: Find out why the text aren't printing
-        receipt = new TextArea(" ");
         receipt.setWrapText(true);
         receipt.setText(bill.getReceipt());
-        receipt.appendText(bill.getReceipt());
-
-        //receiptView.setCellFactory(listView -> new BillBrowserPanel.BillCell());
-        //receiptView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-        //    logger.fine("Selection in list panel changed to : '" + newValue + "'");
-        //    onSelectedBillChange.accept(newValue);
-        //});
-        //selectedBill.addListener(((observable, oldValue, newValue) -> {
-        //    logger.fine("Selected item changed to: " + newValue);
-        //
-        //            // Don't modify selection if we are already selecting the selected item,
-        //            // otherwise we would have an infinite loop.
-        //            if (Objects.equals(receiptView.getSelectionModel().getSelectedItem(), newValue)) {
-        //                return;
-        //            }
-        //
-        //            if (newValue == null) {
-        //                receiptView.getSelectionModel().clearSelection();
-        //            } else {
-        //                int index = receiptView.getItems().indexOf(newValue);
-        //                receiptView.scrollTo(index);
-        //                receiptView.getSelectionModel().clearAndSelect(index);
-        //            }
-        //        }));
     }
-
-    //    /**
-    //     * Custom {@code ListCell} that displays the graphics of a {@code Item} using a {@code ItemCard}.
-    //     */
-    //    class BillCell extends ListCell<Bill> {
-    //        @Override
-    //        protected void updateItem(Bill item, boolean empty) {
-    //            super.updateItem(item, empty);
-    //
-    //            if (empty || item == null) {
-    //                setGraphic(null);
-    //                setText(null);
-    //            } else {
-    //                setGraphic(new BillCard(bill));
-    //            }
-    //        }
-    //    }
-
 }

06790324b47bd9afd64fd15765ddc566ebb415a6
JiaHaoLim <jiahaolimwork@hotmail.com>
2019-03-19 16:32:57 +0800

    Added save validation to SaveCommandParser.java

--- src/main/java/seedu/address/logic/parser/SaveCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SaveCommandParser.java
@@ -13,14 +13,28 @@ public class SaveCommandParser implements Parser<SaveCommand> {
     /**
      * Parses the given argument {@code String} in the context of the SaveCommand
      * and returns an SaveCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public SaveCommand parse(String args) throws ParseException {
         try {
             File file = ParserUtil.parseOpenSave(args);
+            saveValidation(file);
             return new SaveCommand(file);
         } catch (ParseException pe) {
             throw new ParseException(pe.getMessage());
+        }
+    }
+
+    /**
+     * saveValidation() checks if the file is writable if it exists.
+     *
+     * @param file
+     * @throws ParseException
+     */
+    private void saveValidation(File file) throws ParseException {
+        if (file.exists() && !file.canWrite()) {
+            throw new ParseException("File is read only!");
         }
     }
 }

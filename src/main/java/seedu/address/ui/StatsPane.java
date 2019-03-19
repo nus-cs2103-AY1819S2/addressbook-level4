5cfbdf8159eb9bf1c41f8bb26f34f48125ba14f8
bos10 <bostoncheong@gmail.com>
2019-03-19 21:09:52 +0800

    Fixed Checkstyle

--- src/main/java/seedu/address/ui/StatsPane.java
+++ src/main/java/seedu/address/ui/StatsPane.java
@@ -1,11 +1,5 @@
 package seedu.address.ui;
 
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
-
-import seedu.address.logic.statistics.PlayerStatistics;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
@@ -21,7 +15,7 @@ public class StatsPane extends UiPart<Region> {
     //TOREMOVE private static Clock clock = Clock.systemDefaultZone();
 
     private static final String FXML = "StatsPane.fxml";
-   // private PlayerStatistics playerStats = playerStats;
+    // private PlayerStatistics playerStats = playerStats;
     private long startTime;
 
     @FXML
@@ -29,16 +23,16 @@ public class StatsPane extends UiPart<Region> {
     @FXML
     private Label saveLocationStatus;
 
-//    private Timer myTimer = new Timer();
-//    private TimerTask task = new TimerTask() {
-//
-//        @Override
-//        public void run() {
-//            Platform.runLater(()-> {
-//                updateSyncStatus();
-//            });
-//        }
-//    };
+    //    private Timer myTimer = new Timer();
+    //    private TimerTask task = new TimerTask() {
+    //
+    //        @Override
+    //        public void run() {
+    //            Platform.runLater(()-> {
+    //                updateSyncStatus();
+    //            });
+    //        }
+    //    };
 
     public StatsPane() { //StatsPane(PlayerStatistics playerStats)
         super(FXML);
@@ -51,9 +45,9 @@ public class StatsPane extends UiPart<Region> {
     /**
      * Stops the internal Timer that increments the time display.
      */
-  //  public void stopTimer() {
-  //      myTimer.cancel();
-  //  }
+    //  public void stopTimer() {
+    //      myTimer.cancel();
+    //  }
 
     /**
      * Updates elapsed time
@@ -65,33 +59,24 @@ public class StatsPane extends UiPart<Region> {
 
 
     // command to get attacks made
-   // private int getStats(){
+    // private int getStats(){
 
-   //     return
-   // }
+    //     return
+    // }
     /**
      *
      * @param endTime
      * @return
      */
-//    public long getElapsedTime(long endTime) {
-//        long elapsedTimeSeconds = (endTime - this.startTime) / (1000000000);
-//        return elapsedTimeSeconds;
-//    }
+    //    public long getElapsedTime(long endTime) {
+    //        long elapsedTimeSeconds = (endTime - this.startTime) / (1000000000);
+    //        return elapsedTimeSeconds;
+    //    }
 
     /**
      * @param secondsToConvert in seconds to convert
      */
-//    private String convert(long secondsToConvert) {
-//        long millis = secondsToConvert * 1000;
-//        long hours = TimeUnit.MILLISECONDS.toHours(millis);
-//        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis) % TimeUnit.HOURS.toMinutes(1);
-//        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis) % TimeUnit.MINUTES.toSeconds(1);
-//
-//        String format = String.format("%02d:%02d:%02d", Math.abs(hours), Math.abs(minutes), Math.abs(seconds));
-//
-//        return format;
-//    }
+
 
 
 }

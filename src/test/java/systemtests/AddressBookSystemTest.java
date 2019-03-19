37ef33f6b71e26fb48083936968b567507934d77
wSemis <debang@u.nus.edu>
2019-03-19 22:27:03 +0800

    Temporary refrainment on the assertion

--- src/test/java/systemtests/AddressBookSystemTest.java
+++ src/test/java/systemtests/AddressBookSystemTest.java
@@ -173,7 +173,7 @@ public abstract class AddressBookSystemTest {
     protected void assertApplicationDisplaysExpected(String expectedCommandInput, String expectedResultMessage,
             Model expectedModel) {
         assertEquals(expectedCommandInput, getCommandBox().getInput());
-//        assertEquals(expectedResultMessage.trim(), getResultDisplay().getText().trim());
+        //assertEquals(expectedResultMessage.trim(), getResultDisplay().getText().trim());
         assertEquals(new AddressBook(expectedModel.getAddressBook()), testApp.readStorageAddressBook());
         assertListMatching(getPersonListPanel(), expectedModel.getFilteredPersonList());
     }

07bf0ee3f93ecd2a894c51c23272ebb18f655853
eugenefdw <eugenefoodw@gmail.com>
2019-03-20 17:42:01 +0800

    Remove unused imports

--- src/main/java/seedu/address/storage/Storage.java
+++ src/main/java/seedu/address/storage/Storage.java
@@ -1,14 +1,12 @@
 package seedu.address.storage;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.Optional;
 
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.Lessons;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.lesson.Lesson;
 
 /**
  * API of the Storage component

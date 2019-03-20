e1a0125d50c40bfe12f4f928b4e3952785fc00f8
sciffany <sciffany@gmail.com>
2019-03-21 01:37:17 +0800

    responsive UI

--- src/main/java/seedu/address/ui/StudyPanel.java
+++ src/main/java/seedu/address/ui/StudyPanel.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 
 import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
+import javafx.css.PseudoClass;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -19,18 +20,25 @@ import javafx.scene.web.WebView;
 import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.ListItem;
+import seedu.address.logic.StudyView;
 import seedu.address.model.deck.Card;
 
+
 /**
- * The Question Panel of the App.
+ * The Study Panel of the App.
  */
 public class StudyPanel extends UiPart<Region> {
 
     private static final String FXML = "StudyPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(ListPanel.class);
 
+    private static final PseudoClass ANSWER = PseudoClass.getPseudoClass("answer");
+
     @FXML
     private HBox studyPane;
+
+    @FXML
+    private HBox card;
 
     @FXML
     private Label question;
@@ -39,14 +47,21 @@ public class StudyPanel extends UiPart<Region> {
     private Label id;
 
 
-    public StudyPanel(ObservableValue<String> textShown) {
+    public StudyPanel(ObservableValue<String> textShown, ObservableValue<StudyView.studyState> studyState) {
         super(FXML);
 
         question.setText(textShown.getValue());
+        card.pseudoClassStateChanged(ANSWER, false);
 
         textShown.addListener((observable, oldValue, newValue) -> {
             logger.info("textShown changed to: " + newValue);
             question.setText(textShown.getValue());
+            
+        });
+
+        textShown.addListener((observable, oldValue, newValue) -> {
+            logger.info("color changed to: " + newValue);
+            card.pseudoClassStateChanged(ANSWER, studyState.getValue() == StudyView.studyState.ANSWER);
         });
 
 

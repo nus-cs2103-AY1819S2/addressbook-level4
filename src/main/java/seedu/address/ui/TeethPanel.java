a0eab41f93f68887052a9ba0aea51f0271448eea
cchj1995 <choo_jnthn@hotmail.com>
2019-03-21 13:52:35 +0800

    Added relevant imports to TeethPanel.java and added code to TeethPanel.fxml

---
+++ src/main/java/seedu/address/ui/TeethPanel.java
@@ -0,0 +1,27 @@
+package seedu.address.ui;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.logging.Logger;
+
+import javax.imageio.ImageIO;
+
+import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.person.Person;
+
+

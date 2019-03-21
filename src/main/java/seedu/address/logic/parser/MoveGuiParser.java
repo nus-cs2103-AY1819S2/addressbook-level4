78ff74fd92e6722f2c8e2d93d8e5c891077cc17b
ptvrajsk <ptvrajsk@gmail.com>
2019-03-21 18:44:57 +0800

    (v1.3) Checkstyle Ammendments

--- src/main/java/seedu/address/logic/parser/MoveGuiParser.java
+++ src/main/java/seedu/address/logic/parser/MoveGuiParser.java
@@ -6,17 +6,28 @@ import java.util.Optional;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+/**
+ * Parses parses users directory selection and returns the corresponding file object.
+ */
 class MoveGuiParser {
 
     private static final String DIRCHOOSER_TITLE = "Select Directory";
 
     private DirectoryChooser directoryChooser;
 
+    /**
+     * Default Constructor, initializes directory chooser.
+     */
     MoveGuiParser() {
         this.directoryChooser = new DirectoryChooser();
         this.directoryChooser.setTitle(DIRCHOOSER_TITLE);
     }
 
+    /**
+     * Opens the DirectoryChooser UI to allow the user to select
+     * a directory of choice.
+     * @return File Object corresponding to chosen directory.
+     */
     Optional<File> selectDirectory() {
         return Optional.ofNullable(this.directoryChooser.showDialog(new Stage()));
     }

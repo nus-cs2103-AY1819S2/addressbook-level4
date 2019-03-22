4de3894061b738ca1f078b89acac4ee70baa1360
chung-ming <chungming_tsen@outlook.com>
2019-03-22 21:17:41 +0800

    Fix test errors

--- src/test/java/seedu/travel/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/travel/storage/JsonUserPrefsStorageTest.java
@@ -82,7 +82,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setTravelBuddyFilePath(Paths.get("data\\travelBuddy.json"));
+        userPrefs.setTravelBuddyFilePath(Paths.get("data", "travelBuddy.json"));
         return userPrefs;
     }
 

a36fbe686da943ab0b6606b5fbcc569219f051a2
JiaHaoLim <jiahaolimwork@hotmail.com>
2019-03-22 15:48:47 +0800

    Added title, subtitle and image to InOutAddressBookStorage.java. Refactored InOutAddressBookStorage.java.

--- src/main/java/seedu/address/storage/InOutAddressBookStorage.java
+++ src/main/java/seedu/address/storage/InOutAddressBookStorage.java
@@ -11,8 +11,10 @@ import java.util.logging.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.DataConversionException;
@@ -26,6 +28,11 @@ import seedu.address.model.ReadOnlyAddressBook;
  * a json file or PDF file.
  */
 public class InOutAddressBookStorage implements AddressBookStorage {
+
+    private static final String TITLE = "OurTeeth";
+    private static final int TOPMARGIN = 20;
+    private static final int LINE_SPACING = 3;
+    private static final String TEETH_IMAGE_PATH = "src\\main\\resources\\images\\tooth.png";
 
     private static final Logger logger = LogsCenter.getLogger(InOutAddressBookStorage.class);
 
@@ -106,40 +113,88 @@ public class InOutAddressBookStorage implements AddressBookStorage {
         try (PDDocument doc = new PDDocument()) {
             PDFont font = PDType1Font.HELVETICA;
             int fontSize = 12;
+            PDFont titleFont = PDType1Font.HELVETICA_BOLD;
+            int titleFontSize = 20;
 
             for (PdfAdaptedPerson person : toWrite.getPersons()) {
-                printModelObject(doc, font, fontSize, person);
+                writeModelObject(doc, titleFont, titleFontSize, font, fontSize, person, "Patients");
             }
 
             for (PdfAdaptedTask task : toWrite.getTasks()) {
-                printModelObject(doc, font, fontSize, task);
+                writeModelObject(doc, titleFont, titleFontSize, font, fontSize, task, "Tasks");
             }
 
             doc.save(filePath.toFile());
+        } catch (IOException e) {
+            throw new IOException();
         }
     }
 
     /**
      * Outputs the PDF adapted class object contents to a PDF page.
      * @param doc The PDF document
+     * @param titleFont The font type for the title to be used
+     * @param titleFontSize The font size for the title to be used
      * @param font The font type to be used
      * @param fontSize The font size to be used
      * @param pdfAdaptedObj The object to be printed
      * @throws IOException If file cannot be written
      */
-    private void printModelObject(PDDocument doc, PDFont font, int fontSize,
-                                  PdfAdaptedInterface pdfAdaptedObj) throws IOException {
-        PDPage page = new PDPage();
+    private void writeModelObject(PDDocument doc, PDFont titleFont, int titleFontSize, PDFont font,
+                                  int fontSize, PdfAdaptedInterface pdfAdaptedObj, String type) throws IOException {
+        PDPage page = new PDPage(PDRectangle.A4);
         doc.addPage(page);
         ArrayList<String> stringArr = pdfAdaptedObj.getStrings();
+        float textWidth = font.getStringWidth(TITLE) / 1000 * titleFontSize;
+        float textHeight = font.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * titleFontSize;
+        float tx;
+        float ty;
+
         try (PDPageContentStream contents = new PDPageContentStream(doc, page)) {
+            contents.setFont(titleFont, titleFontSize);
+            PDImageXObject pdImage = PDImageXObject.createFromFile(TEETH_IMAGE_PATH, doc);
+            tx = ((page.getMediaBox().getWidth() - textWidth) - (pdImage.getWidth() * textHeight / 1000)) / 2;
+            ty = page.getMediaBox().getHeight() - TOPMARGIN - textHeight;
+            writeLine(contents, TITLE, tx, ty);
+            contents.drawImage(pdImage, tx + textWidth + (pdImage.getWidth() * textHeight / 1000) - 7, ty - 1,
+                        pdImage.getWidth() * textHeight / 1000,
+                        pdImage.getHeight() * textHeight / 1000);
+
+            int subtitleFontSize = fontSize + Math.abs((titleFontSize - fontSize) / 2);
+            contents.setFont(titleFont, subtitleFontSize);
+            textWidth = font.getStringWidth(type) / 1000 * subtitleFontSize;
+            textHeight = font.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * subtitleFontSize;
+            tx = (page.getMediaBox().getWidth() - textWidth) / 2;
+            ty = page.getMediaBox().getHeight() - TOPMARGIN - textHeight - subtitleFontSize - LINE_SPACING * 4;
+            writeLine(contents, type, tx, ty);
+
+            contents.addRect(tx, ty, textWidth, 1);
+
             contents.setFont(font, fontSize);
             for (int i = 0; i < stringArr.size(); i++) {
-                contents.beginText();
-                contents.newLineAtOffset(100, 700 - (i * (fontSize + 3)));
-                contents.showText(stringArr.get(i));
-                contents.endText();
+                writeLine(contents, stringArr.get(i), 50, 700 - (i * (fontSize + LINE_SPACING)));
             }
+        } catch (IOException e) {
+            throw new IOException();
+        }
+    }
+
+    /**
+     * Writes a single line of content.
+     * @param contents The content stream for writing
+     * @param toWrite The String to write
+     * @param tx The x coordinates to write at
+     * @param ty The y coordinates to write at
+     * @throws IOException If file cannot be written
+     */
+    private void writeLine(PDPageContentStream contents, String toWrite, float tx, float ty) throws IOException {
+        try {
+            contents.beginText();
+            contents.newLineAtOffset(tx, ty);
+            contents.showText(toWrite);
+            contents.endText();
+        } catch (IOException e) {
+            throw new IOException();
         }
     }
 }

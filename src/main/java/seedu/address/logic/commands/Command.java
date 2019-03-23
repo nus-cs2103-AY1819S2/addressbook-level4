358dc6875509d6e338ce031ac0263c0fd4117ffa
s-tr <swchong25+github@gmail.com>
2019-03-23 12:37:23 +0800

    Add state-based execution permission mechanism to Command

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -1,6 +1,12 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.EnumSet;
+import java.util.Set;
+
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.battle.state.BattleState;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
@@ -8,7 +14,10 @@ import seedu.address.model.Model;
  * Represents a command with hidden internal logic and the ability to be executed.
  */
 public abstract class Command {
-
+    /**
+     * The states that this command can be executed in.
+     */
+    private Set<BattleState> permissibleStates = EnumSet.allOf(BattleState.class);
     /**
      * Executes the command and returns the result message.
      *
@@ -19,4 +28,21 @@ public abstract class Command {
      */
     public abstract CommandResult execute(Model model, CommandHistory history) throws CommandException;
 
+    /**
+     * Changes the permissible states of the command.
+     */
+    protected final void setPermissibleStates(Set<BattleState> permissibleStates) {
+        requireNonNull(permissibleStates);
+        this.permissibleStates = permissibleStates;
+    }
+
+    /**
+     * Checks if this command can be executed in a certain state.
+     * <br>
+     * By default, commands can be run in all states. Those commands which are not
+     * should set the permissible states using <code>setPermissibleStates()</code>.
+     */
+    public final boolean canExecuteIn(BattleState battleState) {
+        return permissibleStates.contains(battleState);
+    }
 }

b905a981920eac3a0eb2ed629131beec762aafed
ckjohn000 <ckjohn000@gmail.com>
2019-03-24 23:16:04 +0800

    add delete deckcommandtest

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -9,6 +9,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_DISPLAYED_INDEX = "The index provided is invalid";
     public static final String MESSAGE_CARDS_LISTED_OVERVIEW = "%1$d cards listed!";
+    public static final String MESSAGE_INVALID_DECK_DISPLAYED_INDEX = "The index provided is invalid";
 
     public static final String MESSAGE_DUPLICATE_DECK = "A deck with this name already exists";
 

3fd6548b8ca99700183c6791e29cfbd2099efe4d
0blivious <lyw@415309@gmail.com>
2019-03-24 20:53:01 +0800

    style fix

--- src/main/java/seedu/address/logic/commands/DeleteBookCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteBookCommand.java
@@ -1,9 +1,6 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_BOOK;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_BOOKS;
 
 import java.util.List;
 
@@ -13,7 +10,6 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.book.Book;
-import seedu.address.model.book.BookNameContainsExactKeywordsPredicate;
 
 /**
  * Deletes a book identified using it's displayed index from the book shelf.

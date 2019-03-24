44df51695a9d2f762e6a5153caadae1bf498eac5
siyingpoof <alvina.ongg@gmail.com>
2019-03-25 01:48:29 +0800

    fixed files for checkstyle errors

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -213,7 +213,7 @@ public class AddressBook implements ReadOnlyAddressBook {
         return medHists.asUnmodifiableObservableList();
     }
 
-//    @Override
+    @Override
     public ObservableList<Doctor> getDoctorList() {
         return doctors.asUnmodifiableObservableList();
     }

9b12d0c12c75ca848740c5c7f0775687ec016fb1
lormee94 <35290589+lormee94@users.noreply.github.com>
2019-03-25 11:20:45 +0800

    Revert "(v1.3) Implement sorting & removing deadline"

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -2,9 +2,7 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.model.Model.COMPARATOR_ASCENDING_DEADLINE_PDFS;
 import static seedu.address.model.Model.COMPARATOR_ASCENDING_NAME_PDFS;
-import static seedu.address.model.Model.COMPARATOR_DESCENDING_DEADLINE_PDFS;
 import static seedu.address.model.Model.COMPARATOR_DESCENDING_NAME_PDFS;
 
 import seedu.address.logic.commands.SortCommand;
@@ -15,10 +13,8 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class SortCommandParser implements Parser<SortCommand> {
 
-    private static final String name_ascending = "name up";
-    private static final String name_descending = "name down";
-    private static final String deadline_ascending = "date up";
-    private static final String deadline_descending = "date down";
+    private static final String ascending = "up";
+    private static final String descending = "down";
 
     /**
      * Parses the given {@code String} of arguments in the context of the SortCommand
@@ -29,14 +25,10 @@ public class SortCommandParser implements Parser<SortCommand> {
         requireNonNull(args);
         try {
             String parseArgs = args.trim();
-            if (parseArgs.equals(name_ascending)) {
+            if (parseArgs.equals(ascending)) {
                 return new SortCommand(COMPARATOR_ASCENDING_NAME_PDFS);
-            } else if (parseArgs.equals(name_descending)) {
+            } else if (parseArgs.equals(descending)) {
                 return new SortCommand(COMPARATOR_DESCENDING_NAME_PDFS);
-            } else if (parseArgs.equals(deadline_ascending)) {
-                return new SortCommand(COMPARATOR_ASCENDING_DEADLINE_PDFS);
-            } else if (parseArgs.equals(deadline_descending)) {
-                return new SortCommand(COMPARATOR_DESCENDING_DEADLINE_PDFS);
             } else {
                 throw new ParseException(SortCommand.MESSAGE_USAGE);
             }

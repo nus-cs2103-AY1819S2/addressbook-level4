f9f31214c16e5b259e04e8438fd5ea7c6e5329b4
whyjayxp <whyjayxp@hotmail.com>
2019-03-26 04:10:57 +0800

    Add UI tests for Orders and addToOrder integration test

--- src/test/java/seedu/address/ui/OrderItemListPanelTest.java
+++ src/test/java/seedu/address/ui/OrderItemListPanelTest.java
@@ -1,11 +1,16 @@
 package seedu.address.ui;
 
 import static java.time.Duration.ofMillis;
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_ITEM;
 import static seedu.address.testutil.TypicalRestOrRant.getTypicalOrderItems;
+import static seedu.address.ui.testutil.OrdersGuiTestAssert.assertCardDisplaysOrderItem;
+import static seedu.address.ui.testutil.OrdersGuiTestAssert.assertCardEquals;
 
 import org.junit.Test;
 
+import guitests.guihandles.OrderItemCardHandle;
 import guitests.guihandles.OrderItemListPanelHandle;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
@@ -24,32 +29,32 @@ public class OrderItemListPanelTest extends GuiUnitTest {
     private final SimpleObjectProperty<OrderItem> selectedOrderItem = new SimpleObjectProperty<>();
     private OrderItemListPanelHandle orderItemListPanelHandle;
 
-    //    @Test TODO
-    //    public void display() {
-    //        initUi(TYPICAL_ORDER_ITEMS);
-    //
-    //        for (int i = 0; i < TYPICAL_ORDER_ITEMS.size(); i++) {
-    //            orderItemListPanelHandle.navigateToCard(TYPICAL_ORDER_ITEMS.get(i));
-    //            OrderItem expectedOrderItem = TYPICAL_ORDER_ITEMS.get(i);
-    //            OrderItemCardHandle actualCard = orderItemListPanelHandle.getOrderItemCardHandle(i);
-    //
-    //            assertCardDisplaysOrderItem(expectedOrderItem, actualCard);
-    //            assertEquals(Integer.toString(i + 1) + ". ", actualCard.getId());
-    //        }
-    //    }
+    @Test
+    public void display() {
+        initUi(TYPICAL_ORDER_ITEMS);
 
-    //    @Test TODO
-    //    public void selection_modelSelectedOrderItemChanged_selectionChanges() {
-    //        initUi(TYPICAL_ORDER_ITEMS);
-    //        OrderItem secondOrderItem = TYPICAL_ORDER_ITEMS.get(INDEX_SECOND_ITEM.getZeroBased());
-    //        guiRobot.interact(() -> selectedOrderItem.set(secondOrderItem));
-    //        guiRobot.pauseForHuman();
-    //
-    //        OrderItemCardHandle expectedOrderItem =
-    //                orderItemListPanelHandle.getOrderItemCardHandle(INDEX_SECOND_ITEM.getZeroBased());
-    //        OrderItemCardHandle selectedOrderItem = orderItemListPanelHandle.getHandleToSelectedCard();
-    //        assertCardEquals(expectedOrderItem, selectedOrderItem);
-    //    }
+        for (int i = 0; i < TYPICAL_ORDER_ITEMS.size(); i++) {
+            orderItemListPanelHandle.navigateToCard(TYPICAL_ORDER_ITEMS.get(i));
+            OrderItem expectedOrderItem = TYPICAL_ORDER_ITEMS.get(i);
+            OrderItemCardHandle actualCard = orderItemListPanelHandle.getOrderItemCardHandle(i);
+
+            assertCardDisplaysOrderItem(expectedOrderItem, actualCard);
+            assertEquals(Integer.toString(i + 1) + ". ", actualCard.getId());
+        }
+    }
+
+    @Test
+    public void selection_modelSelectedOrderItemChanged_selectionChanges() {
+        initUi(TYPICAL_ORDER_ITEMS);
+        OrderItem secondOrderItem = TYPICAL_ORDER_ITEMS.get(INDEX_SECOND_ITEM.getZeroBased());
+        guiRobot.interact(() -> selectedOrderItem.set(secondOrderItem));
+        guiRobot.pauseForHuman();
+
+        OrderItemCardHandle expectedOrderItem =
+                orderItemListPanelHandle.getOrderItemCardHandle(INDEX_SECOND_ITEM.getZeroBased());
+        OrderItemCardHandle selectedOrderItem = orderItemListPanelHandle.getHandleToSelectedCard();
+        assertCardEquals(expectedOrderItem, selectedOrderItem);
+    }
 
     /**
      * Verifies that creating and deleting large number of orderItems in {@code OrderItemListPanel} requires lesser than

996e804d910f894f5b671b9810bb62cfe299dee9
Yi Chong Ong <yichong96@gmail.com>
2019-03-25 23:48:08 +0800

    make import method throw CommandException

--- src/main/java/seedu/address/storage/csvmanager/CsvCommands.java
+++ src/main/java/seedu/address/storage/csvmanager/CsvCommands.java
@@ -3,6 +3,7 @@ package seedu.address.storage.csvmanager;
 import java.io.IOException;
 import java.util.List;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.ReadOnlyCardFolder;
 
 
@@ -11,7 +12,7 @@ import seedu.address.model.ReadOnlyCardFolder;
  */
 interface CsvCommands {
 
-    public void readFoldersToCsv(CsvFile csvFile) throws IOException;
+    public void readFoldersToCsv(CsvFile csvFile) throws IOException, CommandException;
 
     public void writeFoldersToCsv(List<ReadOnlyCardFolder> cardFolders) throws IOException;
 

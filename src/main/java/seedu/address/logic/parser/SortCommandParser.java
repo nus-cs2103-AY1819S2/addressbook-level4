51a375238e04eadbefe8e190cc69961e4465dc3e
jetkan-yk <jetkan.yk@gmail.com>
2019-03-26 02:45:31 +0800

    (v1.3) Implement sort deadline function
     - Sort function can sort based on alphabetical or deadline order
     - Update UserGuide to reflect new sort format:
        * `sort CRITERIA (name/date) ORDER (up/down)
        * CRITERIA can only be either name or date corresponding to
          alphabetical or deadline order
        * ORDER can only be either up or down corresponding to
          ascending or descending order
        * Example: `sort name up`
        * Example: `sort date down`
     - Tested working

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -2,8 +2,10 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.model.Model.COMPARATOR_ASCENDING_NAME_PDFS;
-import static seedu.address.model.Model.COMPARATOR_DESCENDING_NAME_PDFS;
+import static seedu.address.model.Model.COMPARATOR_DATE_ASCENDING_PDFS;
+import static seedu.address.model.Model.COMPARATOR_DATE_DESCENDING_PDFS;
+import static seedu.address.model.Model.COMPARATOR_NAME_ASCENDING_PDFS;
+import static seedu.address.model.Model.COMPARATOR_NAME_DESCENDING_PDFS;
 
 import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -13,8 +15,10 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class SortCommandParser implements Parser<SortCommand> {
 
-    private static final String ascending = "up";
-    private static final String descending = "down";
+    private static final String name_ascending = "name up";
+    private static final String name_descending = "name down";
+    private static final String date_ascending = "date up";
+    private static final String date_descending = "date down";
 
     /**
      * Parses the given {@code String} of arguments in the context of the SortCommand
@@ -25,10 +29,14 @@ public class SortCommandParser implements Parser<SortCommand> {
         requireNonNull(args);
         try {
             String parseArgs = args.trim();
-            if (parseArgs.equals(ascending)) {
-                return new SortCommand(COMPARATOR_ASCENDING_NAME_PDFS);
-            } else if (parseArgs.equals(descending)) {
-                return new SortCommand(COMPARATOR_DESCENDING_NAME_PDFS);
+            if (parseArgs.equals(name_ascending)) {
+                return new SortCommand(COMPARATOR_NAME_ASCENDING_PDFS);
+            } else if (parseArgs.equals(name_descending)) {
+                return new SortCommand(COMPARATOR_NAME_DESCENDING_PDFS);
+            } else if (parseArgs.equals(date_ascending)) {
+                return new SortCommand(COMPARATOR_DATE_ASCENDING_PDFS);
+            } else if (parseArgs.equals(date_descending)) {
+                return new SortCommand(COMPARATOR_DATE_DESCENDING_PDFS);
             } else {
                 throw new ParseException(SortCommand.MESSAGE_USAGE);
             }

c206b6323c20d3f867eafa9af07919f800e17712
Wayne <39239411+wayneswq@users.noreply.github.com>
2019-03-25 23:02:21 +0800

    Update JsonSerializableAddressBookTest.java

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -4,11 +4,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.junit.Rule;
-import org.junit.Test;
+//import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.commons.util.JsonUtil;
+//import seedu.address.commons.exceptions.IllegalValueException;
+//import seedu.address.commons.util.JsonUtil;
 
 public class JsonSerializableAddressBookTest {
 

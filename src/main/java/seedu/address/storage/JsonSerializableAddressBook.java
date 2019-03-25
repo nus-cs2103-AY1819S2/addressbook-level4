055b207ef2516abb0f8262692ccc2c10b2c6ac0b
Wayne <39239411+wayneswq@users.noreply.github.com>
2019-03-25 22:21:28 +0800

    Update JsonSerializableAddressBook.java

--- src/main/java/seedu/address/storage/JsonSerializableAddressBook.java
+++ src/main/java/seedu/address/storage/JsonSerializableAddressBook.java
@@ -11,8 +11,8 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.person.Patient;
 import seedu.address.model.medicalhistory.MedicalHistory;
+import seedu.address.model.person.Patient;
 
 /**
  * An Immutable AddressBook that is serializable to JSON format.

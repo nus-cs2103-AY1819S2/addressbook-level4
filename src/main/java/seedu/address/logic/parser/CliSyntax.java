f8091d372a4771e46a1a1568c175f7557a9546f3
mmdlow <mmdlow@gmail.com>
2019-03-26 17:00:24 +0800

    Add CLI Prefix for Option

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -8,6 +8,7 @@ public class CliSyntax {
     /* Prefix definitions */
     public static final Prefix PREFIX_QUESTION = new Prefix("q/");
     public static final Prefix PREFIX_ANSWER = new Prefix("a/");
+    public static final Prefix PREFIX_INCORRECT_OPTION = new Prefix("i/");
     public static final Prefix PREFIX_HINT = new Prefix("h/");
     public static final Prefix PREFIX_FILENAME = new Prefix("n/");
     public static final Prefix PREFIX_FOLDERNAME = new Prefix("f/");

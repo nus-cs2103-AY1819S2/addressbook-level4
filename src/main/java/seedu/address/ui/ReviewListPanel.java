fa419a7b04919dd67a59d2b1b090484c9df6e0a9
weixin-koh <e0201350@u.nus.edu>
2019-03-27 00:00:14 +0800

    Fix checkstyleMain

--- src/main/java/seedu/address/ui/ReviewListPanel.java
+++ src/main/java/seedu/address/ui/ReviewListPanel.java
@@ -6,7 +6,6 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;

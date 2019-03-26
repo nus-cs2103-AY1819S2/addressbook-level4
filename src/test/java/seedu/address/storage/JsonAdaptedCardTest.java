25fe9e869477ba441e3e386c467178194772cd6d
mmdlow <mmdlow@gmail.com>
2019-03-26 20:56:30 +0800

    Update tests and commands to support Options

--- src/test/java/seedu/address/storage/JsonAdaptedCardTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedCardTest.java
@@ -23,6 +23,9 @@ public class JsonAdaptedCardTest {
     private static final String VALID_QUESTION = BENSON.getQuestion().toString();
     private static final String VALID_ANSWER = BENSON.getAnswer().toString();
     private static final String VALID_SCORE = BENSON.getScore().toString();
+    private static final List<JsonAdaptedOption> VALID_OPTION = BENSON.getOptions().stream()
+            .map(JsonAdaptedOption::new)
+            .collect(Collectors.toList());
     private static final List<JsonAdaptedHint> VALID_HINT = BENSON.getHints().stream()
             .map(JsonAdaptedHint::new)
             .collect(Collectors.toList());
@@ -35,28 +38,30 @@ public class JsonAdaptedCardTest {
 
     @Test
     public void toModelType_invalidName_throwsIllegalValueException() {
-        JsonAdaptedCard card = new JsonAdaptedCard(INVALID_QUESTION, VALID_ANSWER, VALID_SCORE, VALID_HINT);
+        JsonAdaptedCard card = new JsonAdaptedCard(INVALID_QUESTION, VALID_ANSWER, VALID_SCORE, VALID_OPTION,
+                VALID_HINT);
         String expectedMessage = Question.MESSAGE_CONSTRAINTS;
         Assert.assertThrows(IllegalValueException.class, expectedMessage, card::toModelType);
     }
 
     @Test
     public void toModelType_nullName_throwsIllegalValueException() {
-        JsonAdaptedCard card = new JsonAdaptedCard(null, VALID_ANSWER, VALID_SCORE, VALID_HINT);
+        JsonAdaptedCard card = new JsonAdaptedCard(null, VALID_ANSWER, VALID_SCORE, VALID_OPTION, VALID_HINT);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Question.class.getSimpleName());
         Assert.assertThrows(IllegalValueException.class, expectedMessage, card::toModelType);
     }
 
     @Test
     public void toModelType_invalidAnswer_throwsIllegalValueException() {
-        JsonAdaptedCard card = new JsonAdaptedCard(VALID_QUESTION, INVALID_ANSWER, VALID_SCORE, VALID_HINT);
+        JsonAdaptedCard card = new JsonAdaptedCard(VALID_QUESTION, INVALID_ANSWER, VALID_SCORE, VALID_OPTION,
+                VALID_HINT);
         String expectedMessage = Answer.MESSAGE_CONSTRAINTS;
         Assert.assertThrows(IllegalValueException.class, expectedMessage, card::toModelType);
     }
 
     @Test
     public void toModelType_nullAnswer_throwsIllegalValueException() {
-        JsonAdaptedCard card = new JsonAdaptedCard(VALID_QUESTION, null, VALID_SCORE, VALID_HINT);
+        JsonAdaptedCard card = new JsonAdaptedCard(VALID_QUESTION, null, VALID_SCORE, VALID_OPTION, VALID_HINT);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Answer.class.getSimpleName());
         Assert.assertThrows(IllegalValueException.class, expectedMessage, card::toModelType);
     }
@@ -66,7 +71,7 @@ public class JsonAdaptedCardTest {
         List<JsonAdaptedHint> invalidHint = new ArrayList<>(VALID_HINT);
         invalidHint.add(new JsonAdaptedHint(INVALID_HINT));
         JsonAdaptedCard card = new JsonAdaptedCard(VALID_QUESTION, VALID_ANSWER,
-                VALID_SCORE, invalidHint);
+                VALID_SCORE, VALID_OPTION, invalidHint);
         Assert.assertThrows(IllegalValueException.class, card::toModelType);
     }
 

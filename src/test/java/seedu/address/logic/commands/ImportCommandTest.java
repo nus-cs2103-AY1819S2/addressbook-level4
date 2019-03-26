c62e412614461c9e36898d103d0638d62b01ec60
Yi Chong Ong <yichong96@gmail.com>
2019-03-26 15:16:04 +0800

    add import typical cards success test

--- src/test/java/seedu/address/logic/commands/ImportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -3,14 +3,19 @@ package seedu.address.logic.commands;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import org.junit.rules.ExpectedException;
-import org.junit.rules.TemporaryFolder;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.*;
+import seedu.address.storage.csvmanager.CsvFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalCards.getTypicalCardFolders;
+import static seedu.address.testutil.TypicalCards.getTypicalCards;
 
 public class ImportCommandTest {
     @Rule
@@ -28,7 +33,18 @@ public class ImportCommandTest {
     @Before
     public void setUp() throws Exception {
         file = new File("./Typical Cards.csv").getCanonicalFile();
-        
+
+    }
+
+    @Test
+    public void execute_importTypicalCards_success() throws Exception {
+        String filename = "Typical Cards.csv";
+        ImportCommand importCommand = new ImportCommand(new CsvFile(filename));
+
+        Model expectedModel = new ModelManager(getTypicalCardFolders(), new UserPrefs());
+        CommandResult commandResult = importCommand.execute(model, commandHistory);
+        String expectedMessage = String.format(ImportCommand.MESSAGE_SUCCESS, filename);
+        assertCommandSuccess(importCommand, model, commandHistory, expectedMessage, expectedModel);
     }
 
 

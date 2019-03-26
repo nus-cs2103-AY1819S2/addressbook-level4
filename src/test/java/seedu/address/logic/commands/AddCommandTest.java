75ebaa7f8bda6e10c59475ae4f5ed91eec243af5
dlqs <donaldleeqs@gmail.com>
2019-03-26 21:45:35 +0800

    add model stubs

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -197,6 +197,21 @@ public class AddCommandTest {
         }
 
         @Override
+        public boolean inReportDisplay() {
+            throw new AssertionError("This method should not be called.");
+        };
+
+        @Override
+        public void enterReportDisplay() {
+            throw new AssertionError("This method should not be called.");
+        };
+
+        @Override
+        public void exitReportDisplay() {
+            throw new AssertionError("This method should not be called.");
+        };
+
+        @Override
         public void exportCardFolders(Set<CardFolderExport> cardFolderExports, CsvFile csvFile) throws IOException {
             throw new AssertionError("This method should not be called.");
         }

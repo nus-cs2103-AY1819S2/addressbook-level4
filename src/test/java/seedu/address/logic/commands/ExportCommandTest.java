deb65e5d727bb3e8c0f5a2b3e07e5eff49fb948d
Yi Chong Ong <yichong96@gmail.com>
2019-03-26 14:36:43 +0800

    add export single card folder success and failure methods

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -2,25 +2,26 @@ package seedu.address.logic.commands;
 
 import java.io.File;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static seedu.address.testutil.TypicalCards.*;
+
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-import static seedu.address.testutil.TypicalCards.getTypicalCardFolders;
 
 public class ExportCommandTest {
 
@@ -34,18 +35,40 @@ public class ExportCommandTest {
     private CommandHistory commandHistory = new CommandHistory();
 
 
+
     @Test
-    public void execute_exportSingleValidCardFolderIndex_success() {
+    public void execute_exportSingleValidCardFolderIndex_success() throws Exception {
+        System.out.println(tmpFolder.getRoot());
         List<Integer> myList = new ArrayList<>(Arrays.asList(1));
         ExportCommand exportCommand = new ExportCommand(myList);
 
 
+        CommandResult commandResult = exportCommand.execute(model, commandHistory);
+        assertEquals(ExportCommand.MESSAGE_SUCCESS, commandResult.getFeedbackToUser());
     }
 
+    @After
+    public void deleteExportFile() {
+        File file = new File("./Typical Cards.csv");
+        if (file.exists()) {
+            boolean isDeleted = file.delete();
+            assert (isDeleted);
+        }
+    }
 
     @Test
-    public void testXYZ(File expected, File actual) throws IOException {
-        byte[] f1 = Files.readAllBytes(expected.toPath());
-        byte[] f2 = Files.readAllBytes(actual.toPath());
+    public void execute_exportSingleInvalidCardFolderIndex_failure() throws Exception {
+        List<Integer> myList = new ArrayList<>(Arrays.asList(1,3));
+        ExportCommand exportCommand = new ExportCommand(myList);
+
+        thrown.expect(CommandException.class);
+        thrown.expectMessage(ExportCommand.MESSAGE_MISSING_CARD_FOLDERS);
+        CommandResult commandResult = exportCommand.execute(model, commandHistory);
     }
+
+    @Test
+    public void execute_exportMultipleValidCardFolderIndex_success() throws Exception {
+
+    }
+
 }

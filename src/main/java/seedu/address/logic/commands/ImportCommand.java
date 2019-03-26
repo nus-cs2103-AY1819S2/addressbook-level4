e1a98e0522d561aa690c4280015e7a442198a537
Yi Chong Ong <yichong96@gmail.com>
2019-03-26 09:59:38 +0800

    add equals method to import command

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -52,7 +52,6 @@ public class ImportCommand extends Command {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || other instanceof ImportCommand // instanceof handles nulls
-                && csvFile.filename.equals()
-        }
+                && csvFile.filename.equals(((ImportCommand) other).csvFile);
     }
 }

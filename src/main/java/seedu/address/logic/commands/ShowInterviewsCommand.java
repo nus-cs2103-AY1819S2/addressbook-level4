482323b241fbbb4005a04db94660c821eff5e1f8
WeeSooJun <weesj96@gmail.com>
2019-03-26 23:48:45 +0800

    Update ShowInterviews

--- src/main/java/seedu/address/logic/commands/ShowInterviewsCommand.java
+++ src/main/java/seedu/address/logic/commands/ShowInterviewsCommand.java
@@ -11,11 +11,15 @@ import seedu.address.model.Model;
 public class ShowInterviewsCommand extends Command {
 
     public static final String COMMAND_WORD = "showInterviews";
-    public static final String MESSAGE_SUCCESS = "";
+    public static final String MESSAGE_INTERVIEWS_NOT_PRESENT = "Interviews not present";
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
-        return new CommandResult(MESSAGE_SUCCESS + model.getInterviews());
+        String result = model.getInterviews().toString();
+        if (result.isEmpty()) {
+            return new CommandResult(MESSAGE_INTERVIEWS_NOT_PRESENT);
+        }
+        return new CommandResult(result);
     }
 }

f2013216f610d58147d7436f997208aa876eb407
tieze zhang <e0196638@u.nus.edu>
2019-03-26 15:27:28 +0800

    minor updates

--- src/main/java/seedu/address/logic/commands/DeleteReviewCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteReviewCommand.java
@@ -7,11 +7,14 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_REVIEWTITLE;
 
 import java.util.List;
 
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.book.Book;
 import seedu.address.model.book.BookNameContainsExactKeywordsPredicate;
+import seedu.address.model.book.Review;
 
 /**
  * Deletes the review of the specified book with the specified title.
@@ -21,42 +24,37 @@ public class DeleteReviewCommand extends Command {
     public static final String COMMAND_WORD = "deleteReview";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the review of the specified book with the specified title. \n"
-            + "Parameters: "
-            + PREFIX_NAME + "BOOK NAME"
-            + PREFIX_REVIEWTITLE + "REVIEW TITLE"
-            + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + "Alice in Wonderland"
-            + PREFIX_REVIEWTITLE + "A great fairytale";
+            + ": Deletes the review identified by the index number used in the displayed review list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_DELETE_REVIEW_SUCCESS = "Deleted Review: %1$s";
 
-    private final BookNameContainsExactKeywordsPredicate bookPredicate;
+    private final Index targetIndex;
 
-    public DeleteReviewCommand(BookNameContainsExactKeywordsPredicate bookPredicate) {
-        this.bookPredicate = bookPredicate;
+    public DeleteReviewCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
     }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
-        model.updateFilteredBookList(bookPredicate);
-        List<Book> lastShownList = model.getFilteredBookList();
+        List<Review> lastShownList = model.getFilteredReviewList();
 
-        if (lastShownList.isEmpty()) {
-            throw new CommandException(MESSAGE_INVALID_BOOK);
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_REVIEW_DISPLAYED_INDEX);
         }
 
-        Book bookToDelete = lastShownList.get(0);
-        model.deleteBook(bookToDelete);
+        Review reviewToDelete = lastShownList.get(targetIndex.getZeroBased());
+        model.deleteReview(reviewToDelete);
         model.commitBookShelf();
-        return new CommandResult(String.format(MESSAGE_DELETE_REVIEW_SUCCESS, bookToDelete));
+        return new CommandResult(String.format(MESSAGE_DELETE_REVIEW_SUCCESS, reviewToDelete.getTitle().fullName));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof DeleteReviewCommand // instanceof handles nulls
-                && bookPredicate.equals(((DeleteReviewCommand) other).bookPredicate)); // state check
+                && targetIndex.equals(((DeleteReviewCommand) other).targetIndex)); // state check
     }
 }
---
+++ src/main/java/seedu/address/logic/parser/DeleteReviewCommandParser.java
@@ -0,0 +1,26 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DeleteReviewCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class DeleteReviewCommandParser implements Parser<DeleteReviewCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the DeleteCommand
+     * and returns an DeleteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public DeleteReviewCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new DeleteReviewCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteReviewCommand.MESSAGE_USAGE), pe);
+        }
+    }
+
+}

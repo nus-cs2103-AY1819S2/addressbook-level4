86105fa4197c91f0215e0ec187fd19766beed8c0
mmdlow <mmdlow@gmail.com>
2019-03-26 16:50:18 +0800

    Fix JavaDoc errors

--- src/main/java/seedu/address/model/hint/Hint.java
+++ src/main/java/seedu/address/model/hint/Hint.java
@@ -5,7 +5,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a Hint in the card folder.
- * Guarantees: immutable; question is valid as declared in {@link #isValidHintName(String)}
+ * Guarantees: immutable; is valid as declared in {@link #isValidHintName(String)}
  */
 public class Hint {
 

49de76f8001b557ed61a319e486cd57a55bbbf24
WeeSooJun <weesj96@gmail.com>
2019-03-26 23:47:06 +0800

    Update GenerateInterviewsCommand
    To include cases where interviews is already present

--- src/main/java/seedu/address/logic/commands/GenerateInterviewsCommand.java
+++ src/main/java/seedu/address/logic/commands/GenerateInterviewsCommand.java
@@ -3,7 +3,9 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.interviews.exceptions.InterviewsPresentException;
 
 /**
  * Generates an interview date list from existing persons in the addressbook.
@@ -12,12 +14,17 @@ public class GenerateInterviewsCommand extends Command {
 
     public static final String COMMAND_WORD = "generateInterviews";
     public static final String MESSAGE_SUCCESS = "Interviews generated";
+    public static final String MESSAGE_PRESENT = "Interviews already present";
 
     @Override
-    public CommandResult execute(Model model, CommandHistory history) {
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
-        model.generateInterviews();
-        model.commitAddressBook();
+        try {
+            model.generateInterviews();
+            model.commitAddressBook();
+        } catch (InterviewsPresentException e) {
+            throw new CommandException(MESSAGE_PRESENT);
+        }
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }

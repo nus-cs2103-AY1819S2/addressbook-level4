826e9b450f6e466579571fcda7d0c5623998d915
LiuXuanIan <liuxuan2472@gmail.com>
2019-03-26 20:34:33 +0800

    Add a command that creates a worklist, added AddWorkListCommandParserTest.

--- src/test/java/seedu/equipment/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/equipment/logic/commands/CommandTestUtil.java
@@ -3,6 +3,8 @@ package seedu.equipment.logic.commands;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.equipment.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.equipment.logic.parser.CliSyntax.PREFIX_ASSIGNEE;
+import static seedu.equipment.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.equipment.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.equipment.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.equipment.logic.parser.CliSyntax.PREFIX_PHONE;
@@ -40,6 +42,10 @@ public class CommandTestUtil {
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
     public static final String VALID_TAG_UNUSED = "unused"; // do not use this tag when creating a equipment
+    public static final String VALID_DATE_LISTA = "2019-02-12";
+    public static final String VALID_ASSIGNEE_LISTA = "Mei Yen";
+    public static final String VALID_DATE_LISTB = "2019-02-22";
+    public static final String VALID_ASSIGNEE_LISTB = "Rachel";
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
@@ -53,6 +59,10 @@ public class CommandTestUtil {
     public static final String SERIAL_NUMBER_DESC_BOB = " " + PREFIX_SERIALNUMBER + VALID_SERIAL_NUMBER_BOB;
     public static final String TAG_DESC_FRIEND = " " + PREFIX_TAG + VALID_TAG_FRIEND;
     public static final String TAG_DESC_HUSBAND = " " + PREFIX_TAG + VALID_TAG_HUSBAND;
+    public static final String DATE_DESC_LISTA = " " + PREFIX_DATE + VALID_DATE_LISTA;
+    public static final String ASSIGNEE_DESC_LISTA = " " + PREFIX_ASSIGNEE + VALID_ASSIGNEE_LISTA;
+    public static final String DATE_DESC_LISTB = " " + PREFIX_DATE + VALID_DATE_LISTB;
+    public static final String ASSIGNEE_DESC_LISTB = " " + PREFIX_ASSIGNEE + VALID_ASSIGNEE_LISTB;
 
     public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
     public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
@@ -61,6 +71,8 @@ public class CommandTestUtil {
     public static final String INVALID_SERIAL_NUMBER_DESC = " " + PREFIX_SERIALNUMBER
             + "A00$"; // '$' not allowed in serial number
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
+    public static final String INVALID_DATE_DESC = " " + PREFIX_DATE + "2019-02";
+    public static final String INVALID_ASSIGNEE_DESC = " " + PREFIX_ASSIGNEE + " ";
 
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";

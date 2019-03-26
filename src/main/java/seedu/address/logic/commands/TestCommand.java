3632123015cc3f061de51f37e7cd05ea4a075bcb
KerrynEer <kerryneer98@gmail.com>
2019-03-26 20:36:52 +0800

    Remove unnecessary lines

--- src/main/java/seedu/address/logic/commands/TestCommand.java
+++ src/main/java/seedu/address/logic/commands/TestCommand.java
@@ -2,14 +2,11 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.List;
-
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.EmptyCardFolderException;
 import seedu.address.model.Model;
-import seedu.address.model.ReadOnlyCardFolder;
 import seedu.address.model.card.Card;
 
 /**

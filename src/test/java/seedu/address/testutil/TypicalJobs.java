9fcd60f7c6e75cfabfba76eaa92f6a6eb430002e
Daniel <DanielDSSim@gmail.com>
2019-03-26 19:35:19 +0800

    Fix style errors

--- src/test/java/seedu/address/testutil/TypicalJobs.java
+++ src/test/java/seedu/address/testutil/TypicalJobs.java
@@ -1,43 +1,13 @@
 package seedu.address.testutil;
 
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_GENDER_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_GENDER_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_GRADE_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_GRADE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_INTERVIEWSCORES_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_INTERVIEWSCORES_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_JOBSAPPLY_ENGINEER;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_JOBSAPPLY_TRADER;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_KNOWNPROGLANG_PYTHON;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_MAJOR_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_MAJOR_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PASTJOB_PROFESSSOR;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_RACE_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_RACE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_SCHOOL_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_SCHOOL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_JOB_NAME_TEACHER;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Test;
-
 import seedu.address.model.AddressBook;
 import seedu.address.model.job.Job;
-import seedu.address.model.person.Person;
 
 
 /**
@@ -45,7 +15,7 @@ import seedu.address.model.person.Person;
  */
 public class TypicalJobs {
 
-    public static final Job TEACHER = new JobBuilder().withName("Teacher").build();
+    public static final Job ENGINEER = new JobBuilder().withName("Engineer").build();
 
     public static final Job PROFESSOR = new JobBuilder().withName("Professor").build();
 
@@ -55,9 +25,7 @@ public class TypicalJobs {
     public static final Job PASTOR = new JobBuilder().withName("Pastor").build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
-    public static final Job ENGINEER = new JobBuilder().withName("Engineer").build();
-
-    public static final Job CHEF = new JobBuilder().withName("Chef").build();
+    public static final Job TEACHER = new JobBuilder().withName(VALID_JOB_NAME_TEACHER).build();
 
     public static final String KEYWORD_MATCHING_PILOT = "Pilot"; // A keyword that matches PILOT
 
@@ -76,6 +44,6 @@ public class TypicalJobs {
     }
 
     public static List<Job> getTypicalJobs() {
-        return new ArrayList<>(Arrays.asList(TEACHER, PROFESSOR, SOLDIER, PASTOR, ENGINEER, ENGINEER, CHEF));
+        return new ArrayList<>(Arrays.asList(TEACHER, PROFESSOR, SOLDIER, PASTOR, ENGINEER, ENGINEER));
     }
 }

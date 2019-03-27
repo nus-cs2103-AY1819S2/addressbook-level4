ada9181eadf68de2bb068189ea7fc5fdf9575d95
Aaron Ong <40322372+ongaaron96@users.noreply.github.com>
2019-03-27 17:41:39 +0800

    Created new Slot class

---
+++ src/main/java/seedu/address/model/Slot.java
@@ -0,0 +1,75 @@
+package seedu.address.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+/**
+ * Represents a time slot in a day, from a start time to an optional end time.
+ */
+public class Slot {
+    private LocalDate date;
+    private LocalTime start;
+    private LocalTime end;
+
+    public Slot (LocalDate date, LocalTime start, LocalTime end) {
+        this.date = date;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Slot() {
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getStart() {
+        return start;
+    }
+
+    public LocalTime getEnd() {
+        return end;
+    }
+
+    /**
+     * Returns true if both slots have the same identity and data fields.
+     * This defines a stronger notion of equality between two slots.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other.getClass() == getClass())) {
+            return false;
+        }
+
+        Slot otherSlot = (Slot) other;
+        return otherSlot.getDate().equals(getDate())
+                && otherSlot.getStart().equals(getStart())
+                && Objects.equals(otherSlot.getEnd(), getEnd());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(date, start, end);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getDate() + ":\n")
+                .append("Date: ")
+                .append(getDate() + "\n")
+                .append("Start Time: ")
+                .append(getStart() + "\n");
+        if (end != null) {
+            builder.append("End Time: ").append(getEnd() + "\n");
+        }
+        return builder.toString();
+    }
+}

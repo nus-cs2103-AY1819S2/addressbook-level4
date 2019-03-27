9904957eb4ba0e959d171f92180d50d20f14f849
ptvrajsk <ptvrajsk@gmail.com>
2019-03-27 21:28:22 +0800

    (v1.3) Rename addressbook.json to pdfplusplus.json
    Renamed the addressbook.json file to pdfplusplus.json so that it
    displays correctly within the UI.
    
    Resolves(Partly): #152

--- src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
@@ -83,7 +83,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setPdfBookFilePath(Paths.get("addressbook.json"));
+        userPrefs.setPdfBookFilePath(Paths.get("pdfplusplus.json"));
         return userPrefs;
     }
 

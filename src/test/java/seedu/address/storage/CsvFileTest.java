fa05b57d06be6a7ff229d117e328ebe95f561e24
Yi Chong Ong <yichong96@gmail.com>
2019-03-27 23:31:02 +0800

    implement check export file correct test

--- src/test/java/seedu/address/storage/CsvFileTest.java
+++ src/test/java/seedu/address/storage/CsvFileTest.java
@@ -1,15 +1,19 @@
-package seedu.address.logic.commands;
+package seedu.address.storage;
 
 import org.junit.Test;
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.*;
 import seedu.address.storage.csvmanager.CsvFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static seedu.address.storage.csvmanager.CsvManager.DEFAULT_TEST_PATH;
 import static seedu.address.testutil.TypicalCards.getTypicalCardFolders;
 
 public class CsvFileTest {
@@ -17,18 +21,28 @@ public class CsvFileTest {
     private Model model = new ModelManager(new ArrayList<ReadOnlyCardFolder>(), new UserPrefs());
     private CommandHistory commandHistory = new CommandHistory();
     private final String TYPICAL_CARD_FOLDER = "Typical Cards.csv";
+    private final String TYPICAL_CARD_FOLDER_TEST = "Typical Cards test.csv";
 
     public CsvFileTest() throws IOException {
+        model.setTestCsvPath();
     }
 
 
     @Test
-    public void execute_importCsvFile_success() throws IOException {
-        List<ReadOnlyCardFolder> readOnlyCardFolders = getTypicalCardFolders();
+    public void execute_importCsvFile_correctFormat() throws IOException {
         Model expectedModel = new ModelManager(getTypicalCardFolders(), new UserPrefs());
-        model.setTestCsvPath();
         model.importCardFolders(new CsvFile(TYPICAL_CARD_FOLDER));
 
-        assertEqual
+        assertEquals(model, expectedModel);
+    }
+
+    @Test
+    void execute_exportCsvFile_correctFile() throws IOException {
+        model.exportCardFolders(new ArrayList<>(Arrays.asList(1)));
+        File testFile = new File(DEFAULT_TEST_PATH + TYPICAL_CARD_FOLDER_TEST);
+        assert(testFile.exists());
+        byte[] f1 = Files.readAllBytes(new File(DEFAULT_TEST_PATH + TYPICAL_CARD_FOLDER);
+        byte[] f2 = Files.readAllBytes(new File(DEFAULT_TEST_PATH + TYPICAL_CARD_FOLDER_TEST);
+        assertEquals(f1,f2);
     }
 }

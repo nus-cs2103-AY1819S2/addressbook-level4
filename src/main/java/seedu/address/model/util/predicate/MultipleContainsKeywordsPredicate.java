7a8ea918c31fbbb3b5a085711830149cc52e8cee
kthSim <kth_sim@hotmail.com>
2019-03-28 20:29:24 +0800

    allows AND for multipleContainsKeywordsPredicate

--- src/main/java/seedu/address/model/util/predicate/MultipleContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/util/predicate/MultipleContainsKeywordsPredicate.java
@@ -21,6 +21,17 @@ public class MultipleContainsKeywordsPredicate extends ContainsKeywordsPredicate
 
     @Override
     public boolean test(Patient patient) {
-        return predicateList.stream().anyMatch(pred -> pred.test(patient));
+        if (isAnd) {
+            return predicateList.stream().anyMatch(pred -> pred.test(patient));
+        } else {
+            return predicateList.stream().allMatch(pred -> pred.test(patient));
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+            || (other instanceof MultipleContainsKeywordsPredicate // instanceof handles nulls
+            && predicateList.equals(((MultipleContainsKeywordsPredicate) other).predicateList)); // state check
     }
 }

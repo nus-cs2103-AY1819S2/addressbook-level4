6b286082a223b114e7f64dbf92d44c63472ebfc4
cchj1995 <choo_jnthn@hotmail.com>
2019-03-28 21:26:39 +0800

    Fixed unused import

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -19,7 +19,6 @@ import seedu.address.logic.Logic;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.patient.Teeth;
 import seedu.address.model.patient.Patient;
 
 /**

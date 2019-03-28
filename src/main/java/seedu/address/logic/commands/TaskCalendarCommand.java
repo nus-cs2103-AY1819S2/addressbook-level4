5e3d1a0aad67da26df29ca7cab92c5eea0b109b6
cchj1995 <choo_jnthn@hotmail.com>
2019-03-28 16:27:59 +0800

    Update attributes and constructor for TaskCalendarCommand

--- src/main/java/seedu/address/logic/commands/TaskCalendarCommand.java
+++ src/main/java/seedu/address/logic/commands/TaskCalendarCommand.java
@@ -1,15 +1,42 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ENDDATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_STARTDATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TITLE;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.datetime.DateCustom;
 import seedu.address.model.task.Task;
 
 public class TaskCalendarCommand extends Command {
+
+    public static final String COMMAND_WORD = "taskcal";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + " DATE\n"
+            + "Example: " + COMMAND_WORD + " "
+            + "13-05-2019\n"
+            + "If no date is provided, the current date will be used\n";
+
+    public static final String MESSAGE_EXPAND_PERSON_SUCCESS = "Task Calendar displayed";
+
+    public final DateCustom dateInput;
+
+    public TaskCalendarCommand(DateCustom dateInput) {
+        requireNonNull(dateInput);
+        this.dateInput = dateInput;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TaskCalendarCommand // instanceof handles nulls
+                && dateInput.equals(((TaskCalendarCommand) other).dateInput)); // state check
+    }
 
 }

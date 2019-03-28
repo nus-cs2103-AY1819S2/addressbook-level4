ef5c624f1b630f9548b4d138e66b5ffe7db0a7c5
kthSim <kth_sim@hotmail.com>
2019-03-28 20:43:05 +0800

    Added Predicate to abstract ContainsKeywordsPredicate for grater control

--- src/main/java/seedu/address/model/util/predicate/ContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/util/predicate/ContainsKeywordsPredicate.java
@@ -3,6 +3,8 @@ package seedu.address.model.util.predicate;
 import java.util.List;
 import java.util.function.Predicate;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * Represents a Predicate with keywords to use with an associated attribute
  */
@@ -10,6 +12,7 @@ public abstract class ContainsKeywordsPredicate<T> implements Predicate<T> {
     protected final List<String> keywords;
     protected final boolean isIgnoreCase;
     protected final boolean isAnd;
+    //protected final Predicate<T> predicate;
 
     /**
      * Basic constructor for predicates.
@@ -20,6 +23,12 @@ public abstract class ContainsKeywordsPredicate<T> implements Predicate<T> {
         this.keywords = keywords;
         this.isIgnoreCase = isIgnoreCase;
         this.isAnd = isAnd;
+    }
+
+    @Override
+    public Predicate<T> and(Predicate<? super T> other) {
+        requireNonNull(other);
+        return (t) -> test(t) && other.test(t);
     }
 
     public ContainsKeywordsPredicate(List<String> keywords) {

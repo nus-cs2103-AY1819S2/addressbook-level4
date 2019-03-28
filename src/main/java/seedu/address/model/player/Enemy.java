3f9b95e0c15b38a9441dbefbfd796c4c89d4dc1f
lucydotc <lucy@u.nus.edu>
2019-03-28 15:51:48 +0800

    Update populateEnemyMap with ability to placeAircraftCarrier()

--- src/main/java/seedu/address/model/player/Enemy.java
+++ src/main/java/seedu/address/model/player/Enemy.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
+import seedu.address.model.battleship.Battleship;
+import seedu.address.model.battleship.Name;
+
+import seedu.address.model.battleship.Orientation;
 import seedu.address.model.cell.Coordinates;
+import seedu.address.model.cell.Status;
+
+
 
 /**
  * Represents an Enemy player in the game.
@@ -14,16 +21,19 @@ public class Enemy extends Player {
 
     private static final String xCoordinates = "abcdefghij";
     private static final Random randGen = new Random();
+    private static final Random randGen2 = new Random();
 
     private static final ArrayList allPossibleTargets = new ArrayList<String>();
+    private static ArrayList<String> allPossiblePopulateCoords = new ArrayList<String>();
     private final HashMap attackStatusHistory = new HashMap();
 
     /**
-     * Default constructor with fleet size 5.
+     * Default constructor with fleet size 8.
      */
     public Enemy() {
         super("EnemyPlayer", 5, 2, 1);
         fillAllPossibleTargets(this.getMapGrid().getMapSize());
+        populateEnemyMap(this.getMapGrid().getMapSize());
     }
 
     /**
@@ -63,5 +73,135 @@ public class Enemy extends Player {
         }
         java.util.Collections.shuffle(allPossibleTargets, randGen);
     }
+    /**
+     * fills allPossiblePopulateCoords ArrayList with
+     * all valid Strings of coordinates for populating with ships
+     */
+    private void fillallPossiblePopulateCoords(int mapSize) {
+        for (int xIndex = 0; xIndex < mapSize; xIndex++) {
+            for (int yIndex = 0; yIndex < mapSize; yIndex++) {
+                allPossiblePopulateCoords.add(Integer.toString(xIndex) + yIndex);
+            }
+        }
+        java.util.Collections.shuffle(allPossiblePopulateCoords, randGen2);
+    }
 
+    /**
+     * randomly puts ships from enemy deployedFleet to enemy mapGrid
+     * is part of enemy constructor
+     */
+    private void populateEnemyMap(int mapSize) {
+
+        ArrayList deployedFleet = this.getFleetContents();
+        int fleetSize = this.getFleetSize();
+
+        int numAirCraftCarrier = this.getFleet().getNumAircraftCarrier();
+        int numDestroyer = this.getFleet().getNumDestroyer();
+        int numCruiser = this.getFleet().getNumCruiser();
+
+        placeAirCraftCarrier(mapSize);
+        placeDestroyer(mapSize, numDestroyer, 3);
+        placeCruiser(mapSize, numCruiser, 2);
+    }
+
+    /**
+     * pulls a head coord from allPossiblePopulateCoords
+     * and justifies the coord before using it as a head for the aircraftCarrier
+     * before placing the aircraftCarrier onto enemy map
+     */
+    private void placeAirCraftCarrier(int mapSize) {
+        Orientation useOrientation = generateOrientation();
+        java.util.Collections.shuffle(allPossiblePopulateCoords, randGen);
+        String xyCoord = allPossiblePopulateCoords.get(0);
+        int x = Character.getNumericValue(xyCoord.charAt(0));
+        int y = Character.getNumericValue(xyCoord.charAt(1));
+
+        if (useOrientation.isHorizontal()) { //rectify X coord of head, Y doesn't matter (all Y will work)
+            x = justifyCoord(x, mapSize, 5);
+        } else {
+            y = justifyCoord(y, mapSize, 5);
+        }
+        Name currentBattleshipName = new Name("enemyAircraftCarrier");
+        Battleship currentBattleship = new Battleship(currentBattleshipName);
+        Coordinates currentBattleshipHead = new Coordinates(x, y);
+        //should have no error, since it is the first ship placed, and all map sizes have a max of 1 aircraft carrier
+        this.getMapGrid().putShip(currentBattleship, currentBattleshipHead, useOrientation);
+        markAsOccupied(x, y, 5, useOrientation);
+    }
+
+    private void placeDestroyer(int mapSize, int numDestroyer, int shipSize) {
+
+    }
+
+    private void placeCruiser (int mapSize, int numCruiser, int shipSize) {
+
+    }
+    /**
+     * removes all the occupied Coords from allPossiblePopulateCoords
+     * when a new ship is placed
+     */
+    private void markAsOccupied(int x, int y, int shipSize, Orientation useOrientation) {
+
+        //increase x
+        if (useOrientation.isHorizontal()) {
+            for (int i = 0; i < shipSize; i++) {
+                allPossiblePopulateCoords.remove(Integer.toString(x + i) + y);
+            }
+        }
+        //increase y
+        else {
+            for (int i = 0; i < shipSize; i++) {
+                allPossiblePopulateCoords.remove(Integer.toString(x) + (y + i));
+            }
+        }
+    }
+
+    /**
+     * justifies the head coord given to ensure
+     * ship to be placed can fit the map boundaries
+     */
+    private int justifyCoord(int coordToJustify, int mapSize, int shipSize) {
+        int lowerHalfceiling = (mapSize / 2) - 1;
+        switch (mapSize) {
+        case 6:
+            if (coordToJustify <= lowerHalfceiling) {
+                coordToJustify = 0;
+            }
+            else {
+                coordToJustify = 1;
+            }
+            break;
+        case 10:
+            lowerHalfceiling += 1;
+            if (coordToJustify > lowerHalfceiling) {
+                coordToJustify -= 4;
+            }
+            break;
+        default:
+            coordToJustify %= lowerHalfceiling + 1;
+        }
+        return coordToJustify;
+    }
+
+
+    /**
+     * randomly generates either a horizontal or vertical orientation
+     */
+    private Orientation generateOrientation() {
+        int seed = randGen2.nextInt();
+        int decision = seed % 2;
+        if (decision == 0) {
+            return new Orientation("horizontal");
+        }
+        else {
+            return new Orientation("vertical");
+        }
+    }
+
+    /**
+     * receives status of an attacked cell from Battle manager
+     */
+    private void receiveStatus(Status lastCellStatus) {
+        //stub
+    }
 }

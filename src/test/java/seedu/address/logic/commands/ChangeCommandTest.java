0448a72c79e4e906833a24e44ed31635c0878216
Liang Jun Au <au.liangjun@gmail.com>
2019-03-29 19:46:00 +0800

    Add ChangeCommandTest

---
+++ src/test/java/seedu/address/logic/commands/ChangeCommandTest.java
@@ -0,0 +1,70 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalCards.getTypicalCardFolders;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.testutil.TypicalIndexes;
+
+/**
+ * Contains integration tests (interaction with the Model) and junit tests for {@code ChangeCommand}.
+ */
+public class ChangeCommandTest {
+    private Model model = new ModelManager(getTypicalCardFolders(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalCardFolders(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void execute_validChangeCommandIntoFolder_success() {
+        expectedModel.exitFoldersToHome();
+        expectedModel.enterFolder(TypicalIndexes.INDEX_FIRST_CARD.getZeroBased());
+
+        model.exitFoldersToHome();
+        ChangeCommand changeCommand = new ChangeCommand(TypicalIndexes.INDEX_FIRST_CARD_FOLDER);
+        CommandResult expectedCommandResult = new CommandResult(
+                String.format(
+                        ChangeCommand.MESSAGE_ENTER_FOLDER_SUCCESS,
+                        TypicalIndexes.INDEX_FIRST_CARD_FOLDER.getOneBased()),
+                CommandResult.Type.ENTERED_FOLDER);
+        assertCommandSuccess(changeCommand, model, commandHistory, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_validChangeCommandExitFolder_success() {
+        expectedModel.exitFoldersToHome();
+
+        ChangeCommand changeCommand = new ChangeCommand();
+        CommandResult expectedCommandResult = new CommandResult(ChangeCommand.MESSAGE_EXIT_FOLDER_SUCCESS,
+                CommandResult.Type.EXITED_FOLDER);
+        assertCommandSuccess(changeCommand, model, commandHistory, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidEnterFolderCommand_failure() {
+        ChangeCommand changeCommand = new ChangeCommand(TypicalIndexes.INDEX_FIRST_CARD_FOLDER);
+        String expectedMessage = Messages.MESSAGE_INVALID_COMMAND_INSIDE_FOLDER;
+        assertCommandFailure(changeCommand, model, commandHistory, expectedMessage);
+
+        model.exitFoldersToHome();
+
+        changeCommand = new ChangeCommand(TypicalIndexes.INDEX_SECOND_CARD_FOLDER);
+        expectedMessage = Messages.MESSAGE_INVALID_FOLDER_DISPLAYED_INDEX;
+        assertCommandFailure(changeCommand, model, commandHistory, expectedMessage);
+    }
+
+    @Test
+    public void execute_invalidExitFolderCommand_failure() {
+        model.exitFoldersToHome();
+
+        ChangeCommand changeCommand = new ChangeCommand();
+        String expectedMessage = Messages.MESSAGE_INVALID_COMMAND_OUTSIDE_FOLDER;
+        assertCommandFailure(changeCommand, model, commandHistory, expectedMessage);
+    }
+}

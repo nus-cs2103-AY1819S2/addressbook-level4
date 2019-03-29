09246fea8a4eedcd2f0728934b19c8c1afd1f3ff
kthSim <kth_sim@hotmail.com>
2019-03-29 20:38:01 +0800

    Updated sample data with next of kin info

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -5,6 +5,8 @@ import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.datetime.DateCustom;
 import seedu.address.model.datetime.DateOfBirth;
 import seedu.address.model.dentist.Dentist;
+import seedu.address.model.nextofkin.NextOfKin;
+import seedu.address.model.nextofkin.NextOfKinRelation;
 import seedu.address.model.patient.Nric;
 import seedu.address.model.patient.Patient;
 import seedu.address.model.patient.Sex;
@@ -15,6 +17,8 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.task.Task;
 import seedu.address.model.task.Title;
+
+import static seedu.address.logic.parser.AddCommandParser.NONE_EMAIL;
 
 /**
  * Contains utility methods for populating {@code AddressBook} with sample data.
@@ -44,22 +48,36 @@ public class SampleDataUtil {
         return new Person[] {
             new Patient(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
-                null, new Nric("S1234567B"), new DateOfBirth("01-05-1800"), new Sex("M")),
+                null, new Nric("S1234567B"), new DateOfBirth("01-05-1800"), new Sex("M"),
+                new NextOfKin(new Name("Jon Yeoh"), new Phone("96969696"), new Email(NONE_EMAIL),
+                    new Address("Blk 30 Geylang Street 29, #06-40"), null, new NextOfKinRelation("Father"))),
             new Patient(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                null, new Nric("S1234567C"), new DateOfBirth("02-06-1800"), new Sex("F")),
+                null, new Nric("S1234567C"), new DateOfBirth("02-06-1800"), new Sex("F"),
+                new NextOfKin(new Name("Janice Yu"), new Phone("69696969"), new Email(NONE_EMAIL),
+                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), null, new NextOfKinRelation("Mother"))),
             new Patient(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                    null, new Nric("S1234567D"), new DateOfBirth("11-12-1800"), new Sex("F")),
+                    null, new Nric("S1234567D"), new DateOfBirth("11-12-1800"), new Sex("F"),
+                new NextOfKin(new Name("Charlie Oliveiro"), new Phone("91548629"), new Email(NONE_EMAIL),
+                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), null, new NextOfKinRelation("Brother"))),
             new Patient(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                    null, new Nric("S1234567E"), new DateOfBirth("30-05-1800"), new Sex("M")),
+                    null, new Nric("S1234567E"), new DateOfBirth("30-05-1800"), new Sex("M"),
+                new NextOfKin(new Name("Li Sheng Yao"), new Phone("84751234"), new Email(NONE_EMAIL),
+                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), null,
+                    new NextOfKinRelation("Stepmother"))),
             new Patient(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
-                    null, new Nric("S1234567F"), new DateOfBirth("31-01-1800"), new Sex("M")),
+                    null, new Nric("S1234567F"), new DateOfBirth("31-01-1800"), new Sex("M"),
+                new NextOfKin(new Name("Muhammad bin Ishmal"), new Phone("86415377"), new Email(NONE_EMAIL),
+                    new Address("53 Meyer Road #03-05"), null, new NextOfKinRelation("Grandfather"))),
             new Patient(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
-                    null, new Nric("S1234567G"), new DateOfBirth("01-12-1800"), new Sex("M"))
+                    null, new Nric("S1234567G"), new DateOfBirth("01-12-1800"), new Sex("M"),
+                new NextOfKin(new Name("Chris Balakrishnan"), new Phone("85169742"), new Email(NONE_EMAIL),
+                    new Address("Blk 45 Aljunied Street 85, #11-31"), null,
+                    new NextOfKinRelation("Father")))
         };
     }
 

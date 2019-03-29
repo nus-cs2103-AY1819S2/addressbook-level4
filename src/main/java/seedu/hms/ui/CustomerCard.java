cc85d090fd2159f9fb9e7de60e86514f5c38375f
Tejas2805 <tejasbhuwania2805@gmail.com>
2019-03-30 06:34:42 +0800

    Corrected error.

--- src/main/java/seedu/hms/ui/CustomerCard.java
+++ src/main/java/seedu/hms/ui/CustomerCard.java
@@ -49,10 +49,10 @@ public class CustomerCard extends UiPart<Region> {
         name.setText(customer.getName().toString());
         phone.setText("Phone: " + customer.getPhone().toString());
         dob.setText(customer.getDateOfBirth().toString().equals("")
-                ? "Date of Birth: Not provided" : "Date of Birth: " + customer.getDateOfBirth().value);
+            ? "Date of Birth: Not provided" : "Date of Birth: " + customer.getDateOfBirth().value);
         identificationNo.setText("Identification no: " + customer.getIdNum().value);
         address.setText(customer.getAddress().toString().equals("")
-                ? "Address: Not provided" : "Address: " +customer.getAddress().toString());
+            ? "Address: Not provided" : "Address: " + customer.getAddress().toString());
         email.setText("Email: " + customer.getEmail().toString());
         customer.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }

3a000ecb6c1e49474d876736b32fdbf2ad23b7ae
cchj1995 <choo_jnthn@hotmail.com>
2019-03-29 23:29:31 +0800

    Updated TaskAdd message usage

--- src/main/java/seedu/address/logic/commands/TaskAddCommand.java
+++ src/main/java/seedu/address/logic/commands/TaskAddCommand.java
@@ -4,7 +4,9 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_DATE_CLASH;
 import static seedu.address.commons.core.Messages.MESSAGE_TIME_CLASH;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ENDDATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ENDTIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STARTDATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STARTTIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TITLE;
 
 import seedu.address.logic.CommandHistory;
@@ -28,7 +30,9 @@ public class TaskAddCommand extends Command {
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_TITLE + "Teeth removal surgery "
             + PREFIX_STARTDATE + "10-11-2019 "
-            + PREFIX_ENDDATE + "22-12-2019 ";
+            + PREFIX_ENDDATE + "22-12-2019 "
+            + PREFIX_STARTTIME + "1100"
+            + PREFIX_ENDTIME + "1200";
 
 
     public static final String MESSAGE_SUCCESS = "New task added: %1$s";

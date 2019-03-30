349910ffa1b42c4ecc2da71285a332c96a7014e1
huichun66 <pewpewt97@gmail.com>
2019-03-30 20:17:10 +0800

    Modified some sample command messages.

--- src/main/java/seedu/address/commons/core/Commands.java
+++ src/main/java/seedu/address/commons/core/Commands.java
@@ -9,20 +9,24 @@ import java.util.List;
 public class Commands {
 
     public static final String ADD = "add";
-    public static final String ADD_REQUEST = "add request n/John Doe p/98765432";
-    public static final String ADD_REQUEST_MODE = "add 1 n/John Doe p/98765432";
-    public static final String ADD_HEALTH_WORKER = "add healthworker n/Daenerys Targaryen p/98765432";
-    public static final String ADD_HEALTH_WORKER_MODE = "add 2 n/Daenerys Targaryen p/98765432";
+    public static final String ADD_HEALTH_WORKER = "add healthworker n/Daenerys Targaryen i/S8974421C p/98765432 "
+            + "o/SGH s/GENERAL_PRACTICE s/ORTHOPAEDIC";
+    public static final String ADD_HEALTH_WORKER_MODE = "add 1 n/Daenerys Targaryen i/S8974421C p/98765432 "
+            + "o/SGH s/GENERAL_PRACTICE s/ORTHOPAEDIC";
+    public static final String ADD_REQUEST = "add request n/John Doe i/S8974421C p/98765432 "
+            + "a/123 Brick Road, #01-01 dt/01-01-2019 14:50 c/Diabetic c/Physiotherapy";
+    public static final String ADD_REQUEST_MODE = "add 2 n/John Doe i/S8974421C p/98765432 "
+            + "a/123 Brick Road, #01-01 dt/01-01-2019 14:50 c/Diabetic c/Physiotherapy";
     public static final String EDIT = "edit";
-    public static final String EDIT_REQUEST = "edit request n/John Doe p/98765432";
-    public static final String EDIT_REQUEST_MODE = "edit 1 n/John Doe";
-    public static final String EDIT_HEALTH_WORKER = "edit healthworker n/Daenerys Targaryen p/98765432";
-    public static final String EDIT_HEALTH_WORKER_MODE = "edit 2 n/Daenerys Targaryen p/98765432";
+    public static final String EDIT_HEALTH_WORKER = "edit healthworker n/Daenerys Targaryen";
+    public static final String EDIT_HEALTH_WORKER_MODE = "edit 1 n/Daenerys Targaryen";
+    public static final String EDIT_REQUEST = "edit request n/John Doe";
+    public static final String EDIT_REQUEST_MODE = "edit 2 n/John Doe";
     public static final String LIST = "list";
-    public static final String LIST_ALL_REQUEST = "list request";
-    public static final String LIST_ALL_REQUEST_MODE = "list 2";
     public static final String LIST_ALL_HEALTHWORKER = "list healthworker";
     public static final String LIST_ALL_HEALTHWORKER_MODE = "list 1";
+    public static final String LIST_ALL_REQUEST = "list request";
+    public static final String LIST_ALL_REQUEST_MODE = "list 2";
     public static final String ASSIGN = "assign";
     public static final String ASSIGN_REQUEST = "assign hw/1 r/2";
     public static final String COMPLETE = "complete";
@@ -33,13 +37,15 @@ public class Commands {
     public static final String CLEAR = "clr";
     public static final String EXPLAIN = "explain";
     public static final String HISTORY = "history";
+    public static final String STATISTICS = "statistics";
     public static final String HELP = "help";
     public static final String EXIT = "exit";
 
     public static List<String> getAllCommands() {
-        return Arrays.asList(ADD, ADD_REQUEST, ADD_REQUEST_MODE, ADD_HEALTH_WORKER, ADD_HEALTH_WORKER_MODE, EDIT,
-                EDIT_REQUEST, EDIT_REQUEST_MODE, EDIT_HEALTH_WORKER, EDIT_HEALTH_WORKER_MODE, LIST, LIST_ALL_REQUEST,
-                LIST_ALL_REQUEST_MODE, LIST_ALL_HEALTHWORKER, LIST_ALL_HEALTHWORKER_MODE, ASSIGN, ASSIGN_REQUEST,
-                COMPLETE, FILTER, FIND_REQUEST, SELECT, DELETE, CLEAR, EXPLAIN, HISTORY, HELP, EXIT);
+        return Arrays.asList(ADD, ADD_HEALTH_WORKER, ADD_HEALTH_WORKER_MODE, ADD_REQUEST, ADD_REQUEST_MODE, EDIT,
+                EDIT_HEALTH_WORKER, EDIT_HEALTH_WORKER_MODE, EDIT_REQUEST, EDIT_REQUEST_MODE, LIST,
+                LIST_ALL_HEALTHWORKER, LIST_ALL_HEALTHWORKER_MODE, LIST_ALL_REQUEST, LIST_ALL_REQUEST_MODE,
+                ASSIGN, ASSIGN_REQUEST, COMPLETE, FILTER, FIND_REQUEST, SELECT, DELETE, CLEAR, EXPLAIN, HISTORY,
+                STATISTICS, HELP, EXIT);
     }
 }

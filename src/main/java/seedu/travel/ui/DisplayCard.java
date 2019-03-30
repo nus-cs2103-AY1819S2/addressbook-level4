aa84777c1ad903ca2cfeac649e5697fd2a453ce2
chung-ming <chungming_tsen@outlook.com>
2019-03-30 10:55:44 +0800

    Add Rating and Year Charts

--- src/main/java/seedu/travel/ui/DisplayCard.java
+++ src/main/java/seedu/travel/ui/DisplayCard.java
@@ -1,22 +1,19 @@
 package seedu.travel.ui;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Logger;
 
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
-import seedu.travel.commons.core.LogsCenter;
 import seedu.travel.model.chart.Chart;
 import seedu.travel.model.place.CountryCode;
+import seedu.travel.model.place.Rating;
 
 /**
  * An UI component that displays information of a {@code Place}.
@@ -26,50 +23,91 @@ public class DisplayCard extends UiPart<Region> {
     private static final String FXML = "DisplayListCard.fxml";
 
     public final Chart chart;
-    private final Logger logger = LogsCenter.getLogger(getClass());
 
     @FXML
     private VBox cardPane;
+
     @FXML
     private BarChart countryBarChart;
+
+    @FXML
+    private PieChart ratingPieChart;
+
+    @FXML
+    private LineChart yearLineChart;
 
     @SuppressWarnings("unchecked")
     public DisplayCard(Chart chart) {
         super(FXML);
         this.chart = chart;
 
-        //final CategoryAxis xAxis = new CategoryAxis();
-        //final NumberAxis yAxis = new NumberAxis();
-        //final BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
-        //barChart.setTitle("Number of Places Visited for Each Country");
-        //xAxis.setLabel("Country");
-        //yAxis.setLabel("Number of Places");
+        Map<CountryCode, Integer> mapCountry = chart.getMapCountry();
+        createBarChart(mapCountry);
 
-        Map<CountryCode, Integer> responsesCountry = new HashMap<>();
+        Map<Rating, Double> mapRating = chart.getMapRating();
+        createPieChart(mapRating);
 
-        try {
-            FileReader fileReaderCountry = new FileReader("data/countryChart.json");
-            JsonReader jsonReaderCountry = new JsonReader(fileReaderCountry);
-            Gson gson = new Gson();
-            responsesCountry = gson.fromJson(jsonReaderCountry, HashMap.class);
-            fileReaderCountry.close();
-            jsonReaderCountry.close();
-        } catch (IOException e) {
-            logger.warning(e.getMessage());
+        Map<String, Integer> mapYear = chart.getMapYear();
+        createLineChart(mapYear);
+    }
+
+    /**
+     * Creates a bar chart
+     */
+    @SuppressWarnings("unchecked")
+    private void createBarChart(Map<CountryCode, Integer> mapCountry) {
+        XYChart.Series series = new XYChart.Series();
+        Object[] countries = mapCountry.keySet().toArray();
+
+        for (int i = 0; i < mapCountry.size(); i++) {
+            String countryName = (String) countries[i];
+            series.getData().add(new XYChart.Data(countryName, mapCountry.get(countryName)));
         }
 
-        XYChart.Series seriesCountry = new XYChart.Series();
-        seriesCountry.setName("Country");
-        Object[] countries = responsesCountry.keySet().toArray();
-        for (int i = 0; i < responsesCountry.size(); i++) {
-            String country = (String) countries[i];
-            seriesCountry.getData().add(new XYChart.Data(country, responsesCountry.get(country)));
-            logger.info(country + ", " + responsesCountry.get(country) + "\n");
-        }
-
-        //barChart.getData().add(seriesCountry);
         countryBarChart.setTitle("Number of Places Visited for Each Country");
-        countryBarChart.getData().add(seriesCountry);
+        countryBarChart.getYAxis().setLabel("Number of Places");
+        countryBarChart.getXAxis().setLabel("Country");
+        countryBarChart.setLegendVisible(false);
+        countryBarChart.getData().add(series);
+        countryBarChart.setStyle("-fx-padding: 45 10 45 10;");
+    }
+
+    /**
+     * Creates a pie chart
+     */
+    private void createPieChart(Map<Rating, Double> mapRating) {
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+        Object[] ratings = mapRating.keySet().toArray();
+
+        for (int i = 0; i < mapRating.size(); i++) {
+            String ratingName = (String) ratings[i];
+            pieChartData.add(new PieChart.Data(ratingName + " Stars", mapRating.get(ratingName)));
+        }
+
+        ratingPieChart.setTitle("Number of Places Visited for Each Rating Category");
+        ratingPieChart.setData(pieChartData);
+        ratingPieChart.setStyle("-fx-padding: 45 0 45 0;");
+    }
+
+    /**
+     * Creates a line chart
+     */
+    @SuppressWarnings("unchecked")
+    private void createLineChart(Map<String, Integer> mapYear) {
+        XYChart.Series series = new XYChart.Series();
+        Object[] years = mapYear.keySet().toArray();
+
+        for (int i = 0; i < mapYear.size(); i++) {
+            String year = (String) years[i];
+            series.getData().add(new XYChart.Data(year, mapYear.get(year)));
+        }
+
+        yearLineChart.setTitle("Number of Places Visited for Each Year");
+        yearLineChart.getYAxis().setLabel("Number of Places");
+        yearLineChart.getXAxis().setLabel("Year");
+        yearLineChart.setLegendVisible(false);
+        yearLineChart.getData().add(series);
+        yearLineChart.setStyle("-fx-padding: 45 10 45 10;");
     }
 
     @Override

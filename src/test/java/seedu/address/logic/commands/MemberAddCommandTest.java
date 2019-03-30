6881a80bbb1fb3642e20bd04563f77b210fa3b59
melkwx <melkwx@gmail.com>
2019-03-31 00:47:17 +0800

    checktyle errors

--- src/test/java/seedu/address/logic/commands/MemberAddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MemberAddCommandTest.java
@@ -242,7 +242,8 @@ public class MemberAddCommandTest {
         }
 
         @Override
-        public void updateActivityList() { throw new AssertionError("This method should not be called."); }
+        public void updateActivityList() {
+            throw new AssertionError("This method should not be called."); }
 
         @Override
         public boolean canUndoAddressBook() {
--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -8,7 +8,6 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.junit.Rule;
 import org.junit.Test;

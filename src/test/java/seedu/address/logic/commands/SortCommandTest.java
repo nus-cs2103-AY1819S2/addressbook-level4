31005e92ae4c2793ceaf9ca1b330ca173f8ae652
kthSim <kth_sim@hotmail.com>
2019-03-31 16:37:29 +0800

    Updated existing SortTests

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -35,23 +35,23 @@ public class SortCommandTest {
     private CommandHistory commandHistory = new CommandHistory();
 
     @Test
-    public void execute_sortParameters_orderDefault() throws ParseException {
-        execute_sortParameter("name", false, Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE));
-        execute_sortParameter("phone", false, Arrays.asList(DANIEL, ALICE, ELLE, FIONA, GEORGE, CARL, BENSON));
-        execute_sortParameter("address", false, Arrays.asList(DANIEL, ALICE, BENSON, GEORGE, FIONA, ELLE, CARL));
-        execute_sortParameter("email", false, Arrays.asList(ALICE, GEORGE, DANIEL, CARL, BENSON, FIONA, ELLE));
-        execute_sortParameter("nric", false, Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE));
-        execute_sortParameter("dob", false, Arrays.asList(FIONA, ALICE, CARL, GEORGE, ELLE, BENSON, DANIEL));
+    public void execute_sortPatientParameters_orderDefault() throws ParseException {
+        execute_sortPatientParameter("name", false, Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE));
+        execute_sortPatientParameter("phone", false, Arrays.asList(DANIEL, ALICE, ELLE, FIONA, GEORGE, CARL, BENSON));
+        execute_sortPatientParameter("address", false, Arrays.asList(DANIEL, ALICE, BENSON, GEORGE, FIONA, ELLE, CARL));
+        execute_sortPatientParameter("email", false, Arrays.asList(ALICE, GEORGE, DANIEL, CARL, BENSON, FIONA, ELLE));
+        execute_sortPatientParameter("nric", false, Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE));
+        execute_sortPatientParameter("dob", false, Arrays.asList(FIONA, ALICE, CARL, GEORGE, ELLE, BENSON, DANIEL));
     }
 
     @Test
-    public void execute_sortParameters_orderReverse() throws ParseException {
-        execute_sortParameter("name", true, Arrays.asList(GEORGE, FIONA, ELLE, DANIEL, CARL, BENSON, ALICE));
-        execute_sortParameter("phone", true, Arrays.asList(BENSON, CARL, GEORGE, FIONA, ELLE, ALICE, DANIEL));
-        execute_sortParameter("address", true, Arrays.asList(CARL, ELLE, FIONA, GEORGE, BENSON, ALICE, DANIEL));
-        execute_sortParameter("email", true, Arrays.asList(ELLE, FIONA, BENSON, CARL, DANIEL, GEORGE, ALICE));
-        execute_sortParameter("nric", true, Arrays.asList(GEORGE, FIONA, ELLE, DANIEL, CARL, BENSON, ALICE));
-        execute_sortParameter("dob", true, Arrays.asList(DANIEL, BENSON, ELLE, GEORGE, CARL, ALICE, FIONA));
+    public void execute_sortPatientParameters_orderReverse() throws ParseException {
+        execute_sortPatientParameter("name", true, Arrays.asList(GEORGE, FIONA, ELLE, DANIEL, CARL, BENSON, ALICE));
+        execute_sortPatientParameter("phone", true, Arrays.asList(BENSON, CARL, GEORGE, FIONA, ELLE, ALICE, DANIEL));
+        execute_sortPatientParameter("address", true, Arrays.asList(CARL, ELLE, FIONA, GEORGE, BENSON, ALICE, DANIEL));
+        execute_sortPatientParameter("email", true, Arrays.asList(ELLE, FIONA, BENSON, CARL, DANIEL, GEORGE, ALICE));
+        execute_sortPatientParameter("nric", true, Arrays.asList(GEORGE, FIONA, ELLE, DANIEL, CARL, BENSON, ALICE));
+        execute_sortPatientParameter("dob", true, Arrays.asList(DANIEL, BENSON, ELLE, GEORGE, CARL, ALICE, FIONA));
     }
 
     /**
@@ -62,11 +62,11 @@ public class SortCommandTest {
      * @param expectedList Expected Order
      * @throws ParseException
      */
-    public void execute_sortParameter (String parameter, boolean isReverse,
-                                       List<Person> expectedList) throws ParseException {
+    private void execute_sortPatientParameter(String parameter, boolean isReverse,
+                                              List<Person> expectedList) throws ParseException {
         String expectedMessage = String.format(SortCommand.MESSAGE_SUCCESS, parameter);
         Comparator<Patient> patientComparator = PatientComparator.getPatientComparator(parameter);
-        SortCommand command = new SortCommand(patientComparator, parameter, isReverse);
+        SortCommand command = new SortPatientCommand(patientComparator, parameter, isReverse);
         expectedModel.sortAddressBook(patientComparator, isReverse);
         expectedModel.commitAddressBook();
         assertCommandSuccess(command, model, commandHistory, expectedMessage, expectedModel);

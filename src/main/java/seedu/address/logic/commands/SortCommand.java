4092d78967265e23e2bb5f20a9fc80d6457685de
kthSim <kth_sim@hotmail.com>
2019-03-31 12:48:26 +0800

    Turned SortCommand into a abstract class with generics

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,35 +1,34 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.Comparator;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.patient.Patient;
 
 /**
  * Sorts data in accordance to the given parameter
  */
-public class SortCommand extends Command {
+public abstract class SortCommand<T> extends Command {
 
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-        + " : Sorts the displayed list of patients OR a patient's records according to the desired parameter.\n"
-        + "Parameters: [record] PARAMETER \n"
-        + "Example 1: " + COMMAND_WORD + " name\n"
-        + "Example 2: " + COMMAND_WORD + " record procedure\n";
+        + " : Sorts the displayed list of patients OR a patient's records according to the desired parameter and "
+        + "order.\n"
+        + "Parameters: PARAMETER ORDER\n"
+        + "Example 1: " + COMMAND_WORD + " name asce\n"
+        + "Example 2: " + COMMAND_WORD + " name desc\n";
 
     public static final String MESSAGE_SUCCESS = "List has been sorted by %1$s!";
 
-    public final Comparator<Patient> attrCompare;
+    public final Comparator<T> attrCompare;
     private final String paraType;
     private final boolean isReverse;
 
-    public SortCommand(Comparator<Patient> chosenCompare, String paraType, boolean sortOrder) {
+    public SortCommand(Comparator<T> chosenCompare, String paraType, boolean sortOrder) {
         requireNonNull(chosenCompare);
         this.attrCompare = chosenCompare;
         this.paraType = paraType;
@@ -39,18 +38,19 @@ public class SortCommand extends Command {
     /**
      * Sets default sortOrder to ascending
      */
-    public SortCommand(Comparator<Patient> chosenCompare, String paraType) {
+    public SortCommand(Comparator<T> chosenCompare, String paraType) {
         this(chosenCompare, paraType, false);
     }
 
     @Override
-    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+    public abstract CommandResult execute(Model model, CommandHistory history) throws CommandException;
+    /*{
         //TODO Implement sorting for records
         model.sortAddressBook(this.attrCompare, this.isReverse);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         model.commitAddressBook();
         return new CommandResult(String.format(MESSAGE_SUCCESS, paraType));
-    }
+    }*/
 
     @Override
     public boolean equals(Object other) {

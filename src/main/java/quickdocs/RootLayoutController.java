c809a9097004c2514e932020dbadcf101fae0572
simjiazhi <e0177123@u.nus.edu>
2019-03-31 13:03:19 +0800

    Removed unused import for rootlayoutcontroller

--- src/main/java/quickdocs/RootLayoutController.java
+++ src/main/java/quickdocs/RootLayoutController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;

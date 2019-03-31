e8cda9bfb30100239bde2c1305efff59994aa20d
kthSim <kth_sim@hotmail.com>
2019-03-31 13:25:25 +0800

    Created basic functions for SortRecordCommand

--- src/main/java/seedu/address/logic/commands/SortRecordCommand.java
+++ src/main/java/seedu/address/logic/commands/SortRecordCommand.java
@@ -1,17 +1,41 @@
 package seedu.address.logic.commands;
 
+import java.util.Comparator;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.record.Record;
-
-import java.util.Comparator;
-
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 /**
  * Sorts data in accordance to the given parameter
  */
 public class SortRecordCommand extends SortCommand<Record> {
 
+    public SortRecordCommand(Comparator<Record> chosenCompare, String paraType, boolean sortOrder) {
+        super(chosenCompare, paraType, sortOrder);
+    }
+
+    /**
+     * Sets default sortOrder to ascending
+     */
+    public SortRecordCommand(Comparator<Record> chosenCompare, String paraType) {
+        this(chosenCompare, paraType, false);
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+
+        return new CommandResult(String.format(MESSAGE_SUCCESS, paraType));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+            || (other instanceof SortRecordCommand
+            && attrCompare.equals(((SortRecordCommand) other).attrCompare)
+            && paraType.equals(((SortRecordCommand) other).paraType)
+            && isReverse == ((SortRecordCommand) other).isReverse);
+    }
 }
+

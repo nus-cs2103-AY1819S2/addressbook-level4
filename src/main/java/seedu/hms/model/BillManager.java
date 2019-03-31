abe96ded50825ea028f0420f7f26958dbb094d40
Tejas2805 <tejasbhuwania2805@gmail.com>
2019-04-01 03:21:52 +0800

    Corrected errors.

--- src/main/java/seedu/hms/model/BillManager.java
+++ src/main/java/seedu/hms/model/BillManager.java
@@ -52,7 +52,6 @@ public class BillManager implements BillModel {
         filteredReservations = new FilteredList<>(versionedHotelManagementSystem.getReservationList());
         roomTypeList = new FilteredList<>(versionedHotelManagementSystem.getRoomTypeList());
         filteredReservations.addListener(this::ensureSelectedReservationIsValid);
-
     }
 
     public BillManager() {

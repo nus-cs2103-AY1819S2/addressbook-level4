9e1b8cc843a858d5ea274032fffafd82b06b5c76
CHEN Wen <1155092000@link.cuhk.edu.hk>
2019-03-31 16:19:03 +0800

    fix a bug

--- src/main/java/seedu/address/logic/commands/ArchiveClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ArchiveClearCommand.java
@@ -33,4 +33,9 @@ public class ArchiveClearCommand extends Command {
     public boolean requiresArchiveList() {
         return true;
     }
+
+    @Override
+    public boolean requiresPinList() {
+        return false;
+    }
 }

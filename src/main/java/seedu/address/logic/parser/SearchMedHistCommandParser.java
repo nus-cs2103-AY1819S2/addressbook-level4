e0ed0354cd842451e65133b5366df147d1b14ccb
Liuyy99 <e0253712@u.nus.edu>
2019-03-31 10:30:23 +0800

    add parsers

--- src/main/java/seedu/address/logic/parser/SearchMedHistCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SearchMedHistCommandParser.java
@@ -1,4 +1,31 @@
 package seedu.address.logic.parser;
 
-public class SearchMedHistCommandParser {
+import java.util.Arrays;
+
+import seedu.address.logic.commands.SearchMedHistCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.medicalhistory.MedHistContainsKeywordsPredicate;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new SearchMedHistCommand object
+ */
+public class SearchMedHistCommandParser implements Parser<SearchMedHistCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the SearchMedHistCommand
+     * and returns an SearchMedHistCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public SearchMedHistCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, SearchMedHistCommand.MESSAGE_USAGE));
+        }
+
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        return new SearchMedHistCommand(new MedHistContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    }
 }

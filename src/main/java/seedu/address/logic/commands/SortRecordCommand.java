4d4aee46f3c2eacb0d1b39de0cf329868aebddaa
kthSim <kth_sim@hotmail.com>
2019-03-31 13:23:00 +0800

    Created empty file for SortRecordCommand that extends SortCommand

---
+++ src/main/java/seedu/address/logic/commands/SortRecordCommand.java
@@ -0,0 +1,17 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.record.Record;
+
+import java.util.Comparator;
+
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+/**
+ * Sorts data in accordance to the given parameter
+ */
+public class SortRecordCommand extends SortCommand<Record> {
+
+}

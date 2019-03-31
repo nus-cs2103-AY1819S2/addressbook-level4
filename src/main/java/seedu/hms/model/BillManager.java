79acbbc3d283962dc459f5563539c8e8735fd6fe
Tejas2805 <tejasbhuwania2805@gmail.com>
2019-04-01 03:39:32 +0800

    Corrected errors for checkStyle.

--- src/main/java/seedu/hms/model/BillManager.java
+++ src/main/java/seedu/hms/model/BillManager.java
@@ -52,7 +52,6 @@ public class BillManager implements BillModel {
         filteredReservations = new FilteredList<>(versionedHotelManagementSystem.getReservationList());
         roomTypeList = new FilteredList<>(versionedHotelManagementSystem.getRoomTypeList());
         filteredReservations.addListener(this::ensureSelectedReservationIsValid);
-        
     }
 
     public BillManager() {
@@ -133,15 +132,7 @@ public class BillManager implements BillModel {
     public ObservableList<Booking> getFilteredBookingList() {
         return filteredBookings;
     }
-
-    /**
-     * Returns an unmodifiable view of the list of {@code ServiceType} backed by the internal list of
-     * {@code versionedHotelManagementSystem}
-     */
-    public ObservableList<ServiceType> getServiceTypeList() {
-        return serviceTypeList;
-    }
-
+    
     //=========== Undo/Redo =================================================================================
 
     @Override

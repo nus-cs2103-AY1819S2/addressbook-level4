9894fcd897e9a049508c67b1753398a53ccb8348
alexchong711 <hongyun711@gmail.com>
2019-04-01 01:51:01 +0800

    tests for GRAD counted as valid and invalid semester

--- src/test/java/seedu/address/model/moduletaken/SemesterTest.java
+++ src/test/java/seedu/address/model/moduletaken/SemesterTest.java
@@ -21,21 +21,42 @@ public class SemesterTest {
     }
 
     @Test
-    public void isValidSemester() {
+    public void isValidSemesterForTakingModules() {
         // null semester number
         Assert.assertThrows(NullPointerException.class, () -> Semester.isValidSemesterForTakingModules(null));
 
         // invalid semester numbers
-        assertFalse(Semester.isValidSemesterForTakingModules("")); // empty string
-        assertFalse(Semester.isValidSemesterForTakingModules(" ")); // spaces only
-        assertFalse(Semester.isValidSemesterForTakingModules("S2")); // less than 3 numbers
-        assertFalse(Semester.isValidSemesterForTakingModules("Y3S4")); // non-numeric
-        assertFalse(Semester.isValidSemesterForTakingModules("Y6S2")); // alphabets within digits
-        assertFalse(Semester.isValidSemesterForTakingModules("Y1")); // spaces within digits
+        assertFalse(Semester.isValidSemesterForTakingModules(""));
+        assertFalse(Semester.isValidSemesterForTakingModules(" "));
+        assertFalse(Semester.isValidSemesterForTakingModules("S2"));
+        assertFalse(Semester.isValidSemesterForTakingModules("Y3S4"));
+        assertFalse(Semester.isValidSemesterForTakingModules("Y6S2"));
+        assertFalse(Semester.isValidSemesterForTakingModules("Y1"));
+        assertFalse(Semester.isValidSemester("GRAD"));
 
         // valid semester numbers
-        assertTrue(Semester.isValidSemesterForTakingModules("Y2S1")); // exactly 3 numbers
+        assertTrue(Semester.isValidSemesterForTakingModules("Y2S1"));
         assertTrue(Semester.isValidSemesterForTakingModules("Y1S1"));
-        assertTrue(Semester.isValidSemesterForTakingModules("Y4S2")); // long semester numbers
+        assertTrue(Semester.isValidSemesterForTakingModules("Y4S2"));
+    }
+
+    @Test
+    public void isValidSemester() {
+        // null semester number
+        Assert.assertThrows(NullPointerException.class, () -> Semester.isValidSemester(null));
+
+        // invalid semester numbers
+        assertFalse(Semester.isValidSemester(""));
+        assertFalse(Semester.isValidSemester(" "));
+        assertFalse(Semester.isValidSemester("S2"));
+        assertFalse(Semester.isValidSemester("Y3S4"));
+        assertFalse(Semester.isValidSemester("Y6S2"));
+        assertFalse(Semester.isValidSemester("Y1"));
+
+        // valid semester numbers
+        assertTrue(Semester.isValidSemester("Y2S1"));
+        assertTrue(Semester.isValidSemester("Y1S1"));
+        assertTrue(Semester.isValidSemester("Y4S2"));
+        assertTrue(Semester.isValidSemester("GRAD"));
     }
 }

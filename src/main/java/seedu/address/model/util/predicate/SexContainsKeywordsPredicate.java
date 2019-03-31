c5082d20afcaf9941ab38cd1ff57e6359e165e74
kthSim <kth_sim@hotmail.com>
2019-03-31 18:47:21 +0800

    Implemented functionality for SexContainsKeywordsPredicate

--- src/main/java/seedu/address/model/util/predicate/SexContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/util/predicate/SexContainsKeywordsPredicate.java
@@ -1,4 +1,45 @@
 package seedu.address.model.util.predicate;
 
-public class SexContainsKeywordsPredicate {
+import java.util.List;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.patient.Patient;
+
+
+public class SexContainsKeywordsPredicate extends ContainsKeywordsPredicate<Patient> {
+
+    public SexContainsKeywordsPredicate(List<String> keywords) {
+        super(keywords);
+    }
+
+    public SexContainsKeywordsPredicate(List<String> keywords, boolean isIgnoreCase, boolean isAnd) {
+        super(keywords, isIgnoreCase, isAnd);
+    }
+
+    @Override
+    public boolean test(Patient patient) {
+        if (!isIgnoreCase && !isAnd) {
+            return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordCaseSensitive(patient.getSex().getSex(), keyword));
+
+        } else if (isIgnoreCase && !isAnd) {
+            return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(patient.getSex().getSex(), keyword));
+
+        } else if (!isIgnoreCase && isAnd) {
+            return keywords.stream()
+                .allMatch(keyword -> StringUtil.containsWordCaseSensitive(patient.getSex().getSex(), keyword));
+
+        } else {
+            return keywords.stream()
+                .allMatch(keyword -> StringUtil.containsWordIgnoreCase(patient.getSex().getSex(), keyword));
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+            || (other instanceof SexContainsKeywordsPredicate // instanceof handles nulls
+            && keywords.equals(((SexContainsKeywordsPredicate) other).keywords)); // state check
+    }
 }

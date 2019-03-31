8967e8f0fc242bc1a747f5e7799cd63ea0a7e039
kthSim <kth_sim@hotmail.com>
2019-03-31 14:54:30 +0800

    added skeleton code for RecordComparator

--- src/main/java/seedu/address/logic/comparators/RecordComparator.java
+++ src/main/java/seedu/address/logic/comparators/RecordComparator.java
@@ -1,8 +1,30 @@
 package seedu.address.logic.comparators;
 
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.record.Record;
+
+import java.util.Comparator;
+
 /**
  * Contains comparators for each record attribute
  */
 public class RecordComparator {
 
+    public static Comparator<Record> getRecordComparator(String parameterType) throws ParseException {
+        Comparator<Record> recComp;
+
+        switch (parameterType.trim()) {
+
+        case "date":
+            break;
+
+        case "desc":
+            break;
+
+        default:
+            throw new ParseException("Unknown parameter type");
+        }
+
+        return recComp;
+    }
 }

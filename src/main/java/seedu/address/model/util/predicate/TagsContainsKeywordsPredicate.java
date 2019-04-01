043d2f12fabbb46d4941acced594cfe746d05143
kthSim <kth_sim@hotmail.com>
2019-04-01 18:20:23 +0800

    Implement TagsContainsKeywordsPredicate

--- src/main/java/seedu/address/model/util/predicate/TagsContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/util/predicate/TagsContainsKeywordsPredicate.java
@@ -1,4 +1,41 @@
 package seedu.address.model.util.predicate;
 
-public class TagsContainKeywordsPredicate {
+import java.util.List;
+
+import seedu.address.model.person.Person;
+
+/**
+ * Tests that a {@code Person}'s {@code Tag}s matches any of the keywords given.
+ */
+public class TagsContainsKeywordsPredicate extends ContainsKeywordsPredicate<Person> {
+
+    public TagsContainsKeywordsPredicate(List<String> keywords) {
+        super(keywords);
+    }
+
+    public TagsContainsKeywordsPredicate(List<String> keywords, boolean isIgnoreCase, boolean isAnd) {
+        super(keywords, isIgnoreCase, isAnd);
+    }
+
+    @Override
+    public boolean test(Person person) {
+        if (!isIgnoreCase && !isAnd) {
+            return keywords.stream()
+                .anyMatch(keyword -> person.getTags().stream().anyMatch(tag -> tag.getTagName().equals(keyword)));
+
+        } else if (isIgnoreCase && !isAnd) {
+            return keywords.stream()
+                .anyMatch(keyword -> person.getTags().stream().anyMatch(tag -> tag.getTagName()
+                    .toLowerCase().equals(keyword)));
+
+        } else if (!isIgnoreCase && isAnd) {
+            return keywords.stream()
+                .allMatch(keyword -> person.getTags().stream().anyMatch(tag -> tag.getTagName().equals(keyword)));
+
+        } else {
+            return keywords.stream()
+                .allMatch(keyword -> person.getTags().stream().anyMatch(tag -> tag.getTagName()
+                    .toLowerCase().equals(keyword)));
+        }
+    }
 }

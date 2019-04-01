48822bea3befa07c623991770233252f2cd7a097
dlqs <donaldleeqs@gmail.com>
2019-04-02 02:42:12 +0800

    refactor endcommand display text

--- src/main/java/seedu/knowitall/logic/commands/EndCommand.java
+++ src/main/java/seedu/knowitall/logic/commands/EndCommand.java
@@ -20,27 +20,15 @@ public class EndCommand extends Command {
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-switch(model.getState()) {
-    case State.IN_TEST:
-        model.endTestSession();
-        return new CommandResult(MESSAGE_END_TEST_SESSION_SUCCESS, CommandResult.Type.END_TEST_SESSION);
-    case State.IN_REPORT:
-        model.exitReportDisplay();
-        return new CommandResult(MESSAGE_END_REPORT_DISPLAY_SUCCESS, CommandResult.Type.EXITED_REPORT);
-    default: 
-        throw new CommandException(Messages.MESSAGE_INVALID_COMMAND_OUTSIDE_FULLSCREEN);
-}
-            throw new CommandException(Messages.MESSAGE_INVALID_COMMAND_OUTSIDE_FULLSCREEN);
+        switch(model.getState()) {
+            case IN_TEST:
+                model.endTestSession();
+                return new CommandResult(MESSAGE_END_TEST_SESSION_SUCCESS, CommandResult.Type.END_TEST_SESSION);
+            case IN_REPORT:
+                model.exitReportDisplay();
+                return new CommandResult(MESSAGE_END_REPORT_DISPLAY_SUCCESS, CommandResult.Type.EXITED_REPORT);
+            default:
+                throw new CommandException(Messages.MESSAGE_INVALID_COMMAND_OUTSIDE_FULLSCREEN);
         }
-
-        if (model.getState() == State.IN_TEST) {
-            model.endTestSession();
-            return new CommandResult(MESSAGE_END_TEST_SESSION_SUCCESS, CommandResult.Type.END_TEST_SESSION);
-        } else {
-            model.exitReportDisplay();
-            return new CommandResult(MESSAGE_END_REPORT_DISPLAY_SUCCESS, CommandResult.Type.EXITED_REPORT);
-        }
-
     }
-
 }

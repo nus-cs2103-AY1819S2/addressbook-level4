07b6ca2112ffbceeea7e30d763410acad321aac6
cchj1995 <choo_jnthn@hotmail.com>
2019-04-01 14:52:38 +0800

    Added constructor and isValidLinkedPatient method to LinkedPatient class

--- src/main/java/seedu/address/model/record/LinkedPatient.java
+++ src/main/java/seedu/address/model/record/LinkedPatient.java
@@ -3,5 +3,29 @@ package seedu.address.model.record;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+import seedu.address.model.patient.Nric;
+import seedu.address.model.patient.Patient;
+import seedu.address.model.person.Name;
+
+/**
+ * Represents a person linked to a task in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidLinkedPerson(String)}
+ */
 public class LinkedPatient {
+    public static final String MESSAGE_CONSTRAINTS = "Index of patient should be a positive integer and should be "
+                                                    + "a valid number next to the name of one of the patients in the "
+                                                    + "patient list\n"
+                                                    + "Example: pat/1 or pat/3";
+
+    public Name fullname;
+    public Nric nric;
+
+    public LinkedPatient(Name name, Nric nric) {
+        this.fullname = name;
+        this.nric = nric;
+    }
+
+    public static boolean isValidLinkedPatient(String nametest, String nrictest) {
+        return (Name.isValidName(nametest) && Nric.isValidNric(nrictest));
+    }
 }

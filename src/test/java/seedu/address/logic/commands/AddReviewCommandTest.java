a7f72f2cab3b2aa27a64ad93b7adffb537c9ee0c
tieze zhang <e0196638@u.nus.edu>
2019-04-01 09:30:17 +0800

    style check

--- src/test/java/seedu/address/logic/commands/AddReviewCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddReviewCommandTest.java
@@ -25,12 +25,13 @@ import seedu.address.testutil.ReviewBuilder;
 
 
 public class AddReviewCommandTest {
-    private static final CommandHistory EMPTY_COMMAND_HISTORY = new CommandHistory();
-    private Model model = new ModelManager(getTypicalBookShelf(), new UserPrefs());
-    private CommandHistory commandHistory = new CommandHistory();
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
+    private static final CommandHistory EMPTY_COMMAND_HISTORY = new CommandHistory();
+    private Model model = new ModelManager(getTypicalBookShelf(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
 
     @Test
     public void constructor_nullReview_throwsNullPointerException() {

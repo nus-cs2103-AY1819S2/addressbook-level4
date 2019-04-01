c16e43cb4d3edbb625142c31dbe8ea2930a4b09f
Ian Wu <iwle@u.nus.edu>
2019-04-01 09:52:10 +0800

    Update ListCommand
    Add newline.

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import java.util.Set;
 
 import java.util.stream.Collectors;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.battleship.Name;

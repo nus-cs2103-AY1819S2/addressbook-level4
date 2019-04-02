e9cb49be347a79cbf9948a3165ab4e7b826b45f8
kthSim <kth_sim@hotmail.com>
2019-04-02 16:41:47 +0800

    Changed implementation method of Procedure

--- src/main/java/seedu/address/model/record/Procedure.java
+++ src/main/java/seedu/address/model/record/Procedure.java
@@ -1,5 +1,9 @@
 package seedu.address.model.record;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
@@ -8,7 +12,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  * Guarantees: immutable; is valid as declared in {@link #isValidProcedure(String)}
  */
 public class Procedure {
-
+    private static final Set<String> procedureSet = new HashSet<>(Arrays.asList("surgery", "others"));
     public static final String MESSAGE_CONSTRAINTS = "Procedures should only contain alphanumeric characters, and it "
         + "should not be blank";
 
@@ -19,6 +23,7 @@ public class Procedure {
     public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
     public final String procedureType;
+    public final String typeDetails;
 
     /**
      * Constructs a {@code Procedure}
@@ -28,19 +33,33 @@ public class Procedure {
     public Procedure(String procedure) {
         requireNonNull(procedure);
         checkArgument(isValidProcedure(procedure), MESSAGE_CONSTRAINTS);
-        procedureType = procedure;
+        String[] strSplit = procedure.split("-");
+        this.procedureType = strSplit[0];
+        this.typeDetails = strSplit[1];
     }
 
     /**
      * Returns true if a given string is a valid procedure.
      */
     public static boolean isValidProcedure (String test) {
-        return test.matches(VALIDATION_REGEX);
+        String[] strSplit = test.split("-");
+        if (strSplit.length != 2) {
+            return false;
+        }
+        return procedureSet.contains(strSplit[0].toLowerCase().trim()) && strSplit[1].matches(VALIDATION_REGEX);
+    }
+
+    public String getProcedureType() {
+        return this.procedureType;
+    }
+
+    public String getTypeDetails() {
+        return this.procedureType;
     }
 
     @Override
     public String toString() {
-        return procedureType;
+        return procedureType + "-" + typeDetails;
     }
 
     @Override

6b5a155d39ee8093a86be4c8a72c51325d45a2f1
kthSim <kth_sim@hotmail.com>
2019-04-02 22:17:39 +0800

    StatWindow now populates with recordData

--- src/main/java/seedu/address/ui/StatWindow.java
+++ src/main/java/seedu/address/ui/StatWindow.java
@@ -1,15 +1,29 @@
 package seedu.address.ui;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.Logic;
 import seedu.address.model.patient.Patient;
+import seedu.address.model.record.Procedure;
+import seedu.address.model.record.Record;
 
 /**
  * Controller for a stat window page.
@@ -19,6 +33,7 @@ public class StatWindow extends UiPart<Stage> {
     //TODO: Proper statistic report formatting and data
     static final String FXML = "StatWindow.fxml";
     private static Patient toStat;
+    private final Map<String, Integer> recordNumbers = new HashMap<>();
 
     private final Logger logger = LogsCenter.getLogger(getClass());
 
@@ -39,6 +54,8 @@ public class StatWindow extends UiPart<Stage> {
     private Label address;
     @FXML
     private Label email;
+    @FXML
+    private TableView recordStatTable;
 
 
     public StatWindow(Stage root, Logic logic) {
@@ -75,10 +92,46 @@ public class StatWindow extends UiPart<Stage> {
         phone.setText(statPatient.getPhone().toString());
         address.setText(statPatient.getAddress().toString());
         email.setText(statPatient.getEmail().toString());
+
+        String[] procList = Procedure.PROCEDURE_LIST;
+        for (String procType: procList) {
+            recordNumbers.put(procType, 0);
+        }
+
+        List<Record> recordList = statPatient.getRecords();
+
+        for (Record rec: recordList) {
+            String tempProc = rec.getProcedure().getProcedureType().toLowerCase();
+            Integer count = recordNumbers.get(tempProc);
+            recordNumbers.put(tempProc, count + 1);
+        }
+        populateStatTable();
+
     }
 
     void show() {
         primaryStage.show();
+    }
+
+    private void populateStatTable() {
+        ObservableList<Pair<String, Integer>> data =
+            FXCollections.observableArrayList();
+
+        for (String procType: Procedure.PROCEDURE_LIST) {
+            data.add(new Pair<>(procType, recordNumbers.get(procType)));
+        }
+
+
+        TableColumn<Pair<String, Integer>, String> procCol = new TableColumn<>("Procedure");
+        procCol.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue().getKey()));
+
+        TableColumn<Pair<String, Integer>, Integer> numCol = new TableColumn<>("Number");
+        numCol.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue().getValue()));
+
+        recordStatTable.getColumns().addAll(procCol, numCol);
+        recordStatTable.getItems().addAll(data);
+        //recordStatTable.setMinHeight(200);
+
     }
 
     /**

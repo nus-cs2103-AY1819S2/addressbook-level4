31b9c579c4a0e02a6c44e75c8e2c9ff03c745032
siyingpoof <alvina.ongg@gmail.com>
2019-04-02 12:47:40 +0800

    fixed file for checkstyle error

--- src/main/java/seedu/address/model/person/DoctorNameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/DoctorNameContainsKeywordsPredicate.java
@@ -2,10 +2,11 @@
 
 package seedu.address.model.person;
 
-import seedu.address.commons.util.StringUtil;
-
 import java.util.List;
 import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+
 
 /**
  * Tests that a {@code Doctor}'s {@code Name} matches any of the keywords given.

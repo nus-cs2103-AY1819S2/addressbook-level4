dee9b4a412667dff4cf005a9461e37691df6b763
e0191632 <e0191632@u.nus.edu>
2019-04-03 01:30:55 +0800

    Add test in logicmanagertest

--- src/test/java/seedu/equipment/logic/LogicManagerTest.java
+++ src/test/java/seedu/equipment/logic/LogicManagerTest.java
@@ -105,6 +105,12 @@ public class LogicManagerTest {
         logic.getFilteredEquipment().remove(0);
     }
 
+    @Test
+    public void getFilteredClient_modifyList_throwsUnsupportedOperationException() {
+        thrown.expect(UnsupportedOperationException.class);
+        logic.getFilteredEquipment().remove(0);
+    }
+
     /**
      * Executes the command, confirms that no exceptions are thrown and that the result message is correct.
      * Also confirms that {@code expectedModel} is as specified.

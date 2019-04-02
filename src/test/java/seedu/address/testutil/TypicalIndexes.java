ef6c04593f480c09c974cfe8c1d8b3dc1745cd3c
sciffany <sciffany@gmail.com>
2019-04-02 16:28:37 +0800

    Include empty deck in typical indices

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -9,8 +9,10 @@ public class TypicalIndexes {
     public static final Index INDEX_FIRST_DECK = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_DECK = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_DECK = Index.fromOneBased(3);
+    public static final Index INDEX_EMPTY_DECK = INDEX_SECOND_DECK;
 
     public static final Index INDEX_FIRST_CARD = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_CARD = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_CARD = Index.fromOneBased(3);
+
 }

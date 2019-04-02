92a8545d17c2af70ccc53a2664fae8445c29833f
kthSim <kth_sim@hotmail.com>
2019-04-02 16:18:08 +0800

    Added new Prefix for procedure

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -18,6 +18,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_STARTTIME = new Prefix("st/");
     public static final Prefix PREFIX_ENDTIME = new Prefix("et/");
     public static final Prefix PREFIX_PRIORITY = new Prefix("pri/");
+    public static final Prefix PREFIX_PROCEDURE = new Prefix("pro");
     public static final Prefix PREFIX_SEX = new Prefix("sex/");
     public static final Prefix PREFIX_TAG = new Prefix("tag/");
     public static final Prefix PREFIX_STATUS = new Prefix("s/");

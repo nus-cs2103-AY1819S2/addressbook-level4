1213a78a647ebd3c6c54439b3f71ec3e082426bd
wSemis <debang@u.nus.edu>
2019-04-02 11:23:43 +0800

    Bug fix for orders

--- src/main/java/seedu/address/logic/commands/TaskSortCommand.java
+++ src/main/java/seedu/address/logic/commands/TaskSortCommand.java
@@ -7,6 +7,7 @@ import java.util.Comparator;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.datetime.DateCustom;
 import seedu.address.model.datetime.TimeCustom;
@@ -20,7 +21,8 @@ public class TaskSortCommand extends Command {
 
     public static final String COMMAND_WORD = "tasksort";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": sort tasks in place by start time, end time or priority. Sort ascendingly by default.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": sort tasks in place by start time,"
+                                                            + " end time or priority. Sort ascendingly by default.\n"
                                                             + "Parameters: sortField [sortOrder]\n"
                                                             + "Example 1: tasksort starttime\n"
                                                             + "Example 2: tasksort priority desc";
@@ -35,23 +37,31 @@ public class TaskSortCommand extends Command {
      * @param sortField start date, end date or priority
      * @param isAscending true for ascending order
      */
-    public TaskSortCommand(String sortField, boolean isAscending) throws CommandException{
+    public TaskSortCommand(String sortField, boolean isAscending) throws ParseException {
         requireNonNull(sortField);
 
         this.sortField = sortField;
         this.isAscending = isAscending;
-        this.sortComparator = createComparator(sortField);
+        this.sortComparator = createComparator(sortField, isAscending);
     }
 
-    private Comparator<Task> createComparator(String sortField) throws CommandException {
+    /**
+     *
+     * @param sortField
+     * @param isAscending
+     * @return the corresponding comparator to sortfield and is Ascending
+     * @throws CommandException
+     */
+    private Comparator<Task> createComparator(String sortField, boolean isAscending) throws ParseException {
         Comparator<Task> t;
+        int ascendingCoefficient = isAscending ? 1 : -1;
         if (sortField.equals("starttime")) {
             t = (Task o1, Task o2) -> {
                 DateCustom d1 = o1.getStartDate();
                 DateCustom d2 = o2.getStartDate();
                 TimeCustom t1 = o1.getStartTime();
                 TimeCustom t2 = o2.getStartTime();
-                return compareDateTime(d1, d2, t1, t2);
+                return compareDateTime(d1, d2, t1, t2) * ascendingCoefficient;
             };
         } else if (sortField.equals("endtime")) {
             t = (Task o1, Task o2) -> {
@@ -59,21 +69,29 @@ public class TaskSortCommand extends Command {
                 DateCustom d2 = o2.getEndDate();
                 TimeCustom t1 = o1.getEndTime();
                 TimeCustom t2 = o2.getEndTime();
-                return compareDateTime(d1, d2, t1, t2);
+                return compareDateTime(d1, d2, t1, t2) * ascendingCoefficient;
             };
         } else if (sortField.equals("priority")) {
             t = (Task o1, Task o2) -> {
                 Priority p1 = o1.getPriority();
                 Priority p2 = o2.getPriority();
-                return p1.compareTo(p2);
+                return p1.compareTo(p2) * ascendingCoefficient;
             };
         } else {
-            throw new CommandException(MESSAGE_SORT_TASK_WRONG_FILED);
+            throw new ParseException(MESSAGE_SORT_TASK_WRONG_FILED);
         }
 
         return t;
     }
 
+    /**
+     *
+     * @param d1 Date of task1
+     * @param d2 Date of task2
+     * @param t1 Time of task1
+     * @param t2 Time of task2
+     * @return 1 if task1 is later, -1 earlier, 0 the same time
+     */
     private int compareDateTime(DateCustom d1, DateCustom d2, TimeCustom t1, TimeCustom t2) {
         int dateCompare = d1.compareTo(d2);
         if (dateCompare == 0) {
@@ -86,9 +104,11 @@ public class TaskSortCommand extends Command {
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
 
+        model.sortTasks(sortComparator);
         model.updateFilteredTaskList(PREDICATE_SHOW_ALL_TASKS);
         model.commitAddressBook();
-        return new CommandResult(String.format(MESSAGE_SORT_TASK_SUCCESS, sortField, isAscending?"ascending":"descending"));
+        return new CommandResult(
+                String.format(MESSAGE_SORT_TASK_SUCCESS, sortField, isAscending ? "ascending" : "descending"));
     }
 
 

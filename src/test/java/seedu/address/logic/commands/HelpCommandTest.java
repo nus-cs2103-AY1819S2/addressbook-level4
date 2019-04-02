c659041ab5ff6836c8a9c6320719df6017f09056
sciffany <sciffany@gmail.com>
2019-04-02 17:29:37 +0800

    Remove imports from HelpCommandTest

--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -1,15 +1,11 @@
 package seedu.address.logic.commands;
 
-import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
-import static seedu.address.testutil.TypicalCards.getTypicalDeck;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
-import seedu.address.logic.StudyView;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 

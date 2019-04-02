0e71f3b292c23353ebae8907d68e01a57535d394
halcyoneee <ting00733@gmail.com>
2019-04-02 14:27:53 +0800

    Fix MESSAGE_USAGE and add MESSAGE_SUCCESS_ONE

--- src/main/java/seedu/address/logic/commands/quiz/QuizHintCommand.java
+++ src/main/java/seedu/address/logic/commands/quiz/QuizHintCommand.java
@@ -11,17 +11,17 @@ import seedu.address.model.modelmanager.Model;
 import seedu.address.model.modelmanager.QuizModel;
 
 /**
- * Displays hint of the current card
+ * Displays a list of hints of the current card
  */
 public class QuizHintCommand extends QuizCommand {
     public static final String COMMAND_WORD = "\\hint";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Force quits this session, "
-        + "only progress of attempted questions will still be saved.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Displays hint(s) for this card.\n"
         + "Example: " + COMMAND_WORD + "\n";
 
     public static final String MESSAGE_SUCCESS = "Hints: %1$s\n";
-    public static final String MESSAGE_EMPTY = "No hint for this card\n";
+    public static final String MESSAGE_SUCCESS_ONE = "Hint: %1$s\n";
+    public static final String MESSAGE_EMPTY = "No hint for this card.\n";
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
@@ -34,6 +34,10 @@ public class QuizHintCommand extends QuizCommand {
             throw new CommandException(MESSAGE_EMPTY);
         }
 
-        return new CommandResult(String.format(MESSAGE_SUCCESS, quizModel.getOpt().toString()));
+        if (hints.size() == 1) {
+            return new CommandResult(String.format(MESSAGE_SUCCESS_ONE, hints.toString()));
+        }
+
+        return new CommandResult(String.format(MESSAGE_SUCCESS, hints.toString()));
     }
 }
--- src/test/java/seedu/address/logic/commands/quiz/QuizHintCommandTest.java
+++ src/test/java/seedu/address/logic/commands/quiz/QuizHintCommandTest.java
@@ -3,15 +3,16 @@ package seedu.address.logic.commands.quiz;
 import static seedu.address.logic.commands.quiz.QuizCommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.quiz.QuizHintCommand.MESSAGE_EMPTY;
 import static seedu.address.logic.commands.quiz.QuizHintCommand.MESSAGE_SUCCESS;
+import static seedu.address.logic.commands.quiz.QuizHintCommand.MESSAGE_SUCCESS_ONE;
 import static seedu.address.testutil.CardBuilder.DEFAULT_OPTIONAL;
 import static seedu.address.testutil.TypicalLessons.LESSON_DEFAULT;
 import static seedu.address.testutil.TypicalSession.SESSION_DEFAULT_2;
 import static seedu.address.testutil.TypicalSession.SESSION_DEFAULT_2_ACTUAL;
 
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
@@ -35,15 +36,6 @@ public class QuizHintCommandTest {
     private QuizModel expectedModel = new QuizModelManager(new ManagementModelManager());
     private CommandHistory commandHistory = new CommandHistory();
 
-    @Before
-    public void setUp() {
-        Quiz quizExpected = new Quiz(SESSION_DEFAULT_2.generateSession(), SESSION_DEFAULT_2.getMode());
-        Quiz quizActual = new Quiz(SESSION_DEFAULT_2_ACTUAL.generateSession(), SESSION_DEFAULT_2_ACTUAL.getMode());
-
-        expectedModel.init(quizExpected, SESSION_DEFAULT_2);
-        actualModel.init(quizActual, SESSION_DEFAULT_2_ACTUAL);
-    }
-
     @Test
     public void execute_wrongModel_throwsCommandException() {
         Model model = new ManagementModelManager();
@@ -52,11 +44,43 @@ public class QuizHintCommandTest {
     }
 
     @Test
-    public void execute_withOpt_success() {
+    public void execute_withMoreThanOneOpt_success() {
+        Card oneOptCard = new CardBuilder().withOptionals("opt", "opt2").build();
+        CardSrsData cardSrsData = new CardSrsData(oneOptCard.hashCode(), 1, 1,
+            Instant.ofEpochMilli(123), false);
+        SrsCard srsCard = new SrsCard(oneOptCard, cardSrsData, LESSON_DEFAULT);
+        Session session = new SessionBuilder()
+            .withSrsCards(List.of(srsCard))
+            .build();
+
+        Session actualSession = new SessionBuilder()
+            .withSrsCards(List.of(srsCard))
+            .build();
+
+        expectedModel.init(new Quiz(session.generateSession(), session.getMode()), session);
         expectedModel.getNextCard();
+
+        actualModel.init(new Quiz(actualSession.generateSession(), actualSession.getMode()), actualSession);
         actualModel.getNextCard();
 
         CommandResult expectedCommandResult = new CommandResult(String.format(MESSAGE_SUCCESS,
+            Arrays.asList("opt", "opt2")));
+        assertCommandSuccess(new QuizHintCommand(), actualModel, commandHistory, expectedCommandResult, expectedModel);
+
+    }
+
+    @Test
+    public void execute_withOnlyOneOpt_success() {
+        Quiz quizExpected = new Quiz(SESSION_DEFAULT_2.generateSession(), SESSION_DEFAULT_2.getMode());
+        Quiz quizActual = new Quiz(SESSION_DEFAULT_2_ACTUAL.generateSession(), SESSION_DEFAULT_2_ACTUAL.getMode());
+
+        expectedModel.init(quizExpected, SESSION_DEFAULT_2);
+        actualModel.init(quizActual, SESSION_DEFAULT_2_ACTUAL);
+
+        expectedModel.getNextCard();
+        actualModel.getNextCard();
+
+        CommandResult expectedCommandResult = new CommandResult(String.format(MESSAGE_SUCCESS_ONE,
             DEFAULT_OPTIONAL));
         assertCommandSuccess(new QuizHintCommand(), actualModel, commandHistory, expectedCommandResult, expectedModel);
 

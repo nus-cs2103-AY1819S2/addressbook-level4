69ec1b2f16d31de0a342ff42f9c649a3505b04fd
s-tr <swchong25+github@gmail.com>
2019-04-03 20:04:08 +0800

    Remove unused imports

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -6,10 +6,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.Set;
-
 import seedu.address.model.cell.Cell;
-import seedu.address.model.tag.Tag;
 
 /**
  * A utility class for Cell.

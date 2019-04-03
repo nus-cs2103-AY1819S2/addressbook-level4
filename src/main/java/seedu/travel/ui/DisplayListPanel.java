3316902563ce61c5e17ccda8d27c3cff7294e31e
chung-ming <chungming_tsen@outlook.com>
2019-04-03 13:54:48 +0800

    Fix a bug where the mouse is unable to select on the scroll panel, and another bug where an empty chart is displayed even when the list of places is empty.

--- src/main/java/seedu/travel/ui/DisplayListPanel.java
+++ src/main/java/seedu/travel/ui/DisplayListPanel.java
@@ -5,14 +5,12 @@ import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
 import seedu.travel.commons.core.LogsCenter;
-import seedu.travel.model.chart.Chart;
+import seedu.travel.model.ChartBook;
 import seedu.travel.model.place.Place;
 
 /**
@@ -21,55 +19,27 @@ import seedu.travel.model.place.Place;
 public class DisplayListPanel extends UiPart<Region> {
     private static final String FXML = "DisplayListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(DisplayListPanel.class);
+    private ObservableList<Place> placeList;
 
     @FXML
-    private ListView<Chart> displayListView;
+    private ListView<ChartBook> displayListView;
 
     public DisplayListPanel(ObservableList<Place> placeList) {
         super(FXML);
-
-        if (placeList.size() == 0) {
-            return;
-        }
-        ObservableList<Chart> charts = FXCollections.observableArrayList();
-        charts.add(new Chart(placeList));
-        displayListView.setItems(charts);
+        this.placeList = placeList;
+        ObservableList<ChartBook> chartBooks = FXCollections.observableArrayList();
+        chartBooks.add(new ChartBook(placeList));
+        displayListView.setItems(chartBooks);
         displayListView.setCellFactory(listView -> new DisplayListViewCell());
-        System.out.println("generate outer");
 
         placeList.addListener(new ListChangeListener<Place>() {
             @Override
             public void onChanged(Change<? extends Place> c) {
-                if (placeList.size() == 0) {
-                    return;
-                }
-                ObservableList<Chart> charts = FXCollections.observableArrayList();
-                charts.add(new Chart(placeList));
-                displayListView.setItems(charts);
+                System.out.println("placeList changes detected");
+                ObservableList<ChartBook> chartBooks = FXCollections.observableArrayList();
+                chartBooks.add(new ChartBook(placeList));
+                displayListView.setItems(chartBooks);
                 displayListView.setCellFactory(listView -> new DisplayListViewCell());
-                System.out.println("generate inner 1");
-            }
-        });
-
-        charts.addListener(new ListChangeListener<Chart>() {
-            @Override
-            public void onChanged(Change<? extends Chart> c) {
-                if (placeList.size() == 0) {
-                    return;
-                }
-                ObservableList<Chart> charts = FXCollections.observableArrayList();
-                charts.add(new Chart(placeList));
-                displayListView.setItems(charts);
-                displayListView.setCellFactory(listView -> new DisplayListViewCell());
-                System.out.println("generate inner 2");
-            }
-        });
-
-        // disables selection in list view
-        displayListView.addEventFilter(MouseEvent.ANY, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                mouseEvent.consume();
             }
         });
     }
@@ -77,16 +47,16 @@ public class DisplayListPanel extends UiPart<Region> {
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Place} using a {@code DisplayCard}.
      */
-    class DisplayListViewCell extends ListCell<Chart> {
+    class DisplayListViewCell extends ListCell<ChartBook> {
         @Override
-        protected void updateItem(Chart chart, boolean empty) {
-            super.updateItem(chart, empty);
+        protected void updateItem(ChartBook chartBook, boolean empty) {
+            super.updateItem(chartBook, empty);
 
-            if (empty || chart == null) {
+            if (empty || chartBook == null || placeList.isEmpty()) {
                 setGraphic(null);
                 setText(null);
             } else {
-                setGraphic(new DisplayCard(chart).getRoot());
+                setGraphic(new DisplayCard(chartBook).getRoot());
             }
         }
     }

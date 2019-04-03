20171dd4048794da64d60fa3eac0746222e7b32c
kjiaxuan <kjiaxuan12@gmail.com>
2019-04-03 16:31:02 +0800

    Edited message usage in the two commands to correspond

--- src/main/java/seedu/finance/logic/commands/SortCommand.java
+++ src/main/java/seedu/finance/logic/commands/SortCommand.java
@@ -22,13 +22,13 @@ public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sorts the records in the list "
-            + "either by name, amount, date or category.\n"
+            + "either by name, amount, date or category based on the selected flag.\n"
             + "Parameters: FLAG \n"
-            + "Possible flags: "
-            + COMMAND_FLAG_NAME + ", "
-            + COMMAND_FLAG_AMOUNT + ", "
-            + COMMAND_FLAG_DATE + ", "
-            + COMMAND_FLAG_CATEGORY + "\n"
+            + "Flags: \n"
+            + COMMAND_FLAG_NAME + ": Sort records by name in lexicographical order\n"
+            + COMMAND_FLAG_AMOUNT + ": Sort records by amount in descending order\n"
+            + COMMAND_FLAG_DATE + ": Sort records by date from most recent to least recent\n"
+            + COMMAND_FLAG_CATEGORY + ": Sort records by category in lexicographical order\n"
             + "Example: " + COMMAND_WORD + " " + COMMAND_FLAG_NAME;
 
     public static final String MESSAGE_SUCCESS = "List is sorted.";

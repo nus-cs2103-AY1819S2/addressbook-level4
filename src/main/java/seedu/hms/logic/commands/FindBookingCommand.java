761f4c52c0c6f376f44366ce6fd6bd3c9bcb33a0
ZhangCX10032 <549191059@qq.com>
2019-04-04 15:17:43 +0800

    update the usage message of addCustomerCommand and findBookingCommand

--- src/main/java/seedu/hms/logic/commands/FindBookingCommand.java
+++ src/main/java/seedu/hms/logic/commands/FindBookingCommand.java
@@ -26,11 +26,14 @@ public class FindBookingCommand extends BookingCommand {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all bookings belonging to the selected "
         + "customer\n"
-        + "Parameters: CUSTOMER_IDENTIFICATION_NUMBER\n"
+        + "Parameters: "
+        + "[" + PREFIX_IDENTIFICATION_NUMBER + "CUSTOMER_IDENTIFICATION_NUMBER] "
+        + "[" + PREFIX_SERVICE + "SERVICE_TYPE] "
+        + "[" + PREFIX_TIMING + "TIME_RANGE]\n"
         + "Example: " + COMMAND_WORD + " "
         + PREFIX_IDENTIFICATION_NUMBER + "1234567 "
-        + "[" + PREFIX_SERVICE + "GYM] "
-        + "[" + PREFIX_TIMING + "10 - 11]";
+        + PREFIX_SERVICE + "GYM "
+        + PREFIX_TIMING + "10 - 11";
 
     private final Predicate<Booking> bookingPredicate;
 

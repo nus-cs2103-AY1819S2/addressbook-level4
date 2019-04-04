0f8d86dd51543a1595a06a5394b352f5828599d5
cchj1995 <choo_jnthn@hotmail.com>
2019-04-04 15:45:45 +0800

    Added imports for PatientInfoPanel .java and .fxml files

---
+++ src/main/java/seedu/address/ui/PatientInfoPanel.java
@@ -0,0 +1,24 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.patient.Patient;
+import seedu.address.model.person.Person;
+import seedu.address.model.record.Record;
+

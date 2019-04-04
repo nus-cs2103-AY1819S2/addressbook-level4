30d89314ff4a95430f8a49049b6f73132311642b
cchj1995 <choo_jnthn@hotmail.com>
2019-04-04 18:00:26 +0800

    Removed unused imports

--- src/main/java/seedu/address/logic/commands/TaskDoneCommand.java
+++ src/main/java/seedu/address/logic/commands/TaskDoneCommand.java
@@ -2,8 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 

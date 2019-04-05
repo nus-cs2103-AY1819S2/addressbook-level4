944106426936ede5a5e6237717cc0daec39a57d3
weixin-koh <e0201350@u.nus.edu>
2019-04-05 16:51:09 +0800

    Solve unscrollable ReviewListPanel bug

--- src/main/java/seedu/address/ui/ReviewCard.java
+++ src/main/java/seedu/address/ui/ReviewCard.java
@@ -29,7 +29,7 @@ public class ReviewCard extends UiPart<Region> {
     public final Review review;
 
     @FXML
-    private HBox cardPane;
+    private HBox cardPaneReview;
     @FXML
     private Label id;
     @FXML

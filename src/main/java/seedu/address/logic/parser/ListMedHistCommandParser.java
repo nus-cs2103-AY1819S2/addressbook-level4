a7a67ac30131ec0665647bcee5e06fcdde6ce10e
Liuyy99 <e0253712@u.nus.edu>
2019-04-05 21:32:52 +0800

    list medical history by multiple things

--- src/main/java/seedu/address/logic/parser/ListMedHistCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListMedHistCommandParser.java
@@ -1,10 +1,14 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE_OF_MEDHIST;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DOCTOR_ID;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PATIENT_ID;
 
 import seedu.address.logic.commands.ListMedHistCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+
+import java.time.LocalDate;
 
 /**
  * Parses input arguments and creates a new ListMedHistCommand object
@@ -16,27 +20,38 @@ public class ListMedHistCommandParser implements Parser<ListMedHistCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public ListMedHistCommand parse(String args) throws ParseException {
+
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(
-                args, PREFIX_PATIENT_ID);
+                args, PREFIX_PATIENT_ID, PREFIX_DOCTOR_ID, PREFIX_DATE_OF_MEDHIST);
 
-        if (!arePrefixPresent(argMultimap, PREFIX_PATIENT_ID) && argMultimap.getPreamble().isEmpty()) {
-            return new ListMedHistCommand();
+        String patientId = null;
+        String doctorId = null;
+        LocalDate date = null;
+
+        if (!argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListMedHistCommand.MESSAGE_USAGE));
         }
 
-        if (arePrefixPresent(argMultimap, PREFIX_PATIENT_ID) && argMultimap.getPreamble().isEmpty()) {
-            String patientId = argMultimap.getValue(PREFIX_PATIENT_ID).get();
-            return new ListMedHistCommand(patientId);
+        if (isPrefixPresent(argMultimap, PREFIX_PATIENT_ID)) {
+            patientId = argMultimap.getValue(PREFIX_PATIENT_ID).get();
         }
 
-        throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListMedHistCommand.MESSAGE_USAGE));
+        if (isPrefixPresent(argMultimap, PREFIX_DOCTOR_ID)) {
+            doctorId = argMultimap.getValue(PREFIX_DOCTOR_ID).get();
+        }
 
+        if (isPrefixPresent(argMultimap, PREFIX_DATE_OF_MEDHIST)) {
+            date = LocalDate.parse(argMultimap.getValue(PREFIX_DATE_OF_MEDHIST).get());
+        }
+
+        return new ListMedHistCommand(patientId, doctorId, date);
     }
 
     /**
      * Returns true if none of the prefix contains empty {@code Optional} values in the given
      * {@code ArgumentMultimap}.
      */
-    private static boolean arePrefixPresent(ArgumentMultimap argumentMultimap, Prefix prefix) {
+    private static boolean isPrefixPresent(ArgumentMultimap argumentMultimap, Prefix prefix) {
         return argumentMultimap.getValue(prefix).isPresent();
     }
 }

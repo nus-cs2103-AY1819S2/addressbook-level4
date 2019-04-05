1e7715457d95e5a65941ef3bdd3e9633b4848860
lucydotc <lucy@u.nus.edu>
2019-04-05 09:46:55 +0800

    Add logging msg

--- src/main/java/seedu/address/logic/commands/EndTurnCommand.java
+++ src/main/java/seedu/address/logic/commands/EndTurnCommand.java
@@ -3,9 +3,12 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.util.EnumSet;
+import java.util.logging.Logger;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.battle.AttackResult;
+import seedu.address.logic.battle.BattleManager;
 import seedu.address.logic.battle.state.BattleState;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -17,6 +20,7 @@ public class EndTurnCommand extends Command {
     public static final String COMMAND_WORD = "end";
     public static final String COMMAND_ALIAS1 = "endturn";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Ends the player's turn.\n";
+    private static final Logger logger = LogsCenter.getLogger(BattleManager.class);
 
     public EndTurnCommand() {
         setPermissibleStates(EnumSet.of(BattleState.PLAYER_ATTACK, BattleState.PLAYER_ATTACK_AFTER));
@@ -28,6 +32,8 @@ public class EndTurnCommand extends Command {
         // assert canExecuteIn(model.getBattleState());
         model.setBattleState(BattleState.ENEMY_ATTACK);
         AttackResult res = model.getBattle().takeComputerTurn();
+        logger.info(String.format("+++++++ENDTURNCOMMAND SAYS: LAST HIT ON: "
+                + " status: " + res.getStatus().toString()));
         model.getEnemyPlayer().receiveStatus(res.getStatus());
         model.updateUi();
         model.setBattleState(BattleState.PLAYER_ATTACK);

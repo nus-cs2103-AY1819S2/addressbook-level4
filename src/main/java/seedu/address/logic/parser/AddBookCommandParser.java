a41f64761ed9ce3c82952c8367fcf145006e62ec
0blivious <lyw@415309@gmail.com>
2019-04-06 18:42:04 +0800

    limit user input to 50 characters, limit each prefix to be supplied only once

--- src/main/java/seedu/address/logic/parser/AddBookCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddBookCommandParser.java
@@ -32,7 +32,8 @@ public class AddBookCommandParser implements Parser<AddBookCommand> {
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_AUTHOR, PREFIX_RATING, PREFIX_TAG);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_AUTHOR, PREFIX_RATING)
-                || !argMultimap.getPreamble().isEmpty()) {
+                || !argMultimap.getPreamble().isEmpty()
+                || !arePrefixesUnique(argMultimap, PREFIX_NAME, PREFIX_AUTHOR, PREFIX_RATING)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddBookCommand.MESSAGE_USAGE));
         }
 
@@ -54,4 +55,11 @@ public class AddBookCommandParser implements Parser<AddBookCommand> {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
+    /**
+     * Returns true if none of the prefixes appear more than once in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesUnique(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> (argumentMultimap.getNumberOfPrefix(prefix)==1));
+    }
 }

819eca256c3d7dea7fbc3163cf4899d65e55ca0f
beesaycheese <beesaycheese@users.noreply.github.com>
2019-04-06 23:46:58 +0800

    Change the switch case statements to an array

--- src/main/java/seedu/address/ui/MonthlyStatisticsCard.java
+++ src/main/java/seedu/address/ui/MonthlyStatisticsCard.java
@@ -1,5 +1,8 @@
 package seedu.address.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -32,50 +35,15 @@ public class MonthlyStatisticsCard extends UiPart<Region> {
     @FXML
     private Label totalMonthlyRevenue;
 
+    private final ArrayList<String> monthsInWords = new
+            ArrayList<>(List.of("JAN ", "FEB ", "MAR ", "APR ", "MAY ", "JUN ", "JUL ", "AUG ", "SEP ", "OCT ", "NOV ",
+            "DEC "));
+
     public MonthlyStatisticsCard(MonthlyRevenue monthlyRevenue) {
         super(FXML);
         this.monthlyRevenue = monthlyRevenue;
 
-        switch(Integer.parseInt(monthlyRevenue.getMonth().toString())) {
-        case 1:
-            month.setText("JAN ");
-            break;
-        case 2:
-            month.setText("FEB ");
-            break;
-        case 3:
-            month.setText("MAR ");
-            break;
-        case 4:
-            month.setText("APR ");
-            break;
-        case 5:
-            month.setText("MAY ");
-            break;
-        case 6:
-            month.setText("JUN ");
-            break;
-        case 7:
-            month.setText("JUL ");
-            break;
-        case 8:
-            month.setText("AUG ");
-            break;
-        case 9:
-            month.setText("SEP ");
-            break;
-        case 10:
-            month.setText("OCT ");
-            break;
-        case 11:
-            month.setText("NOV ");
-            break;
-        case 12:
-            month.setText("DEC ");
-            break;
-        default:
-            month.setText(this.monthlyRevenue.getMonth().toString());
-        }
+        month.setText(monthsInWords.get(Integer.parseInt(monthlyRevenue.getMonth().toString()) - 1));
         year.setText(this.monthlyRevenue.getYear().toString());
         totalMonthlyRevenue.setText("$ " + String.format("%.2f", this.monthlyRevenue.getTotalMonthlyRevenue()));
     }

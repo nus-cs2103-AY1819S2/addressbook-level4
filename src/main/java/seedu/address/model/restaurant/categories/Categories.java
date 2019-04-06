0deb5b785b11202b487cf45b175672dffac7295b
rbth7e5 <caijie96@gmail.com>
2019-04-06 19:39:20 +0800

    implement getCategoriesSuggestions method

--- src/main/java/seedu/address/model/restaurant/categories/Categories.java
+++ src/main/java/seedu/address/model/restaurant/categories/Categories.java
@@ -1,10 +1,18 @@
 package seedu.address.model.restaurant.categories;
 
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CUISINE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_OCCASION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PRICE_RANGE;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import javafx.scene.control.Label;
 
 import seedu.address.commons.util.StringUtil;
+import seedu.address.logic.parser.Prefix;
 
 /**
  * Encapsulates the categories of a restaurant.
@@ -40,6 +48,24 @@ public class Categories {
 
     public static Categories empty() {
         return new Categories(Optional.empty(), Optional.empty(), Optional.empty());
+    }
+
+
+    /**
+     * Gets a list of suggestions based on the entered prefix.
+     * @param prefix The prefix decides which type of category (Cuisine, Occasion, Price) are suggestions
+     *               retrieved from.
+     */
+    public static List<String> getCategoriesSuggestions(Prefix prefix) {
+        if (prefix.equals(PREFIX_CUISINE)) {
+            return Cuisine.SUGGESTED_CUISINES;
+        } else if (prefix.equals(PREFIX_OCCASION)) {
+            return Occasion.SUGGESTED_OCCASIONS;
+        } else if (prefix.equals(PREFIX_PRICE_RANGE)) {
+            return PriceRange.SUGGESTED_PRICE_RANGES;
+        } else {
+            return new ArrayList<>();
+        }
     }
 
     /**

ed124ad4bd125a51a90f226655d5a35aae9fafd0
beesaycheese <beesaycheese@users.noreply.github.com>
2019-04-08 02:13:11 +0800

    Adjust Daily, Monthly and Yearly revenue to subclasses that inherits revenue.

--- src/main/java/seedu/address/model/statistics/DailyRevenue.java
+++ src/main/java/seedu/address/model/statistics/DailyRevenue.java
@@ -1,74 +1,15 @@
 package seedu.address.model.statistics;
 
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-
-import java.util.Objects;
-
 /**
  * Daily Revenue which contains the day, month year and total revenue earned on that specific day.
  */
-public class DailyRevenue {
-
-    private final Day day;
-    private final Month month;
-    private final Year year;
-    private float totalDailyRevenue;
+public class DailyRevenue extends Revenue {
 
     /**
      * Every field must be present and not null.
      */
     public DailyRevenue(Day day, Month month, Year year, float totalDailyRevenue) {
-        requireAllNonNull(day, month, year, totalDailyRevenue);
-        this.day = day;
-        this.month = month;
-        this.year = year;
-        this.totalDailyRevenue = totalDailyRevenue;
-    }
-
-    /**
-     * Creates a daily revenue that accepts null as it's field.
-     * Mainly used for revenueCommand
-     */
-    public DailyRevenue(Day day, Month month, Year year) {
-        this.day = day;
-        this.month = month;
-        this.year = year;
-        this.totalDailyRevenue = 0;
-    }
-
-    /**
-     * Adds the bill's total bill into the revenue.
-     */
-    public void addToRevenue(Bill bill) {
-        totalDailyRevenue += bill.getTotalBill();
-    }
-
-    /**
-     * Gets the day.
-     */
-    public Day getDay() {
-        return day;
-    }
-
-    /**
-     * Gets the month.
-     */
-    public Month getMonth() {
-        return month;
-    }
-
-    /**
-     * Gets the year.
-     */
-    public Year getYear() {
-        return year;
-    }
-
-    /**
-     * Gets the total revenue for the day.
-     */
-    public float getTotalDailyRevenue() {
-        return totalDailyRevenue;
+        super(day, month, year, totalDailyRevenue);
     }
 
     /**
@@ -100,26 +41,20 @@ public class DailyRevenue {
 
         DailyRevenue otherDailyRevenue = (DailyRevenue) other;
         return isSameDailyRevenue(otherDailyRevenue)
-                && otherDailyRevenue.getTotalDailyRevenue() == getTotalDailyRevenue();
-    }
-
-    @Override
-    public int hashCode() {
-        // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(day, month, year, totalDailyRevenue);
+                && otherDailyRevenue.getTotalRevenue() == getTotalRevenue();
     }
 
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
-        builder.append("Total Revenue for ")
+        builder.append("Total Daily Revenue for ")
                 .append(getDay())
                 .append(".")
                 .append(getMonth())
                 .append(".")
                 .append(getYear())
                 .append(" : $")
-                .append(getTotalDailyRevenue());
+                .append(getTotalRevenue());
         return builder.toString();
     }
 

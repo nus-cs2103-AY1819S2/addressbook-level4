9b2d958889ae1254ebe5e3e08b9a8023c909b41c
skpai27 <skpai27@yahoo.com.sg>
2019-04-07 19:55:04 +0800

    implemented AddDefaultReviewCommandParser class

--- src/main/java/seedu/address/logic/parser/AddDefaultReviewCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddDefaultReviewCommandParser.java
@@ -1,4 +1,37 @@
 package seedu.address.logic.parser;
 
-public class AddDefaultReviewParser {
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.AddDefaultReviewCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates new AddDefaultReviewCommand object
+ */
+public class AddDefaultReviewCommandParser implements Parser<AddDefaultReviewCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddDefaultReviewCommand
+     * and returns an AddDefaultReviewCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddDefaultReviewCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddDefaultReviewCommand.MESSAGE_USAGE));
+        }
+
+        String[] indexAndIndicator = trimmedArgs.split("\\s+");
+
+        if (indexAndIndicator.length != 2) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddDefaultReviewCommand.MESSAGE_USAGE));
+        }
+
+        Index index = Index.fromOneBased(Integer.parseInt(indexAndIndicator[0]));
+        String defaultReviewIndicator = indexAndIndicator[1];
+
+        return new AddDefaultReviewCommand(index, defaultReviewIndicator);
+    }
 }

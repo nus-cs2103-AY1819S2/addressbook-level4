419cf8a3441366d0eaaebdc5716eee55936ca86f
skpai27 <skpai27@yahoo.com.sg>
2019-04-07 17:34:49 +0800

    created and implemented AddDefaultReviewCommand class, also AddDefaultReviewUtil which stores the constant strings for the default review entries

--- src/main/java/seedu/address/logic/commands/AddDefaultReviewCommand.java
+++ src/main/java/seedu/address/logic/commands/AddDefaultReviewCommand.java
@@ -1,4 +1,80 @@
 package seedu.address.logic.commands;
 
-public class AddDefaultReviewCommand {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.commands.AddDefaultReviewUtil.DEFAULT_ENTRIES;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.review.Entry;
+import seedu.address.model.review.Rating;
+import seedu.address.model.review.Review;
+
+/**
+ * Adds a default review to the specified restaurant in the food diary.
+ */
+public class AddDefaultReviewCommand extends Command {
+
+    public static final String COMMAND_WORD = "addR";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a default review tagged to a restaurant to the "
+            + "food diary.\n"
+            + "Parameters: "
+            + "INDEX (Must be a positive integer) "
+            + "(DEFAULT REVIEW NUMBER(integer from 1 - 5 inclusive)) \n"
+            + "Example: " + COMMAND_WORD + " "
+            + "2 \n"
+            + "will add a review of (Rating: 2.0, Entry: Below average, try not to go again) to the specified "
+            + " restaurant.";
+
+    public static final String MESSAGE_SUCCESS = "New review added to: %1$s";
+    public static final String MESSAGE_DUPLICATE_RESTAURANT = "This restaurant already exists in the food diary";
+
+    private final Index targetIndex;
+    private final String defaultReviewIndicator;
+
+    /**
+     * Creates an AddCommand to add the specified {@code Restaurant}
+     */
+    public AddDefaultReviewCommand(Index targetIndex, String defaultReviewIndicator) {
+        requireNonNull(defaultReviewIndicator);
+        requireNonNull(targetIndex);
+        this.targetIndex = targetIndex;
+        this.defaultReviewIndicator = defaultReviewIndicator;
+    }
+
+    public String getDefaultReviewIndicator() {
+        return defaultReviewIndicator;
+    }
+
+    public Index getTargetIndex() {
+        return targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+
+        Entry newEntry = new Entry(DEFAULT_ENTRIES.get(Integer.parseInt(defaultReviewIndicator) - 1));
+        Rating newRating = new Rating(defaultReviewIndicator);
+        Review reviewToAdd = new Review(newEntry, newRating);
+
+        CommandResult result = new AddReviewCommand(targetIndex, reviewToAdd).execute(model, history);
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AddDefaultReviewCommand // instanceof handles nulls
+                && defaultReviewIndicator.equals(((AddDefaultReviewCommand) other).getDefaultReviewIndicator())
+                && targetIndex.equals(((AddReviewCommand) other).getTargetIndex()));
+    }
+
+    @Override
+    public String toString() {
+        return this.getTargetIndex().getOneBased() + " " + this.getDefaultReviewIndicator();
+    }
 }

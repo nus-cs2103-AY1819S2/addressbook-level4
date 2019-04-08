429f52d59d823fe39fd07a2c0b9f9b82118b1529
ckjohn000 <ckjohn000@gmail.com>
2019-04-09 02:24:30 +0800

    add export deck command parser

--- src/main/java/seedu/address/logic/parser/ExportDeckCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ExportDeckCommandParser.java
@@ -1,4 +1,30 @@
 package seedu.address.logic.parser;
 
-public class ExportDeckCommandParser {
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.ExportDeckCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new ExportCommand object
+ */
+public class ExportDeckCommandParser implements Parser<ExportDeckCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ExportDeckCommand
+     * and returns an ExportDeckCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ExportDeckCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new ExportDeckCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    ExportDeckCommand.MESSAGE_USAGE), pe);
+        }
+    }
+
 }

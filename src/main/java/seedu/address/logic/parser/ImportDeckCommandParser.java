cfc0d7ae7f28010876f1fc03ed04d080f7895825
ckjohn000 <ckjohn000@gmail.com>
2019-04-09 02:24:12 +0800

    add import deck command parser

--- src/main/java/seedu/address/logic/parser/ImportDeckCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ImportDeckCommandParser.java
@@ -1,4 +1,28 @@
 package seedu.address.logic.parser;
 
-public class ImportDeckCommandParser {
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.logic.commands.ImportDeckCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new ImportDeckCommand object
+ */
+public class ImportDeckCommandParser implements Parser<ImportDeckCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ImportDeckCommand
+     * and returns an ImportDeckCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ImportDeckCommand parse(String args) throws ParseException {
+        String targetPath = args.trim();
+        if (targetPath.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ImportDeckCommand.MESSAGE_USAGE));
+        }
+
+        return new ImportDeckCommand(targetPath);
+    }
+
 }

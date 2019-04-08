fd1eb6ffabb21ff7ba1ef035560134e68e4d19d7
skpai27 <skpai27@yahoo.com.sg>
2019-04-08 15:21:20 +0800

    modularised AddDefaultReviewCommandParser

--- src/main/java/seedu/address/logic/parser/AddDefaultReviewCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddDefaultReviewCommandParser.java
@@ -1,10 +1,14 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.AddDefaultReviewUtil.DEFAULT_ENTRIES;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.AddDefaultReviewCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.review.Entry;
+import seedu.address.model.review.Rating;
+import seedu.address.model.review.Review;
 
 /**
  * Parses input arguments and creates new AddDefaultReviewCommand object
@@ -24,21 +28,52 @@ public class AddDefaultReviewCommandParser implements Parser<AddDefaultReviewCom
 
         String[] indexAndIndicator = trimmedArgs.split("\\s+");
 
-        //checks if there are NOT 2 arguments, this command must have 2 arguments only.
+        checkForTwoArguments(indexAndIndicator);
+        checkForIntegerArguments(indexAndIndicator);
+        checkForValidDefaultIndex(indexAndIndicator);
+
+        Index index;
+        int defaultReviewIndicatorInt;
+
+        index = Index.fromOneBased(Integer.parseInt(indexAndIndicator[0]));
+        defaultReviewIndicatorInt = Integer.parseInt(indexAndIndicator[1]);
+
+        Rating newRating = new Rating(String.valueOf(defaultReviewIndicatorInt));
+        Entry newEntry = new Entry(DEFAULT_ENTRIES.get(defaultReviewIndicatorInt - 1));
+        Review newReview = new Review(newEntry, newRating);
+
+        return new AddDefaultReviewCommand(index, newReview);
+    }
+
+    /**
+     * Checks that the user input only two arguments.
+     */
+    private void checkForTwoArguments(String[] indexAndIndicator) throws ParseException {
         if (indexAndIndicator.length != 2) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddDefaultReviewCommand.MESSAGE_USAGE));
         }
-        Index index;
-        String defaultReviewIndicator;
-        //checks if the 2 arguments are integers
+    }
+
+    /**
+     * Checks that the user input arguments are integers.
+     */
+    private void checkForIntegerArguments(String[] indexAndIndicator) throws ParseException {
         if (!indexAndIndicator[0].matches("[1-9]\\d*") || !indexAndIndicator[1].matches("[1-9]\\d*")) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddDefaultReviewCommand.MESSAGE_USAGE));
         }
-        index = Index.fromOneBased(Integer.parseInt(indexAndIndicator[0]));
-        defaultReviewIndicator = indexAndIndicator[1];
+    }
 
-        return new AddDefaultReviewCommand(index, defaultReviewIndicator);
+    /**
+     * Checks that the input default review index is between 1 to 5 inclusive.
+     */
+    private void checkForValidDefaultIndex(String[] indexAndIndicator) throws ParseException {
+        int defaultReviewIndicatorInt = Integer.parseInt(indexAndIndicator[1]);
+
+        if (defaultReviewIndicatorInt > 5 || defaultReviewIndicatorInt < 1) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddDefaultReviewCommand.MESSAGE_USAGE));
+        }
     }
 }

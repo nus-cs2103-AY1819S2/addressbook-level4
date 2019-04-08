7690b1416f3df055210f17153e405e65d361268f
Wklee96 <lweikang96@gmail.com>
2019-04-08 18:36:38 +0800

    removed unused static var

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -1,8 +1,5 @@
 package seedu.address.ui;
 
-import static java.util.Objects.requireNonNull;
-
-import java.net.URL;
 import java.util.logging.Logger;
 
 import javafx.application.Platform;
@@ -10,7 +7,6 @@ import javafx.fxml.FXML;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.NoInternetException;
 import seedu.address.commons.util.WebUtil;
@@ -21,8 +17,6 @@ import seedu.address.model.restaurant.Weblink;
  */
 public class BrowserPanel extends UiPart<Stage> {
 
-    public static final URL DEFAULT_PAGE =
-            requireNonNull(MainApp.class.getResource(FXML_FILE_FOLDER + "default.html"));
     public static final String SEARCH_PAGE_URL = "https://se-education.org/dummy-search-page/?name=";
 
     private static final String FXML = "BrowserPanel.fxml";

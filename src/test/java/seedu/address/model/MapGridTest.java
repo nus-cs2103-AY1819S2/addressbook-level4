fe654d2b9053ab9b4f45841261df22a43bff1a91
erjillsison <erjillsison@gmail.com>
2019-04-08 16:55:42 +0800

    Update MapGridTest.java

--- src/test/java/seedu/address/model/MapGridTest.java
+++ src/test/java/seedu/address/model/MapGridTest.java
@@ -2,19 +2,14 @@ package seedu.address.model;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import static seedu.address.testutil.SizeTenMapGrid.getSizeTenMapGrid;
-import static seedu.address.testutil.TypicalPersons.ALICE;
-
-import java.util.Collections;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
-import javafx.beans.InvalidationListener;
-import javafx.beans.property.SimpleIntegerProperty;
 
 import seedu.address.model.battleship.Battleship;
 import seedu.address.model.battleship.Orientation;
@@ -50,45 +45,13 @@ public class MapGridTest {
     }
 
     @Test
-    public void constructor() {
-        assertEquals(Collections.emptyList(), mapGrid.getPersonList());
-    }
+    public void equals() {
+        MapGrid firstMapGrid = getSizeTenMapGrid();
+        MapGrid sameMapGrid = new MapGrid(firstMapGrid);
 
-    @Test
-    public void resetData_null_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        mapGrid.resetData(null);
-    }
+        assertEquals(firstMapGrid, sameMapGrid);
 
-    @Test
-    public void resetData_withValidReadOnlyAddressBook_replacesData() {
-        MapGrid newData = new MapGrid();
-        mapGrid.resetData(newData);
-        assertEquals(newData, mapGrid);
-    }
-
-    @Test
-    public void getPersonList_modifyList_throwsUnsupportedOperationException() {
-        thrown.expect(UnsupportedOperationException.class);
-        mapGrid.getPersonList().remove(0);
-    }
-
-    @Test
-    public void addListener_withInvalidationListener_listenerAdded() {
-        SimpleIntegerProperty counter = new SimpleIntegerProperty();
-        InvalidationListener listener = observable -> counter.set(counter.get() + 1);
-        mapGrid.addListener(listener);
-        mapGrid.addPerson(ALICE);
-        assertEquals(1, counter.get());
-    }
-
-    @Test
-    public void removeListener_withInvalidationListener_listenerRemoved() {
-        SimpleIntegerProperty counter = new SimpleIntegerProperty();
-        InvalidationListener listener = observable -> counter.set(counter.get() + 1);
-        mapGrid.addListener(listener);
-        mapGrid.removeListener(listener);
-        mapGrid.addPerson(ALICE);
-        assertEquals(0, counter.get());
+        sameMapGrid.putShip(new Battleship(), new Coordinates(0, 0), new Orientation("vertical"));
+        assertNotEquals(firstMapGrid, sameMapGrid);
     }
 }

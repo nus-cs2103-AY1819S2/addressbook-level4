c120d504805ced300382a6f677f88a525ad62ba4
halcyoneee <ting00733@gmail.com>
2019-04-08 21:24:03 +0800

    Skeleton for systemtests

--- src/test/java/seedu/address/TestApp.java
+++ src/test/java/seedu/address/TestApp.java
@@ -1,18 +1,24 @@
 package seedu.address;
 
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
 
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-
 import seedu.address.commons.core.Config;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.lesson.Lesson;
 import seedu.address.model.lesson.LessonList;
 import seedu.address.model.modelmanager.ManagementModel;
 import seedu.address.model.modelmanager.ManagementModelManager;
+import seedu.address.model.modelmanager.QuizModel;
+import seedu.address.model.modelmanager.QuizModelManager;
 import seedu.address.model.user.User;
+import seedu.address.storage.LessonListStorage;
 import seedu.address.storage.UserPrefsStorage;
+import seedu.address.storage.UserStorage;
 import seedu.address.testutil.TestUtil;
 
 /**
@@ -21,13 +27,22 @@ import seedu.address.testutil.TestUtil;
  */
 public class TestApp extends MainApp {
 
-    public static final Path SAVE_LOCATION_FOR_TESTING = TestUtil.getFilePathInSandboxFolder("sampleData.json");
+    public static final Path SAVE_LOCATION_FOR_TESTING = TestUtil.getFilePathInSandboxFolder("data");
+    public static final Path SAVE_USER_LOCATION_FOR_TESTING = TestUtil.getFilePathInSandboxFolder("data/user");
 
     protected static final Path DEFAULT_PREF_FILE_LOCATION_FOR_TESTING =
             TestUtil.getFilePathInSandboxFolder("pref_testing.json");
     protected Path saveFileLocation = SAVE_LOCATION_FOR_TESTING;
+    protected Path saveUserFileLocation = SAVE_USER_LOCATION_FOR_TESTING;
+    protected ManagementModel testManagementModel;
 
     public TestApp() {
+    }
+
+    public TestApp(Path saveFileLocation, Path saveUserFileLocation) {
+        super();
+        this.saveFileLocation = saveFileLocation;
+        this.saveUserFileLocation = saveUserFileLocation;
     }
 
     @Override
@@ -43,15 +58,50 @@ public class TestApp extends MainApp {
         double x = Screen.getPrimary().getVisualBounds().getMinX();
         double y = Screen.getPrimary().getVisualBounds().getMinY();
         userPrefs.setGuiSettings(new GuiSettings(600.0, 600.0, (int) x, (int) y));
+        userPrefs.setLessonListFolderPath(saveFileLocation);
+        userPrefs.setUserFilePath(saveUserFileLocation);
         return userPrefs;
     }
 
+    @Override
+    protected LessonList initLessonList(LessonListStorage storage) {
+        return super.initLessonList(storage);
+    }
+
+    @Override
+    protected User initUser(UserStorage storage) {
+        return super.initUser(storage);
+    }
+
     /**
-     * Returns a defensive copy of the management.
+     * Returns a defensive copy of the lesson list data stored inside the storage file.
      */
-    public ManagementModel getModel() {
-        ManagementModel copy = new ManagementModelManager(new UserPrefs(), new LessonList(), new User());
-        return copy;
+    public List<Lesson> readStorageLessonList() {
+        LessonList lessonList = new LessonList();
+        Optional<LessonList> optionalLessonList = storage.readLessonList();
+        if (optionalLessonList.isPresent()) {
+            lessonList = optionalLessonList.get();
+        }
+
+        return lessonList.getLessons();
+    }
+    /**
+     * Returns a defensive copy of the management model.
+     */
+    public ManagementModel getManagementModel() {
+
+        this.testManagementModel =
+            new ManagementModelManager(new UserPrefs(), managementModel.getLessonList(), new User());
+        return testManagementModel;
+    }
+
+    /**
+     * Returns a defensive copy of the quiz model.
+     */
+    public QuizModel getQuizModel() {
+        QuizModel quizModel = new QuizModelManager(new ManagementModelManager(new UserPrefs(), new LessonList(),
+            managementModel.getUser()));
+        return quizModel;
     }
 
     @Override

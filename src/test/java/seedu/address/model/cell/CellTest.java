4a344889a830c5255c1de9e960fc7bbfba7ed666
erjillsison <erjillsison@gmail.com>
2019-04-08 17:46:52 +0800

    Update CellTest.java

--- src/test/java/seedu/address/model/cell/CellTest.java
+++ src/test/java/seedu/address/model/cell/CellTest.java
@@ -1,7 +1,9 @@
 package seedu.address.model.cell;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -30,6 +32,15 @@ public class CellTest {
 
         battleShipCell.receiveAttack();
         assertEquals(battleShipCell.getStatus(), Status.DESTROYED);
+    }
+
+    @Test
+    public void hasBattleship() {
+        Cell cell = new Cell();
+        assertFalse(cell.hasBattleShip());
+
+        cell.putShip(new Battleship());
+        assertTrue(cell.hasBattleShip());
     }
 
     @Test

6f86eab9e63be6d0468c9622e42838b459fc759c
Yi Chong Ong <yichong96@gmail.com>
2019-04-08 22:08:27 +0800

    add csv utils class

---
+++ src/main/java/seedu/knowitall/storage/csvmanager/CsvUtils.java
@@ -0,0 +1,11 @@
+package seedu.knowitall.storage.csvmanager;
+
+/**
+ * Csv utility class that contains file paths for normal and testing usage.
+ */
+public class CsvUtils {
+
+    public static final String DEFAULT_IMPORT_TEST_PATH = "/src/test/data/CsvCardFolderTest/Import";
+    public static final String DEFAULT_EXPORT_TEST_PATH = "/src/test/data/CsvCardFolderTest/Export";
+    public static final String DEFAULT_FILE_PATH = "./";
+}

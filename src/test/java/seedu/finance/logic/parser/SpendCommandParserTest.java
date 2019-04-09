85ecce7a76d418ad280868fd2dcfefb3ece5388f
eugeneyl <eugeneyl96@gmail.com>
2019-04-10 00:36:13 +0800

    Updated test cases for spend command parser

--- src/test/java/seedu/finance/logic/parser/SpendCommandParserTest.java
+++ src/test/java/seedu/finance/logic/parser/SpendCommandParserTest.java
@@ -1,23 +1,10 @@
 package seedu.finance.logic.parser;
 
 import static seedu.finance.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.finance.logic.commands.CommandTestUtil.AMOUNT_DESC_AMY;
-import static seedu.finance.logic.commands.CommandTestUtil.AMOUNT_DESC_BOB;
-import static seedu.finance.logic.commands.CommandTestUtil.CATEGORY_DESC_FRIEND;
-import static seedu.finance.logic.commands.CommandTestUtil.CATEGORY_DESC_HUSBAND;
-import static seedu.finance.logic.commands.CommandTestUtil.DATE_DESC_AMY;
-import static seedu.finance.logic.commands.CommandTestUtil.DATE_DESC_BOB;
-import static seedu.finance.logic.commands.CommandTestUtil.INVALID_AMOUNT_DESC;
-import static seedu.finance.logic.commands.CommandTestUtil.INVALID_CATEGORY_DESC;
-import static seedu.finance.logic.commands.CommandTestUtil.INVALID_DATE_DESC;
-import static seedu.finance.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
-import static seedu.finance.logic.commands.CommandTestUtil.NAME_DESC_AMY;
-import static seedu.finance.logic.commands.CommandTestUtil.NAME_DESC_BOB;
-import static seedu.finance.logic.commands.CommandTestUtil.PREAMBLE_NON_EMPTY;
-import static seedu.finance.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
-import static seedu.finance.logic.commands.CommandTestUtil.VALID_CATEGORY_FRIEND;
+import static seedu.finance.logic.commands.CommandTestUtil.*;
 import static seedu.finance.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.finance.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.finance.testutil.TypicalRecords.AMY;
 import static seedu.finance.testutil.TypicalRecords.BOB;
 
 import org.junit.Test;
@@ -29,6 +16,8 @@ import seedu.finance.model.record.Date;
 import seedu.finance.model.record.Name;
 import seedu.finance.model.record.Record;
 import seedu.finance.testutil.RecordBuilder;
+
+import java.time.LocalDate;
 
 public class SpendCommandParserTest {
     private SpendCommandParser parser = new SpendCommandParser();
@@ -59,17 +48,15 @@ public class SpendCommandParserTest {
                 new SpendCommand(expectedRecord));
     }
 
-    // Category is now a compulsory field
-    /*@Test
-    public void parse_optionalFieldsMissing_success() {
-        // zero categories
-        Record expectedRecord = new RecordBuilder(AMY).withCategory().build();
-        assertParseSuccess(parser, NAME_DESC_AMY + AMOUNT_DESC_AMY + DATE_DESC_AMY,
-                new SpendCommand(expectedRecord));
-    }*/
+    @Test
+    public void parse_optionalDateFieldMissing_success() {
+        Date date = new Date(LocalDate.now());
+        Record expectedRecord = new RecordBuilder(AMY).withDate(date.toString()).build();
+        assertParseSuccess(parser, NAME_DESC_AMY + AMOUNT_DESC_AMY + CATEGORY_DESC_FRIEND, new SpendCommand(expectedRecord));
+    }
 
     // TODO: Failed Test; need to update
-    /*
+
     @Test
     public void parse_compulsoryFieldMissing_failure() {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, SpendCommand.MESSAGE_USAGE);
@@ -82,17 +69,13 @@ public class SpendCommandParserTest {
         assertParseFailure(parser, NAME_DESC_BOB + VALID_AMOUNT_BOB + DATE_DESC_BOB
                 + CATEGORY_DESC_FRIEND, expectedMessage);
 
-        // missing date prefix
-        assertParseFailure(parser, NAME_DESC_BOB + AMOUNT_DESC_BOB + VALID_DATE_BOB
-                + CATEGORY_DESC_FRIEND, expectedMessage);
-
         // missing category prefix
         assertParseFailure(parser, NAME_DESC_BOB + AMOUNT_DESC_BOB + DATE_DESC_BOB
                 + VALID_CATEGORY_FRIEND, expectedMessage);
 
         // all prefixes missing
         assertParseFailure(parser, VALID_NAME_BOB + VALID_AMOUNT_BOB + VALID_DATE_BOB, expectedMessage);
-    }*/
+    }
 
     @Test
     public void parse_invalidValue_failure() {

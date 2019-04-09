1aa723cb8168f83758c76e92f161271ea8e808f1
Kuan Wei Heng <kuanwh1996@gmail.com>
2019-04-09 21:22:12 +0800

    Remove list onChange listeners

--- src/main/java/seedu/address/logic/CardsView.java
+++ src/main/java/seedu/address/logic/CardsView.java
@@ -8,7 +8,6 @@ import java.util.function.Predicate;
 
 import javafx.beans.property.ReadOnlyProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.scene.layout.Region;
@@ -44,7 +43,6 @@ public class CardsView implements ListViewState<Card> {
     public CardsView(Deck deck) {
         this.activeDeck = deck;
         filteredCards = new FilteredList<>(deck.getCards().asUnmodifiableObservableList());
-        filteredCards.addListener(this::ensureSelectedItemIsValid);
     }
 
     public CardsView(CardsView cardsView) {
@@ -77,37 +75,6 @@ public class CardsView implements ListViewState<Card> {
 
     public Deck getActiveDeck() {
         return activeDeck;
-    }
-
-    /**
-     * Ensures {@code selectedItem} is a valid card in {@code filteredItems}.
-     */
-    private void ensureSelectedItemIsValid(ListChangeListener.Change<? extends Card> change) {
-        while (change.next()) {
-            if (selectedCard.getValue() == null) {
-                // null is always a valid selected card, so we do not need to check that it is valid anymore.
-                return;
-            }
-
-            boolean wasSelectedItemReplaced =
-                    change.wasReplaced() && change.getAddedSize() == change.getRemovedSize() && change
-                            .getRemoved().contains(selectedCard.getValue());
-            if (wasSelectedItemReplaced) {
-                // Update selectedCard to its new value.
-                int index = change.getRemoved().indexOf(selectedCard.getValue());
-                selectedCard.setValue(change.getAddedSubList().get(index));
-                continue;
-            }
-
-            boolean wasSelectedItemRemoved = change.getRemoved().stream().anyMatch(
-                removedItem -> selectedCard.getValue().equals(removedItem));
-            if (wasSelectedItemRemoved) {
-                // Select the card that came before it in the list,
-                // or clear the selection if there is no such card.
-                selectedCard
-                        .setValue(change.getFrom() > 0 ? change.getList().get(change.getFrom() - 1) : null);
-            }
-        }
     }
 
     /**
--- src/main/java/seedu/address/logic/DecksView.java
+++ src/main/java/seedu/address/logic/DecksView.java
@@ -8,7 +8,6 @@ import java.util.function.Predicate;
 
 import javafx.beans.property.ReadOnlyProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.scene.layout.Region;
@@ -46,7 +45,6 @@ public class DecksView implements ListViewState<Deck> {
 
     public DecksView(FilteredList<Deck> deckList) {
         filteredDecks = deckList;
-        filteredDecks.addListener(this::ensureSelectedItemIsValid);
     }
 
     public DecksView(DecksView decksView) {
@@ -78,36 +76,6 @@ public class DecksView implements ListViewState<Deck> {
                 return new ImportDeckCommandParser().parse(arguments);
             default:
                 throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
-        }
-    }
-
-    /**
-     * Ensures {@code selectedItem} is a valid card in {@code filteredItems}.
-     */
-    private void ensureSelectedItemIsValid(ListChangeListener.Change<? extends Deck> change) {
-        while (change.next()) {
-            if (selectedDeck.getValue() == null) {
-                // null is always a valid selected card, so we do not need to check that it is valid anymore.
-                return;
-            }
-
-            boolean wasSelectedItemReplaced =
-                    change.wasReplaced() && change.getAddedSize() == change.getRemovedSize() && change
-                            .getRemoved().contains(selectedDeck.getValue());
-            if (wasSelectedItemReplaced) {
-                // Update selectedDeck to its new value.
-                int index = change.getRemoved().indexOf(selectedDeck.getValue());
-                selectedDeck.setValue(change.getAddedSubList().get(index));
-                continue;
-            }
-
-            boolean wasSelectedItemRemoved = change.getRemoved().stream().anyMatch(
-                removedItem -> selectedDeck.getValue().equals(removedItem));
-            if (wasSelectedItemRemoved) {
-                // Select the card that came before it in the list,
-                // or clear the selection if there is no such card.
-                selectedDeck.setValue(change.getFrom() > 0 ? change.getList().get(change.getFrom() - 1) : null);
-            }
         }
     }
 

75d6a9efd5623f2926da58736eb821cb63ebca7a
itszp <tayzp@u.nus.edu>
2019-04-09 17:11:20 +0800

    checkstyle

--- src/main/java/seedu/address/model/Album.java
+++ src/main/java/seedu/address/model/Album.java
@@ -5,8 +5,6 @@ import static seedu.address.commons.core.Config.ASSETS_FOLDER_TEMP_NAME;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 

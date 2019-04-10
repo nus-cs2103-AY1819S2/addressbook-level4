f5e1b682e93906d078c959924f56519dee36f2af
sciffany <sciffany@gmail.com>
2019-04-10 22:48:32 +0800

    Fix Back Command Test

--- src/test/java/seedu/address/logic/commands/BackCommandTest.java
+++ src/test/java/seedu/address/logic/commands/BackCommandTest.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalCards.getTypicalDeck;
 import static seedu.address.testutil.TypicalDecks.getTypicalTopDeck;
 
@@ -10,17 +11,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.CardsView;
 import seedu.address.logic.CommandHistory;
-import seedu.address.logic.StudyView;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
 public class BackCommandTest {
 
-    private static final CommandHistory EMPTY_COMMAND_HISTORY = new CommandHistory();
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -30,18 +27,12 @@ public class BackCommandTest {
     private Model getExpectedModelCard = new ModelManager(getTypicalTopDeck(), new UserPrefs());
     private Model getExpectedModelStudy = new ModelManager(getTypicalTopDeck(), new UserPrefs());
     private CommandHistory commandHistory = new CommandHistory();
-    //from cards view
-    private CardsView cardsView;
-    private Index index;
-    //from study view
-    private StudyView studyView;
 
 
     @Before
     public void initializeCardView() {
         modelCard.changeDeck(getTypicalDeck());
         assertTrue(modelCard.isAtCardsView());
-        cardsView = (CardsView) modelCard.getViewState();
         getExpectedModelCard.changeDeck(getTypicalDeck());
         assertTrue(getExpectedModelCard.isAtCardsView());
     }
@@ -50,32 +41,31 @@ public class BackCommandTest {
     public void initializeStudyView() {
         modelStudy.studyDeck(getTypicalDeck());
         assertTrue(modelStudy.isAtStudyView());
-        studyView = (StudyView) modelStudy.getViewState();
         getExpectedModelStudy.studyDeck(getTypicalDeck());
         assertTrue(getExpectedModelStudy.isAtStudyView());
     }
-    //TODO
-    //    @Test
-    //    //    public void execute_fromStudy_success() {
-    //    //        BackCommand backCommand = new BackCommand();
-    //    //        String expectedMessage = String
-    //    //                .format(BackCommand.MESSAGE_CLOSE_DECK_SUCCESS, getTypicalDeck().getName());
-    //    //        CommandResult expectedCommandResult = new UpdatePanelCommandResult(expectedMessage);
-    //    //        getExpectedModelStudy.studyDeck(getTypicalDeck());
-    //    //        assertCommandSuccess(backCommand, modelStudy, commandHistory, expectedCommandResult,
-    //    //                             getExpectedModelStudy);
-    //    //    }
-    //TODO
-    //    //    @Test
-    //    //    public void execute_fromCard_success() {
-    //    //        BackCommand backCommand = new BackCommand();
-    //    //        String expectedMessage = String
-    //    //                .format(BackCommand.MESSAGE_CLOSE_DECK_SUCCESS, getTypicalDeck().getName());
-    //    //        CommandResult expectedCommandResult = new UpdatePanelCommandResult(expectedMessage);
-    //    //        getExpectedModelCard.changeDeck(getTypicalDeck());
-    //    //        assertCommandSuccess(backCommand, modelStudy, commandHistory, expectedCommandResult,
-    //    //                             getExpectedModelStudy);
-    //    //   }
+
+    @Test
+    public void execute_fromStudy_success() {
+        BackCommand backCommand = new BackCommand();
+        String expectedMessage = String
+                .format(BackCommand.MESSAGE_CLOSE_DECK_SUCCESS, getTypicalDeck().getName());
+        CommandResult expectedCommandResult = new UpdatePanelCommandResult(expectedMessage);
+        getExpectedModelStudy.goToDecksView();
+        assertCommandSuccess(backCommand, modelStudy, commandHistory, expectedCommandResult,
+                             getExpectedModelStudy);
+    }
+
+    @Test
+    public void execute_fromCard_success() {
+        BackCommand backCommand = new BackCommand();
+        String expectedMessage = String
+                .format(BackCommand.MESSAGE_CLOSE_DECK_SUCCESS, getTypicalDeck().getName());
+        CommandResult expectedCommandResult = new UpdatePanelCommandResult(expectedMessage);
+        getExpectedModelCard.goToDecksView();
+        assertCommandSuccess(backCommand, modelCard, commandHistory, expectedCommandResult,
+                             getExpectedModelCard);
+    }
 
     @Test
     public void equals() {

a13728ab83b888145f43835681ea6256fcd2f948
JiaHaoLim <jiahaolimwork@hotmail.com>
2019-04-10 12:21:52 +0800

    Refactored PdfSerializableAddressBookTest.java.

--- src/test/java/seedu/address/storage/PdfSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/PdfSerializableAddressBookTest.java
@@ -22,20 +22,27 @@ public class PdfSerializableAddressBookTest {
     private static final Path TYPICAL_PERSONS_PDF_TEST = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBookTest.pdf");
     private static final Path TYPICAL_PERSONS_PDF_SAVE = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBookSaved.pdf");
     private static final Path INVALID_PERSON_PDF = TEST_DATA_FOLDER.resolve("invalidPersonAddressBookTest.pdf");
+    private static final String IGNORE_DATE_TIME_REGEX = "^(TeethHub Date saved/exported:)"
+                                                        + "|([0-9]{2}/[0-9]{2}/[0-9]{4})[\\r\\n]+([^\\r\\n]+)"
+                                                        + "|([0-9]{2}:[0-9]{2}:[0-9]{2})$";
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    @Test
-    public void saveAsPdf_typicalPersonsFileTime_failure() throws Exception {
+    /**
+     * Saves TypicalPersons to a PDF, then reads the saved PDF and the test PDF.
+     * @return The string contents of the saved PDF and the string contents of the test PDF
+     * @throws Exception if loading fails
+     */
+    private String[] saveThenCreateString(Path test, Path saved) throws Exception {
         InOutAddressBookStorage inOutAddressBookStorage = new InOutAddressBookStorage(TEST_DATA_FOLDER);
         ReadOnlyAddressBook addressBook = TypicalPersons.getTypicalAddressBook();
-        inOutAddressBookStorage.saveAsPdf(addressBook, TYPICAL_PERSONS_PDF_SAVE);
+        inOutAddressBookStorage.saveAsPdf(addressBook, saved);
 
         // Loading an existing document
-        File testFile = TYPICAL_PERSONS_PDF_TEST.toFile();
+        File testFile = test.toFile();
         PDDocument testDocument = PDDocument.load(testFile);
-        File savedFile = TYPICAL_PERSONS_PDF_SAVE.toFile();
+        File savedFile = saved.toFile();
         PDDocument savedDocument = PDDocument.load(savedFile);
 
         // Instantiate PDFTextStripper class
@@ -49,40 +56,24 @@ public class PdfSerializableAddressBookTest {
         testDocument.close();
         savedDocument.close();
 
-        assertNotEquals(testText, savedText);
+        savedFile.delete();
+
+        return new String[]{testText, savedText};
+    }
+
+    @Test
+    public void saveAsPdf_typicalPersonsFileTime_failure() throws Exception {
+        String[] results = saveThenCreateString(TYPICAL_PERSONS_PDF_TEST, TYPICAL_PERSONS_PDF_SAVE);
+        assertNotEquals(results[0], results[1]);
     }
 
     @Test
     public void saveAsPdf_typicalPersonsFile_success() throws Exception {
-        InOutAddressBookStorage inOutAddressBookStorage = new InOutAddressBookStorage(TEST_DATA_FOLDER);
-        ReadOnlyAddressBook addressBook = TypicalPersons.getTypicalAddressBook();
-        inOutAddressBookStorage.saveAsPdf(addressBook, TYPICAL_PERSONS_PDF_SAVE);
-
-        // Loading an existing document
-        File testFile = TYPICAL_PERSONS_PDF_TEST.toFile();
-        PDDocument testDocument = PDDocument.load(testFile);
-        File savedFile = TYPICAL_PERSONS_PDF_SAVE.toFile();
-        PDDocument savedDocument = PDDocument.load(savedFile);
-
-        // Instantiate PDFTextStripper class
-        PDFTextStripper pdfStripper = new PDFTextStripper();
-
-        // Retrieving text from PDF document
-        String testText = pdfStripper.getText(testDocument);
-        String savedText = pdfStripper.getText(savedDocument);
-
-        // Closing the document
-        testDocument.close();
-        savedDocument.close();
+        String[] results = saveThenCreateString(TYPICAL_PERSONS_PDF_TEST, TYPICAL_PERSONS_PDF_SAVE);
 
         // As saveAsPdf saves the date and time, it needs to be ignored for file compare
-        String ignoreDateTimeRegex = "^(TeethHub Date saved/exported:)"
-                                        + "|([0-9]{2,2}/[0-9]{2,2}/[0-9]{4,4})[\\r\\n]+([^\\r\\n]+)"
-                                        + "|([0-9]{2,2}:[0-9]{2,2}:[0-9]{2,2})$";
-
-        String[] splitTest = testText.split(ignoreDateTimeRegex);
-        String[] splitSaved = savedText.split(ignoreDateTimeRegex);
-
+        String[] splitTest = results[0].split(IGNORE_DATE_TIME_REGEX);
+        String[] splitSaved = results[1].split(IGNORE_DATE_TIME_REGEX);
 
         assertEquals(splitTest.length, splitSaved.length);
 
@@ -93,34 +84,11 @@ public class PdfSerializableAddressBookTest {
 
     @Test
     public void saveAsPdf_invalidTypicalPersonsFile_failure() throws Exception {
-        InOutAddressBookStorage inOutAddressBookStorage = new InOutAddressBookStorage(TEST_DATA_FOLDER);
-        ReadOnlyAddressBook addressBook = TypicalPersons.getTypicalAddressBook();
-        inOutAddressBookStorage.saveAsPdf(addressBook, TYPICAL_PERSONS_PDF_SAVE);
-
-        // Loading an existing document
-        File testFile = INVALID_PERSON_PDF.toFile();
-        PDDocument testDocument = PDDocument.load(testFile);
-        File savedFile = TYPICAL_PERSONS_PDF_SAVE.toFile();
-        PDDocument savedDocument = PDDocument.load(savedFile);
-
-        // Instantiate PDFTextStripper class
-        PDFTextStripper pdfStripper = new PDFTextStripper();
-
-        // Retrieving text from PDF document
-        String testText = pdfStripper.getText(testDocument);
-        String savedText = pdfStripper.getText(savedDocument);
-
-        // Closing the document
-        testDocument.close();
-        savedDocument.close();
+        String[] results = saveThenCreateString(INVALID_PERSON_PDF, TYPICAL_PERSONS_PDF_SAVE);
 
         // As saveAsPdf saves the date and time, it needs to be ignored for file compare
-        String ignoreDateTimeRegex = "^(TeethHub Date saved/exported:)"
-            + "|([0-9]{2,2}/[0-9]{2,2}/[0-9]{4,4})[\\r\\n]+([^\\r\\n]+)"
-            + "|([0-9]{2,2}:[0-9]{2,2}:[0-9]{2,2})$";
-
-        String[] splitTest = testText.split(ignoreDateTimeRegex);
-        String[] splitSaved = savedText.split(ignoreDateTimeRegex);
+        String[] splitTest = results[0].split(IGNORE_DATE_TIME_REGEX);
+        String[] splitSaved = results[1].split(IGNORE_DATE_TIME_REGEX);
 
         StringBuilder testSb = new StringBuilder();
         StringBuilder saveSb = new StringBuilder();

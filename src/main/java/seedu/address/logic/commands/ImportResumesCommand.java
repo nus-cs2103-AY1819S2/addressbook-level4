c467ad1852280c60ec3f2dcf2feb5e6399ba8861
Ong Suyi <thecognitiveaspie@gmail.com>
2019-04-10 11:46:12 +0800

    error correction (changed visibilty)

--- src/main/java/seedu/address/logic/commands/ImportResumesCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportResumesCommand.java
@@ -20,7 +20,7 @@ public class ImportResumesCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Resumes have been imported";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
 
-    Set<Person> toAdd;
+    private Set<Person> toAdd;
 
     public ImportResumesCommand(Set<Person> people) {
         toAdd = people;

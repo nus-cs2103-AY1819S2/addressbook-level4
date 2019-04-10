1ce675bae441cb11bf17a705f20dce470b7310e3
s-tr <swchong25+github@gmail.com>
2019-04-10 10:45:44 +0800

    Remove unused 'stage' attribute from BattleState

--- src/main/java/seedu/address/logic/battle/state/BattleState.java
+++ src/main/java/seedu/address/logic/battle/state/BattleState.java
@@ -7,86 +7,42 @@ public enum BattleState {
     /**
      * The battle has not begun yet.
      */
-    PRE_BATTLE("Preparing for battle...", Stage.STAGE_BEFORE),
+    PRE_BATTLE("Preparing for battle..."),
     /**
      * The human player is currently putting their ships.
      */
-    PLAYER_PUT_SHIP("You are currently placing ships...", Stage.STAGE_SHIP),
+    PLAYER_PUT_SHIP("You are currently placing ships..."),
     /**
      * The enemy player is currently putting their ships.
      */
-    ENEMY_PUT_SHIP("Enemy is currently placing ships...", Stage.STAGE_SHIP),
+    ENEMY_PUT_SHIP("Enemy is currently placing ships..."),
     /**
      * The human player is currently planning their attack.
      */
-    PLAYER_ATTACK("You are currently planning attack...", Stage.STAGE_BATTLE),
+    PLAYER_ATTACK("You are currently planning attack..."),
     /**
      * The human player is currently planning their attack.
      */
-    ENEMY_ATTACK("Enemy is currently planning attack...", Stage.STAGE_BATTLE),
+    ENEMY_ATTACK("Enemy is currently planning attack..."),
     /**
      * The human player has won the game.
      */
-    PLAYER_WIN("You have won!", Stage.STAGE_AFTER),
+    PLAYER_WIN("You have won!"),
     /**
      * The human player has lost the game..
      */
-    ENEMY_WIN("You have lost...", Stage.STAGE_AFTER);
-
-    private static final String STAGE_BEFORE = "game has not started";
-    private static final String STAGE_SHIP = "placing ships";
-    private static final String STAGE_BATTLE = "battling";
-    private static final String STAGE_AFTER = "game has finished";
+    ENEMY_WIN("You have lost...");
     /**
      * A human-readable description of the state.
      * Usage: as-is
      */
     private final String description;
 
-    /**
-     * A human-readable description of the current stage of battle.
-     * <br>
-     * Usage: "Cannot (action) while (stage)"
-     * e.g. "Cannot put ship while battle is happening"
-     * e.g. "Cannot attack while game is finished"
-     * e.g. "Cannot attack while game has not started"
-     */
-    private final Stage stage;
-
-    private BattleState(String description, Stage stage) {
+    BattleState(String description) {
         this.description = description;
-        this.stage = stage;
     }
 
     public String getDescription() {
         return description;
-    }
-
-    public String getStage() {
-        return stage.toString();
-    }
-
-    /**
-     * Stores the current stage of the game.
-     * <br>
-     * Reason for this class is because of this bug:
-     * https://bugs.java.com/bugdatabase/view_bug.do?bug_id=6676362
-     */
-    private enum Stage {
-        STAGE_BEFORE("game has not started"),
-        STAGE_SHIP("placing ships"),
-        STAGE_BATTLE("battling"),
-        STAGE_AFTER("game has finished");
-
-        private final String stage;
-
-        Stage(String stage) {
-            this.stage = stage;
-        }
-
-        @Override
-        public String toString() {
-            return stage;
-        }
     }
 }

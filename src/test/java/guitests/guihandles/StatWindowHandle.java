914ddfb93ebb19a2e751ea1424d0c358b7553337
kthSim <kth_sim@hotmail.com>
2019-04-11 20:46:11 +0800

    Implementation for StatWindowHandle

--- src/test/java/guitests/guihandles/StatWindowHandle.java
+++ src/test/java/guitests/guihandles/StatWindowHandle.java
@@ -1,4 +1,18 @@
 package guitests.guihandles;
 
-public class StatWindowHandle {
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class StatWindowHandle extends StageHandle {
+    private Stage statStage;
+
+    public StatWindowHandle(Stage statWindowStage) {
+        super(statWindowStage);
+        statStage = statWindowStage;
+    }
+
+    private void closeStatWindowExternally() {
+        guiRobot.interact(() -> statStage.fireEvent(new WindowEvent(statStage, WindowEvent.WINDOW_CLOSE_REQUEST)));
+    }
+
 }

c2fac87137e03763c97c5552ebfe08a0f9740f8f
thamsimun <e0201281@u.nus.edu>
2019-04-11 16:33:51 +0800

    add brightnesscommand test

---
+++ src/test/java/seedu/address/logic/commands/BrightnessCommandTest.java
@@ -0,0 +1,61 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.parser.BrightnessCommandParser;
+import seedu.address.logic.parser.ImportCommandParser;
+import seedu.address.logic.parser.OpenCommandParser;
+import seedu.address.model.Album;
+import seedu.address.model.CurrentEdit;
+import seedu.address.model.CurrentEditManager;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class BrightnessCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private CurrentEdit currentEdit = new CurrentEditManager();
+    private CommandHistory commandHistory = new CommandHistory();
+    @Before
+    public void createImagesInAlbum() {
+        try {
+            BrightnessCommandParser parserBrightness = new BrightnessCommandParser();
+            BrightnessCommand brightness = parserBrightness.parse(" 1.3");
+            String expectedMessage = Messages.MESSAGE_DID_NOT_OPEN;
+            assertCommandFailure(brightness, model, commandHistory, expectedMessage, currentEdit);
+            ImportCommandParser parser = new ImportCommandParser();
+            parser.parse("sample").execute(currentEdit, model, commandHistory);
+            OpenCommandParser parser2 = new OpenCommandParser();
+            parser2.parse("iu.jpg").execute(currentEdit, model, commandHistory);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    @Test
+    public void execute_validBrightnessCheckIsNewCommand_success() {
+        try {
+            BrightnessCommandParser parser = new BrightnessCommandParser();
+            BrightnessCommand command = parser.parse(" 1.3");
+            String expectedMessage = Messages.MESSAGE_BRIGHTNESS_SUCCESS;
+            assertCommandSuccess(command, model, commandHistory, expectedMessage, currentEdit);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    @After
+    public void clearAlbum() {
+        Album album = Album.getInstance();
+        album.clearAlbum();
+        currentEdit.clearTemp();
+    }
+}

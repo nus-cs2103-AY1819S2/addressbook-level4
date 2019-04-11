1326119b04e868f1f38a539fca1ae69ff21f3d52
Kuan Wei Heng <kuanwh1996@gmail.com>
2019-04-11 16:47:44 +0800

    Fix select card test

--- src/test/java/seedu/address/logic/parser/SelectCardCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SelectCardCommandParserTest.java
@@ -1,11 +1,14 @@
 package seedu.address.logic.parser;
 
+import static org.junit.Assert.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalCards.getTypicalDeck;
 import static seedu.address.testutil.TypicalCards.getTypicalTopDeck;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CARD;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import seedu.address.logic.commands.SelectCardCommand;
@@ -22,7 +25,14 @@ import seedu.address.model.UserPrefs;
 public class SelectCardCommandParserTest {
 
     private Model model = new ModelManager(getTypicalTopDeck(), new UserPrefs());
-    private SelectCardCommandParser parser = new SelectCardCommandParser((CardsView) model.getViewState());
+    private SelectCardCommandParser parser;
+
+    @Before
+    public void initialize() {
+        model.changeDeck(getTypicalDeck());
+        assertTrue(model.isAtCardsView());
+        parser = new SelectCardCommandParser((CardsView) model.getViewState());
+    }
 
     @Test
     public void parse_validArgs_returnsSelectCommand() {

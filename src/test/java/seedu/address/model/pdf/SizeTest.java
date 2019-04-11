8f6db9618c7565a5215b7d55a677238ada0ed924
ptvrajsk <ptvrajsk@gmail.com>
2019-04-11 14:30:21 +0800

    (v1.4) SizeTest In Progress
    From prior testing, the NameTest already works (implementation in v1.3
    of pdf++). SizeTest however was incomplete. Fixed and revamped the test
    format to get it to work. SizeTest now works completely HOWEVER. From
    the trial demo, it was highlighted that we should make the size value
    more "readable" and this will require several changes in the Size,
    JsonAdaptedPdf and other class files. Proceeding to work on those
    changes now.
    
    Track Changes: #217

--- src/test/java/seedu/address/model/pdf/SizeTest.java
+++ src/test/java/seedu/address/model/pdf/SizeTest.java
@@ -5,65 +5,51 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
-import java.nio.file.Paths;
+import static seedu.address.logic.commands.CommandTestUtil.SIZE_1_VALID;
+import static seedu.address.logic.commands.CommandTestUtil.SIZE_2_VALID;
+import static seedu.address.logic.commands.CommandTestUtil.SIZE_3_VALID;
+import static seedu.address.logic.commands.CommandTestUtil.SIZE_INVALID_ALPHABET;
+import static seedu.address.logic.commands.CommandTestUtil.SIZE_INVALID_NEGATIVE;
 
 import org.junit.Test;
 
-import seedu.address.logic.commands.CommandTestUtil;
 import seedu.address.testutil.Assert;
 
 public class SizeTest {
 
     @Test
-    public void constructor_nullValue_throwsNullPointerException() {
+    public void constructor() {
+
+        // null value -> expect NullPointerException to be thrown.
         Assert.assertThrows(NullPointerException.class, () -> new Size(null));
-    }
 
-    @Test
-    public void constructor_emptyValue_throwsIllegalArgumentException() {
-        String invalidSize = "";
-        Assert.assertThrows(IllegalArgumentException.class, () -> new Size(invalidSize));
-    }
+        // no value -> expect IllegalArgumentException to be thrown.
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Size(""));
 
-    @Test
-    public void constructor_emptySpaceValue_throwsIllegalArgumentException() {
-        String invalidSize = "";
-        Assert.assertThrows(IllegalArgumentException.class, () -> new Size(invalidSize));
-    }
+        // blank space -> expect IllegalArgumentException to be thrown.
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Size(" "));
 
-    @Test
-    public void constructor_negativeValue_throwsIllegalArgumentException() {
-        String invalidSize = CommandTestUtil.SIZE_INVALID_NEGATIVE;
-        Assert.assertThrows(IllegalArgumentException.class, () -> new Size(invalidSize));
-    }
+        // negative value -> expect IllegalArgumentException to be thrown.
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Size(SIZE_INVALID_NEGATIVE));
 
-    @Test
-    public void constructor_alphabetValue_throwsIllegalArgumentException() {
-        String invalidSize = CommandTestUtil.SIZE_INVALID_ALPHABET;
-        Assert.assertThrows(IllegalArgumentException.class, () -> new Size(invalidSize));
-    }
+        // alphabet value -> expect IllegalArgumentException to be thrown.
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Size(SIZE_INVALID_ALPHABET));
 
-    @Test
-    public void constructor_normalValue_expectNoException() {
+        // legitimate value -> expect no exceptions to be thrown.
         try {
-            Size s = new Size(CommandTestUtil.SIZE_1_VALID);
+            Size s = new Size(SIZE_1_VALID);
         } catch (Exception e) {
             fail("Should not fail valid inputs.");
         }
-    }
 
-    @Test
-    public void constructor_singleValue_expectNoException() {
+        // single digit value -> expect no exceptions to be thrown.
         try {
             Size s = new Size("1");
         } catch (Exception e) {
             fail("Should not fail valid inputs.");
         }
-    }
 
-    @Test
-    public void constructor_longValue_expectNoException() {
+        // long value -> expect no exceptions to be thrown.
         try {
             Size s = new Size("12345678910111213141516171819202122232425");
         } catch (Exception e) {
@@ -72,35 +58,41 @@ public class SizeTest {
     }
 
     @Test
-    public void toString_matchInput_expectedTrue() {
+    public void toStringTest() {
+
+        // same value -> expected match
         String expected = new StringBuilder()
                 .append("Size: ")
-                .append(CommandTestUtil.SIZE_1_VALID)
+                .append(SIZE_1_VALID)
                 .append("\n").toString();
 
-        assertEquals(expected, new Size(CommandTestUtil.SIZE_1_VALID).toString());
-    }
+        assertEquals(expected, new Size(SIZE_1_VALID).toString());
 
-    @Test
-    public void toString_doesNotMatchDifferentValue_expectedFalse() {
+        // different value -> expected non-match
         String notExpected = new StringBuilder()
                 .append("Size: ")
-                .append(CommandTestUtil.SIZE_1_VALID)
+                .append(SIZE_1_VALID)
                 .append("\n").toString();
 
-        assertNotEquals(notExpected, new Size(CommandTestUtil.SIZE_2_VALID).toString());
+        assertNotEquals(notExpected, new Size(SIZE_2_VALID).toString());
+
     }
 
     @Test
-    public void equals_matchInput_expectedTrue() {
-        Size expected = new Size(CommandTestUtil.SIZE_3_VALID);
-        assertTrue(new Size(CommandTestUtil.SIZE_3_VALID).equals(
-                new Size(Long.toString(Paths.get(CommandTestUtil.FILEPATH_3_VALID).toFile().length()))));
-    }
+    public void equals() {
 
-    @Test
-    public void equals_nullInput_expectedFalse() {
-        assertFalse(new Size(CommandTestUtil.SIZE_3_VALID).equals(null));
+        // same value same object -> expected true
+        Size size = new Size(SIZE_3_VALID);
+        assertTrue(size.equals(size));
+
+        // same value different object -> expected true
+        assertTrue(size.equals(new Size(SIZE_3_VALID)));
+
+        // different value -> expected false
+        assertFalse(size.equals(new Size(SIZE_2_VALID)));
+
+        // null value -> expected false
+        assertFalse(size.equals(null));
     }
 
 }

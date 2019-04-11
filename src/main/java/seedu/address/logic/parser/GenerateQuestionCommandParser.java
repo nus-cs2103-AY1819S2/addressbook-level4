14f2101d342cf6efef204a09683aef2570a6fab5
Kuan Wei Heng <kuanwh1996@gmail.com>
2019-04-11 10:34:00 +0800

    Update imports after merge

--- src/main/java/seedu/address/logic/parser/GenerateQuestionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/GenerateQuestionCommandParser.java
@@ -2,7 +2,7 @@ package seedu.address.logic.parser;
 
 import static java.lang.Integer.parseInt;
 
-import seedu.address.logic.StudyView;
+import seedu.address.model.StudyView;
 import seedu.address.logic.commands.GenerateQuestionCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
--- src/test/java/seedu/address/logic/commands/ImportDeckCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportDeckCommandTest.java
@@ -16,7 +16,7 @@ import org.junit.rules.ExpectedException;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.logic.CommandHistory;
-import seedu.address.logic.ViewState;
+import seedu.address.model.ViewState;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;

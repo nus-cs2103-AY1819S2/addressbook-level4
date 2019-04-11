e59ba8d2c952dfced5d13d818637eb0b7ba49d2b
nivenang <46707122+nivenang@users.noreply.github.com>
2019-04-11 22:25:38 +0800

    Update JsonChartBookStorageTest

--- src/test/java/seedu/travel/storage/JsonChartBookStorageTest.java
+++ src/test/java/seedu/travel/storage/JsonChartBookStorageTest.java
@@ -1,23 +1,16 @@
 package seedu.travel.storage;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.travel.commons.util.CollectionUtil.requireAllNonNull;
+import static org.junit.Assert.assertNotEquals;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.ArrayList;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 
 import seedu.travel.model.ChartBook;
@@ -27,6 +20,14 @@ import seedu.travel.model.ReadOnlyYearChart;
 
 public class JsonChartBookStorageTest {
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonChartBookStorageTest");
+
+    private static Path validCountryPath = Paths.get("src", "test", "data",
+            "JsonChartBookStorageTest", "validCountryChart.json");
+    private static Path validRatingPath = Paths.get("src", "test", "data",
+            "JsonChartBookStorageTest", "validRatingChart.json");
+    private static Path validYearPath = Paths.get("src", "test", "data",
+            "JsonChartBookStorageTest", "validYearChart.json");
+
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -42,9 +43,10 @@ public class JsonChartBookStorageTest {
 
     @Test
     public void read_notJsonFormat_exceptionThrown() throws JsonSyntaxException {
-
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(JsonSyntaxException.class);
-        readCountryChart("notJsonFormat.json");
+        testChartBookStorage.readCountryChart(addToTestDataPathIfNotNull("notJsonFormat.json"));
 
         // IMPORTANT: Any code below an exception-throwing line (like the one above) will be ignored.
         // That means you should not have more than one exception test in one method
@@ -52,208 +54,151 @@ public class JsonChartBookStorageTest {
 
     @Test
     public void readCountryChart_nullFilePath_throwsNullPointerException() {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(NullPointerException.class);
-        readCountryChart(null);
-    }
-
-    /**
-     * Reads the {@code CountryChart} data from the Json file path
-     * @param filePath the place where the file is stored
-     * @return a list of {@code CountryChart} data
-     * @throws JsonSyntaxException due to Json formatting issues
-     */
-    @SuppressWarnings("unchecked")
-    private List<ReadOnlyCountryChart> readCountryChart(String filePath) throws JsonSyntaxException {
-        requireNonNull(filePath);
-
-        Gson gson = new Gson();
-        List<ReadOnlyCountryChart> countryList;
-        try {
-            FileReader fileReader = new FileReader(String.valueOf(addToTestDataPathIfNotNull(filePath)));
-            countryList = gson.fromJson(fileReader, List.class);
-        } catch (FileNotFoundException fnfe) {
-            throw new AssertionError("There should not be an error in finding the file.", fnfe);
-        }
-
-        return countryList;
+        testChartBookStorage.readCountryChart(null);
     }
 
     @Test
     public void readCountryChart_invalidCountryChart_throwDataConversionException() throws JsonSyntaxException {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(JsonSyntaxException.class);
-        readCountryChart("invalidCountryChart.json");
+        testChartBookStorage.readCountryChart(addToTestDataPathIfNotNull("invalidCountryChart.json"));
     }
 
     @Test
     public void readCountryChart_invalidAndValidCountryChart_throwDataConversionException()
             throws JsonSyntaxException {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(JsonSyntaxException.class);
-        readCountryChart("invalidAndValidCountryChart.json");
+        testChartBookStorage.readCountryChart(
+                addToTestDataPathIfNotNull("invalidAndValidCountryChart.json"));
+    }
+
+    @Test
+    public void readCountryChart_validCountryChart_readSuccess() {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(validCountryPath,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
+        assertNotEquals(testChartBookStorage.readCountryChart(), new ArrayList<ReadOnlyCountryChart>());
     }
 
     @Test
     public void readRatingChart_nullFilePath_throwsNullPointerException() {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(NullPointerException.class);
-        readCountryChart(null);
-    }
-
-    /**
-     * Reads the {@code RatingChart} data from the Json file path
-     * @param filePath the place where the file is stored
-     * @return a list of {@code RatingChart} data
-     * @throws JsonSyntaxException due to Json formatting issues
-     */
-    @SuppressWarnings("unchecked")
-    private List<ReadOnlyRatingChart> readRatingChart(String filePath) throws JsonSyntaxException {
-        requireNonNull(filePath);
-
-        Gson gson = new Gson();
-        List<ReadOnlyRatingChart> ratingList;
-        try {
-            FileReader fileReader = new FileReader(String.valueOf(addToTestDataPathIfNotNull(filePath)));
-            ratingList = gson.fromJson(fileReader, List.class);
-        } catch (FileNotFoundException fnfe) {
-            throw new AssertionError("There should not be an error in finding the file.", fnfe);
-        }
-
-        return ratingList;
+        testChartBookStorage.readCountryChart(null);
     }
 
     @Test
     public void readRatingChart_invalidRatingChart_throwDataConversionException() throws JsonSyntaxException {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(JsonSyntaxException.class);
-        readRatingChart("invalidRatingChart.json");
+        testChartBookStorage.readRatingChart(addToTestDataPathIfNotNull("invalidRatingChart.json"));
     }
 
     @Test
     public void readRatingChart_invalidAndValidRatingChart_throwDataConversionException()
             throws JsonSyntaxException {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(JsonSyntaxException.class);
-        readRatingChart("invalidAndValidRatingChart.json");
+        testChartBookStorage.readRatingChart(
+                addToTestDataPathIfNotNull("invalidAndValidRatingChart.json"));
+    }
+
+    @Test
+    public void readRatingChart_validRatingChart_readSuccess() {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                validRatingPath, TEST_DATA_FOLDER);
+        assertNotEquals(testChartBookStorage.readRatingChart(), new ArrayList<ReadOnlyRatingChart>());
     }
 
     @Test
     public void readYearChart_nullFilePath_throwsNullPointerException() {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(NullPointerException.class);
-        readYearChart(null);
-    }
-
-    /**
-     * Reads the {@code YearChart} data from the Json file path
-     * @param filePath the place where the file is stored
-     * @return a list of {@code YearChart} data
-     * @throws JsonSyntaxException due to Json formatting issues
-     */
-    @SuppressWarnings("unchecked")
-    private List<ReadOnlyYearChart> readYearChart(String filePath) throws JsonSyntaxException {
-        requireNonNull(filePath);
-
-        Gson gson = new Gson();
-        List<ReadOnlyYearChart> yearList;
-        try {
-            FileReader fileReader = new FileReader(String.valueOf(addToTestDataPathIfNotNull(filePath)));
-            yearList = gson.fromJson(fileReader, List.class);
-        } catch (FileNotFoundException fnfe) {
-            throw new AssertionError("There should not be an error in finding the file.", fnfe);
-        }
-
-        return yearList;
+        testChartBookStorage.readYearChart(null);
     }
 
     @Test
     public void readYearChart_invalidYearChart_throwDataConversionException() throws JsonSyntaxException {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(JsonSyntaxException.class);
-        readYearChart("invalidYearChart.json");
+        testChartBookStorage.readYearChart(addToTestDataPathIfNotNull("invalidYearChart.json"));
     }
 
     @Test
     public void readYearChart_invalidAndValidYearChart_throwDataConversionException()
             throws JsonSyntaxException {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(JsonSyntaxException.class);
-        readYearChart("invalidAndValidYearChart.json");
+        testChartBookStorage.readYearChart(
+                addToTestDataPathIfNotNull("invalidAndValidYearChart.json"));
+    }
+
+    @Test
+    public void readYearChart_validYearChart_readSuccess() {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, validYearPath);
+        assertNotEquals(testChartBookStorage.readYearChart(), new ArrayList<ReadOnlyYearChart>());
     }
 
     @Test
     public void saveCountryChart_nullCountryChart_throwsNullPointerException() {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(NullPointerException.class);
-        saveCountryChart(null, "SomeFile.json");
-    }
-
-    /**
-     * Saves {@code countryChart} at the specified {@code filePath}.
-     */
-    private void saveCountryChart(ReadOnlyCountryChart countryChart, String filePath) {
-        requireAllNonNull(countryChart, filePath);
-
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        try {
-            FileWriter fileWriter = new FileWriter(String.valueOf(filePath));
-            gson.toJson(countryChart, fileWriter);
-            fileWriter.flush();
-        } catch (IOException ioe) {
-            throw new AssertionError("There should not be an error writing to the file.", ioe);
-        }
+        testChartBookStorage.saveCountryChart(
+                null, addToTestDataPathIfNotNull("validCountryChart.json"));
     }
 
     @Test
     public void saveCountryChart_nullFilePath_throwsNullPointerException() {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(NullPointerException.class);
-        saveCountryChart(new ChartBook(), null);
+        testChartBookStorage.saveCountryChart(new ChartBook(), null);
     }
 
     @Test
     public void saveRatingChart_nullRatingChart_throwsNullPointerException() {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(NullPointerException.class);
-        saveRatingChart(null, "SomeFile.json");
-    }
-
-    /**
-     * Saves {@code ratingChart} at the specified {@code filePath}.
-     */
-    private void saveRatingChart(ReadOnlyRatingChart ratingChart, String filePath) {
-        requireAllNonNull(ratingChart, filePath);
-
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        try {
-            FileWriter fileWriter = new FileWriter(String.valueOf(filePath));
-            gson.toJson(ratingChart, fileWriter);
-            fileWriter.flush();
-        } catch (IOException ioe) {
-            throw new AssertionError("There should not be an error writing to the file.", ioe);
-        }
+        testChartBookStorage.saveRatingChart(
+                null, addToTestDataPathIfNotNull("validRatingChart.json"));
     }
 
     @Test
     public void saveRatingChart_nullFilePath_throwsNullPointerException() {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(NullPointerException.class);
-        saveRatingChart(new ChartBook(), null);
+        testChartBookStorage.saveRatingChart(new ChartBook(), null);
     }
 
     @Test
     public void saveYearChart_nullRatingChart_throwsNullPointerException() {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(NullPointerException.class);
-        saveYearChart(null, "SomeFile.json");
-    }
-
-    /**
-     * Saves {@code ratingChart} at the specified {@code filePath}.
-     */
-    private void saveYearChart(ReadOnlyYearChart yearChart, String filePath) {
-        requireAllNonNull(yearChart, filePath);
-
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        try {
-            FileWriter fileWriter = new FileWriter(String.valueOf(filePath));
-            gson.toJson(yearChart, fileWriter);
-            fileWriter.flush();
-        } catch (IOException ioe) {
-            throw new AssertionError("There should not be an error writing to the file.", ioe);
-        }
+        testChartBookStorage.saveYearChart(
+                null, addToTestDataPathIfNotNull("validYearChart.json"));
     }
 
     @Test
     public void saveYearChart_nullFilePath_throwsNullPointerException() {
+        JsonChartBookStorage testChartBookStorage = new JsonChartBookStorage(TEST_DATA_FOLDER,
+                TEST_DATA_FOLDER, TEST_DATA_FOLDER);
         thrown.expect(NullPointerException.class);
-        saveYearChart(new ChartBook(), null);
+        testChartBookStorage.saveYearChart(new ChartBook(), null);
     }
 }

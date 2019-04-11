caf11477cd2819fd8271d5bf0aa15d015aa50342
Wklee96 <lweikang96@gmail.com>
2019-04-11 20:23:20 +0800

    implemented a placeholder for Email

--- src/main/java/seedu/address/model/restaurant/Email.java
+++ src/main/java/seedu/address/model/restaurant/Email.java
@@ -26,6 +26,7 @@ public class Email {
     private static final String DOMAIN_LAST_CHARACTER_REGEX = "[^\\W_]$";
     public static final String VALIDATION_REGEX = LOCAL_PART_REGEX + "@"
             + DOMAIN_FIRST_CHARACTER_REGEX + DOMAIN_MIDDLE_REGEX + DOMAIN_LAST_CHARACTER_REGEX;
+    private static final String EMAIL_PLACEHOLDER = "No email added";
 
     public final String value;
 
@@ -44,8 +45,23 @@ public class Email {
      * Returns if a given string is a valid email.
      */
     public static boolean isValidEmail(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return test.matches(VALIDATION_REGEX) || test.equals(EMAIL_PLACEHOLDER);
     }
+
+    /**
+     * @return a default Email object
+     */
+    public static Email makeDefaultEmail() {
+        return new Email(EMAIL_PLACEHOLDER);
+    }
+
+    /**
+     * Returns if email is a dummy email
+     */
+    public boolean isDummy() {
+        return value.equals(EMAIL_PLACEHOLDER);
+    }
+
 
     @Override
     public String toString() {

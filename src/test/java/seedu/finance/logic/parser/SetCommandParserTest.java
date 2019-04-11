a516bb10b497c836128d17f1806f9d9a39b5b283
eugeneyl <eugeneyl96@gmail.com>
2019-04-11 15:05:30 +0800

    Implemented SetCommandParserTest

--- src/test/java/seedu/finance/logic/parser/SetCommandParserTest.java
+++ src/test/java/seedu/finance/logic/parser/SetCommandParserTest.java
@@ -1,4 +1,33 @@
 package seedu.finance.logic.parser;
 
+import static seedu.finance.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.finance.logic.commands.CommandTestUtil.*;
+import static seedu.finance.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.finance.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.Test;
+
+import seedu.finance.logic.commands.SetCommand;
+import seedu.finance.model.record.Amount;
+
 public class SetCommandParserTest {
+    private SetCommandParser parser = new SetCommandParser();
+
+    @Test
+    public void parse_validField_success() {
+        assertParseSuccess(parser, AMOUNT_DESC_BOB, new SetCommand(VALID_AMOUNT_BOB));
+
+    }
+
+    @Test
+    public void parse_invalidField_failure() {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetCommand.MESSAGE_USAGE);
+
+        //no amount
+        assertParseFailure(parser, " ", expectedMessage);
+
+        //invalid amount
+        System.out.println(SetCommand.COMMAND_WORD + INVALID_AMOUNT_DESC);
+        assertParseFailure(parser, INVALID_AMOUNT_DESC, Amount.MESSAGE_CONSTRAINTS);
+    }
 }

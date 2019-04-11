02131ba6c2b1f2093babb0b59b99a5e4cfa36870
cchj1995 <choo_jnthn@hotmail.com>
2019-04-11 14:21:18 +0800

    Added constructors for TaskBuilder

--- src/test/java/seedu/address/testutil/TaskBuilder.java
+++ src/test/java/seedu/address/testutil/TaskBuilder.java
@@ -4,8 +4,12 @@ import seedu.address.model.datetime.DateCustom;
 import seedu.address.model.datetime.TimeCustom;
 import seedu.address.model.task.LinkedPatient;
 import seedu.address.model.task.Priority;
+import seedu.address.model.task.Task;
 import seedu.address.model.task.Title;
 
+/**
+ * A utility class to help with building Task objects.
+ */
 public class TaskBuilder {
 
     public static final String DEFAULT_TITLE = "Review Patients' records";
@@ -24,4 +28,26 @@ public class TaskBuilder {
     private Priority priority;
     private LinkedPatient linkedPatient;
 
+    public TaskBuilder() {
+        title = new Title(DEFAULT_TITLE);
+        startDate = new DateCustom(DEFAULT_STARTDATE);
+        endDate = new DateCustom(DEFAULT_ENDDATE);
+        startTime = new TimeCustom(DEFAULT_STARTTIME);
+        endTime = new TimeCustom(DEFAULT_ENDTIME);
+        priority = Priority.returnPriority(DEFAULT_PRIORITY);
+        linkedPatient = null;
+    }
+
+    /**
+     * Initializes the TaskBuilder with the data of {@code taskToCopy}.
+     */
+    public TaskBuilder(Task taskToCopy) {
+        title = taskToCopy.getTitle();
+        startDate = taskToCopy.getStartDate();
+        endDate = taskToCopy.getEndDate();
+        startTime = taskToCopy.getStartTime();
+        endTime = taskToCopy.getEndTime();
+        priority = taskToCopy.getPriority();
+        linkedPatient = taskToCopy.getLinkedPatient();
+    }
 }

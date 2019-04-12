974b77991a86180aa655bf3ad221d79b03cc810f
DigiPie <evan.it.sg@gmail.com>
2019-04-13 02:07:42 +0800

    Update Syntax

--- src/main/java/seedu/address/logic/parser/Syntax.java
+++ src/main/java/seedu/address/logic/parser/Syntax.java
@@ -4,7 +4,6 @@ package seedu.address.logic.parser;
  * Contains Command Line Interface (CLI) syntax definitions common to multiple commands
  */
 public class Syntax {
-
     /* Prefix definitions */
     // Used by Lesson commands
     public static final Prefix PREFIX_LESSON_NAME = new Prefix("n/");
@@ -20,4 +19,10 @@ public class Syntax {
     public static final Prefix PREFIX_START_NAME = new Prefix("n/");
     public static final Prefix PREFIX_START_COUNT = new Prefix("c/");
     public static final Prefix PREFIX_START_MODE = new Prefix("m/");
+
+    /**
+     * This is a constants-only (utility) class which should not be instantiated.
+     * Note that this is not a singleton class given that not even a single instance is allowed.
+     */
+    private Syntax() { }
 }

5ccdb6b843e828064e6bacf97bc11682ec98a9d0
flxffy <aihui@comp.nus.edu.sg>
2019-04-12 21:21:31 +0800

    warning card test added

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -8,7 +8,7 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_MEDICINE_DISPLAYED_INDEX = "The medicine index provided is invalid";
-    public static final String MESSAGE_MEDICINES_LISTED_OVERVIEW = "%1$d medicines listed!";
+    public static final String MESSAGE_MEDICINES_LISTED_OVERVIEW = "%1$d medicine(s) listed!";
     public static final String MESSAGE_SHOW_CURRENT_THRESHOLDS = "Expiry threshold: %1$d day%2$s\n"
                                                                  + "Low stock threshold: %3$d";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT_DETAILED = "Invalid command format! %1$s\n%2$s";
---
+++ src/test/java/guitests/guihandles/WarningCardHandle.java
@@ -0,0 +1,68 @@
+package guitests.guihandles;
+
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import seedu.address.commons.util.warning.WarningPanelPredicateType;
+import seedu.address.model.medicine.Batch;
+import seedu.address.model.medicine.Medicine;
+
+/**
+ * Provides a handle to a medicine card in the medicine list panel.
+ */
+public class WarningCardHandle extends NodeHandle<Node> {
+    private static final String ID_FIELD_ID = "#id";
+    private static final String NAME_FIELD_ID = "#name";
+    private static final String FIELD_FIELD_ID = "#field"; // medicine details, total qty or batch exp
+
+    private final Label idText;
+    private final Label nameText;
+    private final Label fieldText;
+
+    private WarningPanelPredicateType type;
+
+    public WarningCardHandle(Node cardNode, WarningPanelPredicateType listType) {
+        super(cardNode);
+
+        idText = getChildNode(ID_FIELD_ID);
+        nameText = getChildNode(NAME_FIELD_ID);
+        fieldText = getChildNode(FIELD_FIELD_ID);
+        type = listType;
+    }
+
+    public String getId() {
+        return idText.getText();
+    }
+
+    public String getName() {
+        return nameText.getText();
+    }
+
+    public String getDetail() {
+        return fieldText.getText();
+    }
+
+    /**
+     * Returns true if this handle contains {@code medicine}.
+     */
+    public boolean equals(Medicine medicine) {
+        if (!getName().equals(medicine.getName().fullName)) {
+            return false;
+        }
+
+        if (type.equals(WarningPanelPredicateType.EXPIRY)) {
+            String actualDetail = "";
+            for (Batch batch: medicine.getBatches().values()) {
+                actualDetail += String.format("%s [Exp: %s]\n",
+                        batch.getBatchNumber().toString(),
+                        batch.getExpiry().toString());
+            }
+
+            return getDetail().equals(actualDetail);
+        } else {
+            // List is for medicines with low stock
+            return getDetail().equals(String.format("Qty: %s\n", medicine.getTotalQuantity().toString()));
+        }
+
+    }
+}

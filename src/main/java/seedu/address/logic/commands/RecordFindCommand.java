a1874253714497ceab507dc5b1dd1e035563d4db
kthSim <kth_sim@hotmail.com>
2019-04-12 18:26:12 +0800

    Added command words. usage, and basic constructor for rfind

--- src/main/java/seedu/address/logic/commands/RecordFindCommand.java
+++ src/main/java/seedu/address/logic/commands/RecordFindCommand.java
@@ -1,4 +1,27 @@
 package seedu.address.logic.commands;
 
+import seedu.address.model.util.predicate.ContainsKeywordsPredicate;
+
+/**
+ * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Keyword matching is case insensitive unless specified.
+ */
 public class RecordFindCommand {
+
+    public static final String COMMAND_WORD = "recordfind";
+    public static final String COMMAND_WORD2 = "rfind";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + " or " + COMMAND_WORD2
+        + ": Finds all records whose particulars contain any of "
+        + "the specified parameter's keywords and displays them as a list with index numbers.\n"
+        + "Specifying CS will search for case sensitivity, while specifying AND will search for patients containing"
+        + "all of the keywords.\n"
+        + "Parameters: [CS] [AND] prefix/KEYWORD [MORE_KEYWORDS]...\n"
+        + "Example: " + COMMAND_WORD + " pro/crown brace";
+
+    private ContainsKeywordsPredicate predicate;
+
+    public RecordFindCommand(ContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
 }

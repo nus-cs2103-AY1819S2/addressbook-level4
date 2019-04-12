bd58b2c301c709162aec53aab54dc12da773947a
JiaHaoLim <jiahaolimwork@hotmail.com>
2019-04-12 19:51:40 +0800

    Refactored ExportCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
@@ -25,99 +25,54 @@ public class ExportCommandParserTest {
 
     private ExportCommandParser parser = new ExportCommandParser();
 
-    @Test
-    public void parse_validArgs_returnsExportCommandSimple() {
-        File test = new File("data" + File.separator + "test.json");
+    private void parse_validArgs(String filename, String type, String range, HashSet<Integer> expectedHash) {
+        File test = new File("data" + File.separator + filename + "." + type);
         try {
-            HashSet<Integer> expectedHash = new HashSet<>();
-            expectedHash.add(1 - 1);
-            ParsedInOut expected = new ParsedInOut(test, "json", expectedHash);
-            ParsedInOut actual = parser.parse(" test.json 1").getParsedInOut();
+            ParsedInOut expected;
+            if (range.equals("all")) {
+                expected = new ParsedInOut(test, type);
+            } else {
+                expected = new ParsedInOut(test, type, expectedHash);
+            }
+            ParsedInOut actual = parser.parse(" " + filename + "." + type + " " + range).getParsedInOut();
             assertEquals(actual.getFile(), expected.getFile());
             assertEquals(actual.getType(), expected.getType());
             assertEquals(actual.getArgIsAll(), expected.getArgIsAll());
-            HashSet<Integer> actualHash = actual.getParsedIndex();
-            for (Integer i : actualHash) {
-                assertTrue(expectedHash.contains(i));
+            if (range.equals("all")) {
+                assertEquals(actual.getParsedIndex().isEmpty(), expected.getParsedIndex().isEmpty());
+            } else {
+                HashSet<Integer> actualHash = actual.getParsedIndex();
+                for (Integer i : actualHash) {
+                    assertTrue(expectedHash.contains(i));
+                }
             }
         } catch (ParseException pe) {
             throw new IllegalArgumentException("Invalid userInput.", pe);
         }
+    }
+
+    @Test
+    public void parse_validArgs_returnsExportCommandSimple() {
+        parse_validArgs("test", "json", "1",
+                new HashSet<>(Arrays.asList(1 - 1)));
     }
 
     @Test
     public void parse_validArgs_returnsExportCommandPermittedSpecialCharacters() {
-        File test = new File("data" + File.separator + "test ! @ # $ % ^ & ( ) _ + - = { } [ ] ; ' , .json");
-        try {
-            HashSet<Integer> expectedHash = new HashSet<>();
-            expectedHash.add(1 - 1);
-            ParsedInOut expected = new ParsedInOut(test, "json", expectedHash);
-            ParsedInOut actual = parser.parse(" test ! @ # $ % ^ & ( ) _ + - = { } [ ] ; ' , .json 1")
-                .getParsedInOut();
-            assertEquals(actual.getFile(), expected.getFile());
-            assertEquals(actual.getType(), expected.getType());
-            assertEquals(actual.getArgIsAll(), expected.getArgIsAll());
-            HashSet<Integer> actualHash = actual.getParsedIndex();
-            for (Integer i : actualHash) {
-                assertTrue(expectedHash.contains(i));
-            }
-        } catch (ParseException pe) {
-            throw new IllegalArgumentException("Invalid userInput.", pe);
-        }
-    }
-
-    @Test
-    public void parse_invalidArgs_returnsExportCommandForbiddenSpecialCharacters() {
-        assertParseFailure(parser, " test < > : \" | ? *.json 1",
-            "Special characters such as\n> < : \" | ? *\nare not allowed."
-                + "\n" + ExportCommand.MESSAGE_USAGE);
+        parse_validArgs("test ! @ # $ % ^ & ( ) _ + - = { } [ ] ; ' , .json", "json", "0",
+                new HashSet<>(Arrays.asList(0 - 1)));
     }
 
     @Test
     public void parse_validArgs_returnsExportCommandPdf() {
-        File test = new File("data" + File.separator + "test.pdf");
-        try {
-            HashSet<Integer> expectedHash = new HashSet<>();
-            expectedHash.add(1 - 1);
-            ParsedInOut expected = new ParsedInOut(test, "pdf", expectedHash);
-            ParsedInOut actual = parser.parse(" test.pdf 1").getParsedInOut();
-            assertEquals(actual.getFile(), expected.getFile());
-            assertEquals(actual.getType(), expected.getType());
-            assertEquals(actual.getArgIsAll(), expected.getArgIsAll());
-            HashSet<Integer> actualHash = actual.getParsedIndex();
-            for (Integer i : actualHash) {
-                assertTrue(expectedHash.contains(i));
-            }
-        } catch (ParseException pe) {
-            throw new IllegalArgumentException("Invalid userInput.", pe);
-        }
+        parse_validArgs("test", "pdf", "1",
+                new HashSet<>(Arrays.asList(1 - 1)));
     }
 
     @Test
     public void parse_validArgs_returnsExportCommandIndexOutOfBound() {
-        File test = new File("data" + File.separator + "test.json");
-        try {
-            HashSet<Integer> expectedHash = new HashSet<>();
-            expectedHash.add(0 - 1);
-            ParsedInOut expected = new ParsedInOut(test, "json", expectedHash);
-            ParsedInOut actual = parser.parse(" test.json 0").getParsedInOut();
-            assertEquals(actual.getFile(), expected.getFile());
-            assertEquals(actual.getType(), expected.getType());
-            assertEquals(actual.getArgIsAll(), expected.getArgIsAll());
-            HashSet<Integer> actualHash = actual.getParsedIndex();
-            for (Integer i : actualHash) {
-                assertTrue(expectedHash.contains(i));
-            }
-        } catch (ParseException pe) {
-            throw new IllegalArgumentException("Invalid userInput.", pe);
-        }
-    }
-
-    @Test
-    public void parse_invalidArgs_throwsParseException() {
-        File test = new File("data" + File.separator + "test.pdf");
-        assertParseFailure(parser, " records.txt 1",
-            ParserUtil.MESSAGE_NOT_JSON_OR_PDF + "\n" + ExportCommand.MESSAGE_USAGE);
+        parse_validArgs("test", "json", "0",
+                new HashSet<>(Arrays.asList(0 - 1)));
     }
 
     @Test
@@ -206,108 +161,74 @@ public class ExportCommandParserTest {
 
     @Test
     public void parse_validArgs_returnsExportCommand1Comma3() {
-        File test = new File("data" + File.separator + "test.json");
-        try {
-            HashSet<Integer> expectedHash = new HashSet<>(Arrays.asList(1 - 1, 3 - 1));
-            ParsedInOut expected = new ParsedInOut(test, "json", expectedHash);
-            ParsedInOut actual = parser.parse(" test.json 1,3").getParsedInOut();
-            assertEquals(actual.getFile(), expected.getFile());
-            assertEquals(actual.getType(), expected.getType());
-            assertEquals(actual.getArgIsAll(), expected.getArgIsAll());
-            HashSet<Integer> actualHash = actual.getParsedIndex();
-            for (Integer i : actualHash) {
-                assertTrue(expectedHash.contains(i));
-            }
-        } catch (ParseException pe) {
-            throw new IllegalArgumentException("Invalid userInput.", pe);
-        }
+        parse_validArgs("test", "json", "1,3",
+                new HashSet<>(Arrays.asList(1 - 1, 3 - 1)));
     }
 
     @Test
     public void parse_validArgs_returnsExportCommand1Dash3() {
-        File test = new File("data" + File.separator + "test.json");
-        try {
-            HashSet<Integer> expectedHash = new HashSet<>(Arrays.asList(1 - 1, 2 - 1, 3 - 1));
-            ParsedInOut expected = new ParsedInOut(test, "json", expectedHash);
-            ParsedInOut actual = parser.parse(" test.json 1-3").getParsedInOut();
-            assertEquals(actual.getFile(), expected.getFile());
-            assertEquals(actual.getType(), expected.getType());
-            assertEquals(actual.getArgIsAll(), expected.getArgIsAll());
-            HashSet<Integer> actualHash = actual.getParsedIndex();
-            for (Integer i : actualHash) {
-                assertTrue(expectedHash.contains(i));
-            }
-        } catch (ParseException pe) {
-            throw new IllegalArgumentException("Invalid userInput.", pe);
-        }
+        parse_validArgs("test", "json", "1-3",
+                new HashSet<>(Arrays.asList(1 - 1, 2 - 1, 3 - 1)));
     }
 
     @Test
     public void parse_validArgs_returnsExportCommand1Comma3Comma5() {
-        File test = new File("data" + File.separator + "test.json");
-        try {
-            HashSet<Integer> expectedHash = new HashSet<>(Arrays.asList(1 - 1, 3 - 1, 5 - 1));
-            ParsedInOut expected = new ParsedInOut(test, "json", expectedHash);
-            ParsedInOut actual = parser.parse(" test.json 1,3,5").getParsedInOut();
-            assertEquals(actual.getFile(), expected.getFile());
-            assertEquals(actual.getType(), expected.getType());
-            assertEquals(actual.getArgIsAll(), expected.getArgIsAll());
-            HashSet<Integer> actualHash = actual.getParsedIndex();
-            for (Integer i : actualHash) {
-                assertTrue(expectedHash.contains(i));
-            }
-        } catch (ParseException pe) {
-            throw new IllegalArgumentException("Invalid userInput.", pe);
-        }
+        parse_validArgs("test", "json", "1,3,5",
+                new HashSet<>(Arrays.asList(1 - 1, 3 - 1, 5 - 1)));
     }
+
 
     @Test
     public void parse_validArgs_returnsExportCommand1Comma3Dash5() {
-        File test = new File("data" + File.separator + "test.json");
-        try {
-            HashSet<Integer> expectedHash = new HashSet<>(Arrays.asList(1 - 1, 3 - 1, 4 - 1, 5 - 1));
-            ParsedInOut expected = new ParsedInOut(test, "json", expectedHash);
-            ParsedInOut actual = parser.parse(" test.json 1,3-5").getParsedInOut();
-            assertEquals(actual.getFile(), expected.getFile());
-            assertEquals(actual.getType(), expected.getType());
-            assertEquals(actual.getArgIsAll(), expected.getArgIsAll());
-            HashSet<Integer> actualHash = actual.getParsedIndex();
-            for (Integer i : actualHash) {
-                assertTrue(expectedHash.contains(i));
-            }
-        } catch (ParseException pe) {
-            throw new IllegalArgumentException("Invalid userInput.", pe);
-        }
+        parse_validArgs("test", "json", "1,3-5",
+                new HashSet<>(Arrays.asList(1 - 1, 3 - 1, 4 - 1, 5 - 1)));
     }
 
     @Test
     public void parse_validArgs_returnsExportCommandAll() {
-        File test = new File("data" + File.separator + "test.json");
-        try {
-            ParsedInOut expected = new ParsedInOut(test, "json");
-            ParsedInOut actual = parser.parse(" test.json all").getParsedInOut();
-            assertEquals(actual.getFile(), expected.getFile());
-            assertEquals(actual.getType(), expected.getType());
-            assertEquals(actual.getArgIsAll(), expected.getArgIsAll());
-            assertEquals(actual.getParsedIndex().isEmpty(), expected.getParsedIndex().isEmpty());
-        } catch (ParseException pe) {
-            throw new IllegalArgumentException("Invalid userInput.", pe);
-        }
+        parse_validArgs("test", "json", "all", new HashSet<>());
     }
 
 
     @Test
     public void parse_validArgs_returnsExportCommandEmptyFilename() {
-        File test = new File("data" + File.separator + ".json");
-        try {
-            ParsedInOut expected = new ParsedInOut(test, "json");
-            ParsedInOut actual = parser.parse(" .json all").getParsedInOut();
-            assertEquals(actual.getFile(), expected.getFile());
-            assertEquals(actual.getType(), expected.getType());
-            assertEquals(actual.getArgIsAll(), expected.getArgIsAll());
-            assertEquals(actual.getParsedIndex().isEmpty(), expected.getParsedIndex().isEmpty());
-        } catch (ParseException pe) {
-            throw new IllegalArgumentException("Invalid userInput.", pe);
-        }
+        parse_validArgs("", "json", "all", new HashSet<>());
+    }
+
+    @Test
+    public void parse_invalidArgs_ExportCommandForbiddenSpecialCharacters() {
+        assertParseFailure(parser, " test < > : \" | ? *.json 1",
+                "Special characters such as\n> < : \" | ? *\nare not allowed."
+                        + "\n" + ExportCommand.MESSAGE_USAGE);
+    }
+
+    @Test
+    public void parse_invalidArgs_NotJsonOrPdf() {
+        assertParseFailure(parser, " records.txt 1",
+                ParserUtil.MESSAGE_NOT_JSON_OR_PDF + "\n" + ExportCommand.MESSAGE_USAGE);
+    }
+
+    @Test
+    public void parse_invalidArgs_DoubleDash() {
+        assertParseFailure(parser, " records.json 1--3",
+                ParserUtil.MESSAGE_INVALID_INDEX_RANGE + "\n" + ExportCommand.MESSAGE_USAGE);
+    }
+
+    @Test
+    public void parse_invalidArgs_DoubleComma() {
+        assertParseFailure(parser, " records.json 1,,3",
+                ParserUtil.MESSAGE_INVALID_INDEX_RANGE + "\n" + ExportCommand.MESSAGE_USAGE);
+    }
+
+    @Test
+    public void parse_invalidArgs_CommaDashMash() {
+        assertParseFailure(parser, " records.json 1,-,3",
+                ParserUtil.MESSAGE_INVALID_INDEX_RANGE + "\n" + ExportCommand.MESSAGE_USAGE);
+    }
+
+    @Test
+    public void parse_invalidArgs_DashChain() {
+        assertParseFailure(parser, " records.json 1-3-5",
+                ParserUtil.MESSAGE_INVALID_INDEX_RANGE + "\n" + ExportCommand.MESSAGE_USAGE);
     }
 }

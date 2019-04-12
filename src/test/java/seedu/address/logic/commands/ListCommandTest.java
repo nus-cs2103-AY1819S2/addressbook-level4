98d8b6de29cedcef98bb3ca0e5d78a0c27fda004
Ian Wu <iwle@u.nus.edu>
2019-04-12 20:54:56 +0800

    Add tests for ListCommand
    Add testing for different equivalence partitions of name and tag
    combinations: single tag, single name, single name and multiple tags,
    multiple names and single tag, and multiple names and multiple tags.

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -1,10 +1,12 @@
 package seedu.address.logic.commands;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -19,9 +21,9 @@ import seedu.address.logic.battle.state.BattleState;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.battleship.Battleship;
-import seedu.address.model.battleship.BattleshipTest;
 import seedu.address.model.battleship.CruiserBattleship;
 import seedu.address.model.battleship.DestroyerBattleship;
+import seedu.address.model.battleship.Name;
 import seedu.address.model.battleship.Orientation;
 import seedu.address.model.cell.Coordinates;
 import seedu.address.model.tag.Tag;
@@ -45,16 +47,19 @@ public class ListCommandTest {
     private Logic logic;
     private CommandHistory commandHistory = new CommandHistory();
 
-    private Set<Tag> testTagSet1 = new HashSet<>(); // to contain testTag1, testTag2
-    private Set<Tag> testTagSet2 = new HashSet<>(); // to contain testTag1 only
-    private Set<Tag> emptyTagSet = new HashSet<>(); // to contain no tags
+    private Set<Tag> testTagSet1 = new HashSet<>(); // to contain testTag1, testTag2 only battleship 1
+    private Set<Tag> testTagSet2 = new HashSet<>(); // to contain testTag1 both battleship 1 and 2
+    private Set<Tag> emptyTagSet = new HashSet<>(); // to contain no tags all battleships
+
+    private Set<Name> destroyerOnlySet = new HashSet<>(); // to contain only battleship 1
+    private Set<Name> cruiserOnlySet = new HashSet<>(); // to contain only battleship 2
+    private Set<Name> cruiserAndDestroyerSet = new HashSet<>(); // to contain both battleship 1 and 2
 
     private Tag testTag1 = new Tag("testTag1");
     private Tag testTag2 = new Tag("testTag2");
 
     private Battleship battleshipOne;
     private Battleship battleshipTwo;
-    private Battleship battleshipThree;
 
     @Before
     public void setUp() {
@@ -72,11 +77,15 @@ public class ListCommandTest {
 
         battleshipOne = new DestroyerBattleship(testTagSet1);
         battleshipTwo = new CruiserBattleship(testTagSet2);
-        battleshipThree = new DestroyerBattleship(emptyTagSet);
+
+        destroyerOnlySet.add(Battleship.BattleshipType.DESTROYER.getName());
+        cruiserOnlySet.add(Battleship.BattleshipType.CRUISER.getName());
+
+        cruiserAndDestroyerSet.add(Battleship.BattleshipType.CRUISER.getName());
+        cruiserAndDestroyerSet.add(Battleship.BattleshipType.DESTROYER.getName());
 
         model.deployBattleship(battleshipOne, VALID_COORDINATES_A1, VERTICAL_ORIENTATION);
         model.deployBattleship(battleshipTwo, VALID_COORDINATES_A2, VERTICAL_ORIENTATION);
-        model.deployBattleship(battleshipThree, VALID_COORDINATES_A3, VERTICAL_ORIENTATION);
     }
 
     @Test
@@ -89,14 +98,114 @@ public class ListCommandTest {
         expectedOutput.append(buildOutput(battleshipOne, VALID_COORDINATES_A1, VERTICAL_ORIENTATION))
                 .append("\n")
                 .append(buildOutput(battleshipTwo, VALID_COORDINATES_A2, VERTICAL_ORIENTATION))
-                .append("\n")
-                .append(buildOutput(battleshipThree, VALID_COORDINATES_A3, VERTICAL_ORIENTATION))
                 .append("\n");
 
         CommandResult expectedCommandResult = new CommandResult(expectedOutput.toString(), false, false);
-        CommandResult command = listCommand.execute(model, commandHistory);
-
         assertCommandSuccess(listCommand, model, commandHistory, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_listByName_success() {
+        // The command is: list n/destroyers
+        ListCommand listCommand = new ListCommand(Optional.of(destroyerOnlySet), Optional.empty());
+
+        StringBuilder expectedOutput = new StringBuilder();
+
+        expectedOutput.append(buildOutput(battleshipOne, VALID_COORDINATES_A1, VERTICAL_ORIENTATION))
+                .append("\n");
+
+        CommandResult expectedCommandResult = new CommandResult(expectedOutput.toString(), false, false);
+        assertCommandSuccess(listCommand, model, commandHistory, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_listByMultipleTags_success() {
+        // The command is: list t/testTag1 t/testTag2
+        ListCommand listCommand = new ListCommand(Optional.empty(), Optional.of(testTagSet1));
+
+        StringBuilder expectedOutput = new StringBuilder();
+
+        expectedOutput.append(buildOutput(battleshipOne, VALID_COORDINATES_A1, VERTICAL_ORIENTATION))
+                .append("\n");
+
+        CommandResult expectedCommandResult = new CommandResult(expectedOutput.toString(), false, false);
+        assertCommandSuccess(listCommand, model, commandHistory, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_listBySingleTag_success() {
+        // The command is: list t/testTag1
+        ListCommand listCommand = new ListCommand(Optional.empty(), Optional.of(testTagSet2));
+        List<String> expectedStringList = new ArrayList<>();
+
+        expectedStringList.add(buildOutput(battleshipOne, VALID_COORDINATES_A1, VERTICAL_ORIENTATION));
+        expectedStringList.add(buildOutput(battleshipTwo, VALID_COORDINATES_A2, VERTICAL_ORIENTATION));
+
+        assertTrue(isCommandResultEqual(expectedStringList,
+                listCommand.execute(model, commandHistory).getFeedbackToUser()));
+    }
+
+    @Test
+    public void execute_listByMultipleNames_success() {
+        // The command is: list n/destroyer n/cruiser
+        ListCommand listCommand = new ListCommand(Optional.of(cruiserAndDestroyerSet), Optional.empty());
+        List<String> expectedStringList = new ArrayList<>();
+
+        expectedStringList.add(buildOutput(battleshipOne, VALID_COORDINATES_A1, VERTICAL_ORIENTATION));
+        expectedStringList.add(buildOutput(battleshipTwo, VALID_COORDINATES_A2, VERTICAL_ORIENTATION));
+
+        Collections.sort(expectedStringList);
+
+        assertTrue(isCommandResultEqual(expectedStringList,
+                listCommand.execute(model, commandHistory).getFeedbackToUser()));
+    }
+
+    @Test
+    public void execute_listBySingleNameAndMultipleTags_success() {
+        // The command is: list n/destroyer t/testTag1 t/testTag2
+        setUp();
+        ListCommand listCommand = new ListCommand(Optional.of(destroyerOnlySet), Optional.of(testTagSet1));
+
+        StringBuilder expectedOutput = new StringBuilder();
+
+        expectedOutput.append(buildOutput(battleshipOne, VALID_COORDINATES_A1, VERTICAL_ORIENTATION))
+                .append("\n");
+
+        CommandResult expectedCommandResult = new CommandResult(expectedOutput.toString(), false, false);
+        assertCommandSuccess(listCommand, model, commandHistory, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_listByMultipleNamesAndSingleTag_success() {
+        // The command is: list n/destroyer n/cruiser t/tagSet1
+        ListCommand listCommand = new ListCommand(Optional.of(cruiserAndDestroyerSet), Optional.of(testTagSet2));
+        List<String> expectedStringList = new ArrayList<>();
+
+        expectedStringList.add(buildOutput(battleshipTwo, VALID_COORDINATES_A2, VERTICAL_ORIENTATION));
+        expectedStringList.add(buildOutput(battleshipOne, VALID_COORDINATES_A1, VERTICAL_ORIENTATION));
+
+        Collections.sort(expectedStringList);
+
+        assertTrue(isCommandResultEqual(expectedStringList,
+                listCommand.execute(model, commandHistory).getFeedbackToUser()));
+    }
+
+
+    /**
+     * Compares a command result string as a sorted list with an expected list.
+     */
+    private boolean isCommandResultEqual(List<String> expectedList, String commandResultString) {
+        List<String> resultStringList = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String entry : commandResultString.split("\n")) {
+            resultStringList.add(entry);
+        }
+
+        Collections.sort(resultStringList);
+        Collections.sort(expectedList);
+
+        return expectedList.equals(resultStringList);
     }
 
     /**

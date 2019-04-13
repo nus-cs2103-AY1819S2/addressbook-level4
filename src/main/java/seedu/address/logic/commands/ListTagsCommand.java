8496ca844b28e690424396706801f843faa9aaf5
Ian Wu <iwle@u.nus.edu>
2019-04-13 23:36:43 +0800

    Add JavaDocs to ListTagsCommand

--- src/main/java/seedu/address/logic/commands/ListTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListTagsCommand.java
@@ -11,7 +11,7 @@ import seedu.address.model.Model;
 import seedu.address.model.tag.Tag;
 
 /**
- * Lists all persons in the address book to the user.
+ * Lists all the tags that have been used by ships deployed in the game.
  */
 public class ListTagsCommand extends Command {
 
@@ -31,20 +31,27 @@ public class ListTagsCommand extends Command {
             BattleState.ENEMY_ATTACK));
     }
 
+    /**
+     * Lists all the tags that have been used by ships deployed in the game.
+     * @param model {@code Model} which the command should operate on.
+     * @param history {@code CommandHistory} which the command should operate on.
+     * @return
+     */
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
         assert canExecuteIn(model.getBattleState());
 
         StringBuilder stringBuilder = new StringBuilder();
-
         tagSet = model.getHumanPlayer().getFleet().getAllTags();
+
         if (tagSet.isEmpty()) {
             stringBuilder.append(TAGS_NOT_FOUND);
         } else {
             int tagSetSize = tagSet.size();
             int counter = 0;
             stringBuilder.append(tagSetSize).append(TAGS_FOUND);
+
             for (Tag tag : tagSet) {
                 counter++;
                 stringBuilder.append(tag.getTagName());
@@ -58,10 +65,19 @@ public class ListTagsCommand extends Command {
         return new CommandResult(MESSAGE_SUCCESS + stringBuilder.toString());
     }
 
+    /**
+     * Getter method for {@code this.tagSet}.
+     * @return {@code this.tagSet}.
+     */
     public Set<Tag> getTagSet() {
         return tagSet;
     }
 
+    /**
+     * Checks equality of two ListTagsCommand objects by comparing both {@code tagSet}s.
+     * @param other any object.
+     * @return boolean of whether the objects are equal.
+     */
     @Override
     public boolean equals(Object other) {
         if (other == this) {

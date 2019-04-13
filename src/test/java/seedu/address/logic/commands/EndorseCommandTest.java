3aee6086299bb6c49308fb30a8a6aa857c8312df
abwilli <abwilli@live.unc.edu>
2019-04-13 12:21:22 +0800

    checkstyle tests

--- src/test/java/seedu/address/logic/commands/EndorseCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EndorseCommandTest.java
@@ -1,23 +1,19 @@
 package seedu.address.logic.commands;
 
-
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Set;
 
+import org.junit.Test;
 import seedu.address.logic.CommandHistory;
-
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.SkillsTag;
-
-import org.junit.Test;
-
 /**
  * Contains integration tests and unit tests for EndorseCommand.
  */

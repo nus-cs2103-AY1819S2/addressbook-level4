f745a2b3d40f7ae359f4d864a4f914833faea6dc
Liuyy99 <e0253712@u.nus.edu>
2019-04-13 15:14:49 +0800

    delete test cases

--- src/test/java/seedu/address/logic/commands/DeletePatientCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeletePatientCommandTest.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
@@ -21,7 +20,7 @@ import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Patient;
 
 /**
- * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for
+ * Contains integration tests (interaction with the Model) and unit tests for
  * {@code DeletePatientCommand}.
  */
 public class DeletePatientCommandTest {
--- src/test/java/seedu/address/logic/commands/EditDoctorCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditDoctorCommandTest.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_ALVINA;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_STEVEN;

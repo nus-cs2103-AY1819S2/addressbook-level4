1d3b3796f66ef2552b06e160ae136c4c5b64ca97
JeremyLoye <jeremyloye@gmail.com>
2019-04-13 21:56:35 +0800

    Added BudgetTest implementation

--- src/test/java/seedu/finance/logic/commands/SetFileCommandTest.java
+++ src/test/java/seedu/finance/logic/commands/SetFileCommandTest.java
@@ -13,6 +13,7 @@ import java.nio.file.Paths;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
 import seedu.finance.logic.CommandHistory;
 import seedu.finance.model.FinanceTracker;
 import seedu.finance.model.Model;
@@ -26,11 +27,11 @@ public class SetFileCommandTest {
     private static final Path BLANK_DATA_FILE = TEST_DATA_FOLDER.resolve("blankDataFile.json");
     private static final Path INVALID_DATA_FILE = TEST_DATA_FOLDER.resolve("invalidDataFile.json");
 
-    private Model model = new ModelManager(getTypicalFinanceTrackerWithoutSetBudget(), new UserPrefs());
-    private CommandHistory commandHistory = new CommandHistory();
-
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
+    private Model model = new ModelManager(getTypicalFinanceTrackerWithoutSetBudget(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
 
     @Test
     public void constructor_nullPath_throwsNullPointerException() {
@@ -42,7 +43,7 @@ public class SetFileCommandTest {
     public void execute_incorrectDataFormat_setFileSuccessful() {
         SetFileCommand setFileCommand = new SetFileCommand(INVALID_DATA_FILE);
         CommandResult expectedCommandResult =
-                new CommandResult(String.format(MESSAGE_SUCCESS,INVALID_DATA_FILE), true, false, false);
+                new CommandResult(String.format(MESSAGE_SUCCESS, INVALID_DATA_FILE), true, false, false);
         Model expectedModel = new ModelManager(getTypicalFinanceTrackerWithoutSetBudget(), new UserPrefs());
         expectedModel.addPreviousDataFile(model.getFinanceTrackerFilePath());
         expectedModel.setFinanceTrackerFilePath(INVALID_DATA_FILE);
@@ -56,7 +57,7 @@ public class SetFileCommandTest {
     public void execute_newFile_setFileSuccessful() {
         SetFileCommand setFileCommand = new SetFileCommand(BLANK_DATA_FILE);
         CommandResult expectedCommandResult =
-                new CommandResult(String.format(MESSAGE_SUCCESS,BLANK_DATA_FILE), true, false, false);
+                new CommandResult(String.format(MESSAGE_SUCCESS, BLANK_DATA_FILE), true, false, false);
         Model expectedModel = new ModelManager(getTypicalFinanceTrackerWithoutSetBudget(), new UserPrefs());
         expectedModel.addPreviousDataFile(model.getFinanceTrackerFilePath());
         expectedModel.setFinanceTrackerFilePath(BLANK_DATA_FILE);

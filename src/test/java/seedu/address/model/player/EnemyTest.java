44a03c5ca9b9bb8de851606ddb5494682fb339bf
lucydotc <lucy@u.nus.edu>
2019-04-13 15:27:53 +0800

    Add tests for public methods in Enemy:
    receiveStatus()
    getters
    prepEnemy()

--- src/test/java/seedu/address/model/player/EnemyTest.java
+++ src/test/java/seedu/address/model/player/EnemyTest.java
@@ -1,95 +1,175 @@
 package seedu.address.model.player;
 
 import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.assertj.core.api.Assertions.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.testutil.SizeTenMapGrid.initialisePlayerSizeTen;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
+
+import seedu.address.model.cell.Coordinates;
+import seedu.address.model.cell.Status;
 
 public class EnemyTest {
 
-    public final Enemy testPlayer = new Enemy();
+    private Enemy testEnemy;
+    private Player testPlayer;
+
+    @Before
+    public void readyEnemyForTesting() {
+
+        testEnemy = new Enemy();
+        testPlayer = new Player();
+        initialisePlayerSizeTen(testEnemy);
+        initialisePlayerSizeTen(testPlayer);
+    }
+
+    boolean isSubset(List<?> list, List<?> sublist) {
+        return Collections.indexOfSubList(list, sublist) != -1;
+    }
+
+    public static <T> boolean isSameListContents(List<T> list1, List<T> list2) {
+        return new HashSet<>(list1).equals(new HashSet<>(list2));
+    }
 
     //Lucy TO-FIX:
-    /*  @Test
-
+    @Test
     public void constructor_default() {
-        Player newPlayer1 = new Player();
-        Player newPlayer2 = new Player("Player1", 5);
-        assertEquals(newPlayer1, newPlayer2);
+        //stub
     }
-    */
 
     @Test
     public void test_getName() {
-        assertEquals("EnemyPlayer", testPlayer.getName());
+        assertEquals("EnemyPlayer", testEnemy.getName());
     }
 
     @Test
     public void test_getFleetSize() {
-        assertEquals(8, testPlayer.getFleetSize());
+        assertEquals(8, testEnemy.getFleetSize());
     }
 
     @Test
     public void test_getFleetContents() {
-        Player newPlayer1 = new Player("Bob", 5, 2, 1);
-        assertEquals(newPlayer1.getFleetContents(), testPlayer.getFleetContents());
+        assertEquals(testPlayer.getFleetContents(), testEnemy.getFleetContents());
     }
 
     @Test
     public void test_getMapGrid() {
-        Player newPlayer1 = new Player("Bob", 5, 2, 1);
-        assertEquals(newPlayer1.getMapGrid(), testPlayer.getMapGrid());
+        assertEquals(testPlayer.getMapGrid(), testEnemy.getMapGrid());
     }
 
     @Test public void test_getTargetHistory() {
-        Player newPlayer1 = new Player("Bob", 5, 2, 1);
-        assertEquals(newPlayer1.getTargetHistory(), testPlayer.getTargetHistory());
+        assertEquals(testPlayer.getTargetHistory(), testEnemy.getTargetHistory());
+    }
+
+    @Test public void test_getLastAttackStatus() {
+
+        testEnemy.receiveStatus(Status.SHIP);
+
+        assertThat(testEnemy.getLastAttackStatus(), is(Status.SHIP));
+    }
+
+    @Test public void test_getAllPossibleTargets() {
+        assertTrue(testEnemy.getAllPossibleTargets() instanceof ArrayList);
+    }
+
+    @Test public void test_getAllPossiblePopulateCoords() {
+        assertTrue(testEnemy.getAllPossiblePopulateCoords() instanceof ArrayList);
+    }
+
+    @Test public void test_getAllParityTargets() {
+        assertTrue(testEnemy.getAllParityTargets() instanceof ArrayList);
+
+    }
+
+
+    /**
+     * this method calls two other private methods:  fillWithAllCoords() and populateEnemyMap();
+     * testing will be done in two phases, one for  fillWithAllCoords() and one for populateEnemyMap();
+     *
+     * Setup:
+     *     Enemy should first have:
+     *          empty lists: allPossibleTargets, allPossiblePopulateCoords, allParityTargets
+     *          empty deployed fleet
+     * Expected result:
+     *     Enemy should have:
+     *          filled allPossibleTargets, allPossiblePopulateCoords, allParityTargets
+     *          allParityTargets should only contain coordinates that hasParity()
+     *          allPossibleTargets must be size 100, allPossiblePopulateCoords must be < 100, allParityTargets must be 50
+     *          allParityTargets should be proper subset of allPossibleTargets
+     *          allPossiblePopulateCoords should be proper subset of allPossibleTargets
+     *          filled deployed fleet with the correct number of each type of battleships
+     *
+     */
+    @Test public void test_prepEnemy() {
+        //tests for fillWithAllCoords()
+
+        //first check empty lists: allPossibleTargets, allPossiblePopulateCoords, allParityTargets
+        assertTrue(testEnemy.getAllPossibleTargets().isEmpty());
+        assertTrue(testEnemy.getAllPossiblePopulateCoords().isEmpty());
+        assertTrue(testEnemy.getAllParityTargets().isEmpty());
+
+        //check that there are no deployed ships yet - deployedFleet is empty
+        assertTrue(testEnemy.getFleet().getDeployedFleet().isEmpty());
+
+        testEnemy.prepEnemy();
+
+        //check that allPossibleTargets, allPossiblePopulateCoords, allParityTargets are not empty
+        assertFalse(testEnemy.getAllPossibleTargets().isEmpty());
+        assertFalse(testEnemy.getAllPossiblePopulateCoords().isEmpty());
+        assertFalse(testEnemy.getAllParityTargets().isEmpty());
+
+        //check that allParityTargets should only contain coordinates that hasParity()
+        for (Coordinates parityElement : testEnemy.getAllParityTargets()) {
+            assertTrue(testEnemy.hasParity(parityElement.getRowIndex().getZeroBased(),
+                    parityElement.getColIndex().getZeroBased()));
+        }
+
+        //check sizes of allPossibleTargets, allPossiblePopulateCoords, allParityTargets
+        assertEquals(testEnemy.getAllPossibleTargets().size(), 100);
+        assertTrue(testEnemy.getAllPossiblePopulateCoords().size() < 100);
+        assertEquals(testEnemy.getAllParityTargets().size(), 50);
+
+
+        //check that allParityTargets should be proper subset of allPossibleTargets
+        assertFalse(isSameListContents(testEnemy.getAllParityTargets(), testEnemy.getAllPossibleTargets()));
+        assertTrue(isSubset(testEnemy.getAllPossibleTargets(), testEnemy.getAllParityTargets()));
+
+        //check that allPossiblePopulateCoords should be proper subset of allPossibleTargets
+        assertFalse(isSameListContents(testEnemy.getAllPossiblePopulateCoords(), testEnemy.getAllPossibleTargets()));
+        assertTrue(isSubset(testEnemy.getAllPossiblePopulateCoords(), testEnemy.getAllParityTargets()));
+
+        //check that the correct number of ships were deployed into deployedFleet
+        assertTrue(testEnemy.getFleet().isAllDeployed());
     }
 
     @Test public void test_enemyShootAt() {
         //stub
     }
 
-    @Test public void test_prepEnemy() {
-        //stub
+
+    /**
+     * Expected result:
+     *
+     *  testEnemy should have its lastAttackStatus attribute be updated to
+     *  the status passed into the receiveStatus method
+     *
+     */
+    @Test public void test_receiveStatus() {
+
+        Status giveStatus = Status.EMPTY;
+        testEnemy.receiveStatus(giveStatus);
+
+        assertThat(testEnemy.getLastAttackStatus(), is(giveStatus));
     }
 
-    @Test public void test_drawPossibleTarget() {
-        //stub
-    }
 
-    @Test public void test_fillallPossibleTargets() {
-        //stub
-    }
-
-    @Test public void test_fillallPossiblePopulateCoords() {
-        //stub
-    }
-
-    @Test public void test_populateEnemyMap() {
-        //stub
-    }
-
-    @Test public void test_placeAirCraftCarrier () {
-        //stub
-    }
-
-    @Test public void test_placeMultipleDestroyerAndCruiser () {
-        //stub
-    }
-
-    @Test public void test_generateBattleships () {
-        //stub
-    }
-
-    @Test public void test_markAsOccupied () {
-        //stub
-    }
-
-    @Test public void test_justifyCoord () {
-        //stub
-    }
-
-    @Test public void test_generateOrientation() {
-        //stub
-    }
 }

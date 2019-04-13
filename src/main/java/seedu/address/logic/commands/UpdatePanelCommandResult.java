624cd730138faf8c5ee7e2b6fe2f98cea1f944d7
sciffany <sciffany@gmail.com>
2019-04-13 13:50:35 +0800

    Try to solve editcarderror

--- src/main/java/seedu/address/logic/commands/UpdatePanelCommandResult.java
+++ src/main/java/seedu/address/logic/commands/UpdatePanelCommandResult.java
@@ -8,4 +8,12 @@ public class UpdatePanelCommandResult extends CommandResult {
     public UpdatePanelCommandResult(String feedbackToUser) {
         super(feedbackToUser);
     }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof UpdatePanelCommandResult // instanceof handles nulls
+                && feedbackToUser.equals(((UpdatePanelCommandResult) other).feedbackToUser));
+    }
 }
+

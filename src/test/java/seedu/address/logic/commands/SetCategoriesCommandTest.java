a76487464cd992c780a904de88dbb1c398ad349e
rbth7e5 <caijie96@gmail.com>
2019-04-13 16:43:18 +0800

    add tests for Categories

--- src/test/java/seedu/address/logic/commands/SetCategoriesCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SetCategoriesCommandTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_CUISINE_FAST_FOOD;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_OCCASION_CASUAL;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PRICE_RANGE;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_RESTAURANT;
 import static seedu.address.testutil.TypicalRestaurants.getTypicalFoodDiary;
@@ -31,18 +32,38 @@ public class SetCategoriesCommandTest {
     private CommandHistory commandHistory = new CommandHistory();
 
     @Test
-    public void execute_validCuisineUnfilteredList_success() {
-        Restaurant restaurantCuisineAdded = new RestaurantBuilder(model.getFilteredRestaurantList().get(0))
-                .withCategories(VALID_CUISINE_FAST_FOOD, VALID_OCCASION_CASUAL, VALID_PRICE_RANGE).build();
+    public void execute_validCategoriesUnfilteredList_success() {
+        Restaurant restaurantCategoriesAdded = new RestaurantBuilder(model.getFilteredRestaurantList().get(0))
+                .withCategories(new Cuisine(VALID_CUISINE_FAST_FOOD), new Occasion(VALID_OCCASION_CASUAL),
+                        new PriceRange(VALID_PRICE_RANGE)).build();
         Categories validCategories = new Categories(new Cuisine(VALID_CUISINE_FAST_FOOD),
                 new Occasion(VALID_OCCASION_CASUAL), new PriceRange(VALID_PRICE_RANGE));
         SetCategoriesCommand categoryCommand = new SetCategoriesCommand(INDEX_FIRST_RESTAURANT, validCategories);
 
-        String expectedMessage = String.format(SetCategoriesCommand.MESSAGE_SET_CUISINE_SUCCESS,
-                restaurantCuisineAdded);
+        String expectedMessage = String.format(SetCategoriesCommand.MESSAGE_SET_CATEGORIES_SUCCESS,
+                restaurantCategoriesAdded);
 
         Model expectedModel = new ModelManager(getTypicalFoodDiary(), new UserPrefs(), new PostalDataSet());
-        expectedModel.setRestaurant(model.getFilteredRestaurantList().get(0), restaurantCuisineAdded);
+        expectedModel.setRestaurant(model.getFilteredRestaurantList().get(0), restaurantCategoriesAdded);
+        expectedModel.commitFoodDiary();
+
+        assertCommandSuccess(categoryCommand, model, commandHistory, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_someCategoriesUnfilteredList_success() {
+        Restaurant restaurantCategoriesAdded = new RestaurantBuilder(model.getFilteredRestaurantList().get(0))
+                .withCategories(new Cuisine(VALID_CUISINE_FAST_FOOD), null, new PriceRange(VALID_PRICE_RANGE))
+                .build();
+        Categories someCategories = new Categories(new Cuisine(VALID_CUISINE_FAST_FOOD), null,
+                new PriceRange(VALID_PRICE_RANGE));
+        SetCategoriesCommand categoryCommand = new SetCategoriesCommand(INDEX_FIRST_RESTAURANT, someCategories);
+
+        String expectedMessage = String.format(SetCategoriesCommand.MESSAGE_SET_CATEGORIES_SUCCESS,
+                restaurantCategoriesAdded);
+
+        Model expectedModel = new ModelManager(getTypicalFoodDiary(), new UserPrefs(), new PostalDataSet());
+        expectedModel.setRestaurant(model.getFilteredRestaurantList().get(0), restaurantCategoriesAdded);
         expectedModel.commitFoodDiary();
 
         assertCommandSuccess(categoryCommand, model, commandHistory, expectedMessage, expectedModel);
--- src/test/java/seedu/address/testutil/RestaurantBuilder.java
+++ src/test/java/seedu/address/testutil/RestaurantBuilder.java
@@ -55,7 +55,7 @@ public class RestaurantBuilder {
         tags = new HashSet<>();
         weblink = new Weblink(DEFAULT_WEBLINK);
         openingHours = new OpeningHours(DEFAULT_OPENING_HOURS);
-        categories = null;
+        categories = Categories.empty();
     }
 
     /**
@@ -159,8 +159,8 @@ public class RestaurantBuilder {
     /**
      * Sets the {@code Cuisine} of the {@code Restaurant} that we are building.
      */
-    public RestaurantBuilder withCategories(String cuisine, String occasion, String priceRange) {
-        this.categories = new Categories(new Cuisine(cuisine), new Occasion(occasion), new PriceRange(priceRange));
+    public RestaurantBuilder withCategories(Cuisine cuisine, Occasion occasion, PriceRange priceRange) {
+        this.categories = new Categories(cuisine, occasion, priceRange);
         return this;
     }
 

9e702e869d5b31086bcd9d81640093c4a548ee78
chiuyuhua <chiuyuhua@u.nus.edu>
2019-04-14 05:46:42 +0800

    Adjust UI

--- src/test/java/seedu/address/model/analytics/AnalyticsTest.java
+++ src/test/java/seedu/address/model/analytics/AnalyticsTest.java
@@ -1,4 +1,24 @@
 package seedu.address.model.analytics;
 
 public class AnalyticsTest {
+
+    //test null values
+    //test empty person list
+
+    //test if calculate properly?
+    //test meanGrade
+    //test jobApplicationBreakdown
+    //test meanInterviewScores
+    //test genderBreakdown
+    //test raceBreakdown
+    //test majorBreakdown
+    //test schoolBreakdown
+    //test pastJobBreakdown
+
+    //test generateMeanGradeData
+    //test generateInterviewScoresData
+    //test generateRaceData
+    //test generateGenderData
+    //test generateBarChartDataFromSet
+
 }

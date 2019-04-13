d9de626d1ed17e5c418f7efd30cdee9dd8a9f846
abwilli <abwilli@live.unc.edu>
2019-04-13 15:42:14 +0800

    endorse clear test

--- src/test/java/seedu/address/logic/commands/EndorseCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EndorseCommandTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -50,4 +51,31 @@ public class EndorseCommandTest {
 
         assertCommandSuccess(endorseCommand, model, commandHistory, expectedMessage, expectedModel);
     }
+    
+    @Test
+    public void execute_clearEndorsePerson_success() throws CommandException {
+        Person samplePerson = model.getFilteredPersonList().get(0);
+        SkillsTag sampleEndorseTag = new SkillsTag(VALID_ENDORSE_NAME, "endorse");
+        Set<SkillsTag> samplePersonTags = new HashSet<>(samplePerson.getTags());
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        EndorseCommand endorseCommand = new EndorseCommand(ENDORSE_PROCESS, INDEX_FIRST_PERSON, VALID_ENDORSE_NAME);
+        endorseCommand.execute(model, new CommandHistory());
+
+        endorseCommand.execute(expectedModel, new CommandHistory());
+        EndorseCommand clearEndorseCommand = new EndorseCommand(CLEAR_PROCESS, INDEX_FIRST_PERSON, VALID_ENDORSE_NAME);
+
+        samplePersonTags.remove(sampleEndorseTag);
+        Person editedPerson = new Person(samplePerson.getName(),
+                samplePerson.getPhone(), samplePerson.getEmail(),
+                samplePerson.getEducation(), samplePerson.getGpa(),
+                samplePerson.getAddress(), samplePersonTags);
+        String expectedMessage = String.format(EndorseCommand.MESSAGE_REMOVE_ENDORSE_SUCESS, editedPerson.getName());
+
+
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
+        expectedModel.commitAddressBook();
+
+        assertCommandSuccess(clearEndorseCommand, model, commandHistory, expectedMessage, expectedModel);
+    }
+
 }

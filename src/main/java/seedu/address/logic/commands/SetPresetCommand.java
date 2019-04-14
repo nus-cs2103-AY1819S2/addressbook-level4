35d423110a8b93e767a8c4db576fd2d0cecac6e2
thamsimun <e0201281@u.nus.edu>
2019-04-14 14:22:53 +0800

    edit userguide

--- src/main/java/seedu/address/logic/commands/SetPresetCommand.java
+++ src/main/java/seedu/address/logic/commands/SetPresetCommand.java
@@ -49,9 +49,6 @@ public class SetPresetCommand extends Command {
         }
         List<Command> presetList = transformationSet.findTransformation(presetName);
         this.hasWaterMarkCommand = transformationSet.hasWaterMarkCommand(presetName);
-        initialImage.getUrl();
-
-        System.out.print(presetList);
         StringBuilder toPrint = new StringBuilder();
         for (Command command: presetList) {
             try {

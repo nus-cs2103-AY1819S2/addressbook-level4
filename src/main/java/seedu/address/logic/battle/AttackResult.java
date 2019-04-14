27fcc0e76be544b6bc99b57cb58cacd4b6f6ca1a
s-tr <swchong25+github@gmail.com>
2019-04-14 20:55:41 +0800

    Update javadoc for AttackResult

--- src/main/java/seedu/address/logic/battle/AttackResult.java
+++ src/main/java/seedu/address/logic/battle/AttackResult.java
@@ -26,31 +26,48 @@ public abstract class AttackResult {
         this.coords = coords;
     }
 
+    /**
+     * Sets whether this AttackResult represents the result of a successful attack.
+     * @param succeeds whether the attack was a success
+     */
     protected void succeeds(boolean succeeds) {
         this.succeeds = succeeds;
     }
 
+    /**
+     * Sets whether this AttackResult represents the result of an attack that hit a ship.
+     * @param hitsShip whether the attack hit an enemy ship
+     */
     protected void hitsShip(boolean hitsShip) {
         this.hitsShip = hitsShip;
     }
 
+    /**
+     * Sets whether this AttackResult represents the result of an attack that destroyed an enemy ship
+     * @param destroysShip whether the attack destroyed an enemy ship
+     */
     protected void destroysShip(boolean destroysShip) {
         this.destroysShip = destroysShip;
     }
 
+    /**
+     * Sets whether this AttackResult represents the result of an attack that
+     * caused the attacker to win.
+     * @param winsGame whether the attack was a winning attack
+     */
     protected void winsGame(boolean winsGame) {
         this.winsGame = winsGame;
     }
 
     /**
-     * Checks if this AttackResult actually succeeds in hitting a cell
+     * Checks if this AttackResult is of an attack which succeeded
      */
     public final boolean isSuccessful() {
         return succeeds;
     }
 
     /**
-     * Checks if this AttackResult is a hit or a miss
+     * Checks if this AttackResult is of an attack which hit
      * (guess they never miss, huh?)
      */
     public final boolean isHit() {
@@ -58,7 +75,7 @@ public abstract class AttackResult {
     }
 
     /**
-     * Checks if this AttackResult is of the destruction of a ship
+     * Checks if this AttackResult is of an attack which destroyed a ship
      */
     public final boolean isDestroy() {
         return destroysShip;
@@ -71,18 +88,22 @@ public abstract class AttackResult {
         return winsGame;
     }
 
+    /**
+     * Gets the {@code Status} of the cell which was hit, if any.
+     * @return the {@code Status} of the hit cell
+     * @see Status
+     */
     public Status getStatus() {
         return attacker.getMapGrid().getCellStatus(coords);
     }
 
     /**
-     * Returns a short form of the attack result, without the front portion
-     * "Player __ attacked __ __"
+     * Returns a concise String representation of the attack result.
      */
     public abstract String resultString();
 
     /**
-     * Returns a String representation of the attack result.
+     * Returns a verbose String representation of the attack result.
      */
     @Override
     public String toString() {
@@ -90,14 +111,14 @@ public abstract class AttackResult {
     }
 
     /**
-     * Formats the string as if the user attacked.
+     * Formats this AttackResult as if a human user was the attacker.
      */
     public String formatAsUserAttack() {
         return String.format("You attacked %s and ", coords) + resultString();
     }
 
     /**
-     * Formats the string as if the enemy attacked.
+     * Formats this AttackResult as if a computer enemy was the attacker.
      */
     public String formatAsEnemyAttack() {
         return String.format("Enemy attacked %s and ", coords) + resultString();

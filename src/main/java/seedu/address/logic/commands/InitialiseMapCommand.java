be9588ecb995e265511bca9b23df57b5ca905cd3
erjillsison <erjillsison@gmail.com>
2019-04-14 12:37:55 +0800

    Update InitialiseMapCommand usage prompt

--- src/main/java/seedu/address/logic/commands/InitialiseMapCommand.java
+++ src/main/java/seedu/address/logic/commands/InitialiseMapCommand.java
@@ -25,9 +25,9 @@ public class InitialiseMapCommand extends Command {
     public static final int MAXIMUM_MAP_SIZE = 10;
     public static final int MINIMUM_MAP_SIZE = 6;
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Initialise the map to specified size. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Initialise the map to specified size.\n"
             + "Parameters: "
-            + "MAPSIZE (must be a positive integer)\n"
+            + String.format("MAPSIZE (must be between %d to %d, inclusive)\n", MINIMUM_MAP_SIZE, MAXIMUM_MAP_SIZE)
             + "Example: " + COMMAND_WORD + " "
             + "8 ";
 

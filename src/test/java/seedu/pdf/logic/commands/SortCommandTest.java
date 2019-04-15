b98638738de43bb4452cd896e3ebf65d46172964
jetkan-yk <jetkan.yk@gmail.com>
2019-04-15 17:28:52 +0800

    Checkstyle fixes

--- src/test/java/seedu/pdf/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/pdf/logic/commands/SortCommandTest.java
@@ -3,20 +3,11 @@ package seedu.pdf.logic.commands;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import seedu.pdf.logic.CommandHistory;
-import seedu.pdf.model.Model;
-import seedu.pdf.model.ModelManager;
-import seedu.pdf.model.UserPrefs;
-
-import static seedu.pdf.testutil.TypicalPdfs.getTypicalPdfBook;
 
 public class SortCommandTest {
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
-
-    private Model model = new ModelManager(getTypicalPdfBook(), new UserPrefs());
-    private CommandHistory commandHistory = new CommandHistory();
 
     @Test
     public void constructor_invalidNullComparator_throwsNullPointerException() {

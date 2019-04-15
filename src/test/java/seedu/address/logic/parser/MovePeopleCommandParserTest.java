0b8ec2a9f72f151465fb4a273cb74a0ee1908827
DanielDSSim <DanielDSSim@gmail.com>
2019-04-15 10:23:10 +0800

    Fix style

--- src/test/java/seedu/address/logic/parser/MovePeopleCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/MovePeopleCommandParserTest.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_JOB_NAME_TEACHER;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
@@ -14,7 +13,6 @@ import org.junit.Test;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.MovePeopleCommand;
 import seedu.address.model.job.JobListName;
-import seedu.address.model.job.JobName;
 
 
 /**

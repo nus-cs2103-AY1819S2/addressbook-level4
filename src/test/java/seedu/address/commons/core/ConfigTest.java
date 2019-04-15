9441ec8d254b059b1d139541648e59077f57aada
s-tr <swchong25+github@gmail.com>
2019-04-15 20:09:19 +0800

    Add ConfigTest

--- src/test/java/seedu/address/commons/core/ConfigTest.java
+++ src/test/java/seedu/address/commons/core/ConfigTest.java
@@ -1,8 +1,13 @@
 package seedu.address.commons.core;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,6 +16,21 @@ import org.junit.rules.ExpectedException;
 public class ConfigTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void setLogLevel() {
+        Config defaultConfig = new Config();
+        defaultConfig.setLogLevel(Level.ALL);
+        assertEquals(defaultConfig.getLogLevel(), Level.ALL);
+    }
+
+    @Test
+    public void setUserPrefsFilePath() {
+        Config defaultConfig = new Config();
+        Path path = Paths.get(".");
+        defaultConfig.setUserPrefsFilePath(path);
+        assertEquals(defaultConfig.getUserPrefsFilePath(), path);
+    }
 
     @Test
     public void toString_defaultObject_stringReturned() {
@@ -25,6 +45,7 @@ public class ConfigTest {
         Config defaultConfig = new Config();
         assertNotNull(defaultConfig);
         assertTrue(defaultConfig.equals(defaultConfig));
+        assertFalse(defaultConfig.equals(5));
     }
 
 
